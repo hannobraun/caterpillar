2be9681994c7491eb1d1d7dfa551478b61885e80
Hanno Braun <hanno@braun-odw.eu>
2023-01-18 22:48:00 +0100

    Print log message, if text area changes

--- caterpillar/src/html.rs
+++ caterpillar/src/html.rs
@@ -2,8 +2,11 @@ use sycamore::prelude::*;
 
 pub fn render(canvas_id: u32) {
     sycamore::render(|cx| {
+        let signal = create_signal(cx, String::new());
+        create_effect(cx, || log::warn!("{}", signal.get()));
+
         view! { cx,
-            textarea {}
+            textarea(bind:value=signal) {}
             canvas(data-raw-handle=(canvas_id)) {}
         }
     });
