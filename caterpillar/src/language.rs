8ce1359351cc7754888d5c6070577ffc89142b6f
Hanno Braun <hanno@braun-odw.eu>
2023-01-20 20:50:41 +0100

    Refactor

--- caterpillar/src/language.rs
+++ caterpillar/src/language.rs
@@ -2,17 +2,13 @@ use std::{cell::RefCell, rc::Rc};
 
 pub fn init() -> (Language, Rc<RefCell<[f64; 4]>>) {
     let background_color = Rc::new(RefCell::new([0., 0., 0., 1.]));
-    let language = Language::new(&background_color);
+    let language = Language {
+        background_color: background_color.clone(),
+    };
+
     (language, background_color)
 }
 
 pub struct Language {
     pub background_color: Rc<RefCell<[f64; 4]>>,
-}
-
-impl Language {
-    pub fn new(background_color: &Rc<RefCell<[f64; 4]>>) -> Self {
-        let background_color = background_color.clone();
-        Self { background_color }
-    }
 }
