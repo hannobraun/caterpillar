6fe9bed4e28d46af53240d1ac88dc7940c2969f0
Hanno Braun <hanno@braun-odw.eu>
2023-01-20 21:33:51 +0100

    Update variable name

--- caterpillar/src/html.rs
+++ caterpillar/src/html.rs
@@ -4,13 +4,13 @@ use crate::language::Interpreter;
 
 pub fn render(canvas_id: u32, interpreter: Interpreter) {
     sycamore::render(|cx| {
-        let value = create_signal(cx, String::new());
+        let code = create_signal(cx, String::new());
         create_effect(cx, move || {
-            interpreter.interpret(&value.get());
+            interpreter.interpret(&code.get());
         });
 
         view! { cx,
-            textarea(bind:value=value) {}
+            textarea(bind:value=code) {}
             canvas(data-raw-handle=(canvas_id)) {}
         }
     });
