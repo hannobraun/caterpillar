9d5863b262b7fdc2fcff4cabb067f18764087803
Hanno Braun <hanno@braun-odw.eu>
2023-01-21 19:31:15 +0100

    Update argument name

--- caterpillar/src/language/tokenizer.rs
+++ caterpillar/src/language/tokenizer.rs
@@ -4,12 +4,13 @@ pub struct Tokenizer;
 
 impl Tokenizer {
     pub fn tokenize(
-        code: &mut impl Iterator<Item = char>,
+        chars: &mut impl Iterator<Item = char>,
     ) -> impl Iterator<Item = String> + '_ {
         iter::from_fn(|| {
             let mut token = String::new();
             token.extend(
-                code.by_ref()
+                chars
+                    .by_ref()
                     .skip_while(|ch| ch.is_whitespace())
                     .take_while(|ch| !ch.is_whitespace()),
             );
