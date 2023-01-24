8a8bd9148e51dac39293d5031ede7110a1ee4a56
Hanno Braun <hanno@braun-odw.eu>
2023-01-24 21:35:25 +0100

    Add `evaluate_fn`

--- caterpillar/src/language/evaluator.rs
+++ caterpillar/src/language/evaluator.rs
@@ -10,33 +10,36 @@ pub type Stack = VecDeque<Value>;
 pub fn evaluate(tokens: impl Iterator<Item = Syntax>, stack: &mut Stack) {
     for token in tokens {
         let Syntax::Fn { name } = token;
+        evaluate_fn(name, stack);
+    }
+}
 
-        match name.as_str() {
-            "color" => {
-                let b = stack.pop_back();
-                let g = stack.pop_back();
-                let r = stack.pop_back();
+fn evaluate_fn(name: String, stack: &mut Stack) {
+    match name.as_str() {
+        "color" => {
+            let b = stack.pop_back();
+            let g = stack.pop_back();
+            let r = stack.pop_back();
 
-                if let (
-                    Some(Value::U8(r)),
-                    Some(Value::U8(g)),
-                    Some(Value::U8(b)),
-                ) = (r, g, b)
-                {
-                    let color = Color { r, g, b };
-                    let value = Value::Color(color);
+            if let (
+                Some(Value::U8(r)),
+                Some(Value::U8(g)),
+                Some(Value::U8(b)),
+            ) = (r, g, b)
+            {
+                let color = Color { r, g, b };
+                let value = Value::Color(color);
 
-                    stack.push_back(value);
-                }
+                stack.push_back(value);
             }
-            "drop" => {
-                stack.pop_back();
-            }
-            _ => {
-                if let Ok(value) = name.parse::<u8>() {
-                    let value = Value::U8(value);
-                    stack.push_back(value);
-                }
+        }
+        "drop" => {
+            stack.pop_back();
+        }
+        _ => {
+            if let Ok(value) = name.parse::<u8>() {
+                let value = Value::U8(value);
+                stack.push_back(value);
             }
         }
     }
