3f0584f3204fa8113b7411fdf411e862a7d72249
Hanno Braun <hanno@braun-odw.eu>
2023-01-25 19:45:05 +0100

    Refactor

--- caterpillar/src/language/parser.rs
+++ caterpillar/src/language/parser.rs
@@ -10,12 +10,16 @@ pub enum SyntaxTree {
 pub fn parse(
     mut tokens: impl Iterator<Item = Token>,
 ) -> impl Iterator<Item = SyntaxTree> {
-    iter::from_fn(move || loop {
-        match tokens.next()? {
-            Token::Fn { name } => {
-                return Some(SyntaxTree::Fn { name });
+    iter::from_fn(move || {
+        for token in &mut tokens {
+            match token {
+                Token::Fn { name } => {
+                    return Some(SyntaxTree::Fn { name });
+                }
+                Token::ArrayOpen => {}
             }
-            Token::ArrayOpen => {}
         }
+
+        None
     })
 }
