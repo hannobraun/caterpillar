72940df95e44840a49f8a125ecd98d9b4af1b23e
Hanno Braun <hanno@braun-odw.eu>
2023-01-28 14:18:58 +0100

    Update variable name

--- caterpillar/src/language/mod.rs
+++ caterpillar/src/language/mod.rs
@@ -24,8 +24,8 @@ impl Interpreter {
 
         let mut chars = code.chars();
         let mut tokens = tokenizer::Tokenizer::new(&mut chars);
-        let syntax_trees = parser::Parser::new(&mut tokens);
-        evaluator::evaluate(syntax_trees, &mut stack);
+        let syntax_tree = parser::Parser::new(&mut tokens);
+        evaluator::evaluate(syntax_tree, &mut stack);
 
         let Some(values::Value::Color(color)) = stack.pop_front() else {
             return;
