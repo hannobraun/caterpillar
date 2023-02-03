425921608e6fb7999bf638a50a6ce8761f9ad435
Hanno Braun <hanno@braun-odw.eu>
2023-02-03 19:14:26 +0100

    Refactor

--- cat2/src/main.rs
+++ cat2/src/main.rs
@@ -2,17 +2,19 @@ mod cells;
 mod cp;
 mod ui;
 
-use std::time::Instant;
+use std::{collections::VecDeque, time::Instant};
 
 fn main() -> anyhow::Result<()> {
-    let mut current = ui::Line::init();
+    let mut lines = VecDeque::new();
 
     loop {
+        let current = lines.back().cloned().unwrap_or_else(ui::Line::init);
+
         let next = cells::next_generation(current.cells);
         let next = ui::Line::from_cells(next);
 
         next.print()?;
-        current = next;
+        lines.push_back(next);
 
         let now = Instant::now();
         while now.elapsed().as_secs_f64() < 0.125 {}
