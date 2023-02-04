0915b3450825d4bc50bf2f84046d5cb9692630fb
Hanno Braun <hanno@braun-odw.eu>
2023-02-04 18:45:09 +0100

    Add comment

--- cat2/src/cp/code.rs
+++ cat2/src/cp/code.rs
@@ -8,6 +8,9 @@ impl Code {
     pub fn new() -> Self {
         let mut functions = BTreeMap::new();
 
+        // Eventually, we'll store the source code in a persistent way. But for
+        // now, we'll just define default code on startup, as a starting point
+        // for the user to modify.
         functions.insert(
             String::from("cell_is_born"),
             String::from("clone 2 = swap 3 = or"),
