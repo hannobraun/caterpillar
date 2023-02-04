c98b55a2ab901cfcbc407c94855b82d1a5e04044
Hanno Braun <hanno@braun-odw.eu>
2023-02-04 18:37:51 +0100

    Store code by function

--- cat2/src/cp/code.rs
+++ cat2/src/cp/code.rs
@@ -1,11 +1,18 @@
+use std::collections::BTreeMap;
+
 pub struct Code {
-    pub inner: String,
+    pub inner: BTreeMap<String, String>,
 }
 
 impl Code {
     pub fn new() -> Self {
-        let inner =
-            String::from(include_str!("../caterpillar/cell_is_born.cp0"));
+        let mut inner = BTreeMap::new();
+
+        inner.insert(
+            String::from("cell_is_born"),
+            String::from(include_str!("../caterpillar/cell_is_born.cp0")),
+        );
+
         Self { inner }
     }
 }
