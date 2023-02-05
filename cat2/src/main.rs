6c4abe5785d801368da643dea74f0a7cbf9db64d
Hanno Braun <hanno@braun-odw.eu>
2023-02-05 11:19:36 +0100

    Refactor

--- cat2/src/main.rs
+++ cat2/src/main.rs
@@ -9,6 +9,8 @@ fn main() -> anyhow::Result<()> {
     let mut buffer = ui::Buffer::new();
     let mut lines = ui::Lines::new();
 
+    let delay = Duration::from_millis(125);
+
     loop {
         let current = lines.current();
 
@@ -18,6 +20,6 @@ fn main() -> anyhow::Result<()> {
         lines.push_next(next);
         lines.print(&mut buffer, &mut stdout)?;
 
-        thread::sleep(Duration::from_millis(125));
+        thread::sleep(delay);
     }
 }
