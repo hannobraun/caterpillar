bb300d83f2e22f645706790a83a23a88b869851d
Hanno Braun <hanno@braun-odw.eu>
2023-02-05 11:18:30 +0100

    Fix busy loop in `main`

--- cat2/src/main.rs
+++ cat2/src/main.rs
@@ -2,7 +2,7 @@ mod cells;
 mod cp;
 mod ui;
 
-use std::{io::stdout, thread, time::Instant};
+use std::{io::stdout, thread, time::Duration};
 
 fn main() -> anyhow::Result<()> {
     let mut stdout = stdout();
@@ -18,13 +18,6 @@ fn main() -> anyhow::Result<()> {
         lines.push_next(next);
         lines.print(&mut buffer, &mut stdout)?;
 
-        // I wrote this in a moment of idiocy, when I had temporarily forgotten
-        // that `thread::sleep` exists. However, trying to replace it with that
-        // function call completely messes up the UI. It starts flickering, but
-        // with this busy loop here, it's perfectly smooth for some reason.
-        let now = Instant::now();
-        while now.elapsed().as_secs_f64() < 0.125 {
-            thread::yield_now();
-        }
+        thread::sleep(Duration::from_millis(125));
     }
 }
