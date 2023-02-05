4c9e08673648f8a957210b4eb45720e4cdc17b59
Hanno Braun <hanno@braun-odw.eu>
2023-02-05 07:42:44 +0100

    Refactor

--- cat2/src/ui/area.rs
+++ cat2/src/ui/area.rs
@@ -14,19 +14,15 @@ pub fn new(out: &mut Stdout) -> Area {
     Area { out }
 }
 
-pub fn move_cursor(area: &mut Area, x: u16, y: u16) -> anyhow::Result<()> {
-    area.out.queue(cursor::MoveTo(x, y))?;
-    Ok(())
-}
-
 pub fn write(
     area: &mut Area,
     x: &mut u16,
     y: u16,
     s: &str,
 ) -> anyhow::Result<()> {
-    move_cursor(area, *x, y)?;
-    area.out.queue(style::PrintStyledContent(s.stylize()))?;
+    area.out
+        .queue(cursor::MoveTo(*x, y))?
+        .queue(style::PrintStyledContent(s.stylize()))?;
 
     let num_chars: u16 = s.chars().count().try_into().expect("String too long");
     *x += num_chars;
