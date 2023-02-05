cdd8b15b1e0bb77c03f6146377692dd9a21cf39d
Hanno Braun <hanno@braun-odw.eu>
2023-02-05 06:35:10 +0100

    Update struct field name

--- cat2/src/cp/functions.rs
+++ cat2/src/cp/functions.rs
@@ -1,26 +1,26 @@
 use std::collections::BTreeMap;
 
 pub struct Functions {
-    functions: BTreeMap<String, String>,
+    inner: BTreeMap<String, String>,
 }
 
 impl Functions {
     pub fn new() -> Self {
-        let mut functions = BTreeMap::new();
+        let mut inner = BTreeMap::new();
 
         // Eventually, we'll store the source code in a persistent way. But for
         // now, we'll just define default code on startup, as a starting point
         // for the user to modify.
-        functions.insert(
+        inner.insert(
             String::from("cell_is_born"),
             String::from("clone 2 = swap 3 = or"),
         );
 
-        Self { functions }
+        Self { inner }
     }
 
     pub fn function(&self, name: &str) -> &str {
-        self.functions
+        self.inner
             .get(name)
             .unwrap_or_else(|| panic!("Function {name} not defined"))
     }
