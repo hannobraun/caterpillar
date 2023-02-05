de175f56f808ad13265b398c77ef690cd5488d36
Hanno Braun <hanno@braun-odw.eu>
2023-02-05 06:55:07 +0100

    Add `move_cursor`

--- cat2/src/ui/area.rs
+++ cat2/src/ui/area.rs
@@ -10,15 +10,19 @@ pub struct Area<'a> {
     pub out: &'a mut Stdout,
 }
 
+pub fn move_cursor(area: &mut Area, x: u16, y: u16) -> anyhow::Result<()> {
+    area.out.queue(cursor::MoveTo(x, y))?;
+    Ok(())
+}
+
 pub fn write(
     area: &mut Area,
     x: &mut u16,
     y: u16,
     s: &str,
 ) -> anyhow::Result<()> {
-    area.out
-        .queue(cursor::MoveTo(*x, y))?
-        .queue(style::PrintStyledContent(s.stylize()))?;
+    move_cursor(area, *x, y)?;
+    area.out.queue(style::PrintStyledContent(s.stylize()))?;
 
     let num_chars: u16 = s.chars().count().try_into().expect("String too long");
     *x += num_chars;
