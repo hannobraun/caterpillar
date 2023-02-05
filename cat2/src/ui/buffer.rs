6ee56a4be0dcb945785c47a0b5e5a9b042dfe17b
Hanno Braun <hanno@braun-odw.eu>
2023-02-05 11:02:52 +0100

    Store previous buffer size

--- cat2/src/ui/buffer.rs
+++ cat2/src/ui/buffer.rs
@@ -8,6 +8,8 @@ use super::vector::Vector;
 pub struct Buffer {
     previous: Vec<char>,
     current: Vec<char>,
+
+    previous_size: Vector,
     size: Vector,
 }
 
@@ -16,11 +18,14 @@ impl Buffer {
         Self {
             previous: Vec::new(),
             current: Vec::new(),
+
+            previous_size: Vector { x: 0, y: 0 },
             size: Vector { x: 0, y: 0 },
         }
     }
 
     pub fn prepare(&mut self, size: Vector) {
+        self.previous_size = self.size;
         self.size = size;
 
         self.previous.clear();
