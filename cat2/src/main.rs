c3d40acc4fccef11d137b95f1bdacefa2a104bba
Hanno Braun <hanno@braun-odw.eu>
2023-02-07 21:12:48 +0100

    Update variable name

--- cat2/src/main.rs
+++ cat2/src/main.rs
@@ -11,7 +11,7 @@ use std::{
 fn main() -> anyhow::Result<()> {
     let mut stdout = stdout();
     let mut buffer = ui::Buffer::new();
-    let mut lines = Vec::new();
+    let mut generations = Vec::new();
 
     let functions = cp::Functions::new();
 
@@ -19,15 +19,15 @@ fn main() -> anyhow::Result<()> {
     let delay = Duration::from_millis(125);
 
     loop {
-        let current = lines.last().cloned().unwrap_or_else(cells::init);
+        let current = generations.last().cloned().unwrap_or_else(cells::init);
 
         // We only add new generations, but never delete them. This is fine for
         // now, I think. Let's just hope nobody runs this for long enough to
         // fill up their main memory.
         let next = cells::next_generation(current, &functions);
-        lines.push(next);
+        generations.push(next);
 
-        ui::draw(&lines, &mut buffer, &mut stdout)?;
+        ui::draw(&generations, &mut buffer, &mut stdout)?;
 
         thread::sleep(delay - time.elapsed());
         time += delay;
