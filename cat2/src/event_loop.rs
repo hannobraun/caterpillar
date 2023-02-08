8450542f367fb8a19e880f2675a655135a7f950a
Hanno Braun <hanno@braun-odw.eu>
2023-02-08 17:27:42 +0100

    Add `event_loop::run_once`

--- cat2/src/event_loop.rs
+++ cat2/src/event_loop.rs
@@ -1,10 +1,37 @@
 use std::io;
 
-use crate::{cells::Generation, cp::Functions, ui};
+use crate::{
+    cells::{self, Generation},
+    cp::Functions,
+    ui,
+};
 
 pub struct State {
     pub functions: Functions,
     pub generations: Vec<Generation>,
     pub buffer: ui::Buffer,
     pub stdout: io::Stdout,
+}
+
+pub fn run_once(state: &mut State) -> anyhow::Result<()> {
+    let current = state
+        .generations
+        .last()
+        .cloned()
+        .unwrap_or_else(cells::init);
+
+    // We only add new generations, but never delete them. This is fine for now,
+    // I think. Let's just hope nobody runs this for long enough to fill up
+    // their main memory.
+    let next = cells::next_generation(current, &state.functions);
+    state.generations.push(next);
+
+    ui::draw(
+        &state.generations,
+        &state.functions,
+        &mut state.buffer,
+        &mut state.stdout,
+    )?;
+
+    Ok(())
 }
--- cat2/src/main.rs
+++ cat2/src/main.rs
@@ -55,24 +55,7 @@ async fn main() -> anyhow::Result<()> {
             }
         }
 
-        let current = state
-            .generations
-            .last()
-            .cloned()
-            .unwrap_or_else(cells::init);
-
-        // We only add new generations, but never delete them. This is fine for
-        // now, I think. Let's just hope nobody runs this for long enough to
-        // fill up their main memory.
-        let next = cells::next_generation(current, &state.functions);
-        state.generations.push(next);
-
-        ui::draw(
-            &state.generations,
-            &state.functions,
-            &mut state.buffer,
-            &mut state.stdout,
-        )?;
+        event_loop::run_once(&mut state)?;
     }
 
     terminal::disable_raw_mode()?;
