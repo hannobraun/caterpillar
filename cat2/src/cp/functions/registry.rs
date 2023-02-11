b7e56ad911a4c63283c39dad2e491a39cfa482b6
Hanno Braun <hanno@braun-odw.eu>
2023-02-11 19:51:24 +0100

    Make variable name more specific

--- cat2/src/cp/functions/registry.rs
+++ cat2/src/cp/functions/registry.rs
@@ -56,10 +56,10 @@ impl Registry {
 
             let mut prime_candidate = with_matching_signature_len.pop();
             for candidate in with_matching_signature_len.drain(..) {
-                let last_value = matched_values.last();
+                let last_matched_value = matched_values.last();
                 let last_arg = candidate.args.last();
 
-                let type_of_last_value = last_value.map(Value::ty);
+                let type_of_last_value = last_matched_value.map(Value::ty);
                 let type_of_last_arg = last_arg.map(Arg::ty);
 
                 let is_correct_type = type_of_last_value == type_of_last_arg;
