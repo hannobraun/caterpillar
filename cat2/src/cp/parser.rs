ea9823b21915036c23def675d94d5f2d5046c08f
Hanno Braun <hanno@braun-odw.eu>
2023-02-15 21:48:32 +0100

    Refactor

--- cat2/src/cp/parser.rs
+++ cat2/src/cp/parser.rs
@@ -10,7 +10,7 @@ pub fn parse(tokens: impl IntoIterator<Item = Token>) -> Expressions {
     let mut tokens = tokens.into_iter();
     let mut expressions = Vec::new();
 
-    while let Some(expression) = parse_expression(&mut tokens) {
+    while let Some(expression) = parse_expression(&mut tokens, None) {
         expressions.push(expression);
     }
 
@@ -19,8 +19,13 @@ pub fn parse(tokens: impl IntoIterator<Item = Token>) -> Expressions {
 
 fn parse_expression(
     mut tokens: impl Iterator<Item = Token>,
+    terminator: Option<Token>,
 ) -> Option<Expression> {
     let token = tokens.next()?;
+
+    if Some(&token) == terminator.as_ref() {
+        return None;
+    }
 
     match token {
         Token::Fn(name) => Some(Expression::Fn(name)),
