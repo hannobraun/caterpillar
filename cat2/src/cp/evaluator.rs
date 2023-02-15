f51dc896a702768118195ffeb27573e098b34051
Hanno Braun <hanno@braun-odw.eu>
2023-02-15 22:21:29 +0100

    Add `evaluator`

---
+++ cat2/src/cp/evaluator.rs
@@ -0,0 +1,48 @@
+use super::{builtins, DataStack, Expression, Functions, Value};
+
+pub fn evaluate(
+    expression: &Expression,
+    functions: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
+    match expression {
+        Expression::Block(_) => {
+            todo!("Evaluating blocks is not supported yet")
+        }
+        Expression::Fn(fn_name) => evaluate_fn(fn_name, functions, data_stack),
+    }
+}
+
+fn evaluate_fn(
+    fn_name: &str,
+    functions: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
+    if let Some(builtin) = builtins::get(fn_name) {
+        builtin(data_stack);
+        return Ok(());
+    }
+
+    if let Ok(value) = fn_name.parse::<u8>() {
+        data_stack.push(Value::U8(value));
+        return Ok(());
+    }
+
+    // If we land here, it's not a builtin function.
+    let function = functions.resolve(fn_name, data_stack).ok_or_else(|| {
+        FunctionNotFound {
+            name: fn_name.into(),
+        }
+    })?;
+
+    for expression in &function.body {
+        evaluate(expression, functions, data_stack)?;
+    }
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct FunctionNotFound {
+    pub name: String,
+}
--- cat2/src/cp/mod.rs
+++ cat2/src/cp/mod.rs
@@ -1,11 +1,13 @@
 mod builtins;
 mod data_stack;
+mod evaluator;
 mod functions;
 mod parser;
 mod tokenizer;
 
 pub use self::{
     data_stack::{DataStack, Type, Value},
+    evaluator::evaluate,
     functions::{Arg, Functions},
     parser::{parse, Expression, Expressions},
     tokenizer::{tokenize, Token, Tokens},
@@ -23,51 +25,4 @@ impl Interpreter {
             data_stack: DataStack::new(),
         }
     }
-}
-
-pub fn evaluate(
-    expression: &Expression,
-    functions: &Functions,
-    data_stack: &mut DataStack,
-) -> Result<(), FunctionNotFound> {
-    match expression {
-        Expression::Block(_) => {
-            todo!("Evaluating blocks is not supported yet")
-        }
-        Expression::Fn(fn_name) => evaluate_fn(fn_name, functions, data_stack),
-    }
-}
-
-fn evaluate_fn(
-    fn_name: &str,
-    functions: &Functions,
-    data_stack: &mut DataStack,
-) -> Result<(), FunctionNotFound> {
-    if let Some(builtin) = builtins::get(fn_name) {
-        builtin(data_stack);
-        return Ok(());
-    }
-
-    if let Ok(value) = fn_name.parse::<u8>() {
-        data_stack.push(Value::U8(value));
-        return Ok(());
-    }
-
-    // If we land here, it's not a builtin function.
-    let function = functions.resolve(fn_name, data_stack).ok_or_else(|| {
-        FunctionNotFound {
-            name: fn_name.into(),
-        }
-    })?;
-
-    for expression in &function.body {
-        evaluate(expression, functions, data_stack)?;
-    }
-
-    Ok(())
-}
-
-#[derive(Debug)]
-pub struct FunctionNotFound {
-    pub name: String,
 }
