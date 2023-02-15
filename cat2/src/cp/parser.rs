c8efb6b9772c9528e44cd495bb4c620e446d4470
Hanno Braun <hanno@braun-odw.eu>
2023-02-15 21:15:50 +0100

    Rename function

--- cat2/src/cp/parser.rs
+++ cat2/src/cp/parser.rs
@@ -7,10 +7,10 @@ pub enum Expression {
 }
 
 pub fn parse(tokens: impl IntoIterator<Item = Token>) -> Expressions {
-    tokens.into_iter().filter_map(parse_token).collect()
+    tokens.into_iter().filter_map(parse_expression).collect()
 }
 
-fn parse_token(token: Token) -> Option<Expression> {
+fn parse_expression(token: Token) -> Option<Expression> {
     match token {
         Token::Fn(name) => Some(Expression::Fn(name)),
         Token::BlockOpen => {
