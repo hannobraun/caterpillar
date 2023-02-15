06f121f307ec7ea036704ff33339d16e8a2f0656
Hanno Braun <hanno@braun-odw.eu>
2023-02-15 21:20:14 +0100

    Refactor

--- cat2/src/cp/parser.rs
+++ cat2/src/cp/parser.rs
@@ -7,19 +7,29 @@ pub enum Expression {
 }
 
 pub fn parse(tokens: impl IntoIterator<Item = Token>) -> Expressions {
-    tokens.into_iter().filter_map(parse_expression).collect()
+    let mut tokens = tokens.into_iter();
+    let mut expressions = Vec::new();
+
+    while let Some(expression) = parse_expression(&mut tokens) {
+        expressions.push(expression);
+    }
+
+    expressions
 }
 
-fn parse_expression(token: Token) -> Option<Expression> {
-    match token {
-        Token::Fn(name) => Some(Expression::Fn(name)),
-        Token::BlockOpen => {
+fn parse_expression(
+    mut tokens: impl Iterator<Item = Token>,
+) -> Option<Expression> {
+    match tokens.next() {
+        Some(Token::Fn(name)) => Some(Expression::Fn(name)),
+        Some(Token::BlockOpen) => {
             // Currently ignored.
             None
         }
-        Token::BlockClose => {
+        Some(Token::BlockClose) => {
             // Currently ignored.
             None
         }
+        None => None,
     }
 }
