e00171a63e9226544e3b0023ae7f9eef5ba28fa4
Hanno Braun <hanno@braun-odw.eu>
2023-02-16 18:21:25 +0100

    Return `Result` from builtins

--- cat2/src/cp/builtins.rs
+++ cat2/src/cp/builtins.rs
@@ -1,6 +1,7 @@
-use super::{DataStack, Functions};
+use super::{evaluator::FunctionNotFound, DataStack, Functions};
 
-pub type Builtin = fn(&Functions, &mut DataStack);
+pub type Builtin =
+    fn(&Functions, &mut DataStack) -> Result<(), FunctionNotFound>;
 
 pub fn get(name: &str) -> Option<Builtin> {
     let builtin = match name {
@@ -18,62 +19,102 @@ pub fn get(name: &str) -> Option<Builtin> {
     Some(builtin)
 }
 
-fn add(_: &Functions, data_stack: &mut DataStack) {
+fn add(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     let b = data_stack.pop_u8();
     let a = data_stack.pop_u8();
 
     let x = a.saturating_add(b);
 
     data_stack.push(x);
+
+    Ok(())
 }
 
-fn clone(_: &Functions, data_stack: &mut DataStack) {
+fn clone(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     let value = data_stack.pop_any();
 
     data_stack.push(value.clone());
     data_stack.push(value);
+
+    Ok(())
 }
 
-fn drop(_: &Functions, data_stack: &mut DataStack) {
+fn drop(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     data_stack.pop_any();
+
+    Ok(())
 }
 
-fn eq(_: &Functions, data_stack: &mut DataStack) {
+fn eq(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     let b = data_stack.pop_u8();
     let a = data_stack.pop_u8();
 
     data_stack.push(a == b);
+
+    Ok(())
 }
 
-fn min(_: &Functions, data_stack: &mut DataStack) {
+fn min(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     let b = data_stack.pop_u8();
     let a = data_stack.pop_u8();
 
     let x = u8::min(a, b);
 
     data_stack.push(x);
+
+    Ok(())
 }
 
-fn or(_: &Functions, data_stack: &mut DataStack) {
+fn or(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     let b = data_stack.pop_bool();
     let a = data_stack.pop_bool();
 
     data_stack.push(a || b);
+
+    Ok(())
 }
 
-fn sub(_: &Functions, data_stack: &mut DataStack) {
+fn sub(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     let b = data_stack.pop_u8();
     let a = data_stack.pop_u8();
 
     let x = a.saturating_sub(b);
 
     data_stack.push(x);
+
+    Ok(())
 }
 
-fn swap(_: &Functions, data_stack: &mut DataStack) {
+fn swap(
+    _: &Functions,
+    data_stack: &mut DataStack,
+) -> Result<(), FunctionNotFound> {
     let b = data_stack.pop_any();
     let a = data_stack.pop_any();
 
     data_stack.push(b);
     data_stack.push(a);
+
+    Ok(())
 }
--- cat2/src/cp/evaluator.rs
+++ cat2/src/cp/evaluator.rs
@@ -32,7 +32,7 @@ fn evaluate_fn(
     data_stack: &mut DataStack,
 ) -> Result<(), FunctionNotFound> {
     if let Some(builtin) = builtins::get(fn_name) {
-        builtin(functions, data_stack);
+        builtin(functions, data_stack)?;
         return Ok(());
     }
 
