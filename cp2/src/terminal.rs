809727fe8f13804337848a34300e3930de7ce104
Hanno Braun <hanno@braun-odw.eu>
2023-03-12 21:31:53 +0100

    Add event loop that handles CTRL-C

--- cp2/src/terminal.rs
+++ cp2/src/terminal.rs
@@ -1,7 +1,16 @@
-use std::panic::{self, AssertUnwindSafe};
+use std::{
+    panic::{self, AssertUnwindSafe},
+    time::Duration,
+};
 
-use crossterm::terminal;
-use futures::FutureExt;
+use crossterm::{
+    event::{
+        Event, EventStream, KeyCode, KeyEvent, KeyEventKind, KeyModifiers,
+    },
+    terminal,
+};
+use futures::{FutureExt, StreamExt};
+use tokio::time;
 
 pub async fn run() -> anyhow::Result<()> {
     terminal::enable_raw_mode()?;
@@ -15,5 +24,42 @@ pub async fn run() -> anyhow::Result<()> {
 }
 
 async fn run_inner() -> anyhow::Result<()> {
-    todo!()
+    let mut events = EventStream::new();
+
+    let delay = Duration::from_millis(125);
+    let mut interval = time::interval(delay);
+    interval.set_missed_tick_behavior(time::MissedTickBehavior::Skip);
+
+    loop {
+        let event = tokio::select! {
+            _ = interval.tick() => {
+                None
+            }
+            event = events.next() => {
+                Some(event)
+            }
+        };
+
+        if let Some(event) = event {
+            let Some(event) = event else {
+                    anyhow::bail!("Error reading input event");
+                };
+            let event = event?;
+
+            if let Event::Key(KeyEvent {
+                code: KeyCode::Char('c'),
+                modifiers,
+                kind: KeyEventKind::Press,
+                ..
+            }) = event
+            {
+                if modifiers.contains(KeyModifiers::CONTROL) {
+                    // CTRL-C
+                    break;
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
