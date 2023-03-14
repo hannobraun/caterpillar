1431f6692614d88454ed0998a099305228e82da0
Hanno Braun <hanno@braun-odw.eu>
2023-03-14 20:36:02 +0100

    Update formatting

--- cp2/src/main.rs
+++ cp2/src/main.rs
@@ -5,9 +5,11 @@ mod ui;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let frame_time = std::time::Duration::from_millis(125);
+
     terminal::run(frame_time, |size, buffer, stdout| {
         std::future::ready(event_loop::run_once(size, buffer, stdout))
     })
     .await?;
+
     Ok(())
 }
