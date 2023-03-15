ba0af8402247d0bfe9ec131378ff64ca2b06021a
Hanno Braun <hanno@braun-odw.eu>
2023-03-15 21:18:30 +0100

    Handle unexpected tokens

--- cp2/src/cp/evaluator.rs
+++ cp2/src/cp/evaluator.rs
@@ -2,7 +2,14 @@ pub fn evaluate(tokens: Vec<String>, data_stack: &mut Vec<bool>) {
     for token in tokens {
         match token.as_str() {
             "true" => data_stack.push(true),
-            _ => data_stack.push(false),
+            "false" => data_stack.push(false),
+            _ => {
+                // Unexpected token. Eventually, it would be great to have some
+                // real error reporting mechanism. For now, let's just make sure
+                // the test won't pass.
+                data_stack.clear();
+                return;
+            }
         }
     }
 }
