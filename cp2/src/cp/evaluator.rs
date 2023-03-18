84dbc9742f5faf58908785d70f3c8f75fcbee647
Hanno Braun <hanno@braun-odw.eu>
2023-03-18 20:59:09 +0100

    Refactor

--- cp2/src/cp/evaluator.rs
+++ cp2/src/cp/evaluator.rs
@@ -7,19 +7,21 @@ pub fn evaluate(
     expressions: Expressions,
     data_stack: &mut DataStack,
 ) -> Result<(), Error> {
-    for Expression::Word(word) in expressions.0 {
-        match word.as_str() {
-            "drop" => data_stack.pop().map(|_| ())?,
-            "true" => data_stack.push(true),
-            "false" => data_stack.push(false),
-            "not" => {
-                let arg = data_stack.pop()?;
-                let value = !arg;
-                data_stack.push(value);
-            }
-            _ => {
-                return Err(Error::UnknownWord(word));
-            }
+    for expression in expressions.0 {
+        match expression {
+            Expression::Word(word) => match word.as_str() {
+                "drop" => data_stack.pop().map(|_| ())?,
+                "true" => data_stack.push(true),
+                "false" => data_stack.push(false),
+                "not" => {
+                    let arg = data_stack.pop()?;
+                    let value = !arg;
+                    data_stack.push(value);
+                }
+                _ => {
+                    return Err(Error::UnknownWord(word));
+                }
+            },
         }
     }
 
