eab84eaf690400a77fa99daf33c6b24277760881
Hanno Braun <hanno@braun-odw.eu>
2023-03-18 19:07:24 +0100

    Refactor

--- cp2/src/cp/parser.rs
+++ cp2/src/cp/parser.rs
@@ -11,7 +11,9 @@ pub fn parse(tokens: Tokens) -> Expressions {
     let expressions = tokens
         .0
         .into_iter()
-        .map(|Token::Word(word)| Expression::Word(word))
+        .map(|token| match token {
+            Token::Word(word) => Expression::Word(word),
+        })
         .collect();
     Expressions(expressions)
 }
