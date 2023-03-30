1b72228b3430ec3dbc67377009584afb9479f73f
Hanno Braun <hanno@braun-odw.eu>
2023-03-30 19:27:03 +0200

    Refactor

--- cp2/src/cp/parser.rs
+++ cp2/src/cp/parser.rs
@@ -5,6 +5,41 @@ use super::{
     Functions,
 };
 
+#[derive(Clone, Debug)]
+pub struct SyntaxTree(Vec<Expression>);
+
+impl<'r> IntoIterator for &'r SyntaxTree {
+    type Item = &'r Expression;
+    type IntoIter = slice::Iter<'r, Expression>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Expression {
+    Function {
+        name: String,
+        body: SyntaxTree,
+    },
+
+    /// Binds values from the stack to provided names
+    Binding(Vec<String>),
+
+    Array {
+        syntax_tree: SyntaxTree,
+    },
+
+    /// A block of code that is lazily evaluated
+    Block {
+        syntax_tree: SyntaxTree,
+    },
+
+    /// A word refers to a function or variable
+    Word(String),
+}
+
 pub fn parse(
     mut tokens: Tokens,
     functions: &mut Functions,
@@ -123,41 +158,6 @@ fn parse_array(
     }
 
     Ok(SyntaxTree(syntax_tree))
-}
-
-#[derive(Clone, Debug)]
-pub struct SyntaxTree(Vec<Expression>);
-
-impl<'r> IntoIterator for &'r SyntaxTree {
-    type Item = &'r Expression;
-    type IntoIter = slice::Iter<'r, Expression>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.iter()
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum Expression {
-    Function {
-        name: String,
-        body: SyntaxTree,
-    },
-
-    /// Binds values from the stack to provided names
-    Binding(Vec<String>),
-
-    Array {
-        syntax_tree: SyntaxTree,
-    },
-
-    /// A block of code that is lazily evaluated
-    Block {
-        syntax_tree: SyntaxTree,
-    },
-
-    /// A word refers to a function or variable
-    Word(String),
 }
 
 #[derive(Debug, thiserror::Error)]
