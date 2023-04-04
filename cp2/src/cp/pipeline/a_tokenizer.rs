6a77512060e9349f6b5502e4bafb60748b6eaa1e
Hanno Braun <hanno@braun-odw.eu>
2023-04-04 10:57:35 +0200

    Refactor

--- cp2/src/cp/pipeline/a_tokenizer.rs
+++ cp2/src/cp/pipeline/a_tokenizer.rs
@@ -7,28 +7,32 @@ pub fn tokenize(code: &str) -> Tokens {
     let mut tokens = VecDeque::new();
 
     for token in code {
-        let token = match token {
-            "fn" => Token::Function,
-            "test" => Token::Test,
-            "=>" => Token::BindingOperator,
-            "." => Token::Period,
-            "{" => Token::CurlyBracketOpen,
-            "}" => Token::CurlyBracketClose,
-            "(" => Token::RoundBracketOpen,
-            ")" => Token::RoundBracketClose,
-            "[" => Token::SquareBracketOpen,
-            "]" => Token::SquareBracketClose,
-            token => {
-                if let Some(("", symbol)) = token.split_once(':') {
-                    Token::Symbol(symbol.into())
-                } else {
-                    Token::Ident(token.into())
-                }
-            }
-        };
+        let token = match_token(token);
 
         tokens.push_back(token);
     }
 
     Tokens(tokens)
+}
+
+fn match_token(token: &str) -> Token {
+    match token {
+        "fn" => Token::Function,
+        "test" => Token::Test,
+        "=>" => Token::BindingOperator,
+        "." => Token::Period,
+        "{" => Token::CurlyBracketOpen,
+        "}" => Token::CurlyBracketClose,
+        "(" => Token::RoundBracketOpen,
+        ")" => Token::RoundBracketClose,
+        "[" => Token::SquareBracketOpen,
+        "]" => Token::SquareBracketClose,
+        token => {
+            if let Some(("", symbol)) = token.split_once(':') {
+                Token::Symbol(symbol.into())
+            } else {
+                Token::Ident(token.into())
+            }
+        }
+    }
 }
