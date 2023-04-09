8c7ebddff907886e2994f467522ef60672cb8be4
Hanno Braun <hanno@braun-odw.eu>
2023-04-09 20:19:56 +0200

    Refactor

--- cp3/src/functions.rs
+++ cp3/src/functions.rs
@@ -1,6 +1,15 @@
 use crate::cp::{self, Functions};
 
 pub fn define() -> anyhow::Result<Functions> {
-    let functions = cp::Functions::new();
+    let mut functions = cp::Functions::new();
+
+    let code = r#"
+    "#;
+
+    let data_stack = cp::execute(code.chars(), &mut functions)?;
+    if !data_stack.is_empty() {
+        anyhow::bail!("Defining functions left values on stack: {data_stack:?}")
+    }
+
     Ok(functions)
 }
