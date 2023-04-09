f5e668f0cb71401806680c0c9143b96fefd58eda
Hanno Braun <hanno@braun-odw.eu>
2023-04-09 12:01:19 +0200

    Improve formatting

--- cp3/src/tests.rs
+++ cp3/src/tests.rs
@@ -13,18 +13,24 @@ pub fn run() -> anyhow::Result<Vec<TestReport>> {
         test "and - true false" { true false and not }
         test "and - false true" { false true and not }
         test "and - false false" { false false and not }
+
         test "drop" { true false drop }
         test "clone" { true clone drop }
         test "binding" { true false => t f . t }
+
         test "block eval" { { true } eval }
         test "block - lazy evaluation" { true { drop } drop }
         test "block - tokenization" { {true}eval{true}eval and }
+
         test "array unwrap" { [ true ] unwrap }
         test "array - eager evaluation" { true false [ drop ] drop }
         test "array - tokenization" { [true]unwrap[true]unwrap and }
+
         test "fn" { fn f { true } f }
+
         test "if then" { true { true } { false } if }
         test "if else" { false { false } { true } if }
+
         test "string =" { "a" "a" = }
         test "string = not" { "a" "b" = not }
         test "string - tokenization" { "a""a"="b""b"= and }
