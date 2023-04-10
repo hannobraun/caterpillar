54c8cb9d62833a7c0e92c112160b2a5a4ed31d13
Hanno Braun <hanno@braun-odw.eu>
2023-04-10 10:22:29 +0200

    Add `u8` values

--- cp3/src/cp/values.rs
+++ cp3/src/cp/values.rs
@@ -6,10 +6,17 @@ pub enum Value {
     Block { expressions: ExpressionGraph },
     Bool(bool),
     String(String),
+    U8(u8),
 }
 
 impl From<bool> for Value {
     fn from(value: bool) -> Self {
         Self::Bool(value)
+    }
+}
+
+impl From<u8> for Value {
+    fn from(value: u8) -> Self {
+        Self::U8(value)
     }
 }
