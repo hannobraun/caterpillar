c5d950758e9079740d76c1ccadfb485f58d8b5b9
Hanno Braun <hanno@braun-odw.eu>
2023-04-10 10:35:21 +0200

    Refactor

--- cp3/src/cp/functions.rs
+++ cp3/src/cp/functions.rs
@@ -5,17 +5,23 @@ use super::expressions::ExpressionGraph;
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Functions {
     functions: BTreeMap<String, Function>,
+    tests: BTreeMap<String, Function>,
 }
 
 impl Functions {
     pub fn new() -> Self {
         Self {
             functions: BTreeMap::new(),
+            tests: BTreeMap::new(),
         }
     }
 
     pub fn define(&mut self, name: String, function: Function) {
-        self.functions.insert(name, function);
+        if function.test {
+            self.tests.insert(name, function);
+        } else {
+            self.functions.insert(name, function);
+        }
     }
 
     pub fn get(&self, name: &str) -> Option<Function> {
@@ -28,7 +34,7 @@ impl Functions {
     }
 
     pub fn tests(&self) -> impl Iterator<Item = (String, Function)> + '_ {
-        self.functions.iter().filter_map(|(name, function)| {
+        self.tests.iter().filter_map(|(name, function)| {
             if function.test {
                 Some((name.clone(), function.clone()))
             } else {
