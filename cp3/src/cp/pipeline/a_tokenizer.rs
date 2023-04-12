e583bdf90fae3a1854b77963777f1db21a0d6190
Hanno Braun <hanno@braun-odw.eu>
2023-04-12 10:11:08 +0200

    Refactor

--- cp3/src/cp/pipeline/a_tokenizer.rs
+++ cp3/src/cp/pipeline/a_tokenizer.rs
@@ -19,59 +19,7 @@ pub fn tokenize(code: impl IntoIterator<Item = char>) -> Tokens {
     let mut tokenizer = new();
 
     for ch in code {
-        match &mut tokenizer.state {
-            State::Searching => {
-                if ch.is_whitespace() {
-                    continue;
-                }
-
-                if ch == '"' {
-                    tokenizer.state =
-                        State::ProcessingString { buf: String::new() };
-                    continue;
-                }
-
-                let buf = String::from(ch);
-                tokenizer.state = State::ProcessingAny { buf }
-            }
-            State::ProcessingAny { buf } => {
-                if Token::match_eagerly(buf, &mut tokens) {
-                    buf.clear();
-
-                    if ch.is_whitespace() {
-                        tokenizer.state = State::Searching;
-                        continue;
-                    }
-                }
-
-                if ch == '"' {
-                    Token::match_delimited(buf.as_str(), &mut tokens);
-                    tokenizer.state =
-                        State::ProcessingString { buf: String::new() };
-                    continue;
-                }
-
-                if !ch.is_whitespace() {
-                    buf.push(ch);
-                    continue;
-                }
-
-                if !buf.is_empty() {
-                    Token::match_delimited(buf.as_str(), &mut tokens);
-                    tokenizer.state = State::Searching;
-                }
-            }
-            State::ProcessingString { buf } => {
-                if ch == '"' {
-                    let string = mem::take(buf);
-                    tokens.push(Token::String(string));
-                    tokenizer.state = State::Searching;
-                    continue;
-                }
-
-                buf.push(ch);
-            }
-        }
+        push_char(ch, &mut tokenizer, &mut tokens);
     }
 
     if let State::ProcessingAny { buf } = tokenizer.state {
@@ -79,6 +27,62 @@ pub fn tokenize(code: impl IntoIterator<Item = char>) -> Tokens {
     }
 
     Tokens(tokens.into())
+}
+
+pub fn push_char(ch: char, tokenizer: &mut Tokenizer, tokens: &mut Vec<Token>) {
+    match &mut tokenizer.state {
+        State::Searching => {
+            if ch.is_whitespace() {
+                return;
+            }
+
+            if ch == '"' {
+                tokenizer.state =
+                    State::ProcessingString { buf: String::new() };
+                return;
+            }
+
+            let buf = String::from(ch);
+            tokenizer.state = State::ProcessingAny { buf }
+        }
+        State::ProcessingAny { buf } => {
+            if Token::match_eagerly(buf, tokens) {
+                buf.clear();
+
+                if ch.is_whitespace() {
+                    tokenizer.state = State::Searching;
+                    return;
+                }
+            }
+
+            if ch == '"' {
+                Token::match_delimited(buf.as_str(), tokens);
+                tokenizer.state =
+                    State::ProcessingString { buf: String::new() };
+                return;
+            }
+
+            if !ch.is_whitespace() {
+                buf.push(ch);
+                return;
+            }
+
+            if !buf.is_empty() {
+                Token::match_delimited(buf.as_str(), tokens);
+                tokenizer.state = State::Searching;
+            }
+        }
+        State::ProcessingString { buf } => {
+            if ch == '"' {
+                let string = mem::take(buf);
+                tokens.push(Token::String(string));
+                tokenizer.state = State::Searching;
+                return;
+            }
+
+            buf.push(ch);
+        }
+    }
 }
 
 #[derive(Debug)]
