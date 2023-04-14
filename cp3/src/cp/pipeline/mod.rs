19de364a3b2499770094d887bba880e65e18db6e
Hanno Braun <hanno@braun-odw.eu>
2023-04-14 08:27:52 +0200

    Add re-export

--- cp3/src/cp/pipeline/mod.rs
+++ cp3/src/cp/pipeline/mod.rs
@@ -4,6 +4,7 @@ pub mod c_analyzer;
 pub mod d_evaluator;
 
 pub use self::{
+    a_tokenizer::Tokenizer,
     b_parser::parse,
     c_analyzer::analyze,
     d_evaluator::{evaluate, Error as EvaluatorError},
