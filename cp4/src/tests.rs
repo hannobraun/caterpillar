6544630fd8dd9cb10ba62448286f5e32495a8b1f
Hanno Braun <hanno@braun-odw.eu>
2023-04-28 20:52:24 +0200

    Refactor

--- cp4/src/tests.rs
+++ cp4/src/tests.rs
@@ -1,11 +1,14 @@
 use crate::{cp, test_report::TestReport};
 
 pub fn run() -> Vec<TestReport> {
+    let mut test_reports = Vec::new();
+
     let module = "bool".into();
     let name = "true".into();
     let code = "true";
 
     let (result, data_stack) = cp::execute(code);
+    test_reports.push(TestReport::new(module, name, result, data_stack));
 
-    vec![TestReport::new(module, name, result, data_stack)]
+    test_reports
 }
