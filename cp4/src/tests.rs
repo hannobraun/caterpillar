2a14849423f773b55852303e2b8855d92650ea22
Hanno Braun <hanno@braun-odw.eu>
2023-04-28 20:51:42 +0200

    Extract variable

--- cp4/src/tests.rs
+++ cp4/src/tests.rs
@@ -3,8 +3,9 @@ use crate::{cp, test_report::TestReport};
 pub fn run() -> Vec<TestReport> {
     let module = "bool".into();
     let name = "true".into();
+    let code = "true";
 
-    let (result, data_stack) = cp::execute("true");
+    let (result, data_stack) = cp::execute(code);
 
     vec![TestReport::new(module, name, result, data_stack)]
 }
