16e6c45157b2b5e08058ad31fe35dd81448537ba
Hanno Braun <hanno@braun-odw.eu>
2023-04-28 20:53:41 +0200

    Refactor

--- cp4/src/tests.rs
+++ cp4/src/tests.rs
@@ -1,14 +1,17 @@
+use std::collections::BTreeMap;
+
 use crate::{cp, test_report::TestReport};
 
 pub fn run() -> Vec<TestReport> {
+    let mut tests = BTreeMap::new();
     let mut test_reports = Vec::new();
 
-    let module = "bool".into();
-    let name = "true".into();
-    let code = "true";
+    tests.insert(("bool".into(), "true".into()), "true");
 
-    let (result, data_stack) = cp::execute(code);
-    test_reports.push(TestReport::new(module, name, result, data_stack));
+    for ((module, name), code) in tests {
+        let (result, data_stack) = cp::execute(code);
+        test_reports.push(TestReport::new(module, name, result, data_stack));
+    }
 
     test_reports
 }
