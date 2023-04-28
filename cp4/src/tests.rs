820440703dc1467d59038f9551d65274055665ef
Hanno Braun <hanno@braun-odw.eu>
2023-04-28 20:54:42 +0200

    Refactor

--- cp4/src/tests.rs
+++ cp4/src/tests.rs
@@ -6,9 +6,12 @@ pub fn run() -> Vec<TestReport> {
     let mut tests = BTreeMap::new();
     let mut test_reports = Vec::new();
 
-    tests.insert(("bool".into(), "true".into()), "true");
+    tests.insert(("bool", "true"), "true");
 
     for ((module, name), code) in tests {
+        let module = module.into();
+        let name = name.into();
+
         let (result, data_stack) = cp::execute(code);
         test_reports.push(TestReport::new(module, name, result, data_stack));
     }
