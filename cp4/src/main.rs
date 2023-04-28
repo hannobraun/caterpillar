0a10ba63f5a2e36557173abde59c885686c59667
Hanno Braun <hanno@braun-odw.eu>
2023-04-28 20:38:12 +0200

    Refactor

--- cp4/src/main.rs
+++ cp4/src/main.rs
@@ -5,11 +5,12 @@ mod cp;
 mod test_report;
 
 fn main() -> anyhow::Result<()> {
+    let result = Ok(());
     let data_stack = DataStack::new();
     let test_reports = vec![TestReport::new(
         "test".into(),
         "test".into(),
-        Ok(()),
+        result,
         data_stack,
     )];
     test_report::print(&test_reports);
