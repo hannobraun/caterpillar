9c0a81618c15c80c0683e64a18dc65117236ac37
Hanno Braun <hanno@braun-odw.eu>
2023-04-28 20:49:08 +0200

    Make initial test pass

--- cp4/src/cp/mod.rs
+++ cp4/src/cp/mod.rs
@@ -12,6 +12,10 @@ impl DataStack {
         self.values.is_empty()
     }
 
+    pub fn push(&mut self, value: bool) {
+        self.values.push(value);
+    }
+
     pub fn pop_bool(&mut self) -> Result<bool, DataStackError> {
         self.values.pop().ok_or(DataStackError::PopFromEmptyStack)
     }
@@ -26,9 +30,15 @@ pub enum DataStackError {
 #[derive(Debug, thiserror::Error)]
 pub enum EvaluatorError {}
 
-pub fn execute(_: &str) -> (Result<(), EvaluatorError>, DataStack) {
-    let result = Ok(());
-    let data_stack = DataStack::new();
+pub fn execute(code: &str) -> (Result<(), EvaluatorError>, DataStack) {
+    let mut data_stack = DataStack::new();
+
+    let (value, result) = match code {
+        "true" => (true, Ok(())),
+        _ => (false, Ok(())),
+    };
+
+    data_stack.push(value);
 
     (result, data_stack)
 }
