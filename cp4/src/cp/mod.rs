a56d94e8184a4d2b8dc79d4294181bbf9a298dba
Hanno Braun <hanno@braun-odw.eu>
2023-04-29 10:37:48 +0200

    Add basic tokenization

--- cp4/src/cp/mod.rs
+++ cp4/src/cp/mod.rs
@@ -36,15 +36,20 @@ pub enum EvaluatorError {
 pub fn execute(code: &str) -> (Result<(), EvaluatorError>, DataStack) {
     let mut data_stack = DataStack::new();
 
-    let value = match code {
-        "true" => true,
-        "false" => false,
-        word => {
-            return (Err(EvaluatorError::UnknownWord(word.into())), data_stack)
-        }
-    };
+    for code in code.split_whitespace() {
+        let value = match code {
+            "true" => true,
+            "false" => false,
+            word => {
+                return (
+                    Err(EvaluatorError::UnknownWord(word.into())),
+                    data_stack,
+                )
+            }
+        };
 
-    data_stack.push(value);
+        data_stack.push(value);
+    }
 
     (Ok(()), data_stack)
 }
