a3f53ff469496b7eb2159190aab78ea8178f147b
Hanno Braun <hanno@braun-odw.eu>
2023-04-30 11:22:03 +0200

    Refactor

--- cp4/src/cp/mod.rs
+++ cp4/src/cp/mod.rs
@@ -11,23 +11,7 @@ pub fn execute(
     data_stack: &mut DataStack,
 ) -> Result<(), EvaluatorError> {
     let tokens = pipeline::a_tokenizer::tokenize(code.chars());
-
-    for token in tokens {
-        match token.as_str() {
-            "true" => {
-                data_stack.push(true);
-            }
-            "false" => {
-                data_stack.push(false);
-            }
-            "not" => {
-                let a = data_stack.pop_bool()?;
-                let x = !a;
-                data_stack.push(x);
-            }
-            word => return Err(EvaluatorError::UnknownWord(word.into())),
-        }
-    }
+    pipeline::d_evaluator::evaluate(tokens, data_stack)?;
 
     Ok(())
 }
--- cp4/src/cp/pipeline/d_evaluator.rs
+++ cp4/src/cp/pipeline/d_evaluator.rs
@@ -1,4 +1,4 @@
-use crate::cp::DataStackError;
+use crate::cp::{DataStack, DataStackError};
 
 #[derive(Debug, thiserror::Error)]
 pub enum EvaluatorError {
@@ -7,4 +7,28 @@ pub enum EvaluatorError {
 
     #[error("Unknown word: `{0}`")]
     UnknownWord(String),
+}
+
+pub fn evaluate(
+    tokens: impl IntoIterator<Item = String>,
+    data_stack: &mut DataStack,
+) -> Result<(), EvaluatorError> {
+    for token in tokens {
+        match token.as_str() {
+            "true" => {
+                data_stack.push(true);
+            }
+            "false" => {
+                data_stack.push(false);
+            }
+            "not" => {
+                let a = data_stack.pop_bool()?;
+                let x = !a;
+                data_stack.push(x);
+            }
+            word => return Err(EvaluatorError::UnknownWord(word.into())),
+        }
+    }
+
+    Ok(())
 }
