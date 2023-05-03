b5458f84a99e068c4ed7c3e794b467ac27d5195c
Hanno Braun <hanno@braun-odw.eu>
2023-05-03 18:41:34 +0200

    Refactor

--- cp4/src/cp/mod.rs
+++ cp4/src/cp/mod.rs
@@ -12,9 +12,10 @@ pub async fn execute(
     code: &str,
     data_stack: &mut DataStack,
 ) -> Result<(), EvaluatorError> {
+    let code = code.chars();
     let mut tokenizer = pipeline::a_tokenizer::Tokenizer::new();
 
-    let tokens = tokenizer.tokenize(code.chars());
+    let tokens = tokenizer.tokenize(code);
     for token in tokens {
         pipeline::d_evaluator::evaluate(future::ready(token), data_stack)
             .await?;
