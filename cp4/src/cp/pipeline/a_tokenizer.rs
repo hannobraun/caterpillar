3f6f5cd62f13e099147bcd47c407ffe5a1bd5535
Hanno Braun <hanno@braun-odw.eu>
2023-05-03 18:10:07 +0200

    Add `Tokenizer`

--- cp4/src/cp/pipeline/a_tokenizer.rs
+++ cp4/src/cp/pipeline/a_tokenizer.rs
@@ -1,31 +1,35 @@
 use std::iter;
 
+pub struct Tokenizer {
+    buf: String,
+}
+
 pub fn tokenize(
     code: impl IntoIterator<Item = char>,
 ) -> impl Iterator<Item = String> {
     let mut code = code.into_iter();
-    let mut buf = String::new();
+    let mut tokenizer = Tokenizer { buf: String::new() };
 
     iter::from_fn(move || loop {
         let ch = match code.next() {
             Some(ch) => ch,
             None => {
-                if buf.is_empty() {
+                if tokenizer.buf.is_empty() {
                     return None;
                 }
 
-                let token = buf.clone();
-                buf.clear();
+                let token = tokenizer.buf.clone();
+                tokenizer.buf.clear();
                 return Some(token);
             }
         };
 
         if ch.is_whitespace() {
-            let token = buf.clone();
-            buf.clear();
+            let token = tokenizer.buf.clone();
+            tokenizer.buf.clear();
             return Some(token);
         }
 
-        buf.push(ch);
+        tokenizer.buf.push(ch);
     })
 }
