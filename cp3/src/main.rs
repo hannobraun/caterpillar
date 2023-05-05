d82de015ef3be9f09b1f8f865335c791ee91cd6d
Hanno Braun <hanno@braun-odw.eu>
2023-05-05 19:16:39 +0200

    Refactor

--- cp3/src/main.rs
+++ cp3/src/main.rs
@@ -3,30 +3,10 @@ mod functions;
 mod test_report;
 mod tests;
 
-use crossterm::style::Stylize;
-
 fn main() -> anyhow::Result<()> {
     let mut functions = functions::define()?;
     let test_reports = tests::run(&mut functions)?;
-
-    for test_report in test_reports {
-        match &test_report.result {
-            Ok(()) => {
-                print!("{}", "PASS".bold().green());
-            }
-            Err(_) => {
-                print!("{}", "FAIL".bold().red());
-            }
-        }
-
-        print!(" {} - {}", test_report.module, test_report.name);
-
-        if let Err(err) = &test_report.result {
-            print!("\n    {err}");
-        }
-
-        println!();
-    }
+    test_report::print(&test_reports);
 
     Ok(())
 }
