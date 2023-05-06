91a4eafaa2fcd24dd1468b0741546d287630c934
Hanno Braun <hanno@braun-odw.eu>
2023-05-06 20:10:42 +0200

    Refactor

--- cp4/src/cp/pipeline/d_evaluator.rs
+++ cp4/src/cp/pipeline/d_evaluator.rs
@@ -18,21 +18,31 @@ impl Evaluator {
         loop {
             let SyntaxElement::Word(word) = self.parser.next().await?;
 
-            match word.as_str() {
-                "true" => {
-                    data_stack.push(true);
-                }
-                "false" => {
-                    data_stack.push(false);
-                }
-                "not" => {
-                    let a = data_stack.pop_bool()?;
-                    let x = !a;
-                    data_stack.push(x);
-                }
-                word => return Err(EvaluatorError::UnknownWord(word.into())),
-            }
+            self.evaluate_word(word, data_stack).await?;
         }
+    }
+
+    async fn evaluate_word(
+        &mut self,
+        word: String,
+        data_stack: &mut DataStack,
+    ) -> Result<(), EvaluatorError> {
+        match word.as_str() {
+            "true" => {
+                data_stack.push(true);
+            }
+            "false" => {
+                data_stack.push(false);
+            }
+            "not" => {
+                let a = data_stack.pop_bool()?;
+                let x = !a;
+                data_stack.push(x);
+            }
+            word => return Err(EvaluatorError::UnknownWord(word.into())),
+        }
+
+        Ok(())
     }
 }
 
