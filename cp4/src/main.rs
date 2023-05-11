506c70f51b9f60264003c19abbf4203c9736845d
Hanno Braun <hanno@braun-odw.eu>
2023-05-11 20:23:48 +0200

    Make `main` async

--- cp4/src/main.rs
+++ cp4/src/main.rs
@@ -2,7 +2,8 @@ mod cp;
 mod test_report;
 mod tests;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let test_reports = tests::run()?;
     test_report::print(&test_reports);
 
