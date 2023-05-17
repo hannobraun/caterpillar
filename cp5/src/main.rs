3b18f60eabf500b24bccf8f52edb236516264f39
Hanno Braun <hanno@braun-odw.eu>
2023-05-17 22:41:30 +0200

    Add `tests::run`

--- cp5/src/main.rs
+++ cp5/src/main.rs
@@ -1,8 +1,9 @@
 mod cp;
 mod test_report;
+mod tests;
 
 fn main() -> anyhow::Result<()> {
-    let test_reports = Vec::new();
+    let test_reports = tests::run();
     test_report::print(&test_reports);
 
     Ok(())
---
+++ cp5/src/tests.rs
@@ -0,0 +1,5 @@
+use crate::test_report::TestReport;
+
+pub fn run() -> Vec<TestReport> {
+    Vec::new()
+}
