922234ee8fc6f8ca1dbf83f6a544a3c59d74c231
Hanno Braun <hanno@braun-odw.eu>
2023-05-18 20:46:27 +0200

    Add `SyntaxElement`

--- cp5/src/cp/pipeline/b_parser.rs
+++ cp5/src/cp/pipeline/b_parser.rs
@@ -2,12 +2,16 @@ use std::collections::VecDeque;
 
 use super::a_tokenizer::Token;
 
-pub fn parse(tokens: &mut VecDeque<Token>) -> Option<String> {
+pub fn parse(tokens: &mut VecDeque<Token>) -> Option<SyntaxElement> {
     match tokens.pop_front()? {
         Token::CurlyBracketOpen => {
             // not supported yet
             None
         }
-        Token::Ident(ident) => Some(ident),
+        Token::Ident(ident) => Some(SyntaxElement::Word(ident)),
     }
+}
+
+pub enum SyntaxElement {
+    Word(String),
 }
