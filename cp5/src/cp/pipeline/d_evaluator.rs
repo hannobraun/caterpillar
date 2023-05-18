17d940e87f90519370bf8f114233ef11464ee977
Hanno Braun <hanno@braun-odw.eu>
2023-05-18 20:47:47 +0200

    Update argument name

--- cp5/src/cp/pipeline/d_evaluator.rs
+++ cp5/src/cp/pipeline/d_evaluator.rs
@@ -1,10 +1,10 @@
 use crate::cp::{DataStack, DataStackError};
 
 pub fn evaluate(
-    ident: String,
+    word: String,
     data_stack: &mut DataStack,
 ) -> Result<(), EvaluatorError> {
-    match ident.as_str() {
+    match word.as_str() {
         "true" => data_stack.push(true),
         "false" => data_stack.push(false),
         "not" => {
@@ -12,7 +12,7 @@ pub fn evaluate(
             let x = !a;
             data_stack.push(x);
         }
-        _ => return Err(EvaluatorError::UnknownWord(ident)),
+        _ => return Err(EvaluatorError::UnknownWord(word)),
     }
 
     Ok(())
