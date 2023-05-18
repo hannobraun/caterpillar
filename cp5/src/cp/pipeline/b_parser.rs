35aee78d3acfcc11966ec06a2cae4ab06cffd048
Hanno Braun <hanno@braun-odw.eu>
2023-05-18 20:31:22 +0200

    Refactor

--- cp5/src/cp/pipeline/b_parser.rs
+++ cp5/src/cp/pipeline/b_parser.rs
@@ -3,6 +3,7 @@ use std::collections::VecDeque;
 use super::a_tokenizer::Token;
 
 pub fn parse(tokens: &mut VecDeque<Token>) -> Option<String> {
-    let Token::Ident(ident) = tokens.pop_front()?;
-    Some(ident)
+    match tokens.pop_front()? {
+        Token::Ident(ident) => Some(ident),
+    }
 }
