f8e1794fdb2c60887a0356af00450abe6f671c86
Hanno Braun <hanno@braun-odw.eu>
2023-05-18 08:26:37 +0200

    Refactor to prepare for factoring out tokenizer

--- cp5/src/cp/mod.rs
+++ cp5/src/cp/mod.rs
@@ -1,6 +1,8 @@
 mod data_stack;
 mod pipeline;
 
+use std::collections::VecDeque;
+
 pub use self::{
     data_stack::{DataStack, DataStackError},
     pipeline::d_evaluator::EvaluatorError,
@@ -10,8 +12,24 @@ pub fn execute(
     code: &str,
     data_stack: &mut DataStack,
 ) -> Result<(), EvaluatorError> {
-    for word in code.split_whitespace() {
-        pipeline::d_evaluator::evaluate(word, data_stack)?;
+    let mut chars = code.chars().collect::<VecDeque<_>>();
+
+    loop {
+        let mut word = String::new();
+
+        while let Some(ch) = chars.pop_front() {
+            if ch.is_whitespace() {
+                break;
+            }
+
+            word.push(ch)
+        }
+
+        if word.is_empty() {
+            break;
+        }
+
+        pipeline::d_evaluator::evaluate(&word, data_stack)?;
     }
 
     Ok(())
