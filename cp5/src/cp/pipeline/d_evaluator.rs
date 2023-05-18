752c5abe1d333421a1c77ac0c90e04cdd9eadc2b
Hanno Braun <hanno@braun-odw.eu>
2023-05-18 08:42:11 +0200

    Update variable name

--- cp5/src/cp/pipeline/d_evaluator.rs
+++ cp5/src/cp/pipeline/d_evaluator.rs
@@ -6,9 +6,9 @@ pub fn evaluate(
     token: Token,
     data_stack: &mut DataStack,
 ) -> Result<(), EvaluatorError> {
-    let Token::Ident(word) = token;
+    let Token::Ident(ident) = token;
 
-    match word.as_str() {
+    match ident.as_str() {
         "true" => data_stack.push(true),
         "false" => data_stack.push(false),
         "not" => {
@@ -16,7 +16,7 @@ pub fn evaluate(
             let x = !a;
             data_stack.push(x);
         }
-        _ => return Err(EvaluatorError::UnknownWord(word)),
+        _ => return Err(EvaluatorError::UnknownWord(ident)),
     }
 
     Ok(())
