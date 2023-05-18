b4da15dcbd2f9d7c924a748c6651b8856e70fed0
Hanno Braun <hanno@braun-odw.eu>
2023-05-18 08:41:20 +0200

    Update variable name

--- cp5/src/cp/pipeline/a_tokenizer.rs
+++ cp5/src/cp/pipeline/a_tokenizer.rs
@@ -1,21 +1,21 @@
 use std::collections::VecDeque;
 
 pub fn tokenize(chars: &mut VecDeque<char>) -> Option<Token> {
-    let mut word = String::new();
+    let mut buf = String::new();
 
     while let Some(ch) = chars.pop_front() {
         if ch.is_whitespace() {
             break;
         }
 
-        word.push(ch)
+        buf.push(ch)
     }
 
-    if word.is_empty() {
+    if buf.is_empty() {
         return None;
     }
 
-    Some(Token::Ident(word))
+    Some(Token::Ident(buf))
 }
 
 pub enum Token {
