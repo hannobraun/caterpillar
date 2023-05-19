0c870fe1f1ac06036592b2cb46b8a42d19515b1e
Hanno Braun <hanno@braun-odw.eu>
2023-05-19 10:37:57 +0200

    Derive `Debug` for `SyntaxTree`

--- cp5/src/cp/syntax.rs
+++ cp5/src/cp/syntax.rs
@@ -1,7 +1,9 @@
+#[derive(Debug)]
 pub struct SyntaxTree {
     pub elements: Vec<SyntaxElement>,
 }
 
+#[derive(Debug)]
 pub enum SyntaxElement {
     Block { syntax_tree: SyntaxTree },
     Word(String),
