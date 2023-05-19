fd7cfc7568144aa5e93f701e52d37a05e6cbcdab
Hanno Braun <hanno@braun-odw.eu>
2023-05-19 10:06:56 +0200

    Refactor

--- cp5/src/cp/pipeline/d_evaluator.rs
+++ cp5/src/cp/pipeline/d_evaluator.rs
@@ -7,7 +7,13 @@ pub fn evaluate(
     data_stack: &mut DataStack,
 ) -> Result<(), EvaluatorError> {
     let SyntaxElement::Word(word) = syntax_element;
+    evaluator_word(word, data_stack)
+}
 
+fn evaluator_word(
+    word: String,
+    data_stack: &mut DataStack,
+) -> Result<(), EvaluatorError> {
     match word.as_str() {
         "true" => data_stack.push(true),
         "false" => data_stack.push(false),
