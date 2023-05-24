8898b84888dcd454c935f7f53e5b0db0f1de7741
Hanno Braun <hanno@braun-odw.eu>
2023-05-24 17:29:10 +0200

    Add `Function::new`

--- cp5/src/cp/functions.rs
+++ cp5/src/cp/functions.rs
@@ -1,1 +1,7 @@
 pub struct Functions;
+
+impl Functions {
+    pub fn new() -> Functions {
+        Functions
+    }
+}
--- cp5/src/tests.rs
+++ cp5/src/tests.rs
@@ -16,7 +16,7 @@ pub fn run() -> anyhow::Result<Vec<TestReport>> {
 
     for ((module, name), code) in tests {
         let mut data_stack = cp::DataStack::new();
-        let mut functions = cp::Functions;
+        let mut functions = cp::Functions::new();
 
         let result = cp::execute(code, &mut data_stack, &mut functions, false)
             .map_err(Error::Language)
