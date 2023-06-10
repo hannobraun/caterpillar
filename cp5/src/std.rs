d57a61955214353f7131e3a5d381ae7244265f49
Hanno Braun <hanno@braun-odw.eu>
2023-06-10 08:42:40 +0200

    Expand infrastructure for defining `std`

--- cp5/src/std.rs
+++ cp5/src/std.rs
@@ -1,5 +1,26 @@
 use crate::cp;
 
 pub fn define() -> anyhow::Result<cp::Functions> {
-    Ok(cp::Functions::new())
+    let mut functions = cp::Functions::new();
+
+    let code = r#"
+    "#;
+
+    let mut data_stack = cp::DataStack::new();
+    let mut bindings = cp::Bindings::new();
+    let mut tests = cp::Functions::new();
+
+    cp::execute(
+        code,
+        &mut data_stack,
+        &mut bindings,
+        &mut functions,
+        &mut tests,
+    )?;
+
+    if !data_stack.is_empty() {
+        anyhow::bail!("Defining `std` left values on stack: {data_stack:?}")
+    }
+
+    Ok(functions)
 }
