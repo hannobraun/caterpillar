a79b6db9ddfe3993abb966c231cc8cdefc94393a
Hanno Braun <hanno@braun-odw.eu>
2023-06-14 23:36:15 +0200

    Read from stdin

--- cp5/src/main.rs
+++ cp5/src/main.rs
@@ -1,3 +1,5 @@
+use ::std::io::stdin;
+
 mod cp;
 mod std;
 mod test_report;
@@ -7,6 +9,11 @@ fn main() -> anyhow::Result<()> {
     let functions = std::define()?;
     let test_reports = tests::run(functions)?;
     test_report::print(&test_reports);
+
+    for input in stdin().lines() {
+        let input = input?;
+        println!("{input}");
+    }
 
     Ok(())
 }
