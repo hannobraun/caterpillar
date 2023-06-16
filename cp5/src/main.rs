dafb501aefc2e13d41933cda193a40df143fa142
Hanno Braun <hanno@braun-odw.eu>
2023-06-16 21:45:06 +0200

    Execute input

--- cp5/src/main.rs
+++ cp5/src/main.rs
@@ -7,13 +7,23 @@ mod tests;
 
 fn main() -> anyhow::Result<()> {
     let mut functions = std::define()?;
-    let tests = tests::define(&mut functions)?;
+    let mut tests = tests::define(&mut functions)?;
     let test_reports = tests::run(&mut functions, &tests)?;
     test_report::print(&test_reports);
 
+    let mut data_stack = cp::DataStack::new();
+    let mut bindings = cp::Bindings::new();
+
     for input in stdin().lines() {
         let input = input?;
-        println!("{input}");
+
+        cp::execute(
+            &input,
+            &mut data_stack,
+            &mut bindings,
+            &mut functions,
+            &mut tests,
+        )?;
     }
 
     Ok(())
