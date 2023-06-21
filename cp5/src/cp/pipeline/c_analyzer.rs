90564d618278ba2952801c6e4a6ed6d64aad8f2a
Hanno Braun <hanno@braun-odw.eu>
2023-06-21 10:11:14 +0200

    Refactor

--- cp5/src/cp/pipeline/c_analyzer.rs
+++ cp5/src/cp/pipeline/c_analyzer.rs
@@ -8,9 +8,13 @@ pub fn analyze(
     mut syntax_elements: StageInputReader<SyntaxElement>,
 ) -> Result<Expression, PipelineError<Infallible>> {
     let syntax_element = syntax_elements.read()?;
-    let expression = Expression::RawSyntaxElement(syntax_element.clone());
+    let expression = analyze_syntax_element(syntax_element);
     syntax_elements.take();
     Ok(expression)
+}
+
+fn analyze_syntax_element(syntax_element: &SyntaxElement) -> Expression {
+    Expression::RawSyntaxElement(syntax_element.clone())
 }
 
 pub enum Expression {
