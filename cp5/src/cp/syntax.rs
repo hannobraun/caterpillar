8548eb01433854de527f06be9d32a6b8d32c7358
Hanno Braun <hanno@braun-odw.eu>
2023-06-21 10:20:11 +0200

    Implement `IntoIterator` for `SyntaxTree`

--- cp5/src/cp/syntax.rs
+++ cp5/src/cp/syntax.rs
@@ -1,6 +1,17 @@
+use std::slice;
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct SyntaxTree {
     pub elements: Vec<SyntaxElement>,
+}
+
+impl<'r> IntoIterator for &'r SyntaxTree {
+    type Item = &'r SyntaxElement;
+    type IntoIter = slice::Iter<'r, SyntaxElement>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.elements.iter()
+    }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
