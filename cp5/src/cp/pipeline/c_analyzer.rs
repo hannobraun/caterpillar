ba06f40269b23a788e8799c4d2aefcea836f6c21
Hanno Braun <hanno@braun-odw.eu>
2023-06-21 10:09:29 +0200

    Refactor

--- cp5/src/cp/pipeline/c_analyzer.rs
+++ cp5/src/cp/pipeline/c_analyzer.rs
@@ -7,8 +7,8 @@ use super::{stage_input::StageInputReader, PipelineError};
 pub fn analyze(
     mut syntax_elements: StageInputReader<SyntaxElement>,
 ) -> Result<Expression, PipelineError<Infallible>> {
-    let expression =
-        Expression::RawSyntaxElement(syntax_elements.read()?.clone());
+    let syntax_element = syntax_elements.read()?;
+    let expression = Expression::RawSyntaxElement(syntax_element.clone());
     syntax_elements.take();
     Ok(expression)
 }
