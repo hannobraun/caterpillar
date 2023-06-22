e078936785af28f2bf66314f47046f55ae099846
Hanno Braun <hanno@braun-odw.eu>
2023-06-22 20:25:48 +0200

    Derive `Default` for `Bindings`

--- cp5/src/cp/bindings.rs
+++ cp5/src/cp/bindings.rs
@@ -2,14 +2,13 @@ use std::collections::BTreeMap;
 
 use super::data_stack::Value;
 
+#[derive(Default)]
 pub struct Bindings {
     pub inner: BTreeMap<String, Value>,
 }
 
 impl Bindings {
     pub fn new() -> Self {
-        Self {
-            inner: BTreeMap::new(),
-        }
+        Self::default()
     }
 }
