c559a0f5da325896c6f6ac502ad0934175beaeb7
Hanno Braun <hanno@braun-odw.eu>
2023-07-06 13:11:11 +0200

    Run tests

--- cp6/src/main.rs
+++ cp6/src/main.rs
@@ -1,12 +1,24 @@
-#[allow(unused)]
 mod cp;
 
 use sycamore::prelude::*;
 
 fn main() -> anyhow::Result<()> {
+    let (mut functions, tests) = cp::define_code()?;
+    let test_reports = cp::run_tests(&mut functions, &tests)?;
+
     sycamore::render(|cx| {
+        let test_reports = create_signal(cx, test_reports);
+
         view! { cx,
             p { "Hello, world!" }
+            ul {
+                Indexed(
+                    iterable=test_reports,
+                    view=|cx, test_report| view! {cx,
+                        li { (test_report.name) }
+                    }
+                )
+            }
         }
     });
 
