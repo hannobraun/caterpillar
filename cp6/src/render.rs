400499cbb79e4143520886bc43b4e00bb8be0589
Hanno Braun <hanno@braun-odw.eu>
2023-07-07 13:20:41 +0200

    Add `TestReports` component

--- cp6/src/render.rs
+++ cp6/src/render.rs
@@ -7,14 +7,29 @@ pub fn render(test_reports: Vec<cp::TestReport>) {
         let test_reports = create_signal(cx, test_reports);
 
         view! { cx,
-            ul {
-                Indexed(
-                    iterable=test_reports,
-                    view=|cx, test_report| view! {cx,
-                        li { (test_report.name) }
-                    }
-                )
-            }
+            TestReports(test_reports=test_reports)
         }
     });
+}
+
+#[component]
+fn TestReports<'r, G: Html>(
+    cx: Scope<'r>,
+    props: TestReportsProps<'r>,
+) -> View<G> {
+    view! { cx,
+        ul {
+            Indexed(
+                iterable=props.test_reports,
+                view=|cx, test_report| view! {cx,
+                    li { (test_report.name) }
+                }
+            )
+        }
+    }
+}
+
+#[derive(Prop)]
+struct TestReportsProps<'r> {
+    test_reports: &'r ReadSignal<Vec<cp::TestReport>>,
 }
