753071f9021f0f50cd9a3bde98d6682494a0c801
Hanno Braun <hanno@braun-odw.eu>
2023-07-11 17:20:55 +0200

    Set up logging

--- cp6/src/main.rs
+++ cp6/src/main.rs
@@ -3,6 +3,8 @@ mod render;
 
 fn main() -> anyhow::Result<()> {
     console_error_panic_hook::set_once();
+    console_log::init_with_level(log::Level::Debug)
+        .expect("Error initializing logging");
 
     let (mut functions, tests) = cp::define_code()?;
     let test_reports = cp::run_tests(&mut functions, &tests)?;
