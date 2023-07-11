9f5e5e477814a1e08e6eecd955e6a90a40fa4eca
Hanno Braun <hanno@braun-odw.eu>
2023-07-11 17:25:12 +0200

    Clear input on enter

--- cp6/src/render.rs
+++ cp6/src/render.rs
@@ -2,16 +2,31 @@ mod pass_fail;
 mod test_report;
 mod test_run_result;
 
-use sycamore::prelude::*;
+use sycamore::{prelude::*, rt::JsCast};
+use web_sys::{Event, KeyboardEvent};
 
 use crate::{cp, render::test_run_result::TestRunResult};
 
 pub fn render(test_reports: Vec<cp::TestReport>) {
     sycamore::render(|cx| {
+        let input = create_signal(cx, String::new());
         let test_reports = create_signal(cx, test_reports);
 
+        let detect_enter = |event: Event| {
+            if let Some(event) = event.dyn_ref::<KeyboardEvent>() {
+                if event.key() == "Enter" {
+                    input.modify().clear();
+                }
+            }
+        };
+
         view! { cx,
-            input(type="text", class="m-4 ring-1")
+            input(
+                bind:value=input,
+                on:keyup=detect_enter,
+                type="text",
+                class="m-4 ring-1"
+            )
             TestRunResult(test_reports=test_reports)
         }
     });
