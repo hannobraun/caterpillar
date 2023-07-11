711a093e14c0b09f03f5bb32eba7c8cc496d7d36
Hanno Braun <hanno@braun-odw.eu>
2023-07-11 17:19:18 +0200

    Set up panic handler

--- cp6/src/main.rs
+++ cp6/src/main.rs
@@ -2,6 +2,8 @@ mod cp;
 mod render;
 
 fn main() -> anyhow::Result<()> {
+    console_error_panic_hook::set_once();
+
     let (mut functions, tests) = cp::define_code()?;
     let test_reports = cp::run_tests(&mut functions, &tests)?;
     render::render(test_reports);
