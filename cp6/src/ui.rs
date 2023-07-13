fb39eca183d5ebfeff676afdaecb681bce695003
Hanno Braun <hanno@braun-odw.eu>
2023-07-13 17:19:02 +0200

    Refactor

--- cp6/src/ui.rs
+++ cp6/src/ui.rs
@@ -1,43 +1,23 @@
+mod code_input;
 mod pass_fail;
 mod test_report;
 mod test_run_result;
 
-use sycamore::{prelude::*, rt::JsCast};
-use web_sys::{Event, KeyboardEvent};
+use sycamore::prelude::*;
 
-use crate::{cp, ui::test_run_result::TestRunResult};
+use crate::{
+    cp,
+    ui::{code_input::CodeInput, test_run_result::TestRunResult},
+};
 
 pub fn render(mut test_runner: cp::TestRunner) {
     let test_reports = test_runner.run_tests();
 
     sycamore::render(|cx| {
-        let input = create_signal(cx, String::new());
         let test_reports = create_signal(cx, test_reports);
 
-        let detect_enter = move |event: Event| {
-            if let Some(event) = event.dyn_ref::<KeyboardEvent>() {
-                if event.key() == "Enter" {
-                    let code = input.get();
-                    input.modify().clear();
-
-                    if let Err(err) = test_runner.run_code(&code) {
-                        log::error!("{err}");
-                    }
-                    let reports = test_runner.run_tests();
-
-                    test_reports.set(reports);
-                }
-            }
-        };
-
         view! { cx,
-            input(
-                bind:value=input,
-                on:keyup=detect_enter,
-                type="text",
-                class="m-4 ring-1",
-                autofocus=true,
-            )
+            CodeInput(test_runner=test_runner, test_reports=test_reports)
             TestRunResult(test_reports=test_reports)
         }
     });
---
+++ cp6/src/ui/code_input.rs
@@ -0,0 +1,48 @@
+use sycamore::{
+    component,
+    reactive::{create_signal, Scope, Signal},
+    rt::JsCast,
+    view,
+    view::View,
+    web::Html,
+};
+use web_sys::{Event, KeyboardEvent};
+
+use crate::cp;
+
+#[component]
+pub fn CodeInput<'r, G: Html>(cx: Scope<'r>, mut props: Props<'r>) -> View<G> {
+    let input = create_signal(cx, String::new());
+
+    let detect_enter = move |event: Event| {
+        if let Some(event) = event.dyn_ref::<KeyboardEvent>() {
+            if event.key() == "Enter" {
+                let code = input.get();
+                input.modify().clear();
+
+                if let Err(err) = props.test_runner.run_code(&code) {
+                    log::error!("{err}");
+                }
+                let reports = props.test_runner.run_tests();
+
+                props.test_reports.set(reports);
+            }
+        }
+    };
+
+    view! { cx,
+        input(
+            bind:value=input,
+            on:keyup=detect_enter,
+            type="text",
+            class="m-4 ring-1",
+            autofocus=true,
+        )
+    }
+}
+
+#[derive(sycamore::Prop)]
+pub struct Props<'r> {
+    test_runner: cp::TestRunner,
+    test_reports: &'r Signal<cp::TestReports>,
+}
