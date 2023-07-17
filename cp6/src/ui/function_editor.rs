196be980f2703f0a8c29aefacff87a60e1846ca6
Hanno Braun <hanno@braun-odw.eu>
2023-07-17 14:00:18 +0200

    Display function bodies

--- cp6/src/ui/function_editor.rs
+++ cp6/src/ui/function_editor.rs
@@ -4,9 +4,12 @@ use crate::cp;
 
 #[component]
 pub fn FunctionEditor<G: Html>(cx: Scope, props: Props) -> View<G> {
+    let body = format!("{:?}", props.function.body);
+
     view! { cx,
         div {
             p { (props.function.name) }
+            p { (body) }
         }
     }
 }
