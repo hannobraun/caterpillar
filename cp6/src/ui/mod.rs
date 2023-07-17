6d81d35933fece80c0ea1ee44eda8abcffe87bcd
Hanno Braun <hanno@braun-odw.eu>
2023-07-17 14:10:27 +0200

    Make tests scroll, if they're higher than screen

--- cp6/src/ui/mod.rs
+++ cp6/src/ui/mod.rs
@@ -22,14 +22,14 @@ pub fn render(mut test_runner: cp::TestRunner) {
         let test_reports = create_signal(cx, test_reports);
 
         view! { cx,
-            div(class="flex flex-row") {
+            div(class="h-screen flex flex-row") {
                 div(class="basis-1/2") {
                     CodeEditor(
                         test_runner=test_runner,
                         test_reports=test_reports
                     )
                 }
-                div(class="basis-1/2") {
+                div(class="h-full overflow-auto basis-1/2") {
                     TestRunResult(test_reports=test_reports)
                 }
             }
