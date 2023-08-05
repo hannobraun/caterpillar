362ab5c0de5060fb11e3b75d1031a2b0dd94ba9f
Hanno Braun <hanno@braun-odw.eu>
2023-08-05 19:01:00 +0200

    Refactor

--- cp7/src/tokenizer.rs
+++ cp7/src/tokenizer.rs
@@ -2,15 +2,18 @@ pub fn tokenize(code: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
 
     for token in code.split_whitespace() {
-        let token = match token.split_once(':') {
-            Some(("", symbol)) => Token::Symbol(symbol.into()),
-            _ => Token::FnRef(token.into()),
-        };
-
+        let token = tokenize_token(token);
         tokens.push(token);
     }
 
     tokens
+}
+
+fn tokenize_token(token: &str) -> Token {
+    match token.split_once(':') {
+        Some(("", symbol)) => Token::Symbol(symbol.into()),
+        _ => Token::FnRef(token.into()),
+    }
 }
 
 pub enum Token {
