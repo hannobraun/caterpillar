8dc4e88d0913f3ec302fd4cd84eee1ec837f3f6c
Hanno Braun <hanno@braun-odw.eu>
2023-08-07 01:25:43 +0200

    Refactor to prepare for follow-on change

--- cp7/src/runtime/call_stack.rs
+++ cp7/src/runtime/call_stack.rs
@@ -1,19 +1,29 @@
 use crate::syntax::SyntaxHandle;
 
 pub struct CallStack {
-    current: Option<SyntaxHandle>,
+    current: Vec<SyntaxHandle>,
 }
 
 impl CallStack {
     pub fn new(start: Option<SyntaxHandle>) -> Self {
-        Self { current: start }
+        Self {
+            current: start.into_iter().collect(),
+        }
     }
 
     pub fn current(&self) -> Option<SyntaxHandle> {
-        self.current
+        self.current.last().copied()
     }
 
     pub fn update(&mut self, next: Option<SyntaxHandle>) {
-        self.current = next;
+        match next {
+            Some(next) => match self.current.last_mut() {
+                Some(current) => *current = next,
+                None => self.current.push(next),
+            },
+            None => {
+                self.current.pop();
+            }
+        }
     }
 }
