ce7af80a1664113497aedd3c68f20b93a8ff4a39
Hanno Braun <hanno@braun-odw.eu>
2023-08-06 12:17:28 +0200

    Refactor

--- cp7/src/data_stack.rs
+++ cp7/src/data_stack.rs
@@ -38,7 +38,7 @@ pub enum Value {
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Value::Number(number) => number.fmt(f),
+            Value::Number(number) => write!(f, "{number}"),
         }
     }
 }
