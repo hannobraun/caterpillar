a6b939cfb77c27abd7c8a1e2174fc98d6114edb1
Hanno Braun <hanno@braun-odw.eu>
2023-08-06 17:25:57 +0200

    Refactor

--- cp7/src/pipeline/d_evaluator.rs
+++ cp7/src/pipeline/d_evaluator.rs
@@ -9,12 +9,18 @@ pub fn evaluate(syntax_tree: SyntaxTree) -> anyhow::Result<()> {
     let mut functions = Functions::new();
     let mut data_stack = DataStack::new();
 
+    evaluate_syntax_tree(syntax_tree, &mut functions, &mut data_stack)?;
+
+    Ok(())
+}
+
+fn evaluate_syntax_tree(
+    syntax_tree: SyntaxTree,
+    functions: &mut Functions,
+    data_stack: &mut DataStack,
+) -> anyhow::Result<()> {
     for syntax_element in syntax_tree.elements {
-        evaluate_syntax_element(
-            syntax_element,
-            &mut functions,
-            &mut data_stack,
-        )?;
+        evaluate_syntax_element(syntax_element, functions, data_stack)?;
     }
 
     Ok(())
