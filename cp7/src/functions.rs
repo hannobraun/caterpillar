275ea0ff2cf90125dec04abd91b63e29f8d46fcf
Hanno Braun <hanno@braun-odw.eu>
2023-08-06 16:26:27 +0200

    Add `Functions`

--- cp7/src/functions.rs
+++ cp7/src/functions.rs
@@ -1,4 +1,27 @@
+use std::collections::BTreeMap;
+
 use crate::data_stack::{value, DataStack, DataStackResult};
+
+pub struct Functions {
+    inner: BTreeMap<&'static str, Intrinsic>,
+}
+
+impl Functions {
+    pub fn new() -> Self {
+        let mut inner = BTreeMap::new();
+
+        inner.insert("+", add as Intrinsic);
+        inner.insert("print_line", print_line);
+
+        Self { inner }
+    }
+
+    pub fn resolve(&self, name: &str) -> Option<Intrinsic> {
+        self.inner.get(name).copied()
+    }
+}
+
+pub type Intrinsic = fn(&mut DataStack) -> DataStackResult<()>;
 
 pub fn add(data_stack: &mut DataStack) -> DataStackResult<()> {
     let b = data_stack.pop_number()?;
--- cp7/src/pipeline/d_evaluator.rs
+++ cp7/src/pipeline/d_evaluator.rs
@@ -1,25 +1,22 @@
 use anyhow::bail;
 
-use crate::{data_stack::DataStack, functions};
+use crate::{data_stack::DataStack, functions::Functions};
 
 use super::c_parser::{SyntaxElement, SyntaxTree};
 
 pub fn evaluate(syntax_tree: SyntaxTree) -> anyhow::Result<()> {
+    let functions = Functions::new();
     let mut data_stack = DataStack::new();
 
     for syntax_element in syntax_tree.elements {
         match syntax_element {
-            SyntaxElement::FnRef(fn_ref) => match fn_ref.as_str() {
-                "+" => {
-                    functions::add(&mut data_stack)?;
-                }
-                "print_line" => {
-                    functions::print_line(&mut data_stack)?;
-                }
-                fn_ref => {
-                    bail!("Unknown function: `{fn_ref}`");
-                }
-            },
+            SyntaxElement::FnRef(fn_ref) => {
+                let intrinsic = match functions.resolve(&fn_ref) {
+                    Some(intrinsic) => intrinsic,
+                    None => bail!("Unknown function: `{fn_ref}`"),
+                };
+                intrinsic(&mut data_stack)?;
+            }
             SyntaxElement::Value(value) => {
                 data_stack.push(value);
             }
