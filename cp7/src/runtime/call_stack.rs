19ed42a80ea5ae845512d4de2f6cff5f08e132fb
Hanno Braun <hanno@braun-odw.eu>
2023-08-07 01:26:07 +0200

    Update struct field name

--- cp7/src/runtime/call_stack.rs
+++ cp7/src/runtime/call_stack.rs
@@ -1,28 +1,28 @@
 use crate::syntax::SyntaxHandle;
 
 pub struct CallStack {
-    current: Vec<SyntaxHandle>,
+    frames: Vec<SyntaxHandle>,
 }
 
 impl CallStack {
     pub fn new(start: Option<SyntaxHandle>) -> Self {
         Self {
-            current: start.into_iter().collect(),
+            frames: start.into_iter().collect(),
         }
     }
 
     pub fn current(&self) -> Option<SyntaxHandle> {
-        self.current.last().copied()
+        self.frames.last().copied()
     }
 
     pub fn update(&mut self, next: Option<SyntaxHandle>) {
         match next {
-            Some(next) => match self.current.last_mut() {
+            Some(next) => match self.frames.last_mut() {
                 Some(current) => *current = next,
-                None => self.current.push(next),
+                None => self.frames.push(next),
             },
             None => {
-                self.current.pop();
+                self.frames.pop();
             }
         }
     }
