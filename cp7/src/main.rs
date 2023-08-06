4ce4453e3029c87f4897b3a2020b5dff4ac74280
Hanno Braun <hanno@braun-odw.eu>
2023-08-06 15:41:13 +0200

    Add `pipeline::run`

--- cp7/src/main.rs
+++ cp7/src/main.rs
@@ -4,11 +4,6 @@ mod pipeline;
 
 fn main() -> anyhow::Result<()> {
     let example = args::example()?;
-
-    let code = pipeline::a_loader::load(example)?;
-    let tokens = pipeline::b_tokenizer::tokenize(&code);
-    let syntax_tree = pipeline::c_parser::parse(tokens)?;
-    pipeline::d_evaluator::evaluate(syntax_tree)?;
-
+    pipeline::run(example)?;
     Ok(())
 }
--- cp7/src/pipeline/mod.rs
+++ cp7/src/pipeline/mod.rs
@@ -2,3 +2,12 @@ pub mod a_loader;
 pub mod b_tokenizer;
 pub mod c_parser;
 pub mod d_evaluator;
+
+pub fn run(path: impl AsRef<std::path::Path>) -> anyhow::Result<()> {
+    let code = a_loader::load(path)?;
+    let tokens = b_tokenizer::tokenize(&code);
+    let syntax_tree = c_parser::parse(tokens)?;
+    d_evaluator::evaluate(syntax_tree)?;
+
+    Ok(())
+}
