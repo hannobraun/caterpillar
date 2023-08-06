46f6c0ea229228d23cac4a5997e663d3a032d0b7
Hanno Braun <hanno@braun-odw.eu>
2023-08-06 22:50:34 +0200

    Refactor to prepare for follow-on change

--- cp7/src/pipeline/d_evaluator.rs
+++ cp7/src/pipeline/d_evaluator.rs
@@ -1,31 +1,43 @@
 use crate::{
     data_stack::DataStack,
     functions::{Function, Functions},
-    syntax::{SyntaxElement, SyntaxTree},
+    syntax::{Syntax, SyntaxElement, SyntaxTree},
 };
 
-pub fn evaluate(syntax_tree: SyntaxTree) -> anyhow::Result<()> {
+pub fn evaluate(syntax: Syntax, syntax_tree: SyntaxTree) -> anyhow::Result<()> {
     let mut functions = Functions::new();
     let mut data_stack = DataStack::new();
 
-    evaluate_syntax_tree(syntax_tree, &mut functions, &mut data_stack)?;
+    evaluate_syntax_tree(
+        &syntax,
+        syntax_tree,
+        &mut functions,
+        &mut data_stack,
+    )?;
 
     Ok(())
 }
 
 fn evaluate_syntax_tree(
+    syntax: &Syntax,
     syntax_tree: SyntaxTree,
     functions: &mut Functions,
     data_stack: &mut DataStack,
 ) -> anyhow::Result<()> {
     for syntax_element in syntax_tree.elements {
-        evaluate_syntax_element(syntax_element.payload, functions, data_stack)?;
+        evaluate_syntax_element(
+            syntax,
+            syntax_element.payload,
+            functions,
+            data_stack,
+        )?;
     }
 
     Ok(())
 }
 
 fn evaluate_syntax_element(
+    syntax: &Syntax,
     syntax_element: SyntaxElement,
     functions: &mut Functions,
     data_stack: &mut DataStack,
@@ -34,7 +46,12 @@ fn evaluate_syntax_element(
         SyntaxElement::FnRef(fn_ref) => match functions.resolve(&fn_ref)? {
             Function::Intrinsic(intrinsic) => intrinsic(functions, data_stack)?,
             Function::UserDefined { body } => {
-                evaluate_syntax_tree(body.0.clone(), functions, data_stack)?;
+                evaluate_syntax_tree(
+                    syntax,
+                    body.0.clone(),
+                    functions,
+                    data_stack,
+                )?;
             }
         },
         SyntaxElement::Value(value) => {
--- cp7/src/pipeline/mod.rs
+++ cp7/src/pipeline/mod.rs
@@ -6,8 +6,8 @@ pub mod d_evaluator;
 pub fn run(path: impl AsRef<std::path::Path>) -> anyhow::Result<()> {
     let code = a_loader::load(path)?;
     let tokens = b_tokenizer::tokenize(&code);
-    let (_, syntax_tree) = c_parser::parse(tokens)?;
-    d_evaluator::evaluate(syntax_tree)?;
+    let (syntax, syntax_tree) = c_parser::parse(tokens)?;
+    d_evaluator::evaluate(syntax, syntax_tree)?;
 
     Ok(())
 }
