cced793a37d335f16404fbedcd009763065f43fa
Hanno Braun <hanno@braun-odw.eu>
2023-08-06 16:56:56 +0200

    Refactor to prepare for follow-on change

--- cp7/src/data_stack.rs
+++ cp7/src/data_stack.rs
@@ -32,8 +32,7 @@ impl DataStack {
 
     fn pop_inner(&mut self, expected: &'static str) -> DataStackResult<Value> {
         self.values.pop().ok_or(DataStackError {
-            kind: DataStackErrorKind::StackIsEmpty,
-            expected,
+            kind: DataStackErrorKind::StackIsEmpty { expected },
         })
     }
 }
@@ -52,8 +51,7 @@ impl Value {
         T: TryFrom<Value, Error = Value>,
     {
         self.try_into().map_err(|value| DataStackError {
-            kind: DataStackErrorKind::UnexpectedValue { value },
-            expected,
+            kind: DataStackErrorKind::UnexpectedValue { value, expected },
         })
     }
 }
@@ -73,21 +71,16 @@ pub type DataStackResult<T> = Result<T, DataStackError>;
 #[derive(Debug, thiserror::Error)]
 pub struct DataStackError {
     pub kind: DataStackErrorKind,
-    pub expected: &'static str,
 }
 
 impl fmt::Display for DataStackError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self.kind {
-            DataStackErrorKind::StackIsEmpty => {
-                write!(f, "Stack is empty (expected {})", self.expected)
+            DataStackErrorKind::StackIsEmpty { expected } => {
+                write!(f, "Stack is empty (expected {})", expected)
             }
-            DataStackErrorKind::UnexpectedValue { value } => {
-                writeln!(
-                    f,
-                    "Unexpected value: {value} (expected {})",
-                    self.expected
-                )
+            DataStackErrorKind::UnexpectedValue { value, expected } => {
+                writeln!(f, "Unexpected value: {value} (expected {})", expected)
             }
         }
     }
@@ -95,6 +88,11 @@ impl fmt::Display for DataStackError {
 
 #[derive(Debug)]
 pub enum DataStackErrorKind {
-    StackIsEmpty,
-    UnexpectedValue { value: Value },
+    StackIsEmpty {
+        expected: &'static str,
+    },
+    UnexpectedValue {
+        value: Value,
+        expected: &'static str,
+    },
 }
