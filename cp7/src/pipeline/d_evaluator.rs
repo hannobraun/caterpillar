5f7623ee52ecfdddb60aaff72bbe1ab5622c3fca
Hanno Braun <hanno@braun-odw.eu>
2023-08-07 01:20:20 +0200

    Refactor to clean up

--- cp7/src/pipeline/d_evaluator.rs
+++ cp7/src/pipeline/d_evaluator.rs
@@ -12,26 +12,17 @@ pub fn evaluate(
     let mut call_stack = CallStack::new(start);
     let mut data_stack = DataStack::new();
 
-    evaluate_syntax(
-        start,
-        &syntax,
-        &mut functions,
-        &mut call_stack,
-        &mut data_stack,
-    )?;
+    evaluate_syntax(&syntax, &mut functions, &mut call_stack, &mut data_stack)?;
 
     Ok(())
 }
 
 fn evaluate_syntax(
-    start: Option<SyntaxHandle>,
     syntax: &Syntax,
     functions: &mut Functions,
     call_stack: &mut CallStack,
     data_stack: &mut DataStack,
 ) -> anyhow::Result<()> {
-    call_stack.current = start;
-
     while let Some(handle) = call_stack.current() {
         let fragment = syntax.get(handle);
 
@@ -60,9 +51,8 @@ fn evaluate_syntax_element(
         SyntaxElement::FnRef(fn_ref) => match functions.resolve(&fn_ref)? {
             Function::Intrinsic(intrinsic) => intrinsic(functions, data_stack)?,
             Function::UserDefined { body } => {
-                evaluate_syntax(
-                    body.0, syntax, functions, call_stack, data_stack,
-                )?;
+                call_stack.current = body.0;
+                evaluate_syntax(syntax, functions, call_stack, data_stack)?;
             }
         },
         SyntaxElement::Value(value) => {
