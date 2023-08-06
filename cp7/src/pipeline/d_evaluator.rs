5659da3b027e9761fddfed8a05bb92ce1d9bad95
Hanno Braun <hanno@braun-odw.eu>
2023-08-06 16:30:17 +0200

    Improve error handling

--- cp7/src/pipeline/d_evaluator.rs
+++ cp7/src/pipeline/d_evaluator.rs
@@ -1,5 +1,3 @@
-use anyhow::bail;
-
 use crate::{data_stack::DataStack, functions::Functions};
 
 use super::c_parser::{SyntaxElement, SyntaxTree};
@@ -11,10 +9,7 @@ pub fn evaluate(syntax_tree: SyntaxTree) -> anyhow::Result<()> {
     for syntax_element in syntax_tree.elements {
         match syntax_element {
             SyntaxElement::FnRef(fn_ref) => {
-                let intrinsic = match functions.resolve(&fn_ref) {
-                    Ok(intrinsic) => intrinsic,
-                    Err(_) => bail!("Unknown function: `{fn_ref}`"),
-                };
+                let intrinsic = functions.resolve(&fn_ref)?;
                 intrinsic(&mut data_stack)?;
             }
             SyntaxElement::Value(value) => {
