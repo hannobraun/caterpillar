a6320a54b00760ec921b587c87ad3257c524504f
Hanno Braun <hanno@braun-odw.eu>
2023-08-07 02:15:41 +0200

    Add re-export

--- cp7/src/pipeline/mod.rs
+++ cp7/src/pipeline/mod.rs
@@ -1,10 +1,12 @@
 mod run;
 mod stages;
 
+pub use self::run::run;
+
 pub fn start(path: impl AsRef<std::path::Path>) -> anyhow::Result<()> {
     let mut syntax = crate::syntax::Syntax::new();
 
-    let start = run::run(path, &mut syntax)?;
+    let start = run(path, &mut syntax)?;
 
     if let Some(start) = start {
         let mut evaluator = crate::runtime::evaluator::Evaluator::new(start);
