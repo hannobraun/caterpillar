5f63daa80b79b6c4bdfc19d78ea6006bcca3d65e
Hanno Braun <hanno@braun-odw.eu>
2023-08-07 02:19:36 +0200

    Improve error handling

--- cp7/src/runtime/start.rs
+++ cp7/src/runtime/start.rs
@@ -1,10 +1,13 @@
 use std::path::Path;
 
-use crate::{pipeline, syntax::Syntax};
+use crate::{
+    pipeline::{self, PipelineError},
+    syntax::Syntax,
+};
 
-use super::evaluator::Evaluator;
+use super::evaluator::{Evaluator, EvaluatorError};
 
-pub fn start(path: impl AsRef<Path>) -> anyhow::Result<()> {
+pub fn start(path: impl AsRef<Path>) -> Result<(), RuntimeError> {
     let mut syntax = Syntax::new();
 
     let start = pipeline::run(path, &mut syntax)?;
@@ -15,4 +18,13 @@ pub fn start(path: impl AsRef<Path>) -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum RuntimeError {
+    #[error("Pipeline error")]
+    Pipeline(#[from] PipelineError),
+
+    #[error("Evaluator error")]
+    Error(#[from] EvaluatorError),
 }
