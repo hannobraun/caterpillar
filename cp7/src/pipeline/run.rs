e33c77f77892ac42b90a2833ba55a256fcf45f18
Hanno Braun <hanno@braun-odw.eu>
2023-08-07 02:11:36 +0200

    Improve error handling

--- cp7/src/pipeline/run.rs
+++ cp7/src/pipeline/run.rs
@@ -1,14 +1,29 @@
+use std::io;
+
 use crate::syntax::{Syntax, SyntaxHandle};
 
-use super::stages::{a_loader::load, b_tokenizer::tokenize, c_parser::parse};
+use super::stages::{
+    a_loader::load,
+    b_tokenizer::tokenize,
+    c_parser::{parse, ParserError},
+};
 
 pub fn run(
     path: impl AsRef<std::path::Path>,
     syntax: &mut Syntax,
-) -> anyhow::Result<Option<SyntaxHandle>> {
+) -> Result<Option<SyntaxHandle>, PipelineError> {
     let code = load(path)?;
     let tokens = tokenize(&code);
     let start = parse(tokens, syntax)?;
 
     Ok(start)
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum PipelineError {
+    #[error("Failed to load code from file")]
+    Loader(#[from] io::Error),
+
+    #[error("Failed to parse")]
+    Parser(#[from] ParserError),
 }
