09ae7d9b509d96d2d3cfa6a91c3c08c6a188b976
Hanno Braun <hanno@braun-odw.eu>
2023-08-08 17:59:00 +0200

    Only watch for "modify" events

--- cp7/src/watcher.rs
+++ cp7/src/watcher.rs
@@ -1,11 +1,21 @@
 use std::path::Path;
 
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 
 pub fn watch(path: impl AsRef<Path>) -> anyhow::Result<RecommendedWatcher> {
-    let mut watcher = notify::recommended_watcher(|event| {
-        let _ = dbg!(event);
-    })?;
+    let mut watcher =
+        notify::recommended_watcher(|event: Result<Event, _>| {
+            if let Ok(event) = event {
+                if let EventKind::Modify(_) = event.kind {
+                    dbg!(event);
+                }
+
+                return;
+            }
+
+            // Not sure what else we can do about it here.
+            eprintln!("Error watching code: {event:?}");
+        })?;
 
     watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
 
