ad4ac7e73ccf2994960e04ee8cea4e62251894a0
Hanno Braun <hanno@braun-odw.eu>
2023-08-09 18:23:02 +0200

    Start test suite for update functionality

--- cp7/src/language/runtime/updater.rs
+++ cp7/src/language/runtime/updater.rs
@@ -7,3 +7,41 @@ pub fn update(syntax: &Syntax, evaluator: &mut Evaluator) {
         evaluator.functions.replace(old, new);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::language::runtime::{
+        functions::Function, interpreter::Interpreter,
+    };
+
+    #[test]
+    fn update_at_beginning_of_named_function() -> anyhow::Result<()> {
+        let original = ":f { 1 + } fn";
+        let updated = ":f { 2 + } fn";
+
+        let mut interpreter = Interpreter::new(original)?;
+        while interpreter.step()?.in_progress() {}
+
+        let f_original = extract_f(&interpreter)?;
+
+        interpreter.update(updated)?;
+        let f_updated = extract_f(&interpreter)?;
+
+        assert_ne!(f_original, f_updated);
+
+        fn extract_f(
+            interpreter: &Interpreter,
+        ) -> anyhow::Result<blake3::Hash> {
+            let function = interpreter.evaluator.functions.resolve("f")?;
+            let Function::UserDefined { body } = function else {
+                panic!("Just defined function, but somehow not user-defined");
+            };
+            let handle =
+                body.0.expect("Function not empty, but body has no syntax");
+
+            Ok(handle.hash)
+        }
+
+        Ok(())
+    }
+}
