13dc91f41e9a878e13daabdb6ab0f5f00c37b0b6
Hanno Braun <hanno@braun-odw.eu>
2023-08-21 10:37:46 +0200

    Store previous syntax/token mapping

--- cp7/src/language/runtime/interpreter.rs
+++ cp7/src/language/runtime/interpreter.rs
@@ -1,7 +1,7 @@
 use crate::language::{
     intrinsics,
     pipeline::{self, PipelineError, PipelineOutput},
-    syntax::Syntax,
+    syntax::{Syntax, SyntaxToTokens},
     tokens::Tokens,
 };
 
@@ -15,14 +15,18 @@ use super::{
 pub struct Interpreter {
     pub tokens: Tokens,
     pub syntax: Syntax,
+    pub syntax_to_tokens: SyntaxToTokens,
     pub evaluator: Evaluator,
 }
 
 impl Interpreter {
     pub fn new(code: &str) -> Result<Self, PipelineError> {
         let mut syntax = Syntax::new();
-        let PipelineOutput { start, tokens, .. } =
-            pipeline::run(code, &mut syntax)?;
+        let PipelineOutput {
+            start,
+            tokens,
+            syntax_to_tokens,
+        } = pipeline::run(code, &mut syntax)?;
 
         let mut evaluator = Evaluator::new();
         if let Some(start) = start {
@@ -44,6 +48,7 @@ impl Interpreter {
         Ok(Interpreter {
             tokens,
             syntax,
+            syntax_to_tokens,
             evaluator,
         })
     }
@@ -54,8 +59,11 @@ impl Interpreter {
 
     pub fn update(&mut self, code: &str) -> Result<(), PipelineError> {
         self.syntax.prepare_update();
-        let PipelineOutput { tokens, .. } =
-            pipeline::run(code, &mut self.syntax)?;
+        let PipelineOutput {
+            tokens,
+            syntax_to_tokens,
+            ..
+        } = pipeline::run(code, &mut self.syntax)?;
         updater::update(
             &self.tokens,
             &tokens,
@@ -64,6 +72,7 @@ impl Interpreter {
         );
 
         self.tokens = tokens;
+        self.syntax_to_tokens = syntax_to_tokens;
 
         Ok(())
     }
