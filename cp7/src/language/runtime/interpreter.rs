a4f876c1d00e7652e1785e8274630e95849f674e
Hanno Braun <hanno@braun-odw.eu>
2023-08-21 10:15:08 +0200

    Remove unused struct field

--- cp7/src/language/runtime/interpreter.rs
+++ cp7/src/language/runtime/interpreter.rs
@@ -1,7 +1,7 @@
 use crate::language::{
     intrinsics,
     pipeline::{self, PipelineError, PipelineOutput},
-    syntax::{Syntax, SyntaxToTokens},
+    syntax::Syntax,
     tokens::Tokens,
 };
 
@@ -15,18 +15,14 @@ use super::{
 pub struct Interpreter {
     pub tokens: Tokens,
     pub syntax: Syntax,
-    pub tokens_to_syntax: SyntaxToTokens,
     pub evaluator: Evaluator,
 }
 
 impl Interpreter {
     pub fn new(code: &str) -> Result<Self, PipelineError> {
         let mut syntax = Syntax::new();
-        let PipelineOutput {
-            start,
-            tokens,
-            syntax_to_tokens: tokens_to_syntax,
-        } = pipeline::run(code, &mut syntax)?;
+        let PipelineOutput { start, tokens, .. } =
+            pipeline::run(code, &mut syntax)?;
 
         let mut evaluator = Evaluator::new();
         if let Some(start) = start {
@@ -48,7 +44,6 @@ impl Interpreter {
         Ok(Interpreter {
             tokens,
             syntax,
-            tokens_to_syntax,
             evaluator,
         })
     }
@@ -59,11 +54,8 @@ impl Interpreter {
 
     pub fn update(&mut self, code: &str) -> Result<(), PipelineError> {
         self.syntax.prepare_update();
-        let PipelineOutput {
-            tokens,
-            syntax_to_tokens,
-            ..
-        } = pipeline::run(code, &mut self.syntax)?;
+        let PipelineOutput { tokens, .. } =
+            pipeline::run(code, &mut self.syntax)?;
         updater::update(
             &self.tokens,
             &tokens,
@@ -72,7 +64,6 @@ impl Interpreter {
         );
 
         self.tokens = tokens;
-        self.tokens_to_syntax = syntax_to_tokens;
 
         Ok(())
     }
