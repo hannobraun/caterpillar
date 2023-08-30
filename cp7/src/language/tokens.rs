453af8dd4fee67a08eb7a0c817f2d22a4ab0b93b
Hanno Braun <hanno@braun-odw.eu>
2023-08-30 12:14:15 +0200

    Remove unsued code

--- cp7/src/language/tokens.rs
+++ cp7/src/language/tokens.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 use enum_variant_type::EnumVariantType;
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash, EnumVariantType)]
@@ -10,16 +8,4 @@ pub enum Token {
     Number(i64),
     Symbol(String),
     Word(String),
-}
-
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Token::CurlyBracketOpen => write!(f, "{{"),
-            Token::CurlyBracketClose => write!(f, "}}"),
-            Token::Number(number) => write!(f, "{number}"),
-            Token::Symbol(symbol) => write!(f, ":{symbol}"),
-            Token::Word(word) => write!(f, "{word}"),
-        }
-    }
 }
