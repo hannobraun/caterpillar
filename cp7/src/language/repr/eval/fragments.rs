fc1929ad39a68551fb9ee8b2d16f8854093ca90c
Hanno Braun <hanno@braun-odw.eu>
2023-09-01 10:18:33 +0200

    Inline redundant variable

--- cp7/src/language/repr/eval/fragments.rs
+++ cp7/src/language/repr/eval/fragments.rs
@@ -24,9 +24,7 @@ impl Fragments {
         };
         let address = fragment.address;
 
-        let existing = self.by_id.insert(id, fragment.clone());
-
-        if let Some(existing) = existing {
+        if let Some(existing) = self.by_id.insert(id, fragment.clone()) {
             // A hash collision should be exceedingly unlikely, but I'm not sure
             // quite *how* unlikely. Also, I don't fully trust my code to work
             // perfectly.
