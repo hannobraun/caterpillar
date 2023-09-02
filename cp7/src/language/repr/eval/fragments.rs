279d8f1a4117567086a51f44081641ba505724d3
Hanno Braun <hanno@braun-odw.eu>
2023-09-02 12:46:08 +0200

    Add more information to debug message

--- cp7/src/language/repr/eval/fragments.rs
+++ cp7/src/language/repr/eval/fragments.rs
@@ -28,7 +28,9 @@ impl Fragments {
 
         {
             let id = id.display_short();
-            debug!("insert {id}");
+            let address = address.display_short();
+
+            debug!("insert {id} at {address}");
         }
 
         if let Some(existing) = self.by_id.insert(id, fragment.clone()) {
@@ -130,6 +132,14 @@ pub struct FragmentAddress {
 }
 
 impl FragmentAddress {
+    pub fn display_short(&self) -> String {
+        format!(
+            "{{ parent: {:?}, next: {:?} }}",
+            self.parent.map(|id| id.display_short()),
+            self.next.map(|id| id.display_short())
+        )
+    }
+
     fn hash(&self, hasher: &mut blake3::Hasher) {
         if let Some(parent) = self.parent {
             hasher.update(parent.hash.as_bytes());
