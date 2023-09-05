27018067fe81274c499a54fafb299aa32f1688b5
Hanno Braun <hanno@braun-odw.eu>
2023-09-05 17:45:34 +0200

    Move `Fragment` to dedicated module

---
+++ cp7/src/language/repr/eval/fragments/fragment.rs
@@ -0,0 +1,26 @@
+use super::{FragmentAddress, FragmentId, FragmentPayload};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Fragment {
+    pub(super) address: FragmentAddress,
+    pub payload: FragmentPayload,
+}
+
+impl Fragment {
+    pub fn new(address: FragmentAddress, payload: FragmentPayload) -> Self {
+        Self { address, payload }
+    }
+
+    pub fn next(&self) -> Option<FragmentId> {
+        self.address.next
+    }
+
+    pub(super) fn hash(&self) -> blake3::Hash {
+        let mut hasher = blake3::Hasher::new();
+
+        self.address.hash(&mut hasher);
+        self.payload.hash(&mut hasher);
+
+        hasher.finalize()
+    }
+}
--- cp7/src/language/repr/eval/fragments/mod.rs
+++ cp7/src/language/repr/eval/fragments/mod.rs
@@ -1,9 +1,11 @@
 mod address;
+mod fragment;
 mod id;
 mod payload;
 
 pub use self::{
-    address::FragmentAddress, id::FragmentId, payload::FragmentPayload,
+    address::FragmentAddress, fragment::Fragment, id::FragmentId,
+    payload::FragmentPayload,
 };
 
 use std::collections::HashMap;
@@ -90,29 +92,4 @@ impl Fragments {
 #[derive(Debug)]
 struct Replacements {
     inner: HashMap<FragmentId, FragmentId>,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Fragment {
-    address: FragmentAddress,
-    pub payload: FragmentPayload,
-}
-
-impl Fragment {
-    pub fn new(address: FragmentAddress, payload: FragmentPayload) -> Self {
-        Self { address, payload }
-    }
-
-    pub fn next(&self) -> Option<FragmentId> {
-        self.address.next
-    }
-
-    fn hash(&self) -> blake3::Hash {
-        let mut hasher = blake3::Hasher::new();
-
-        self.address.hash(&mut hasher);
-        self.payload.hash(&mut hasher);
-
-        hasher.finalize()
-    }
 }
