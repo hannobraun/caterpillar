ff893d8123d4ba0f3ef8f261c9b471ec25bbeae4
Hanno Braun <hanno@braun-odw.eu>
2023-09-09 10:52:27 +0200

    Add comment

--- cp7/src/language/repr/eval/fragments/collection.rs
+++ cp7/src/language/repr/eval/fragments/collection.rs
@@ -68,6 +68,10 @@ impl Fragments {
 
             if let Some(existing) = self.by_address.get(&address).copied() {
                 if existing != id {
+                    // Let's only do the update, if we new id is actually
+                    // different from the existing one, i.e. we're actually
+                    // replacing anything.
+
                     let new = id;
                     self.replacements.insert(existing, new);
 
