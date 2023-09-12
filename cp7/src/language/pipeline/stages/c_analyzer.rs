7d606104f360b8e99fc23f91591fae7228df0636
Hanno Braun <hanno@braun-odw.eu>
2023-09-12 18:54:57 +0200

    Update comment

--- cp7/src/language/pipeline/stages/c_analyzer.rs
+++ cp7/src/language/pipeline/stages/c_analyzer.rs
@@ -55,26 +55,6 @@ fn analyze_syntax_element(
             // the block fragment itself? Well, that hasn't been computed yet,
             // and it's going to depend on the IDs of the fragments *within* the
             // block, leading to a circular dependency. Won't work!
-            //
-            // In principle, we could instead use the address of the *previous*
-            // fragment. That would serve just as well to uniquely address the
-            // fragments within the block. However, since we use the ID of the
-            // next fragment for addressing all fragments, that one is easily
-            // available to us right here and now, while the ID of the previous
-            // fragment is not known yet.
-            //
-            // You might ask, how will this work for blocks that are in the last
-            // position within their context (which is either the top-level
-            // context or a parent block). Won't the fragments within such a
-            // block not all have a `None` parent? Will this not mean, that such
-            // blocks are indistinguishable, if their contents are identical?
-            //
-            // Yes, indeed. The solution I have in mind is to introduce a
-            // terminator fragment at the end of each context. Those will have
-            // an ID that depends on their parents. Since there can only be one
-            // block at the end of each context, that means every block will be
-            // uniquely addressed again, regardless of whether it's identical to
-            // any other blocks.
             let parent = Some(next);
 
             let start = analyze_syntax_tree(syntax_tree, parent, fragments);
