df0046fbda23d581093614f11b5c2d3929d567ac
Hanno Braun <hanno@braun-odw.eu>
2023-09-23 09:40:11 +0200

    Improve error messages

--- src/loader/load.rs
+++ src/loader/load.rs
@@ -4,14 +4,13 @@ use anyhow::Context;
 
 pub fn load(path: impl AsRef<Path>) -> anyhow::Result<String> {
     let path = path.as_ref();
-    let code = load_inner(path)?;
+    let code = load_inner(path)
+        .with_context(|| format!("Loading script `{}`", path.display()))?;
     Ok(code)
 }
 
 fn load_inner(path: &Path) -> anyhow::Result<String> {
     let mut code = String::new();
-    File::open(path)
-        .with_context(|| format!("Opening script `{}`", path.display()))?
-        .read_to_string(&mut code)?;
+    File::open(path)?.read_to_string(&mut code)?;
     Ok(code)
 }
