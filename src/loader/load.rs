d625581b4b7c0f535f6376ebe96761fb909db864
Hanno Braun <hanno@braun-odw.eu>
2023-09-23 09:34:59 +0200

    Refactor to prepare for follow-on change

--- src/loader/load.rs
+++ src/loader/load.rs
@@ -1,7 +1,10 @@
 use std::{fs::File, io::Read, path::Path};
 
 pub fn load(path: impl AsRef<Path>) -> anyhow::Result<String> {
+    let path = path.as_ref();
+
     let mut code = String::new();
     File::open(path)?.read_to_string(&mut code)?;
+
     Ok(code)
 }
