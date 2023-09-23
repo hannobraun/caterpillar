862e11857d468cffb5750594d7ba9fcc27f3fd60
Hanno Braun <hanno@braun-odw.eu>
2023-09-23 09:18:42 +0200

    Make `example` argument mandatory

--- src/args.rs
+++ src/args.rs
@@ -1,29 +1,18 @@
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
-use anyhow::bail;
 use clap::Parser;
 
 pub fn example() -> anyhow::Result<PathBuf> {
     let args = Args::parse();
 
     let example_dir = "examples";
-    if let Some(example) = args.example {
-        let path = format!("{example_dir}/{example}.cp");
-        return Ok(PathBuf::from(path));
-    }
+    let example = args.example;
 
-    eprintln!("Need to specify example. Available examples:");
-
-    for dir_entry in fs::read_dir(example_dir)? {
-        let path = dir_entry?.path();
-        let example = path.file_stem().unwrap().to_string_lossy();
-        eprintln!("- {example}");
-    }
-
-    bail!("No example specified")
+    let path = format!("{example_dir}/{example}.cp");
+    Ok(PathBuf::from(path))
 }
 
 #[derive(clap::Parser)]
 struct Args {
-    example: Option<String>,
+    example: String,
 }
