e968a83a6466adce71fbacf425aca83108c0c6f4
Hanno Braun <hanno@braun-odw.eu>
2023-09-23 09:36:54 +0200

    Improve error message

--- src/loader/load.rs
+++ src/loader/load.rs
@@ -1,10 +1,14 @@
 use std::{fs::File, io::Read, path::Path};
 
+use anyhow::Context;
+
 pub fn load(path: impl AsRef<Path>) -> anyhow::Result<String> {
     let path = path.as_ref();
 
     let mut code = String::new();
-    File::open(path)?.read_to_string(&mut code)?;
+    File::open(path)
+        .with_context(|| format!("Opening script `{}`", path.display()))?
+        .read_to_string(&mut code)?;
 
     Ok(code)
 }
