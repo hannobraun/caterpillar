9b56a55dc1e91ff8e44a59292952a0deb5b94de5
Hanno Braun <hanno@braun-odw.eu>
2023-09-23 09:39:30 +0200

    Refactor

--- src/loader/load.rs
+++ src/loader/load.rs
@@ -4,11 +4,14 @@ use anyhow::Context;
 
 pub fn load(path: impl AsRef<Path>) -> anyhow::Result<String> {
     let path = path.as_ref();
+    let code = load_inner(path)?;
+    Ok(code)
+}
 
+fn load_inner(path: &Path) -> anyhow::Result<String> {
     let mut code = String::new();
     File::open(path)
         .with_context(|| format!("Opening script `{}`", path.display()))?
         .read_to_string(&mut code)?;
-
     Ok(code)
 }
