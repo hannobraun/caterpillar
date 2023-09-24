3eb88718bb63529b9e7e3a4af21efcbbedb83646
Hanno Braun <hanno@braun-odw.eu>
2023-09-24 17:15:46 +0200

    Remove redundant re-export

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod args;
 mod loader;
 
-pub use capi_core as language;
-
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
@@ -11,7 +9,7 @@ fn main() -> anyhow::Result<()> {
     let args = args::Args::parse();
     let code = loader::load::load(&args.script)?;
     let (updates, _watcher) = loader::watch::watch(&args.script)?;
-    language::runtime::start::start(&code, updates)?;
+    capi_core::runtime::start::start(&code, updates)?;
 
     Ok(())
 }
