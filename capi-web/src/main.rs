5f789ac1fc7dccf1593b8aafd51d45ed0658387a
Hanno Braun <hanno@braun-odw.eu>
2023-09-26 11:44:31 +0200

    Fix error handling

--- capi-web/src/main.rs
+++ capi-web/src/main.rs
@@ -1,6 +1,12 @@
-fn main() -> anyhow::Result<()> {
+fn main() {
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
 
+    if let Err(err) = main_inner() {
+        panic!("Error: {err}")
+    }
+}
+
+fn main_inner() -> anyhow::Result<()> {
     Ok(())
 }
