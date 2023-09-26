a459848b36b861c1fb770401d8400dae58fd8a43
Hanno Braun <hanno@braun-odw.eu>
2023-09-26 22:33:15 +0200

    Update argument name

--- capi-cli/src/platform.rs
+++ capi-cli/src/platform.rs
@@ -3,10 +3,10 @@ use std::{thread, time::Duration};
 use capi_core::{value, Context, DataStackResult, RuntimeContext};
 
 pub fn delay_ms(
-    evaluator: RuntimeContext,
+    context: RuntimeContext,
     _: &mut Context,
 ) -> DataStackResult<()> {
-    let (delay_ms, _) = evaluator.data_stack.pop_specific::<value::Number>()?;
+    let (delay_ms, _) = context.data_stack.pop_specific::<value::Number>()?;
     thread::sleep(Duration::from_millis(delay_ms.0.try_into().unwrap()));
     Ok(())
 }
