5767b8fa86d1fee150cdb11716370b81753a3bb9
Hanno Braun <hanno@braun-odw.eu>
2023-09-26 22:53:08 +0200

    Don't use `Context` for web platform

--- capi-web/src/main.rs
+++ capi-web/src/main.rs
@@ -13,20 +13,17 @@ fn main_inner() -> anyhow::Result<()> {
     let mut interpreter = capi_core::Interpreter::new(SCRIPT)?;
     interpreter.register_platform([(
         "print",
-        print as capi_core::PlatformFunction<capi_core::Context>,
+        print as capi_core::PlatformFunction<()>,
     )]);
 
-    while !interpreter
-        .step(&mut capi_core::Context::default())?
-        .finished()
-    {}
+    while !interpreter.step(&mut ())?.finished() {}
 
     Ok(())
 }
 
 fn print(
     context: capi_core::RuntimeContext,
-    _: &mut capi_core::Context,
+    _: &mut (),
 ) -> Result<(), capi_core::runtime::data_stack::DataStackError> {
     let value = context.data_stack.pop_any()?;
     tracing::info!("{}", value.kind);
