543e960db15bc2f430336543ad31b947b85fcbc8
Hanno Braun <hanno@braun-odw.eu>
2023-09-26 11:56:04 +0200

    Run "Hello, world!"

--- capi-web/src/main.rs
+++ capi-web/src/main.rs
@@ -8,5 +8,23 @@ fn main() {
 }
 
 fn main_inner() -> anyhow::Result<()> {
+    let mut interpreter =
+        capi_core::Interpreter::new(r#""Hello, world!" print"#)?;
+    interpreter.register_native_functions([(
+        "print",
+        print as capi_core::NativeFunction,
+    )]);
+
+    while !interpreter.step()?.finished() {}
+
+    Ok(())
+}
+
+fn print(
+    evaluator: &mut capi_core::Evaluator,
+) -> Result<(), capi_core::runtime::data_stack::DataStackError> {
+    let value = evaluator.data_stack.pop_any()?;
+    tracing::info!("{}", value.kind);
+    evaluator.data_stack.push(value);
     Ok(())
 }
