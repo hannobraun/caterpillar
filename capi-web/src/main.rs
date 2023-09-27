4fd9288c18bcb80493a1fb0ce5b3197411abb918
Hanno Braun <hanno@braun-odw.eu>
2023-09-27 20:34:56 +0200

    Move function

--- capi-web/src/main.rs
+++ capi-web/src/main.rs
@@ -7,40 +7,8 @@ fn main() {
     tracing_wasm::set_as_global_default();
 
     wasm_bindgen_futures::spawn_local(async {
-        if let Err(err) = run(SCRIPT).await {
+        if let Err(err) = platform::run(SCRIPT).await {
             panic!("Error: {err:?}");
         }
     })
-}
-
-async fn run(script: &str) -> anyhow::Result<()> {
-    tracing::debug!("Running script:\n{script}");
-
-    let mut interpreter = capi_core::Interpreter::new(script)?;
-    let mut context = platform::Context::default();
-
-    interpreter.register_platform([
-        (
-            "delay_ms",
-            platform::delay_ms
-                as capi_core::PlatformFunction<platform::Context>,
-        ),
-        ("print", platform::print),
-    ]);
-
-    loop {
-        let sleep_duration = match interpreter.step(&mut context)? {
-            capi_core::RuntimeState::Running => None,
-            capi_core::RuntimeState::Sleeping => context.sleep_duration.take(),
-            capi_core::RuntimeState::Finished => break,
-        };
-
-        // Always sleep, even if it's for zero duration, to give the rest of the
-        // website a chance to do its thing between steps.
-        let sleep_duration =
-            sleep_duration.unwrap_or(std::time::Duration::from_millis(0));
-        gloo_timers::future::sleep(sleep_duration).await
-    }
-
-    Ok(())
 }
