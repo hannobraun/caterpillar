2b2b76c91b79462bd7ecc3366e4561a643b2e86b
Hanno Braun <hanno@braun-odw.eu>
2023-09-27 13:23:21 +0200

    Add `functions::store`

---
+++ capi-core/src/runtime/functions/store.rs
@@ -0,0 +1,137 @@
+use std::collections::BTreeMap;
+
+use crate::{
+    intrinsics,
+    repr::eval::{
+        fragments::{FragmentId, FragmentPayload, Fragments},
+        value::ValueKind,
+    },
+    PlatformFunction,
+};
+
+use super::{
+    IntrinsicFunction, NativeFunction, UserDefinedFunction,
+    UserDefinedFunctions,
+};
+
+#[derive(Debug)]
+pub struct Functions<C> {
+    native: BTreeMap<String, NativeFunction<C>>,
+    user_defined: BTreeMap<String, UserDefinedFunction>,
+}
+
+impl<C> Functions<C> {
+    pub fn new() -> Self {
+        let mut native = BTreeMap::new();
+
+        let intrinsics = [
+            ("+", intrinsics::add as IntrinsicFunction),
+            ("clone", intrinsics::clone),
+            ("eval", intrinsics::eval),
+            ("fn", intrinsics::fn_),
+            ("nop", intrinsics::nop),
+            ("over", intrinsics::over),
+            ("swap", intrinsics::swap),
+        ];
+
+        for (name, intrinsic) in intrinsics {
+            native
+                .insert(name.to_string(), NativeFunction::Intrinsic(intrinsic));
+        }
+
+        Self {
+            native,
+            user_defined: BTreeMap::new(),
+        }
+    }
+
+    pub fn register_platform(
+        &mut self,
+        functions: impl IntoIterator<Item = (&'static str, PlatformFunction<C>)>,
+    ) {
+        for (name, function) in functions {
+            self.native
+                .insert(name.into(), NativeFunction::Platform(function));
+        }
+    }
+
+    pub fn user_defined(&mut self) -> UserDefinedFunctions {
+        UserDefinedFunctions {
+            inner: &mut self.user_defined,
+        }
+    }
+
+    pub fn resolve(&self, name: &str) -> Result<Function<C>, ResolveError> {
+        let native = self.native.get(name).map(|native| match native {
+            NativeFunction::Intrinsic(function) => {
+                Function::Intrinsic(function)
+            }
+            NativeFunction::Platform(function) => Function::Platform(function),
+        });
+        let user_defined = self
+            .user_defined
+            .get(name)
+            .map(|user_defined| Function::UserDefined(user_defined));
+
+        native
+            .or(user_defined)
+            .ok_or(ResolveError { name: name.into() })
+    }
+
+    pub fn replace(
+        &mut self,
+        old: FragmentId,
+        new: FragmentId,
+        fragments: &Fragments,
+    ) {
+        let mut renames = Vec::new();
+
+        for (old_name, UserDefinedFunction { name, body }) in
+            self.user_defined.iter_mut()
+        {
+            if name.fragment == Some(old) {
+                let fragment = fragments.get(new);
+                let FragmentPayload::Value(ValueKind::Symbol(new_name)) =
+                    &fragment.payload
+                else {
+                    // If the new fragment is not a symbol, then it's not
+                    // supposed to be a function name. Not sure if we can
+                    // handle this any smarter.
+                    continue;
+                };
+
+                name.value = new_name.clone();
+                name.fragment = Some(new);
+
+                renames.push((old_name.clone(), new_name.clone()));
+            }
+            if body.start == old {
+                body.start = new;
+            }
+        }
+
+        for (old, new) in renames {
+            let function = self.user_defined.remove(&old).unwrap();
+            self.user_defined.insert(new, function);
+        }
+    }
+}
+
+impl<C> Default for Functions<C> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Function<'r, C> {
+    Intrinsic(&'r IntrinsicFunction),
+    Platform(&'r PlatformFunction<C>),
+    UserDefined(&'r UserDefinedFunction),
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("Error resolving function `{name}`")]
+pub struct ResolveError {
+    pub name: String,
+}
