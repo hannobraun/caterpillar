1c4da141906bd44ac64e36e421ac1c9d46e296c3
Hanno Braun <hanno@braun-odw.eu>
2023-09-28 21:20:49 +0200

    Implement rudimantary pixel drawing

--- capi-desktop/src/display.rs
+++ capi-desktop/src/display.rs
@@ -1,12 +1,58 @@
-pub struct Display {}
+use std::cmp;
+
+use pixels::{Pixels, SurfaceTexture};
+use winit::{event_loop::EventLoop, window::Window};
+
+pub struct Display {
+    _event_loop: EventLoop<()>,
+    pixels: Pixels,
+}
 
 impl Display {
     pub fn new() -> anyhow::Result<Self> {
-        Ok(Self {})
+        let factor = 40;
+
+        let buffer_to_surface = |size| size * factor;
+
+        let event_loop = EventLoop::new();
+        let window = Window::new(&event_loop)?;
+
+        let surface_texture = SurfaceTexture::new(
+            buffer_to_surface(WIDTH),
+            buffer_to_surface(HEIGHT),
+            &window,
+        );
+        let pixels = Pixels::new(WIDTH, HEIGHT, surface_texture)?;
+
+        Ok(Self {
+            _event_loop: event_loop,
+            pixels,
+        })
     }
 
     pub fn set(&mut self, position: [i64; 2]) -> anyhow::Result<()> {
-        tracing::info!("Should set pixel at {position:?}");
+        let [x, y] = position.map(|value| {
+            let min = 0;
+            let max = cmp::max(WIDTH, HEIGHT).into();
+
+            value.max(min).min(max) as usize
+        });
+
+        let r = y * WIDTH as usize + x;
+        let g = r + 1;
+        let b = r + 2;
+        let a = r + 3;
+
+        self.pixels.frame_mut()[r] = 255;
+        self.pixels.frame_mut()[g] = 255;
+        self.pixels.frame_mut()[b] = 255;
+        self.pixels.frame_mut()[a] = 255;
+
+        self.pixels.render()?;
+
         Ok(())
     }
 }
+
+const WIDTH: u32 = 10;
+const HEIGHT: u32 = 18;
