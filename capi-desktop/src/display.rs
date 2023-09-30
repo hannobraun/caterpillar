50f5754ef79db2cd2679a8e2a0d100b0cfc96609
Hanno Braun <hanno@braun-odw.eu>
2023-09-30 10:22:45 +0200

    Inline constructor to prepare for follow-on change

--- capi-desktop/src/display.rs
+++ capi-desktop/src/display.rs
@@ -13,7 +13,26 @@ pub fn start(pixel_ops: Receiver<PixelOp>) -> anyhow::Result<()> {
     // If a pixel op has been sent, initialize the display and start handling
     // pixel ops for real.
 
-    let mut display = Display::new()?;
+    let mut display = {
+        let factor = 40;
+
+        let buffer_to_surface = |size| size * factor;
+
+        let surface_width = buffer_to_surface(WIDTH);
+        let surface_height = buffer_to_surface(HEIGHT);
+
+        let event_loop = EventLoop::new();
+        let window = Window::new(&event_loop)?;
+
+        let surface_texture =
+            SurfaceTexture::new(surface_width, surface_height, &window);
+        let pixels = Pixels::new(WIDTH, HEIGHT, surface_texture)?;
+
+        Display {
+            _event_loop: event_loop,
+            pixels,
+        }
+    };
     let pixel_ops = [first_pixel_op].into_iter().chain(pixel_ops.iter());
 
     for PixelOp::Set(position) in pixel_ops {
@@ -29,27 +48,6 @@ pub struct Display {
 }
 
 impl Display {
-    pub fn new() -> anyhow::Result<Self> {
-        let factor = 40;
-
-        let buffer_to_surface = |size| size * factor;
-
-        let surface_width = buffer_to_surface(WIDTH);
-        let surface_height = buffer_to_surface(HEIGHT);
-
-        let event_loop = EventLoop::new();
-        let window = Window::new(&event_loop)?;
-
-        let surface_texture =
-            SurfaceTexture::new(surface_width, surface_height, &window);
-        let pixels = Pixels::new(WIDTH, HEIGHT, surface_texture)?;
-
-        Ok(Self {
-            _event_loop: event_loop,
-            pixels,
-        })
-    }
-
     pub fn set(&mut self, position: [i64; 2]) -> anyhow::Result<()> {
         let [x, y] = position.map(|value| {
             let min = 0;
