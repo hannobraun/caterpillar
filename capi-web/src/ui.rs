b5134e13eaea8b30de227c84f2c38b6ead21f5c3
Hanno Braun <hanno@braun-odw.eu>
2023-10-05 14:26:18 +0200

    Keep output scrolled down

--- capi-web/src/ui.rs
+++ capi-web/src/ui.rs
@@ -1,5 +1,8 @@
 use async_channel::Receiver;
-use sycamore::{reactive::create_rc_signal, view};
+use sycamore::{
+    reactive::{create_effect, create_rc_signal},
+    view,
+};
 
 pub async fn render(output_channel: Receiver<String>) -> anyhow::Result<()> {
     let output_signal = create_rc_signal(String::new());
@@ -7,8 +10,22 @@ pub async fn render(output_channel: Receiver<String>) -> anyhow::Result<()> {
     sycamore::render(|cx| {
         let output = output_signal.clone();
 
+        let output2 = output.clone();
+        create_effect(cx, move || {
+            output2.track();
+
+            let document = web_sys::window().unwrap().document().unwrap();
+            let outputs = document.get_elements_by_class_name("output");
+
+            let mut i = 0;
+            while let Some(output) = outputs.item(i) {
+                output.set_scroll_top(output.scroll_height());
+                i += 1;
+            }
+        });
+
         view! { cx,
-            textarea(readonly=true) {
+            textarea(class="output", readonly=true) {
                 (output.get())
             }
         }
