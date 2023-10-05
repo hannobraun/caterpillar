d03741dfc5fecd648aae2c0d2c6fb1d1fb1e3b28
Hanno Braun <hanno@braun-odw.eu>
2023-10-05 21:57:36 +0200

    Add support for array values

--- capi-core/src/repr/eval/value.rs
+++ capi-core/src/repr/eval/value.rs
@@ -13,6 +13,7 @@ pub struct Value {
 #[derive(Clone, Debug, Eq, PartialEq, Hash, EnumVariantType)]
 #[evt(derive(Clone, Debug, Eq, PartialEq))]
 pub enum ValuePayload {
+    Array(Vec<ValuePayload>),
     Block { start: FragmentId },
     Bool(bool),
     Number(i64),
@@ -41,6 +42,12 @@ impl ValuePayload {
 
     pub(crate) fn hash(&self, hasher: &mut blake3::Hasher) {
         match self {
+            Self::Array(values) => {
+                hasher.update(b"array");
+                for value in values {
+                    value.hash(hasher);
+                }
+            }
             Self::Block { start } => {
                 hasher.update(b"block");
                 hasher.update(start.hash.as_bytes());
@@ -68,6 +75,17 @@ impl ValuePayload {
 impl fmt::Display for ValuePayload {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Self::Array(values) => {
+                write!(f, "[")?;
+
+                for value in values {
+                    write!(f, " {value}")?;
+                }
+
+                write!(f, " ]")?;
+
+                Ok(())
+            }
             Self::Block { start, .. } => write!(f, "{{ {start} }}"),
             Self::Bool(value) => write!(f, "{value}"),
             Self::Number(number) => write!(f, "{number}"),
