b55e5d38348c14d71b7c7149190d759dd3605231
Hanno Braun <hanno@braun-odw.eu>
2023-11-30 19:53:11 +0100

    Clean up imports

--- capi-desktop/src/bin/capi.rs
+++ capi-desktop/src/bin/capi.rs
@@ -1,14 +1,16 @@
+use capi_desktop::{args::Args, display, loader, DesktopThread};
+
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
-    let args = capi_desktop::args::Args::parse();
-    let code = capi_desktop::loader::load(&args.script)?;
-    let (updates, _watcher) = capi_desktop::loader::watch(&args.script)?;
+    let args = Args::parse();
+    let code = loader::load(&args.script)?;
+    let (updates, _watcher) = loader::watch(&args.script)?;
 
-    let desktop_thread = capi_desktop::DesktopThread::run(code, updates)?;
-    capi_desktop::display::start(desktop_thread)?;
+    let desktop_thread = DesktopThread::run(code, updates)?;
+    display::start(desktop_thread)?;
 
     Ok(())
 }
