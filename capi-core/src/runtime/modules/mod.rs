21f4b02ad68179b12f5de6979738f1dc47296680
Hanno Braun <hanno@braun-odw.eu>
2023-12-13 20:45:39 +0100

    Update module name

--- capi-core/src/runtime/modules/mod.rs
+++ capi-core/src/runtime/modules/mod.rs
@@ -1,9 +1,9 @@
-mod module;
+mod namespace;
 mod native_items;
 mod user_defined_items;
 
 pub use self::{
-    module::{ItemInModule, Namespace, ResolveError},
+    namespace::{ItemInModule, Namespace, ResolveError},
     native_items::{
         FunctionState, IntrinsicFunction, NativeFunction, PlatformFunction,
         RuntimeContext,
