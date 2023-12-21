16c4d0f4e6ad94c5df5d3434e4a76e2b82aef1ea
Hanno Braun <hanno@braun-odw.eu>
2023-12-21 10:46:07 +0100

    Update documentation of `Terminator`

--- capi-core/src/repr/eval/fragments/payload.rs
+++ capi-core/src/repr/eval/fragments/payload.rs
@@ -14,17 +14,18 @@ pub enum FragmentPayload {
     ///
     /// By convention, fragments within a block use the fragment *after* the
     /// block as their parents. This is done for practical reasons, as the ID
-    /// of the next fragment is available when a parent ID is needed, as opposed
-    /// to the ID of the block itself or the fragment before it. And they can't
-    /// be made available either, as they depend on the block contents and that
-    /// would be a circular dependency.
+    /// of the next fragment is available when a parent ID is needed, while the
+    /// ID of the block itself or the fragment before it isn't. And they can't
+    /// be made available either, as they depend on the block contents, which
+    /// would result in a circular dependency.
     ///
-    /// However, that means that blocks *must not* be the last fragment in a
-    /// context, or the parent will be `None`, and the items within such blocks
-    /// are no longer uniquely addressable.
+    /// However, this means that blocks *must not* be the last fragment in a
+    /// context, or the items within such blocks are no longer uniquely
+    /// addressable.
     ///
     /// This is why terminators exist. They terminate every context, and thus
-    /// provide a unique parent for the fragments in any block.
+    /// make sure that a unique parent is provided for the fragments in any
+    /// block.
     Terminator,
 }
 
