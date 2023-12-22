8a16aa6408051cb9748d94c6fce18e1da6018d2d
Hanno Braun <hanno@braun-odw.eu>
2023-12-22 10:59:57 +0100

    Inline module

--- capi-desktop/src/loader/mod.rs
+++ capi-desktop/src/loader/mod.rs
@@ -1,14 +1,17 @@
-mod watch;
-
-pub use self::watch::watch;
-
 use std::{
     fs::File,
     io::{self, Read},
-    path::Path,
+    path::{Path, PathBuf},
+    time::Duration,
 };
 
 use anyhow::Context;
+use crossbeam_channel::Receiver;
+use notify::{RecommendedWatcher, RecursiveMode};
+use notify_debouncer_mini::{
+    DebounceEventResult, DebouncedEventKind, Debouncer,
+};
+use tracing::error;
 
 pub struct Loader;
 
@@ -29,4 +32,39 @@ fn load_inner(path: &Path) -> io::Result<String> {
     let mut code = String::new();
     File::open(path)?.read_to_string(&mut code)?;
     Ok(code)
+}
+
+pub fn watch(
+    path: impl AsRef<Path>,
+) -> anyhow::Result<(Receiver<String>, Debouncer<RecommendedWatcher>)> {
+    let path_for_watcher = PathBuf::from(path.as_ref());
+
+    let (sender, receiver) = crossbeam_channel::bounded(0);
+
+    let mut debouncer = notify_debouncer_mini::new_debouncer(
+        Duration::from_millis(50),
+        move |result: DebounceEventResult| {
+            let path = &path_for_watcher;
+
+            if let Ok(events) = result {
+                for event in events {
+                    if let DebouncedEventKind::Any = event.kind {
+                        let code = load(path).unwrap();
+                        sender.send(code).unwrap();
+                    }
+                }
+
+                return;
+            }
+
+            // Not sure what else we can do about it here.
+            error!("Error watching code: {result:?}");
+        },
+    )?;
+
+    debouncer
+        .watcher()
+        .watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    Ok((receiver, debouncer))
 }
