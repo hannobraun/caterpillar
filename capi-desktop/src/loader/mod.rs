d9d2332381dae65241eceff17de63794465854a9
Hanno Braun <hanno@braun-odw.eu>
2024-01-02 20:01:42 +0100

    Increase buffer to prepare for follow-on change

--- capi-desktop/src/loader/mod.rs
+++ capi-desktop/src/loader/mod.rs
@@ -68,7 +68,7 @@ fn load_inner(path: &Path) -> io::Result<String> {
 }
 
 fn watch(path: PathBuf) -> anyhow::Result<ScriptWatcher> {
-    let (sender, receiver) = crossbeam_channel::bounded(0);
+    let (sender, receiver) = crossbeam_channel::bounded(1);
     let script_loader = ScriptLoader::new(path.clone(), sender);
 
     let mut debouncer = notify_debouncer_mini::new_debouncer(
