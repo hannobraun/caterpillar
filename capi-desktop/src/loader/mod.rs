026c91aa01e59731f95ea7329d924eebdd3143dc
Hanno Braun <hanno@braun-odw.eu>
2024-01-02 21:15:15 +0100

    Refactor

--- capi-desktop/src/loader/mod.rs
+++ capi-desktop/src/loader/mod.rs
@@ -1,15 +1,13 @@
 mod script_loader;
+mod watch;
 
-use std::{path::PathBuf, time::Duration};
+use std::path::PathBuf;
 
-use crossbeam_channel::{Receiver, SendError};
-use notify::{RecommendedWatcher, RecursiveMode};
-use notify_debouncer_mini::{
-    DebounceEventResult, DebouncedEventKind, Debouncer,
-};
-use tracing::error;
+use crossbeam_channel::Receiver;
+use notify::RecommendedWatcher;
+use notify_debouncer_mini::Debouncer;
 
-use self::script_loader::ScriptLoader;
+use self::watch::watch;
 
 #[derive(Default)]
 pub struct Loader {
@@ -47,50 +45,4 @@ impl Loader {
 struct ScriptUpdates {
     pub receiver: Receiver<anyhow::Result<String>>,
     pub watcher: Debouncer<RecommendedWatcher>,
-}
-
-fn watch(path: PathBuf) -> anyhow::Result<ScriptUpdates> {
-    let (script_loader, receiver) = ScriptLoader::new(path.clone())?;
-
-    let mut debouncer = notify_debouncer_mini::new_debouncer(
-        Duration::from_millis(50),
-        move |result: DebounceEventResult| {
-            let events = match result {
-                Ok(events) => events,
-                Err(err) => {
-                    if let Err(SendError(err)) = script_loader.on_error(err) {
-                        // If we end up here, the channel has been disconnected.
-                        // Nothing we can do about it here, but log the error.
-                        //
-                        // If the channel is disconnected, the watcher should
-                        // have been dropped too, which would mean this whole
-                        // thread is about to die anyway.
-                        error!("Failed to send code watching error: {err:?}");
-                    }
-                    return;
-                }
-            };
-
-            for event in events {
-                if let DebouncedEventKind::Any = event.kind {
-                    if let Err(SendError(result)) = script_loader.trigger() {
-                        // See comment above on why this is the appropriate way
-                        // to handle this.
-                        error!(
-                            "Failed to send code loading result: {result:?}"
-                        );
-                    }
-                }
-            }
-        },
-    )?;
-
-    debouncer
-        .watcher()
-        .watch(&path, RecursiveMode::NonRecursive)?;
-
-    Ok(ScriptUpdates {
-        receiver,
-        watcher: debouncer,
-    })
 }
---
+++ capi-desktop/src/loader/watch.rs
@@ -0,0 +1,54 @@
+use std::{path::PathBuf, time::Duration};
+
+use crossbeam_channel::SendError;
+use notify::RecursiveMode;
+use notify_debouncer_mini::{DebounceEventResult, DebouncedEventKind};
+use tracing::error;
+
+use super::{script_loader::ScriptLoader, ScriptUpdates};
+
+pub fn watch(path: PathBuf) -> anyhow::Result<ScriptUpdates> {
+    let (script_loader, receiver) = ScriptLoader::new(path.clone())?;
+
+    let mut debouncer = notify_debouncer_mini::new_debouncer(
+        Duration::from_millis(50),
+        move |result: DebounceEventResult| {
+            let events = match result {
+                Ok(events) => events,
+                Err(err) => {
+                    if let Err(SendError(err)) = script_loader.on_error(err) {
+                        // If we end up here, the channel has been disconnected.
+                        // Nothing we can do about it here, but log the error.
+                        //
+                        // If the channel is disconnected, the watcher should
+                        // have been dropped too, which would mean this whole
+                        // thread is about to die anyway.
+                        error!("Failed to send code watching error: {err:?}");
+                    }
+                    return;
+                }
+            };
+
+            for event in events {
+                if let DebouncedEventKind::Any = event.kind {
+                    if let Err(SendError(result)) = script_loader.trigger() {
+                        // See comment above on why this is the appropriate way
+                        // to handle this.
+                        error!(
+                            "Failed to send code loading result: {result:?}"
+                        );
+                    }
+                }
+            }
+        },
+    )?;
+
+    debouncer
+        .watcher()
+        .watch(&path, RecursiveMode::NonRecursive)?;
+
+    Ok(ScriptUpdates {
+        receiver,
+        watcher: debouncer,
+    })
+}
