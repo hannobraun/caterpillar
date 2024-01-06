73a82d19b4f13cdd485451bdb0a2cc1c65d5d371
Hanno Braun <hanno@braun-odw.eu>
2024-01-06 10:23:39 +0100

    Update variable name

--- capi-desktop/src/thread.rs
+++ capi-desktop/src/thread.rs
@@ -138,8 +138,8 @@ impl DesktopThread {
                     run_target.finish();
 
                     match platform_context.loader.updates().recv() {
-                        Ok(new_code) => {
-                            let (parent, new_code) = new_code?;
+                        Ok(update) => {
+                            let (parent, new_code) = update?;
                             interpreter.update(&new_code, parent)?;
                         }
                         Err(RecvError) => break,
