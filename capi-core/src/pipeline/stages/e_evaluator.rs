e437a3bcfbe48740fab80da644c95342ffb42921
Hanno Braun <hanno@braun-odw.eu>
2024-01-14 11:39:59 +0100

    Add placeholder for evaluation stage

---
+++ capi-core/src/pipeline/stages/e_evaluator.rs
@@ -0,0 +1,29 @@
+pub fn evaluate() {
+    // This is only a placeholder, and the pipeline essentially stops after the
+    // `analyze` step. What should happen here, is the evaluation of the
+    // top-level context, to determine which functions and modules were defined.
+    //
+    // Here's what we could do in that case:
+    //
+    // - Functions are defined during evaluation. If we were to always evaluate
+    //   after analyzing, we would have a full picture of which functions are
+    //   defined in the current code.
+    //   This is relevant because there's at least one bugs in the current
+    //   implementation, and by always having a full picture after each update,
+    //   we could simplify that implementation and fix the bug.
+    //   The known bug, for references:
+    //   https://github.com/hannobraun/caterpillar/issues/15
+    // - Same goes for modules. There are probably more holes in the current
+    //   concept, that will be exposed as Caterpillar's functionality gets
+    //   closer to that of a useful language.
+    // - Longer-term, I'd like to have powerful compile-time metaprogramming,
+    //   and I'd like to use that to implement a static type system in the
+    //   language itself. Who knows how realistic that is, but if something like
+    //   that were to happen, it sounds reasonable to define that everything in
+    //   the top-level context happens at compile-time.
+    //
+    // But what we don't want to do, is run the complete program at
+    // compile-time. What we need to make this work is a `main` or `start`
+    // function (or whatever is appropriate for the platform) for doing the
+    // run-time stuff.
+}
--- capi-core/src/pipeline/stages/mod.rs
+++ capi-core/src/pipeline/stages/mod.rs
@@ -2,3 +2,4 @@ pub mod a_tokenizer;
 pub mod b_parser;
 pub mod c_simplifier;
 pub mod d_analyzer;
+pub mod e_evaluator;
