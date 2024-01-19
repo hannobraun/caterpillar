4a261a9d26d144c6d1e534a46df78c0e689ce645
Hanno Braun <hanno@braun-odw.eu>
2024-01-19 19:33:21 +0100

    Document `ScriptPath`

--- capi-core/src/pipeline/scripts.rs
+++ capi-core/src/pipeline/scripts.rs
@@ -12,4 +12,10 @@ pub struct Scripts {
     pub inner: BTreeMap<ScriptPath, String>,
 }
 
+/// The path of a script, represented as a series of symbols
+///
+/// This is a platform-independent way to represent the script path. We can't
+/// use an actual `Path` (or `PathBuf`, rather), because there might not be a
+/// file system available where we're running this (in the browser, for
+/// example).
 pub type ScriptPath = Vec<value::Symbol>;
