a63f8d6ffabedcc31e5e9ad7e7c630ec229e1fff
Hanno Braun <hanno@braun-odw.eu>
2024-01-19 19:04:40 +0100

    Ignore Clippy lint

--- capi-desktop/src/lib.rs
+++ capi-desktop/src/lib.rs
@@ -1,3 +1,11 @@
+// I don't see the point of this lint. That weird case they present in its
+// documentation certainly doesn't apply to me, and I've also never seen it
+// anywhere else.
+//
+// We have a `clippy.toml` that is supposed to allow this for private modules,
+// but it doesn't seem to work. Or I'm holding it wrong. I don't know.
+#![allow(clippy::module_inception)]
+
 pub mod args;
 pub mod display;
 pub mod loader;
