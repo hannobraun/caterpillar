cbd339c3cd582934e599f6a8377c3c7595e4f200
Hanno Braun <hanno@braun-odw.eu>
2024-01-19 19:23:20 +0100

    Add comment

--- capi-core/src/pipeline/scripts.rs
+++ capi-core/src/pipeline/scripts.rs
@@ -4,5 +4,10 @@ use crate::value;
 
 #[derive(Default)]
 pub struct Scripts {
+    // Eventually, I'm going to need to store the path of the entry script here,
+    // so the `Interpreter` always knows where to start the pipeline.
+    //
+    // (Actually, once we store the entry script path here, we don't need to
+    // pass its code in a separate variable at all.)
     pub inner: BTreeMap<Vec<value::Symbol>, String>,
 }
