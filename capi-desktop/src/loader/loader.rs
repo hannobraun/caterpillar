29d2bab62b57dfe4bbabd6c699bafa53c1d47e36
Hanno Braun <hanno@braun-odw.eu>
2024-01-20 09:52:04 +0100

    Add comment

--- capi-desktop/src/loader/loader.rs
+++ capi-desktop/src/loader/loader.rs
@@ -12,6 +12,27 @@ pub struct Loader {
     watchers: Vec<Debouncer<RecommendedWatcher>>,
 }
 
+// To adapt the `Loader` API for the ongoing changes to pipeline, we'll probably
+// need something like the following changes:
+//
+// - Accept the path of the entry script in `new`, search for all scripts that
+//   are reachable from that, and set up the watching/loading infrastructure for
+//   all of them.
+//   It will also initialize a `Scripts` instance and set everything up to keep
+//   that updated.
+// - No more `load`. It will no longer be necessary to load single scripts.
+// - Instead of `updates`, have a method that provides access to the latest
+//   version of `Scripts`.
+//
+// As a first iteration, I can probably reuse the watch/load infrastructure
+// as-is. But if we're going to watch a whole directory tree (at least the
+// `.capi` scripts in it), it'll probably end up simpler, if I adapt the
+// infrastructure to that specific task.
+//
+// Maybe this change can be done in parallel to the existing API? As in, keep
+// `load` and `updates` around while I build up the rest? Then I can use debug
+// output to verify the new functionality works, while building it up step by
+// step, never requiring a single huge change.
 impl Loader {
     pub fn new() -> Self {
         let (sender, receiver) = crossbeam_channel::unbounded();
