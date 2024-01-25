b534167f76ce2d4241c617ef675a903c365172d8
Hanno Braun <hanno@braun-odw.eu>
2024-01-25 19:01:56 +0100

    Remove obsolete comment

--- capi-desktop/src/loader/loader.rs
+++ capi-desktop/src/loader/loader.rs
@@ -19,28 +19,6 @@ pub struct Loader {
     update_available: bool,
 }
 
-// To adapt the `Loader` API for the ongoing changes to pipeline, we'll probably
-// need something like the following changes:
-//
-// - Accept the path of the entry script in `new`, search for all scripts that
-//   are reachable from that, and set up the watching/loading infrastructure for
-//   all of them.
-//   It will also initialize a `Scripts` instance and set everything up to keep
-//   that updated.
-// - No more `load`. It will no longer be necessary to load single scripts.
-// - Instead of `updates`, have a method that provides access to the latest
-//   version of `Scripts`.
-//
-// It would probably be best to build up the new API in parallel to the existing
-// one. Then I can build the new API incrementally, test my work with debug
-// output, and there won't have to be a huge change to switch everything over at
-// once.
-//
-// If I'm going to reuse the existing watch/load infrastructure for that (which
-// would probably be good in the short term, but longer-term, watching a whole
-// directory tree would work better with a different architecture), I have to be
-// careful not to interfere with the current workings. I can run the same code,
-// but I can't use the same channel.
 impl Loader {
     pub fn new(entry_script_path: impl Into<PathBuf>) -> anyhow::Result<Self> {
         let entry_script_path = entry_script_path.into();
