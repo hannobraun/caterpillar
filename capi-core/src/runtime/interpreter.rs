997821a2a513f2ff37677ff437367057f1371c8d
Hanno Braun <hanno@braun-odw.eu>
2024-01-31 20:00:51 +0100

    Remove outdated comment

--- capi-core/src/runtime/interpreter.rs
+++ capi-core/src/runtime/interpreter.rs
@@ -18,17 +18,6 @@ pub struct Interpreter<P: Platform> {
     state: RuntimeState,
 }
 
-// I think we need to end up with the following API here, roughly, to support
-// the ongoing pipeline changes:
-//
-// - The constructor takes the pase (`pipeline::ScriptPath`) of the entry
-//   script, and nothing else. It stores the path for later reference.
-// - The `update` method takes `&Scripts` (and nothing else). It then runs the
-//   pipeline for the entry script (and thus the whole program).
-//
-// Then `Loader` can load all reachable scripts (instead of needing to be told
-// every single script it's supposed to load) and provide a whole `Scripts` on
-// every update.
 impl<P: Platform> Interpreter<P> {
     pub fn new() -> Result<Self, PipelineError> {
         Ok(Interpreter {
