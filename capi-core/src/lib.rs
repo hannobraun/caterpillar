58aa7918aa2732e73b498a5b68e256fe10988370
Hanno Braun <hanno@braun-odw.eu>
2024-02-03 10:06:13 +0100

    Move test to dedicated crate

--- capi-core/src/lib.rs
+++ capi-core/src/lib.rs
@@ -11,28 +11,3 @@ pub mod platform;
 pub mod repr;
 pub mod runtime;
 
-#[cfg(test)]
-mod tests {
-    use std::path::PathBuf;
-
-    use capi_desktop::{
-        core::runtime::interpreter::Interpreter,
-        loader::Loader,
-        platform::{DesktopPlatform, PlatformContext},
-    };
-
-    #[test]
-    fn native_capi_test_suite() -> anyhow::Result<()> {
-        let mut interpreter = Interpreter::<DesktopPlatform>::new()?;
-
-        let script_path = PathBuf::from("../tests.capi");
-        let mut loader = Loader::new(script_path)?;
-
-        let scripts = loader.wait_for_updated_scripts()?;
-
-        interpreter.update(scripts)?;
-        interpreter.run_tests(&mut PlatformContext::new())?;
-
-        Ok(())
-    }
-}
---
+++ capi-tests/src/lib.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use capi_desktop::{
+        core::runtime::interpreter::Interpreter,
+        loader::Loader,
+        platform::{DesktopPlatform, PlatformContext},
+    };
+
+    #[test]
+    fn native_capi_test_suite() -> anyhow::Result<()> {
+        let mut interpreter = Interpreter::<DesktopPlatform>::new()?;
+
+        let script_path = PathBuf::from("../tests.capi");
+        let mut loader = Loader::new(script_path)?;
+
+        let scripts = loader.wait_for_updated_scripts()?;
+
+        interpreter.update(scripts)?;
+        interpreter.run_tests(&mut PlatformContext::new())?;
+
+        Ok(())
+    }
+}
