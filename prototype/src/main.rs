8df74908111a5016a7c5d02b31c611b5997cfe4e
Hanno Braun <hanno@braun-odw.eu>
2024-03-07 00:58:34 +0100

    Convert app into web server

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[rocket::get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", rocket::routes![index])
 }
