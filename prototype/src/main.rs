b31b887274988cf07c2223e57f11dd3bfdc8c3bc
Hanno Braun <hanno@braun-odw.eu>
2024-03-07 01:01:28 +0100

    Serve static files from current directory

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,9 +1,4 @@
-#[rocket::get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", rocket::routes![index])
+    rocket::build().mount("/", rocket::fs::FileServer::from("."))
 }
