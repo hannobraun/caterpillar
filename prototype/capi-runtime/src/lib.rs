b8c3a13146033522a0b0630d9b9928f114d32159
Hanno Braun <hanno@braun-odw.eu>
2024-03-07 03:43:56 +0100

    Refactor

--- prototype/capi-runtime/src/lib.rs
+++ prototype/capi-runtime/src/lib.rs
@@ -23,17 +23,14 @@ pub struct DrawTarget {
 }
 
 #[no_mangle]
-pub extern "C" fn init_draw_target(
-    width: usize,
-    canvas_height: usize,
-) -> *mut u8 {
+pub extern "C" fn init_draw_target(width: usize, height: usize) -> *mut u8 {
     const NUM_COLOR_CHANNELS: usize = 4;
-    let len = width * canvas_height * NUM_COLOR_CHANNELS;
+    let len = width * height * NUM_COLOR_CHANNELS;
 
     let buffer = DrawTarget {
         buffer: iter::repeat(0).take(len).collect(),
         width,
-        height: canvas_height,
+        height,
     };
     DRAW_BUFFER
         .lock()
