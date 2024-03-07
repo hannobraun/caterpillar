0bb458ad60f6ed8308db1110c9ecdf2ed2b221be
Hanno Braun <hanno@braun-odw.eu>
2024-03-07 15:54:48 +0100

    Refactor

--- prototype/capi-runtime/src/lib.rs
+++ prototype/capi-runtime/src/lib.rs
@@ -47,6 +47,14 @@ pub extern "C" fn get_render_target_buffer() -> *mut u8 {
 }
 
 #[no_mangle]
+pub extern "C" fn get_render_target_buffer_len() -> usize {
+    let mut state = STATE.lock().expect("Expected exclusive access");
+    let state = state.as_mut().expect("Expected state to be initialized");
+
+    state.render_target.buffer.len()
+}
+
+#[no_mangle]
 pub extern "C" fn on_input(key: i32) {
     const UP: i32 = 0;
     const LEFT: i32 = 1;
