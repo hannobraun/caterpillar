7ea1ac09f3b564a9fb1e503f54ca95388e0ece67
Hanno Braun <hanno@braun-odw.eu>
2024-03-07 17:58:39 +0100

    Refactor

--- prototype/capi-runtime/src/lib.rs
+++ prototype/capi-runtime/src/lib.rs
@@ -6,6 +6,7 @@ mod world;
 
 use std::{panic, sync::Mutex};
 
+use render_target::draw;
 use state::State;
 
 use self::{
@@ -85,46 +86,4 @@ pub extern "C" fn on_frame(delta_time_ms: f64) {
 
     state.world.update(delta_time_ms);
     draw(&state.world, &mut state.render_target);
-}
-
-fn draw(world: &World, render_target: &mut RenderTarget) {
-    for x in 0..world.cells.size[0] {
-        for y in 0..world.cells.size[1] {
-            let cell_x = x * world.cells.cell_size;
-            let cell_y = y * world.cells.cell_size;
-
-            let color = world.cells.buffer[x + y * world.cells.size[0]];
-
-            draw_cell(
-                world.cells.cell_size,
-                cell_x,
-                cell_y,
-                color,
-                render_target,
-            );
-        }
-    }
-}
-
-fn draw_cell(
-    cell_size: usize,
-    cell_x: usize,
-    cell_y: usize,
-    color: u8,
-    target: &mut RenderTarget,
-) {
-    for x in 0..cell_size {
-        for y in 0..cell_size {
-            let pixel_x = cell_x + x;
-            let pixel_y = cell_y + y;
-
-            let index = (pixel_x + pixel_y * target.width)
-                * RenderTarget::NUM_COLOR_CHANNELS;
-
-            target.buffer[index + 0] = color;
-            target.buffer[index + 1] = color;
-            target.buffer[index + 2] = color;
-            target.buffer[index + 3] = 255;
-        }
-    }
 }
--- prototype/capi-runtime/src/render_target.rs
+++ prototype/capi-runtime/src/render_target.rs
@@ -1,5 +1,7 @@
 use std::iter;
 
+use crate::world::World;
+
 pub struct RenderTarget {
     pub buffer: Vec<u8>,
     pub width: usize,
@@ -17,6 +19,48 @@ impl RenderTarget {
             buffer,
             width,
             height,
+        }
+    }
+}
+
+pub fn draw(world: &World, render_target: &mut RenderTarget) {
+    for x in 0..world.cells.size[0] {
+        for y in 0..world.cells.size[1] {
+            let cell_x = x * world.cells.cell_size;
+            let cell_y = y * world.cells.cell_size;
+
+            let color = world.cells.buffer[x + y * world.cells.size[0]];
+
+            draw_cell(
+                world.cells.cell_size,
+                cell_x,
+                cell_y,
+                color,
+                render_target,
+            );
+        }
+    }
+}
+
+fn draw_cell(
+    cell_size: usize,
+    cell_x: usize,
+    cell_y: usize,
+    color: u8,
+    target: &mut RenderTarget,
+) {
+    for x in 0..cell_size {
+        for y in 0..cell_size {
+            let pixel_x = cell_x + x;
+            let pixel_y = cell_y + y;
+
+            let index = (pixel_x + pixel_y * target.width)
+                * RenderTarget::NUM_COLOR_CHANNELS;
+
+            target.buffer[index + 0] = color;
+            target.buffer[index + 1] = color;
+            target.buffer[index + 2] = color;
+            target.buffer[index + 3] = 255;
         }
     }
 }
