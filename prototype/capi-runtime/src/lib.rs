ce6e79e23375d43ba872738abf0378c5c5bf6e2e
Hanno Braun <hanno@braun-odw.eu>
2024-03-07 03:01:59 +0100

    Refactor

--- prototype/capi-runtime/src/lib.rs
+++ prototype/capi-runtime/src/lib.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::{mem, slice};
 
 #[no_mangle]
 pub fn add(a: u32, b: u32) -> u32 {
@@ -18,4 +18,29 @@ pub extern "C" fn allocate_draw_buffer(
     mem::forget(buffer);
 
     ptr
+}
+
+#[no_mangle]
+pub extern "C" fn draw_cell(
+    cell_size: usize,
+    base_i: usize,
+    base_j: usize,
+    color: u8,
+    buffer: *mut u8,
+    buffer_length: usize,
+    width: usize,
+) {
+    assert!(!buffer.is_null());
+
+    let array = unsafe { slice::from_raw_parts_mut(buffer, buffer_length) };
+
+    for i in 0..cell_size {
+        for j in 0..cell_size {
+            let abs_i = base_i + i;
+            let abs_j = base_j + j;
+
+            let index = abs_i + abs_j * width;
+            array[index] = color;
+        }
+    }
 }
