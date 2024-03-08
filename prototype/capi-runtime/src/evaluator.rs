acdb2dc1a470cccfa7cee19db0d44b10e44a8c84
Hanno Braun <hanno@braun-odw.eu>
2024-03-08 13:27:11 +0100

    Add placeholder for program

--- prototype/capi-runtime/src/evaluator.rs
+++ prototype/capi-runtime/src/evaluator.rs
@@ -1,9 +1,38 @@
-pub struct Evaluator {}
+use std::iter;
+
+pub struct Evaluator {
+    code: Vec<u8>,
+}
 
 impl Evaluator {
     pub fn new() -> Self {
-        Self {}
+        // I want to know when I go beyond certain thresholds, just out of
+        // interest. Keeping the limits as low as possible here, to make sure I
+        // notice.
+        const CODE_SIZE: usize = 1;
+
+        let mut code: Vec<_> = iter::repeat(0).take(CODE_SIZE).collect();
+
+        let program = [b't'];
+        code.copy_from_slice(&program);
+
+        Self { code }
     }
 
-    pub fn evaluate(&mut self) {}
+    pub fn evaluate(&mut self) {
+        let code_ptr = 0;
+
+        loop {
+            let instruction = self.code[code_ptr];
+
+            match instruction {
+                // Terminate the program
+                b't' => {
+                    break;
+                }
+
+                opcode => panic!("Unknown opcode: `{opcode}`"),
+            }
+        }
+    }
 }
