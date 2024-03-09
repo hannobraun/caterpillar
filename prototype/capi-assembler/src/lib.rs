eb1b56b45555195a81ff752d515f2f8baa8ef3a9
Hanno Braun <hanno@braun-odw.eu>
2024-03-09 21:03:27 +0100

    Only return bytecode, if assembling completes

--- prototype/capi-assembler/src/lib.rs
+++ prototype/capi-assembler/src/lib.rs
@@ -1,18 +1,16 @@
 use capi_vm::opcode;
 
-pub fn assemble(assembly: &str) -> Vec<u8> {
+pub fn assemble(assembly: &str) -> Option<Vec<u8>> {
     let mut bytecode = Vec::new();
 
     for instruction in assembly.split_whitespace() {
         match instruction {
             "terminate" => bytecode.push(opcode::TERMINATE),
-            instruction => {
-                panic!("Unknown instruction: `{instruction}`")
-            }
+            _ => return None,
         }
     }
 
-    bytecode
+    Some(bytecode)
 }
 
 #[cfg(test)]
@@ -30,7 +28,7 @@ mod tests {
     fn assemble(assembly: &str) -> [u8; DATA_SIZE] {
         let mut data = [0; DATA_SIZE];
 
-        let bytecode = super::assemble(assembly);
+        let bytecode = super::assemble(assembly).unwrap();
 
         let mut evaluator = Evaluator::new(&data);
         evaluator.evaluate(&bytecode, &mut data);
