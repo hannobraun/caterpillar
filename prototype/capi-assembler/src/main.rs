fb4e8c9cda54343d167f6443a670d595a3ad4931
Hanno Braun <hanno@braun-odw.eu>
2024-03-09 19:25:31 +0100

    Write bytecode into file

--- prototype/capi-assembler/src/main.rs
+++ prototype/capi-assembler/src/main.rs
@@ -1,5 +1,27 @@
+use std::{fs::File, io::Write};
+
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let program = [
+        0x04, // clone
+        0x01, // push
+        0,    // address
+        0x03, // store
+        0x04, // clone
+        0x01, // push
+        1,    // address
+        0x03, // store
+        0x01, // push
+        2,    // address
+        0x03, // store
+        0x01, // push
+        255,  // alpha channel
+        0x01, // push
+        3,    // address
+        0x03, // store
+        0x00, // terminate
+    ];
+
+    File::create("program.bc.capi")?.write_all(&program)?;
 
     Ok(())
 }
