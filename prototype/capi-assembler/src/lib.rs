b71ef6b67081091a1b19a0c10b58b35e8a86c713
Hanno Braun <hanno@braun-odw.eu>
2024-03-09 21:08:19 +0100

    Improve error handling

--- prototype/capi-assembler/src/lib.rs
+++ prototype/capi-assembler/src/lib.rs
@@ -1,16 +1,26 @@
 use capi_vm::opcode;
 
-pub fn assemble(assembly: &str) -> Option<Vec<u8>> {
+pub fn assemble(assembly: &str) -> Result<Vec<u8>, UnknownInstruction> {
     let mut bytecode = Vec::new();
 
     for instruction in assembly.split_whitespace() {
         match instruction {
             "terminate" => bytecode.push(opcode::TERMINATE),
-            _ => return None,
+            instruction => {
+                return Err(UnknownInstruction {
+                    name: instruction.into(),
+                })
+            }
         }
     }
 
-    Some(bytecode)
+    Ok(bytecode)
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("Unknown instruction: `{name}`")]
+pub struct UnknownInstruction {
+    pub name: String,
 }
 
 #[cfg(test)]
