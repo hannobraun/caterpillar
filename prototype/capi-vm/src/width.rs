714cd66aa43795dd3760bcadb8580f70c7b3a9cc
Hanno Braun <hanno@braun-odw.eu>
2024-03-10 20:55:39 +0100

    Derive `Eq` for `Width`

--- prototype/capi-vm/src/width.rs
+++ prototype/capi-vm/src/width.rs
@@ -3,6 +3,7 @@ pub const W16: Width = Width { flag: 0b01 << 6 };
 pub const W32: Width = Width { flag: 0b10 << 6 };
 pub const W64: Width = Width { flag: 0b11 << 6 };
 
+#[derive(Eq, PartialEq)]
 pub struct Width {
     pub flag: u8,
 }
