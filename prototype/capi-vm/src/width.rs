2db83898419b861b9a752bacc3732a7a8450f3e1
Hanno Braun <hanno@braun-odw.eu>
2024-03-10 20:46:19 +0100

    Refactor

--- prototype/capi-vm/src/width.rs
+++ prototype/capi-vm/src/width.rs
@@ -1,4 +1,4 @@
-pub const W8: u8 = 0x00;
-pub const W16: u8 = 0x40;
-pub const W32: u8 = 0x80;
-pub const W64: u8 = 0xc0;
+pub const W8: u8 = 0b00 << 6;
+pub const W16: u8 = 0b01 << 6;
+pub const W32: u8 = 0b10 << 6;
+pub const W64: u8 = 0b11 << 6;
