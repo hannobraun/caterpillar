7732bb6b90bac24df0055b6176475405a1ea41fc
Hanno Braun <hanno@braun-odw.eu>
2024-03-10 21:47:36 +0100

    Refactor

--- prototype/capi-vm/src/width.rs
+++ prototype/capi-vm/src/width.rs
@@ -1,7 +1,11 @@
 pub trait Width {
     const SIZE: usize;
     const ENCODING: u8;
-    const INFO: WidthInfo;
+
+    const INFO: WidthInfo = WidthInfo {
+        size: Self::SIZE,
+        flag: Self::ENCODING << 6,
+    };
 }
 
 pub struct W8;
@@ -9,10 +13,6 @@ pub struct W8;
 impl Width for W8 {
     const SIZE: usize = 1;
     const ENCODING: u8 = 0b00;
-    const INFO: WidthInfo = WidthInfo {
-        size: Self::SIZE,
-        flag: Self::ENCODING << 6,
-    };
 }
 
 pub struct W16;
@@ -20,10 +20,6 @@ pub struct W16;
 impl Width for W16 {
     const SIZE: usize = 2;
     const ENCODING: u8 = 0b01;
-    const INFO: WidthInfo = WidthInfo {
-        size: Self::SIZE,
-        flag: Self::ENCODING << 6,
-    };
 }
 
 pub struct W32;
@@ -31,10 +27,6 @@ pub struct W32;
 impl Width for W32 {
     const SIZE: usize = 4;
     const ENCODING: u8 = 0b10;
-    const INFO: WidthInfo = WidthInfo {
-        size: Self::SIZE,
-        flag: Self::ENCODING << 6,
-    };
 }
 
 pub struct W64;
@@ -42,10 +34,6 @@ pub struct W64;
 impl Width for W64 {
     const SIZE: usize = 8;
     const ENCODING: u8 = 0b11;
-    const INFO: WidthInfo = WidthInfo {
-        size: Self::SIZE,
-        flag: Self::ENCODING << 6,
-    };
 }
 
 #[derive(Eq, PartialEq)]
