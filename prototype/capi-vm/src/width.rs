f4e8ebc6b0a56afdf1f2f514b2a29c526ce2daf9
Hanno Braun <hanno@braun-odw.eu>
2024-03-10 20:57:53 +0100

    Refactor

--- prototype/capi-vm/src/width.rs
+++ prototype/capi-vm/src/width.rs
@@ -1,9 +1,24 @@
-pub const W8: Width = Width { flag: 0b00 << 6 };
-pub const W16: Width = Width { flag: 0b01 << 6 };
-pub const W32: Width = Width { flag: 0b10 << 6 };
-pub const W64: Width = Width { flag: 0b11 << 6 };
+pub const W8: Width = Width {
+    flag: encoding::W8 << 6,
+};
+pub const W16: Width = Width {
+    flag: encoding::W16 << 6,
+};
+pub const W32: Width = Width {
+    flag: encoding::W32 << 6,
+};
+pub const W64: Width = Width {
+    flag: encoding::W64 << 6,
+};
 
 #[derive(Eq, PartialEq)]
 pub struct Width {
     pub flag: u8,
+}
+
+pub mod encoding {
+    pub const W8: u8 = 0b00;
+    pub const W16: u8 = 0b01;
+    pub const W32: u8 = 0b10;
+    pub const W64: u8 = 0b11;
 }
