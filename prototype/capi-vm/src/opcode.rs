c2832ca1d616cde6e73b25be33ed2f1601b76513
Hanno Braun <hanno@braun-odw.eu>
2024-03-12 19:26:49 +0100

    Refactor

--- prototype/capi-vm/src/opcode.rs
+++ prototype/capi-vm/src/opcode.rs
@@ -1,7 +1,7 @@
 pub const TERMINATE: u8 = 0x00;
-pub const PUSH: u8 = 0x01;
-pub const DROP: u8 = 0x02;
-// 0x03 reserved for `load`
-pub const STORE: u8 = 0x04;
-pub const CLONE: u8 = 0x05;
-pub const SWAP: u8 = 0x06;
+pub const PUSH: u8 = 0x02;
+pub const DROP: u8 = 0x03;
+// 0x04 reserved for `load`
+pub const STORE: u8 = 0x05;
+pub const CLONE: u8 = 0x06;
+pub const SWAP: u8 = 0x07;
