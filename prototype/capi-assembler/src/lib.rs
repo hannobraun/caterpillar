eebdf6a1c24085717c5c24a28baa1317f0d3bf09
Hanno Braun <hanno@braun-odw.eu>
2024-03-15 15:45:00 +0100

    Refactor

--- prototype/capi-assembler/src/lib.rs
+++ prototype/capi-assembler/src/lib.rs
@@ -40,18 +40,20 @@ pub fn assemble(assembly: &str) -> Result<Vec<u8>, AssemblerError> {
 
             let mut buffer = [0; 8];
 
-            let mut parse = || -> Result<(), ParseIntError> {
+            let mut parse = || -> Result<(), AssemblerError> {
                 buffer[..W32::INFO.num_bytes].copy_from_slice(
-                    &u32::from_str_radix(value, radix)?.to_le_bytes(),
+                    &u32::from_str_radix(value, radix)
+                        .map_err(|err| AssemblerError::ParseValue {
+                            value: value.to_owned(),
+                            source: err,
+                        })?
+                        .to_le_bytes(),
                 );
 
                 Ok(())
             };
 
-            parse().map_err(|err| AssemblerError::ParseValue {
-                value: value.to_owned(),
-                source: err,
-            })?;
+            parse()?;
             let value = &buffer[..width.num_bytes];
 
             bytecode.push(opcode::PUSH | width.flag);
