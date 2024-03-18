2c743085fe47d76e7ec2f58db5244b7073b63633
Hanno Braun <hanno@braun-odw.eu>
2024-03-18 22:28:16 +0100

    Serve from dedicated directory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use tokio::fs;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    warp::serve(warp::fs::dir("."))
+    let serve_dir = tempfile::tempdir()?;
+    fs::copy("index.html", serve_dir.path().join("index.html")).await?;
+
+    warp::serve(warp::fs::dir(serve_dir.path().to_owned()))
         .run(([127, 0, 0, 1], 8080))
         .await;
 
