77ec85a1067af4772caeb2ccb827a3dbe45e15fe
Hanno Braun <hanno@braun-odw.eu>
2024-03-18 22:35:22 +0100

    Watch `index.html` for changes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::{path::Path, time::Duration};
+
 use tokio::fs;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let mut debouncer = notify_debouncer_mini::new_debouncer(
+        Duration::from_millis(50),
+        |result| {
+            let _ = dbg!(result);
+        },
+    )?;
+    debouncer
+        .watcher()
+        .watch(Path::new("index.html"), notify::RecursiveMode::NonRecursive)?;
+
     let serve_dir = tempfile::tempdir()?;
     fs::copy("index.html", serve_dir.path().join("index.html")).await?;
 
