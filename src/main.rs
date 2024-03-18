57c52d882f7a6601f70c933b5e61faa7924aea4f
Hanno Braun <hanno@braun-odw.eu>
2024-03-18 22:25:49 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     warp::serve(warp::fs::dir("."))
         .run(([127, 0, 0, 1], 8080))
         .await;
+
+    Ok(())
 }
