698fa8a7ab0608a0241e0510adf37e824051b3f1
Hanno Braun <hanno@braun-odw.eu>
2024-03-20 22:31:07 +0100

    Refactor

--- capi/capi-runtime/src/ffi_out.rs
+++ capi/capi-runtime/src/ffi_out.rs
@@ -1,7 +1,7 @@
-extern "C" {
-    fn ffi_print(ptr: *const u8, len: usize);
-}
-
 pub fn print(s: &str) {
     unsafe { ffi_print(s.as_ptr(), s.len()) };
+}
+
+extern "C" {
+    fn ffi_print(ptr: *const u8, len: usize);
 }
