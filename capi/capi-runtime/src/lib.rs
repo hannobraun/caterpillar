182f046e301cbb0871d305e091d1a7199222300c
Hanno Braun <hanno@braun-odw.eu>
2024-03-20 23:13:07 +0100

    Don't depend on `std`

--- capi/capi-runtime/src/lib.rs
+++ capi/capi-runtime/src/lib.rs
@@ -1,2 +1,21 @@
+#![no_std]
+
+extern crate alloc;
+
 mod ffi_in;
 mod ffi_out;
+
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOCATOR: lol_alloc::LockedAllocator<lol_alloc::FreeListAllocator> =
+    lol_alloc::LockedAllocator::new(lol_alloc::FreeListAllocator::new());
+
+#[cfg(target_arch = "wasm32")]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    use alloc::string::ToString;
+    let msg = info.to_string();
+    crate::ffi_out::print(&msg);
+
+    core::arch::wasm32::unreachable()
+}
