0c703a6b629808540e2f045546f50d0e058d3952
Hanno Braun <hanno@braun-odw.eu>
2024-03-20 22:30:24 +0100

    Refactor

--- capi/capi-runtime/src/ffi_in.rs
+++ capi/capi-runtime/src/ffi_in.rs
@@ -2,8 +2,12 @@ extern "C" {
     fn ffi_print(ptr: *const u8, len: usize);
 }
 
+pub fn print(s: &str) {
+    unsafe { ffi_print(s.as_ptr(), s.len()) };
+}
+
 #[no_mangle]
 pub extern "C" fn on_init() {
     let s = "Hello, world!";
-    unsafe { ffi_print(s.as_ptr(), s.len()) };
+    print(s);
 }
