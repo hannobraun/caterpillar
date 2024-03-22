f37adad3e25c859394af194a16f6c8c79f91d921
Hanno Braun <hanno@braun-odw.eu>
2024-03-22 22:54:14 +0100

    Refactor

--- capi/capi-runtime/src/macros.rs
+++ capi/capi-runtime/src/macros.rs
@@ -1,6 +1,5 @@
 macro_rules! println {
     ($($arg:tt)*) => {{
-        use alloc::string::ToString;
         let s = core::format_args!($($arg)*).to_string();
         $crate::ffi_out::console_log(&s);
     }};
