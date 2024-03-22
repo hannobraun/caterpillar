948d07b6d21cf1993583d596183b6ed43657415e
Hanno Braun <hanno@braun-odw.eu>
2024-03-22 22:51:44 +0100

    Refactor

--- capi/capi-runtime/src/lib.rs
+++ capi/capi-runtime/src/lib.rs
@@ -5,8 +5,3 @@ mod macros;
 
 mod ffi_in;
 mod ffi_out;
-
-#[cfg(target_arch = "wasm32")]
-#[global_allocator]
-static ALLOCATOR: lol_alloc::LockedAllocator<lol_alloc::FreeListAllocator> =
-    lol_alloc::LockedAllocator::new(lol_alloc::FreeListAllocator::new());
