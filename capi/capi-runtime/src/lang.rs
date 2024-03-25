7134a8387a446a000c4abc367dc2e235b0304167
Hanno Braun <hanno@braun-odw.eu>
2024-03-25 21:54:25 +0100

    Refactor

--- capi/capi-runtime/src/lang.rs
+++ capi/capi-runtime/src/lang.rs
@@ -1,8 +1,8 @@
-pub fn lang(canvas_width: usize, canvas_height: usize, data: &mut [u8]) {
-    set_all_pixels(canvas_width, canvas_height, data);
+pub fn lang(canvas_width: usize, canvas_height: usize, mem: &mut [u8]) {
+    set_all_pixels(canvas_width, canvas_height, mem);
 }
 
-fn set_all_pixels(canvas_width: usize, canvas_height: usize, data: &mut [u8]) {
+fn set_all_pixels(canvas_width: usize, canvas_height: usize, mem: &mut [u8]) {
     let buffer_len = compute_draw_buffer_len(canvas_width, canvas_height);
     let mut i = draw_buffer_offset();
 
@@ -11,7 +11,7 @@ fn set_all_pixels(canvas_width: usize, canvas_height: usize, data: &mut [u8]) {
             break;
         }
 
-        set_pixel(i, data);
+        set_pixel(i, mem);
         i = inc_pixel(i);
     }
 }
@@ -24,39 +24,39 @@ fn draw_buffer_offset() -> usize {
     0
 }
 
-fn set_pixel(i: usize, data: &mut [u8]) {
-    set_red(i, data);
-    set_green(i, data);
-    set_blue(i, data);
-    set_alpha(i, data);
+fn set_pixel(i: usize, mem: &mut [u8]) {
+    set_red(i, mem);
+    set_green(i, mem);
+    set_blue(i, mem);
+    set_alpha(i, mem);
 }
 
-fn set_red(i: usize, data: &mut [u8]) {
+fn set_red(i: usize, mem: &mut [u8]) {
     let offset = 0;
     let value = 0;
-    set_channel(i, offset, value, data);
+    set_channel(i, offset, value, mem);
 }
 
-fn set_green(i: usize, data: &mut [u8]) {
+fn set_green(i: usize, mem: &mut [u8]) {
     let offset = 1;
     let value = 255;
-    set_channel(i, offset, value, data);
+    set_channel(i, offset, value, mem);
 }
 
-fn set_blue(i: usize, data: &mut [u8]) {
+fn set_blue(i: usize, mem: &mut [u8]) {
     let offset = 2;
     let value = 0;
-    set_channel(i, offset, value, data);
+    set_channel(i, offset, value, mem);
 }
 
-fn set_alpha(i: usize, data: &mut [u8]) {
+fn set_alpha(i: usize, mem: &mut [u8]) {
     let offset = 3;
     let value = 255;
-    set_channel(i, offset, value, data);
+    set_channel(i, offset, value, mem);
 }
 
-fn set_channel(i: usize, offset: usize, value: u8, data: &mut [u8]) {
-    data[i + offset] = value;
+fn set_channel(i: usize, offset: usize, value: u8, mem: &mut [u8]) {
+    mem[i + offset] = value;
 }
 
 fn inc_pixel(i: usize) -> usize {
