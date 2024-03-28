6db28abbc838c55060d26c2a6e9f310f07b52d27
Hanno Braun <hanno@braun-odw.eu>
2024-03-28 22:31:22 +0100

    Add winit-based event handling

--- capi/src/main.rs
+++ capi/src/main.rs
@@ -1,4 +1,25 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::Window,
+};
+
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+
+    let event_loop = EventLoop::new()?;
+    let window = Window::new(&event_loop)?;
+
+    event_loop.run(|event, _| match event {
+        Event::AboutToWait => {
+            window.request_redraw();
+        }
+        Event::WindowEvent {
+            event: WindowEvent::RedrawRequested,
+            ..
+        } => {}
+        _ => {}
+    })?;
+
     Ok(())
 }
