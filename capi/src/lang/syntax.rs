68ef18a4fde395fa39ed8ed38448cbc9d364e5fd
Hanno Braun <hanno@braun-odw.eu>
2024-03-31 18:34:56 +0200

    Refactor

--- capi/src/lang/syntax.rs
+++ capi/src/lang/syntax.rs
@@ -17,11 +17,6 @@ impl<'r> Syntax<'r> {
         }
     }
 
-    pub fn w(&mut self, name: &'static str) -> &mut Self {
-        self.elements.push(SyntaxElement::Word { name });
-        self
-    }
-
     pub fn f(&mut self, name: &'static str) -> &mut Self {
         let address = self.functions.resolve(name);
         self.elements.push(SyntaxElement::CallFunction { address });
@@ -30,6 +25,11 @@ impl<'r> Syntax<'r> {
 
     pub fn v(&mut self, value: usize) -> &mut Self {
         self.elements.push(SyntaxElement::PushValue(value));
+        self
+    }
+
+    pub fn w(&mut self, name: &'static str) -> &mut Self {
+        self.elements.push(SyntaxElement::Word { name });
         self
     }
 }
