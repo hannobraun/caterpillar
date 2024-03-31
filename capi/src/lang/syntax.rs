89b2dd4d4eb3f45c282387ecb995c8fee57f0086
Hanno Braun <hanno@braun-odw.eu>
2024-03-31 18:29:09 +0200

    Refactor

--- capi/src/lang/syntax.rs
+++ capi/src/lang/syntax.rs
@@ -3,34 +3,33 @@ use super::{compiler::Instruction, functions::Functions};
 #[derive(Debug)]
 pub struct Syntax<'r> {
     functions: &'r Functions,
-    instructions: &'r mut Vec<Instruction>,
+    elements: &'r mut Vec<Instruction>,
 }
 
 impl<'r> Syntax<'r> {
     pub fn new(
         functions: &'r Functions,
-        instructions: &'r mut Vec<Instruction>,
+        elements: &'r mut Vec<Instruction>,
     ) -> Self {
         Self {
             functions,
-            instructions,
+            elements,
         }
     }
 
     pub fn b(&mut self, name: &'static str) -> &mut Self {
-        self.instructions.push(Instruction::CallBuiltin { name });
+        self.elements.push(Instruction::CallBuiltin { name });
         self
     }
 
     pub fn f(&mut self, name: &'static str) -> &mut Self {
         let address = self.functions.resolve(name);
-        self.instructions
-            .push(Instruction::CallFunction { address });
+        self.elements.push(Instruction::CallFunction { address });
         self
     }
 
     pub fn v(&mut self, value: usize) -> &mut Self {
-        self.instructions.push(Instruction::PushValue(value));
+        self.elements.push(Instruction::PushValue(value));
         self
     }
 }
