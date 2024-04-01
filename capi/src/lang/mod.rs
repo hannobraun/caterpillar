0b7a45319407814885a247e2ea0e21833a63976a
Hanno Braun <hanno@braun-odw.eu>
2024-04-01 11:14:50 +0200

    Refactor

--- capi/src/lang/mod.rs
+++ capi/src/lang/mod.rs
@@ -9,34 +9,34 @@ mod syntax;
 use self::{evaluator::Evaluator, functions::Functions};
 
 pub fn lang(frame_width: usize, frame_height: usize, frame: &mut [u8]) {
-    let mut capi = Capi::new();
+    let mut functions = Functions::new();
 
-    capi.functions.define("inc_addr", |s| {
+    functions.define("inc_addr", |s| {
         s.v(1).w("add");
     });
-    capi.functions.define("store_channel", |s| {
+    functions.define("store_channel", |s| {
         s.w("store").w("inc_addr");
     });
-    capi.functions.define("store_red", |s| {
+    functions.define("store_red", |s| {
         s.v(0).w("store_channel");
     });
-    capi.functions.define("store_green", |s| {
+    functions.define("store_green", |s| {
         s.v(255).w("store_channel");
     });
-    capi.functions.define("store_blue", |s| {
+    functions.define("store_blue", |s| {
         s.v(0).w("store_channel");
     });
-    capi.functions.define("store_alpha", |s| {
+    functions.define("store_alpha", |s| {
         s.v(255).w("store_channel");
     });
-    capi.functions.define("store_pixel", |s| {
+    functions.define("store_pixel", |s| {
         s.w("store_red")
             .w("store_green")
             .w("store_blue")
             .w("store_alpha");
     });
 
-    let (instructions, symbols) = capi.functions.compile();
+    let (instructions, symbols) = functions.compile();
     let store_pixel = symbols
         .resolve("store_pixel")
         .expect("Can't find function that was just defined.");
@@ -52,19 +52,6 @@ pub fn lang(frame_width: usize, frame_height: usize, frame: &mut [u8]) {
     );
 
     assert_eq!(evaluator.data_stack.num_values(), 0);
-}
-
-#[derive(Debug)]
-pub struct Capi {
-    functions: Functions,
-}
-
-impl Capi {
-    pub fn new() -> Self {
-        Self {
-            functions: Functions::new(),
-        }
-    }
 }
 
 fn store_all_pixels(
