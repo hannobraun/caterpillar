796095e8e63ab52d0d72288f675f144b68567722
Hanno Braun <hanno@braun-odw.eu>
2024-04-01 11:03:17 +0200

    Refactor

--- capi/src/lang/functions.rs
+++ capi/src/lang/functions.rs
@@ -1,24 +1,27 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use super::syntax::SyntaxElement;
 
 #[derive(Debug)]
 pub struct Functions {
+    pub names: BTreeSet<&'static str>,
     pub inner: BTreeMap<&'static str, Vec<SyntaxElement>>,
 }
 
 impl Functions {
     pub fn new() -> Self {
         Self {
+            names: BTreeSet::new(),
             inner: BTreeMap::new(),
         }
     }
 
     pub fn define(&mut self, name: &'static str, syntax: Vec<SyntaxElement>) {
-        if self.inner.contains_key(name) {
+        if self.names.contains(name) {
             panic!("Can't re-define existing function `{name}`.");
         }
 
+        self.names.insert(name);
         self.inner.insert(name, syntax);
     }
 }
