2fe13cb68f752847a849053cc8fffc915059cf6c
Hanno Braun <hanno@braun-odw.eu>
2024-04-03 09:41:22 +0200

    Refactor

--- capi/src/main.rs
+++ capi/src/main.rs
@@ -1,66 +1,6 @@
 mod capi;
-
-use pixels::{Pixels, SurfaceTexture};
-use winit::{
-    event::{Event, KeyEvent, WindowEvent},
-    event_loop::EventLoop,
-    keyboard::{KeyCode, PhysicalKey},
-    window::WindowBuilder,
-};
-
-use crate::capi::capi;
+mod display;
 
 fn main() -> anyhow::Result<()> {
-    const WIDTH: u32 = 640;
-    const HEIGHT: u32 = 480;
-
-    let event_loop = EventLoop::new()?;
-    let window = WindowBuilder::new()
-        .with_title("Caterpillar")
-        .build(&event_loop)?;
-
-    let surface_texture = SurfaceTexture::new(WIDTH, HEIGHT, &window);
-    let mut pixels = Pixels::new(WIDTH, HEIGHT, surface_texture)?;
-
-    event_loop.run(|event, event_loop_window_target| match event {
-        Event::AboutToWait => {
-            capi(
-                WIDTH.try_into().unwrap(),
-                HEIGHT.try_into().unwrap(),
-                pixels.frame_mut(),
-            );
-            window.request_redraw();
-        }
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
-            ..
-        } => {
-            event_loop_window_target.exit();
-        }
-        Event::WindowEvent {
-            event:
-                WindowEvent::KeyboardInput {
-                    event:
-                        KeyEvent {
-                            physical_key: PhysicalKey::Code(KeyCode::Escape),
-                            ..
-                        },
-                    ..
-                },
-            ..
-        } => {
-            event_loop_window_target.exit();
-        }
-        Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            ..
-        } => {
-            if let Err(err) = pixels.render() {
-                eprintln!("Render error: {err}");
-            }
-        }
-        _ => {}
-    })?;
-
-    Ok(())
+    display::run()
 }
