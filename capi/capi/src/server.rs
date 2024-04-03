abd5566f38d5045c4b2486e76933465e52a993f3
Hanno Braun <hanno@braun-odw.eu>
2024-04-03 22:24:23 +0200

    Improve error handling

--- capi/capi/src/server.rs
+++ capi/capi/src/server.rs
@@ -1,11 +1,15 @@
-use std::thread;
+use std::{process::exit, thread};
 
 pub fn start() {
     thread::spawn(|| {
-        serve();
+        if let Err(err) = serve() {
+            eprintln!("Server error: {err}");
+            exit(1);
+        }
     });
 }
 
-fn serve() {
+fn serve() -> anyhow::Result<()> {
     println!("Hello, world!");
+    Ok(())
 }
