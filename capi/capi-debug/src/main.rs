f58beefdb5d76ce88e93694083fef4d4288c737a
Hanno Braun <hanno@braun-odw.eu>
2024-04-03 21:43:47 +0200

    Initialize console logging

--- capi/capi-debug/src/main.rs
+++ capi/capi-debug/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     console_error_panic_hook::set_once();
+    console_log::init_with_level(log::Level::Debug)
+        .expect("Failed to initialize logging to console");
+
     println!("Hello, world!");
 }
