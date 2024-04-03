f23995f3f770a7bc63b609914618deaf5066a607
Hanno Braun <hanno@braun-odw.eu>
2024-04-03 19:39:11 +0200

    Refactor

--- capi/src/capi/mod.rs
+++ capi/src/capi/mod.rs
@@ -14,64 +14,66 @@ pub struct Program {
     pub entry: usize,
 }
 
-pub fn create_program() -> Program {
-    let mut functions = Functions::new();
+impl Program {
+    pub fn create_program() -> Self {
+        let mut functions = Functions::new();
 
-    functions.define("draw_to_frame_buffer", |s| {
-        s.w("compute_frame_buffer_len")
-            .w("frame_buffer_addr")
-            .w("store_all_pixels")
-            .w("drop2");
-    });
-    functions.define("compute_frame_buffer_len", |s| {
-        s.w("mul").w("num_channels").w("mul");
-    });
-    functions.define("num_channels", |s| {
-        s.v(4);
-    });
-    functions.define("frame_buffer_addr", |s| {
-        s.v(0);
-    });
-    functions.define("store_all_pixels", |s| {
-        s.w("clone2")
-            .w("sub")
-            .w("return_if_zero")
-            .w("store_pixel")
-            .w("store_all_pixels");
-    });
-    functions.define("store_pixel", |s| {
-        s.w("store_red")
-            .w("store_green")
-            .w("store_blue")
-            .w("store_alpha");
-    });
-    functions.define("store_red", |s| {
-        s.v(0).w("store_channel");
-    });
-    functions.define("store_green", |s| {
-        s.v(255).w("store_channel");
-    });
-    functions.define("store_blue", |s| {
-        s.v(0).w("store_channel");
-    });
-    functions.define("store_alpha", |s| {
-        s.v(255).w("store_channel");
-    });
-    functions.define("store_channel", |s| {
-        s.w("store").w("inc_addr");
-    });
-    functions.define("inc_addr", |s| {
-        s.v(1).w("add");
-    });
+        functions.define("draw_to_frame_buffer", |s| {
+            s.w("compute_frame_buffer_len")
+                .w("frame_buffer_addr")
+                .w("store_all_pixels")
+                .w("drop2");
+        });
+        functions.define("compute_frame_buffer_len", |s| {
+            s.w("mul").w("num_channels").w("mul");
+        });
+        functions.define("num_channels", |s| {
+            s.v(4);
+        });
+        functions.define("frame_buffer_addr", |s| {
+            s.v(0);
+        });
+        functions.define("store_all_pixels", |s| {
+            s.w("clone2")
+                .w("sub")
+                .w("return_if_zero")
+                .w("store_pixel")
+                .w("store_all_pixels");
+        });
+        functions.define("store_pixel", |s| {
+            s.w("store_red")
+                .w("store_green")
+                .w("store_blue")
+                .w("store_alpha");
+        });
+        functions.define("store_red", |s| {
+            s.v(0).w("store_channel");
+        });
+        functions.define("store_green", |s| {
+            s.v(255).w("store_channel");
+        });
+        functions.define("store_blue", |s| {
+            s.v(0).w("store_channel");
+        });
+        functions.define("store_alpha", |s| {
+            s.v(255).w("store_channel");
+        });
+        functions.define("store_channel", |s| {
+            s.w("store").w("inc_addr");
+        });
+        functions.define("inc_addr", |s| {
+            s.v(1).w("add");
+        });
 
-    functions.print();
+        functions.print();
 
-    let code = functions.compile();
-    let entry = code.symbols.resolve("draw_to_frame_buffer");
+        let code = functions.compile();
+        let entry = code.symbols.resolve("draw_to_frame_buffer");
 
-    let evaluator = Evaluator::new(code);
+        let evaluator = Evaluator::new(code);
 
-    Program { evaluator, entry }
+        Self { evaluator, entry }
+    }
 }
 
 pub fn run_program(
