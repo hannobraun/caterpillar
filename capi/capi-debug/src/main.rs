69f54c4a88d6ee09251025c96e876f6407f8c67a
Hanno Braun <hanno@braun-odw.eu>
2024-04-03 21:50:54 +0200

    Update log message

--- capi/capi-debug/src/main.rs
+++ capi/capi-debug/src/main.rs
@@ -3,11 +3,11 @@ fn main() {
     console_log::init_with_level(log::Level::Debug)
         .expect("Failed to initialize logging to console");
 
-    log::info!("Hello, world!");
-
     leptos::mount_to_body(|| {
         leptos::view! {
             <p>Hello, world!</p>
         }
     });
+
+    log::info!("Capi Debug initialized.");
 }
