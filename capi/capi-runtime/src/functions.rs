a19a7eb73cf7ce8baaabbb2bd518e930afd5c6a7
Hanno Braun <hanno@braun-odw.eu>
2024-04-07 11:35:27 +0200

    Refactor

--- capi/capi-runtime/src/functions.rs
+++ capi/capi-runtime/src/functions.rs
@@ -35,8 +35,8 @@ impl Functions {
     pub fn compile(self) -> Code {
         let mut code = Code::new();
 
-        for Function { name, syntax } in &self.inner {
-            compile(name.clone(), syntax, &self.names, &mut code);
+        for Function { name, syntax } in self.inner {
+            compile(name.clone(), &syntax, &self.names, &mut code);
         }
 
         code
