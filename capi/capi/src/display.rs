0a8ad56b63b59bda19b08c322ae7071bd4f5afe3
Hanno Braun <hanno@braun-odw.eu>
2024-04-08 21:01:11 +0200

    Refactor

--- capi/capi/src/display.rs
+++ capi/capi/src/display.rs
@@ -15,14 +15,15 @@ pub fn run(mut program: Program) -> anyhow::Result<()> {
     // I don't like the `as`, but I can't use `try_into` in a const context.
     // Given this is a screen resolution, this is unlikely to ever be a problem.
     const SIZE: u32 = (TILES_PER_AXIS * PIXELS_PER_TILE_AXIS) as u32;
+    let size_u32 = SIZE;
 
     let event_loop = EventLoop::new()?;
     let window = WindowBuilder::new()
         .with_title("Caterpillar")
         .build(&event_loop)?;
 
-    let surface_texture = SurfaceTexture::new(SIZE, SIZE, &window);
-    let mut pixels = Pixels::new(SIZE, SIZE, surface_texture)?;
+    let surface_texture = SurfaceTexture::new(size_u32, size_u32, &window);
+    let mut pixels = Pixels::new(size_u32, size_u32, surface_texture)?;
 
     event_loop.run(|event, event_loop_window_target| match event {
         Event::AboutToWait => {
