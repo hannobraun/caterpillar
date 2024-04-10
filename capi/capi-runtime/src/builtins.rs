5278e2a8a9a708be0999eb55b859570b1de7737e
Hanno Braun <hanno@braun-odw.eu>
2024-04-10 11:37:22 +0200

    Restore alphabetic ordering

--- capi/capi-runtime/src/builtins.rs
+++ capi/capi-runtime/src/builtins.rs
@@ -11,20 +11,6 @@ pub fn add(data_stack: &mut DataStack) {
     data_stack.push(c);
 }
 
-pub fn drop2(data_stack: &mut DataStack) {
-    data_stack.pop();
-    data_stack.pop();
-}
-
-pub fn mul(data_stack: &mut DataStack) {
-    let b = data_stack.pop();
-    let a = data_stack.pop();
-
-    let c = a * b;
-
-    data_stack.push(c);
-}
-
 pub fn copy(data_stack: &mut DataStack) {
     let mut i = data_stack.pop();
 
@@ -41,6 +27,20 @@ pub fn copy(data_stack: &mut DataStack) {
     }
 
     data_stack.push(a);
+}
+
+pub fn drop2(data_stack: &mut DataStack) {
+    data_stack.pop();
+    data_stack.pop();
+}
+
+pub fn mul(data_stack: &mut DataStack) {
+    let b = data_stack.pop();
+    let a = data_stack.pop();
+
+    let c = a * b;
+
+    data_stack.push(c);
 }
 
 pub fn store(data_stack: &mut DataStack, mem: &mut [u8]) {
