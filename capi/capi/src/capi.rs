72a99085654305928af4893724d4978853cf5060
Hanno Braun <hanno@braun-odw.eu>
2024-04-16 11:54:19 +0200

    Add `Program::define`

--- capi/capi/src/capi.rs
+++ capi/capi/src/capi.rs
@@ -3,34 +3,34 @@ use capi_runtime::Program;
 pub fn program() -> Program {
     let mut program = Program::default();
 
-    program.functions.define("write_to_tile_buffer", |s| {
+    program.define("write_to_tile_buffer", |s| {
         s.w("last_tile_index")
             .w("first_tile_index")
             .w("set_all_tiles")
             .w("clean_up_arguments");
     });
-    program.functions.define("last_tile_index", |s| {
+    program.define("last_tile_index", |s| {
         s.w("mul").w("first_tile_index").w("add");
     });
-    program.functions.define("first_tile_index", |s| {
+    program.define("first_tile_index", |s| {
         s.v(256);
     });
-    program.functions.define("set_all_tiles", |s| {
+    program.define("set_all_tiles", |s| {
         s.w("check_tile_index")
             .w("return_if_zero")
             .w("set_tile")
             .w("set_all_tiles");
     });
-    program.functions.define("check_tile_index", |s| {
+    program.define("check_tile_index", |s| {
         s.v(1).w("copy").v(1).w("copy").w("sub");
     });
-    program.functions.define("set_tile", |s| {
+    program.define("set_tile", |s| {
         s.v(1).w("store").w("inc_tile_index");
     });
-    program.functions.define("inc_tile_index", |s| {
+    program.define("inc_tile_index", |s| {
         s.v(1).w("add");
     });
-    program.functions.define("clean_up_arguments", |s| {
+    program.define("clean_up_arguments", |s| {
         s.v(0).w("drop").v(0).w("drop");
     });
 
