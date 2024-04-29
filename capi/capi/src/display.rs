c0bb8a90fe95106b9a6b0ec86bfc602503cfc5c5
Hanno Braun <hanno@braun-odw.eu>
2024-04-29 11:47:06 +0200

    Refactor to avoid deprecated method call

--- capi/capi/src/display.rs
+++ capi/capi/src/display.rs
@@ -1,8 +1,9 @@
 use capi_runtime::{Program, ProgramState, Value};
 use pixels::{Pixels, SurfaceTexture};
 use winit::{
-    event::{Event, KeyEvent, WindowEvent},
-    event_loop::EventLoop,
+    application::ApplicationHandler,
+    event::{KeyEvent, WindowEvent},
+    event_loop::{ActiveEventLoop, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
     window::Window,
 };
@@ -35,118 +36,7 @@ pub fn run(
         pixels,
     };
 
-    #[allow(deprecated)] // only for the transition to winit 0.30
-    event_loop.run(|event, event_loop_window_target| match event {
-        Event::AboutToWait => {
-            while let Ok(event) = state.events.try_recv() {
-                state.program.apply_debug_event(event);
-                state.updates.send(state.program.clone()).unwrap();
-            }
-
-            if let ProgramState::Finished = state.program.state {
-                state
-                    .program
-                    .push([Value(TILES_PER_AXIS.try_into().unwrap()); 2]);
-                state.program.reset();
-            }
-
-            let previous_state = state.program.state.clone();
-
-            loop {
-                if let ProgramState::Error { .. } = state.program.state {
-                    // If there's an error, never run the program again. As of
-                    // this writing, that's it, and for now that's fine.
-                    //
-                    // Eventually, it would be great if we could get out of this
-                    // by resetting the program, or rewinding the builtin that
-                    // caused the error.
-                    break;
-                }
-
-                match state.program.step(&mut state.mem) {
-                    ProgramState::Running => {}
-                    ProgramState::Paused { .. } => {
-                        break;
-                    }
-                    ProgramState::Finished => {
-                        assert_eq!(
-                            state.program.evaluator.data_stack.num_values(),
-                            0
-                        );
-                        break;
-                    }
-                    ProgramState::Error { .. } => {
-                        break;
-                    }
-                }
-            }
-
-            if state.program.state != previous_state {
-                state.updates.send(state.program.clone()).unwrap();
-            }
-
-            for tile_y in 0..TILES_PER_AXIS {
-                for tile_x in 0..TILES_PER_AXIS {
-                    let i = TILES_OFFSET + tile_y * TILES_PER_AXIS + tile_x;
-                    let tile = state.mem[i];
-
-                    let color = if tile == 0 {
-                        [0, 0, 0, 0]
-                    } else {
-                        [255, 255, 255, 255]
-                    };
-
-                    for offset_y in 0..PIXELS_PER_TILE_AXIS {
-                        for offset_x in 0..PIXELS_PER_TILE_AXIS {
-                            let num_channels = 4;
-
-                            let frame_x = (tile_x * PIXELS_PER_TILE_AXIS
-                                + offset_x)
-                                * num_channels;
-                            let frame_y = (tile_y * PIXELS_PER_TILE_AXIS
-                                + offset_y)
-                                * num_channels;
-
-                            let i = frame_y * SIZE + frame_x;
-                            state.pixels.frame_mut()[i..i + num_channels]
-                                .copy_from_slice(&color);
-                        }
-                    }
-                }
-            }
-
-            state.window.request_redraw();
-        }
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
-            ..
-        } => {
-            event_loop_window_target.exit();
-        }
-        Event::WindowEvent {
-            event:
-                WindowEvent::KeyboardInput {
-                    event:
-                        KeyEvent {
-                            physical_key: PhysicalKey::Code(KeyCode::Escape),
-                            ..
-                        },
-                    ..
-                },
-            ..
-        } => {
-            event_loop_window_target.exit();
-        }
-        Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            ..
-        } => {
-            if let Err(err) = state.pixels.render() {
-                eprintln!("Render error: {err}");
-            }
-        }
-        _ => {}
-    })?;
+    event_loop.run_app(&mut state)?;
 
     Ok(())
 }
@@ -167,4 +57,117 @@ struct State {
     mem: [u8; MEM_SIZE],
     window: Window,
     pixels: Pixels,
+}
+
+impl ApplicationHandler for State {
+    fn resumed(&mut self, _: &ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        event_loop_window_target: &ActiveEventLoop,
+        _: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop_window_target.exit();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => {
+                event_loop_window_target.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                if let Err(err) = self.pixels.render() {
+                    eprintln!("Render error: {err}");
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn about_to_wait(&mut self, _: &ActiveEventLoop) {
+        while let Ok(event) = self.events.try_recv() {
+            self.program.apply_debug_event(event);
+            self.updates.send(self.program.clone()).unwrap();
+        }
+
+        if let ProgramState::Finished = self.program.state {
+            self.program
+                .push([Value(TILES_PER_AXIS.try_into().unwrap()); 2]);
+            self.program.reset();
+        }
+
+        let previous_state = self.program.state.clone();
+
+        loop {
+            if let ProgramState::Error { .. } = self.program.state {
+                // If there's an error, never run the program again. As of
+                // this writing, that's it, and for now that's fine.
+                //
+                // Eventually, it would be great if we could get out of this
+                // by resetting the program, or rewinding the builtin that
+                // caused the error.
+                break;
+            }
+
+            match self.program.step(&mut self.mem) {
+                ProgramState::Running => {}
+                ProgramState::Paused { .. } => {
+                    break;
+                }
+                ProgramState::Finished => {
+                    assert_eq!(
+                        self.program.evaluator.data_stack.num_values(),
+                        0
+                    );
+                    break;
+                }
+                ProgramState::Error { .. } => {
+                    break;
+                }
+            }
+        }
+
+        if self.program.state != previous_state {
+            self.updates.send(self.program.clone()).unwrap();
+        }
+
+        for tile_y in 0..TILES_PER_AXIS {
+            for tile_x in 0..TILES_PER_AXIS {
+                let i = TILES_OFFSET + tile_y * TILES_PER_AXIS + tile_x;
+                let tile = self.mem[i];
+
+                let color = if tile == 0 {
+                    [0, 0, 0, 0]
+                } else {
+                    [255, 255, 255, 255]
+                };
+
+                for offset_y in 0..PIXELS_PER_TILE_AXIS {
+                    for offset_x in 0..PIXELS_PER_TILE_AXIS {
+                        let num_channels = 4;
+
+                        let frame_x = (tile_x * PIXELS_PER_TILE_AXIS
+                            + offset_x)
+                            * num_channels;
+                        let frame_y = (tile_y * PIXELS_PER_TILE_AXIS
+                            + offset_y)
+                            * num_channels;
+
+                        let i = frame_y * SIZE + frame_x;
+                        self.pixels.frame_mut()[i..i + num_channels]
+                            .copy_from_slice(&color);
+                    }
+                }
+            }
+        }
+
+        self.window.request_redraw();
+    }
 }
