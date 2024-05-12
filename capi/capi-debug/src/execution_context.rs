5a06988a1c04dc0c134cd0f994e266322967eb28
Hanno Braun <hanno@braun-odw.eu>
2024-05-12 11:25:27 +0200

    Refactor

--- capi/capi-debug/src/execution_context.rs
+++ capi/capi-debug/src/execution_context.rs
@@ -1,55 +1,61 @@
-use capi_runtime::{Program, ProgramState};
+use capi_runtime::{Function, Program, ProgramState};
 use leptos::{component, view, IntoView, ReadSignal, SignalGet};
 
 #[component]
 pub fn ExecutionContext(program: ReadSignal<Option<Program>>) -> impl IntoView {
     move || {
-        let Some(program) = program.get() else {
-            return view! {
-                <p>"No program available."</p>
-            };
-        };
-
-        let (_effect, address) = match program.state {
-            ProgramState::Running => {
+        let _function = match get_current_function(program) {
+            Ok(function) => function,
+            Err(error) => {
                 return view! {
-                    <p>"Program is running."</p>
-                }
+                    <p>{error}</p>
+                };
             }
-            ProgramState::Finished => {
-                return view! {
-                    <p>"Program has finished running."</p>
-                }
-            }
-            ProgramState::Effect { effect, address } => (effect, address),
-        };
-
-        let Some(location) = program.source_map.address_to_location(&address)
-        else {
-            return view! {
-                <p>
-                    "Program is stopped at instruction with no associated \
-                    source location."
-                </p>
-            };
-        };
-
-        let function = program
-            .functions
-            .inner
-            .iter()
-            .find(|function| function.name == location.function());
-        let Some(_function) = function else {
-            return view! {
-                <p>
-                    "Program stopped at unknown function. This is most likely \
-                    a bug in Caterpillar."
-                </p>
-            };
         };
 
         view! {
             <p>"Placeholder for execution context"</p>
         }
     }
+}
+
+fn get_current_function(
+    program: ReadSignal<Option<Program>>,
+) -> Result<Function, &'static str> {
+    let Some(program) = program.get() else {
+        return Err("No program available.");
+    };
+
+    let (_effect, address) = match program.state {
+        ProgramState::Running => {
+            return Err("Program is running.");
+        }
+        ProgramState::Finished => {
+            return Err("Program has finished running.");
+        }
+        ProgramState::Effect { effect, address } => (effect, address),
+    };
+
+    let Some(location) = program.source_map.address_to_location(&address)
+    else {
+        return Err(
+            "Program is stopped at instruction with no associated source \
+            location.",
+        );
+    };
+
+    let function = program
+        .functions
+        .inner
+        .iter()
+        .find(|function| function.name == location.function())
+        .cloned();
+    let Some(function) = function else {
+        return Err(
+            "Program stopped at unknown function. This is most likely a bug in \
+            Caterpillar.",
+        );
+    };
+
+    Ok(function)
 }
