43c489e47201a8df851fc860a9ecd6ae53411545
Hanno Braun <hello@hannobraun.com>
2024-05-13 11:38:24 +0200

    Move all function components to `expression`

--- capi/capi-debug/src/expression.rs
+++ capi/capi-debug/src/expression.rs
@@ -1,7 +1,130 @@
 use capi_runtime::{
-    Expression, ExpressionKind, Program, ProgramEffect, ProgramState,
+    DebugEvent, Expression, ExpressionKind, InstructionAddress, Program,
+    ProgramEffect, ProgramState,
 };
-use leptos::{component, view, IntoView, ReadSignal, SignalGet};
+use leptos::{
+    component, create_memo, view, CollectView, IntoView, ReadSignal, SignalGet,
+};
+use web_sys::{wasm_bindgen::JsCast, HtmlSpanElement, MouseEvent};
+
+use crate::{send_event, EventsTx};
+
+#[component]
+pub fn Function(
+    program: ReadSignal<Option<Program>>,
+    function: capi_runtime::Function,
+    events: EventsTx,
+) -> impl IntoView {
+    let lines = function
+        .syntax
+        .into_iter()
+        .map(|expression| {
+            view! {
+                <LineWithBreakpoint
+                    program=program
+                    expression=expression
+                    events=events.clone() />
+            }
+        })
+        .collect_view();
+
+    view! {
+        <div class="m-2 mb-4">
+            <div class="font-bold">
+                {function.name}:{'\n'}
+            </div>
+            <ol>
+                {lines}
+            </ol>
+        </div>
+    }
+}
+
+#[component]
+pub fn LineWithBreakpoint(
+    program: ReadSignal<Option<Program>>,
+    expression: Expression,
+    events: EventsTx,
+) -> impl IntoView {
+    let location = expression.location.clone();
+    let address = create_memo(move |_| {
+        program.get()?.source_map.location_to_address(&location)
+    });
+
+    let breakpoint = move || {
+        let address = address.get()?;
+
+        Some(view! {
+            <Breakpoint
+                program=program
+                address=address
+                events=events.clone() />
+        })
+    };
+
+    view! {
+        <li class="ml-8">
+            {breakpoint}
+            <Expression
+                program=program
+                expression=expression />
+        </li>
+    }
+}
+
+#[component]
+pub fn Breakpoint(
+    program: ReadSignal<Option<Program>>,
+    address: InstructionAddress,
+    events: EventsTx,
+) -> impl IntoView {
+    let class = move || {
+        let program = program.get()?;
+
+        let breakpoint_color = if program.breakpoint_at(&address) {
+            "text-green-600"
+        } else {
+            "text-green-300"
+        };
+
+        Some(format!("mr-1 {breakpoint_color}"))
+    };
+
+    let data_address = move || address.to_usize();
+
+    let toggle_breakpoint = move |event: MouseEvent| {
+        let event_target = event.target().unwrap();
+        let element = event_target.dyn_ref::<HtmlSpanElement>().unwrap();
+
+        let address = element
+            .get_attribute("data-address")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        leptos::spawn_local(send_event(
+            DebugEvent::ToggleBreakpoint { address },
+            events.clone(),
+        ));
+    };
+
+    // It would be nicer to have the click handler on the enclosing element, to
+    // make it less finicky for the user. But for some reason, I'm getting a
+    // reference to the window on `event.current_target()`, so I have to rely on
+    // `event.target()` to find the metadata. And that means, I can't have
+    // events coming from multiple elements.
+    //
+    // There are probably better ways to solve this problem, but for now, this
+    // is fine, if unfortunate.
+    view! {
+        <span
+            class=class
+            data-address=data_address
+            on:click=toggle_breakpoint>
+            {'⦿'}
+        </span>
+    }
+}
 
 #[component]
 pub fn Expression(
--- capi/capi-debug/src/main.rs
+++ capi/capi-debug/src/main.rs
@@ -1,7 +1,7 @@
 mod execution_context;
 mod expression;
 
-use capi_runtime::{DebugEvent, Expression, InstructionAddress, Program};
+use capi_runtime::{DebugEvent, Program};
 use futures::{
     channel::mpsc::{self, UnboundedReceiver, UnboundedSender},
     future::{select, Either},
@@ -9,12 +9,11 @@ use futures::{
 };
 use gloo::net::websocket::{futures::WebSocket, Message};
 use leptos::{
-    component, create_memo, create_signal, ev::MouseEvent, view, CollectView,
-    IntoView, ReadSignal, SignalGet, SignalSet, WriteSignal,
+    component, create_signal, view, CollectView, IntoView, ReadSignal,
+    SignalGet, SignalSet, WriteSignal,
 };
-use web_sys::{wasm_bindgen::JsCast, HtmlSpanElement};
 
-use crate::{execution_context::ExecutionContext, expression::Expression};
+use crate::{execution_context::ExecutionContext, expression::Function};
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -191,123 +190,6 @@ pub fn CodeExplorer(
 
     view! {
         <div>{functions}</div>
-    }
-}
-
-#[component]
-pub fn Function(
-    program: ReadSignal<Option<Program>>,
-    function: capi_runtime::Function,
-    events: EventsTx,
-) -> impl IntoView {
-    let lines = function
-        .syntax
-        .into_iter()
-        .map(|expression| {
-            view! {
-                <LineWithBreakpoint
-                    program=program
-                    expression=expression
-                    events=events.clone() />
-            }
-        })
-        .collect_view();
-
-    view! {
-        <div class="m-2 mb-4">
-            <div class="font-bold">
-                {function.name}:{'\n'}
-            </div>
-            <ol>
-                {lines}
-            </ol>
-        </div>
-    }
-}
-
-#[component]
-pub fn LineWithBreakpoint(
-    program: ReadSignal<Option<Program>>,
-    expression: Expression,
-    events: EventsTx,
-) -> impl IntoView {
-    let location = expression.location.clone();
-    let address = create_memo(move |_| {
-        program.get()?.source_map.location_to_address(&location)
-    });
-
-    let breakpoint = move || {
-        let address = address.get()?;
-
-        Some(view! {
-            <Breakpoint
-                program=program
-                address=address
-                events=events.clone() />
-        })
-    };
-
-    view! {
-        <li class="ml-8">
-            {breakpoint}
-            <Expression
-                program=program
-                expression=expression />
-        </li>
-    }
-}
-
-#[component]
-pub fn Breakpoint(
-    program: ReadSignal<Option<Program>>,
-    address: InstructionAddress,
-    events: EventsTx,
-) -> impl IntoView {
-    let class = move || {
-        let program = program.get()?;
-
-        let breakpoint_color = if program.breakpoint_at(&address) {
-            "text-green-600"
-        } else {
-            "text-green-300"
-        };
-
-        Some(format!("mr-1 {breakpoint_color}"))
-    };
-
-    let data_address = move || address.to_usize();
-
-    let toggle_breakpoint = move |event: MouseEvent| {
-        let event_target = event.target().unwrap();
-        let element = event_target.dyn_ref::<HtmlSpanElement>().unwrap();
-
-        let address = element
-            .get_attribute("data-address")
-            .unwrap()
-            .parse()
-            .unwrap();
-
-        leptos::spawn_local(send_event(
-            DebugEvent::ToggleBreakpoint { address },
-            events.clone(),
-        ));
-    };
-
-    // It would be nicer to have the click handler on the enclosing element, to
-    // make it less finicky for the user. But for some reason, I'm getting a
-    // reference to the window on `event.current_target()`, so I have to rely on
-    // `event.target()` to find the metadata. And that means, I can't have
-    // events coming from multiple elements.
-    //
-    // There are probably better ways to solve this problem, but for now, this
-    // is fine, if unfortunate.
-    view! {
-        <span
-            class=class
-            data-address=data_address
-            on:click=toggle_breakpoint>
-            {'⦿'}
-        </span>
     }
 }
 
