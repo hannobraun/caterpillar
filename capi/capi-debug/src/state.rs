db1c378411ad015464153fc7b41e7cdf8752278a
Hanno Braun <hello@hannobraun.com>
2024-05-14 20:05:24 +0200

    Use `message` field to transport message

--- capi/capi-debug/src/state.rs
+++ capi/capi-debug/src/state.rs
@@ -11,25 +11,37 @@ impl ExecutionContext {
         program: ReadSignal<Option<Program>>,
     ) -> Result<Self, &'static str> {
         let Some(program) = program.get() else {
-            return Err("No program available.");
+            return Ok(Self {
+                function: None,
+                message: Some("No program available."),
+            });
         };
 
         let (_effect, address) = match &program.state {
             ProgramState::Running => {
-                return Err("Program is running.");
+                return Ok(Self {
+                    function: None,
+                    message: Some("Program is running."),
+                });
             }
             ProgramState::Finished => {
-                return Err("Program has finished running.");
+                return Ok(Self {
+                    function: None,
+                    message: Some("Program has finished running."),
+                });
             }
             ProgramState::Effect { effect, address } => (effect, address),
         };
 
         let Some(location) = program.source_map.address_to_location(address)
         else {
-            return Err(
-                "Program is stopped at instruction with no associated source \
-                location.",
-            );
+            return Ok(Self {
+                function: None,
+                message: Some(
+                    "Program is stopped at instruction with no associated \
+                    source location.",
+                ),
+            });
         };
 
         let function = program
@@ -39,10 +51,13 @@ impl ExecutionContext {
             .find(|function| function.name == location.function())
             .cloned();
         let Some(function) = function else {
-            return Err(
-                "Program stopped at unknown function. This is most likely a \
-                bug in Caterpillar.",
-            );
+            return Ok(Self {
+                function: None,
+                message: Some(
+                    "Program stopped at unknown function. This is most likely \
+                    a bug in Caterpillar.",
+                ),
+            });
         };
 
         Ok(Self {
