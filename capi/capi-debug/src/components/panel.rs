9673d452aa79688066c4ec86bbd6123b2ad47eeb
Hanno Braun <hello@hannobraun.com>
2024-05-14 22:20:53 +0200

    Refactor to prepare for follow-on change

--- capi/capi-debug/src/components/panel.rs
+++ capi/capi-debug/src/components/panel.rs
@@ -2,8 +2,10 @@ use leptos::{component, view, Children, IntoView};
 
 #[component]
 pub fn Panel(children: Children) -> impl IntoView {
+    let class = "mx-1 my-3 border p-1 relative";
+
     view! {
-        <div class="mx-1 my-3 border p-1 relative">
+        <div class=class>
             {children()}
         </div>
     }
