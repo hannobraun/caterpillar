aa17ee7784a37595900b3c1ca0fa8258a772b2c7
Hanno Braun <hello@hannobraun.com>
2024-05-14 12:18:58 +0200

    Refactor

--- capi/capi-runtime/src/breakpoints.rs
+++ capi/capi-runtime/src/breakpoints.rs
@@ -10,12 +10,12 @@ pub struct Breakpoints {
 }
 
 impl Breakpoints {
-    pub fn durable_breakpoint_at(&self, address: &InstructionAddress) -> bool {
-        self.durable.get(address) == Some(&true)
-    }
-
     pub fn toggle_durable_at(&mut self, address: InstructionAddress) {
         let breakpoint = self.durable.entry(address).or_insert(false);
         *breakpoint = !*breakpoint;
+    }
+
+    pub fn durable_breakpoint_at(&self, address: &InstructionAddress) -> bool {
+        self.durable.get(address) == Some(&true)
     }
 }
