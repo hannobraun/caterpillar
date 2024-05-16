f72e3f38557f0d5f166f8436e75041fcaf63c32d
Hanno Braun <hello@hannobraun.com>
2024-05-16 12:04:11 +0200

    Add `Instructions`

--- capi/capi-runtime/src/code.rs
+++ capi/capi-runtime/src/code.rs
@@ -7,7 +7,7 @@ use super::{compiler::Instruction, symbols::Symbols};
 )]
 pub struct Code {
     pub symbols: Symbols,
-    pub instructions: Vec<Instruction>,
+    pub instructions: Instructions,
 }
 
 impl Code {
@@ -35,6 +35,8 @@ impl fmt::Display for Code {
         Ok(())
     }
 }
+
+pub type Instructions = Vec<Instruction>;
 
 #[derive(
     Copy,
