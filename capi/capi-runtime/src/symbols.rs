94a3f80cddb2ab42f69bf44b3a7d07a861281bd0
Hanno Braun <hello@hannobraun.com>
2024-05-16 12:28:47 +0200

    Add `Symbols::resolve_address`

--- capi/capi-runtime/src/symbols.rs
+++ capi/capi-runtime/src/symbols.rs
@@ -6,6 +6,7 @@ use crate::InstructionAddress;
     Clone, Debug, Eq, PartialEq, Default, serde::Deserialize, serde::Serialize,
 )]
 pub struct Symbols {
+    address_to_name: BTreeMap<InstructionAddress, String>,
     name_to_address: BTreeMap<String, InstructionAddress>,
 }
 impl Symbols {
@@ -14,7 +15,15 @@ impl Symbols {
     }
 
     pub fn define(&mut self, name: String, address: InstructionAddress) {
+        self.address_to_name.insert(address, name.clone());
         self.name_to_address.insert(name, address);
+    }
+
+    pub fn resolve_address(
+        &self,
+        address: &InstructionAddress,
+    ) -> Option<&str> {
+        self.address_to_name.get(address).map(|s| s.as_str())
     }
 
     pub fn resolve_name(&self, name: &str) -> InstructionAddress {
