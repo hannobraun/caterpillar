863252919866aba574dbbcbde56d4372ec30e7c0
Hanno Braun <hello@hannobraun.com>
2024-05-17 22:56:17 +0200

    Refactor to prepare for follow-on change

--- capi/capi-runtime/src/debugger/expression.rs
+++ capi/capi-runtime/src/debugger/expression.rs
@@ -6,10 +6,9 @@ pub struct Expression {
 
 impl Expression {
     pub fn new(expression: &crate::Expression, program: &Program) -> Self {
-        Self {
-            address: program
-                .source_map
-                .location_to_address(&expression.location),
-        }
+        let address =
+            program.source_map.location_to_address(&expression.location);
+
+        Self { address }
     }
 }
