f7467eb076f5c6489fb3dbddf231fcdb55129dfe
Hanno Braun <hello@hannobraun.com>
2024-05-25 00:16:54 +0200

    Remove outdated comment

--- capi/capi/src/runner.rs
+++ capi/capi/src/runner.rs
@@ -76,15 +76,6 @@ impl Runner {
             while let Some(event) =
                 event.take().or_else(|| self.events.try_recv().ok())
             {
-                // This doesn't work so well. This receive loop was moved here,
-                // so we can have some control over the program from the
-                // debugger, while it is stuck in an endless loop.
-                //
-                // And this works somewhat. We can send events. But unless those
-                // events result in the program to stop running, we won't see
-                // any indication of them being received in the debugger, as the
-                // program isn't sent when it's running.
-
                 match event {
                     DebugEvent::Continue => {
                         if let Some(ProgramEffect {
