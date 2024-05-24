c875ba124ff8d1d5b7b0900b686ab1c0f2835310
Hanno Braun <hello@hannobraun.com>
2024-05-24 21:37:12 +0200

    Redirect from `/daily` to daily thoughts

--- website/main.ts
+++ website/main.ts
@@ -1,3 +1,12 @@
-Deno.serve((_request) => {
+Deno.serve((request) => {
+    const url = new URL(request.url);
+
+    if (url.pathname == "/daily") {
+        return Response.redirect(
+            "https://github.com/hannobraun/caterpillar/blob/main/daily.md",
+            307,
+        );
+    }
+
     return new Response("Hello, world!");
 });
