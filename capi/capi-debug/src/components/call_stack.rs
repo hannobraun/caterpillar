89a26b516b0359c44ee8e62a9b1f60604f0ccd83
Hanno Braun <hello@hannobraun.com>
2024-05-28 11:38:20 +0200

    Prepare for follow-on change

--- capi/capi-debug/src/components/call_stack.rs
+++ capi/capi-debug/src/components/call_stack.rs
@@ -6,15 +6,14 @@ use crate::components::panel::Panel;
 #[component]
 pub fn CallStack(program: ReadSignal<Option<Program>>) -> impl IntoView {
     let addresses = move || {
-        let program = program.get()?;
-
         let view = program
+            .get()?
             .evaluator
             .call_stack
             .into_iter()
             .filter_map(|address| {
                 let location =
-                    program.source_map.address_to_location(&address)?;
+                    program.get()?.source_map.address_to_location(&address)?;
 
                 Some(view! {
                     <li>{format!("{location:?}")}</li>
