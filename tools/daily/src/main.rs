e287152941c3087be729a496a24d8a7a75319f41
Hanno Braun <hello@hannobraun.com>
2024-05-28 16:18:26 +0200

    Fail, if daily file already exists

--- tools/daily/src/main.rs
+++ tools/daily/src/main.rs
@@ -6,7 +6,7 @@ fn main() -> anyhow::Result<()> {
     let date = Local::now().format("%Y-%m-%d");
     let path = format!("website/daily/{date}.md");
 
-    File::create(&path)?;
+    File::create_new(&path)?;
     println!("Created {path}");
 
     Ok(())
