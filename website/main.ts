065664dd62350d519f00690b59fb879aab5e035d
Hanno Braun <hello@hannobraun.com>
2024-05-28 20:50:32 +0200

    Prepare for follow-on change

--- website/main.ts
+++ website/main.ts
@@ -14,15 +14,16 @@ Deno.serve((request) => {
             307,
         );
     }
-    if (url.pathname == "/daily") {
-        return Response.redirect(
-            "https://github.com/hannobraun/caterpillar/blob/main/daily.md",
-            307,
-        );
-    }
     if (url.pathname == "/daily/") {
         return Response.redirect(
             `${url.origin}/daily`,
+            307,
+        );
+    }
+
+    if (url.pathname == "/daily") {
+        return Response.redirect(
+            "https://github.com/hannobraun/caterpillar/blob/main/daily.md",
             307,
         );
     }
