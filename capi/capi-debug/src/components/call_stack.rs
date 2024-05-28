d87abc2df216dcce824dd3597cf7e9cf75cdda30
Hanno Braun <hello@hannobraun.com>
2024-05-28 11:41:31 +0200

    Render full functions in call stack

--- capi/capi-debug/src/components/call_stack.rs
+++ capi/capi-debug/src/components/call_stack.rs
@@ -1,15 +1,16 @@
 use capi_runtime::Program;
 use leptos::{component, view, CollectView, IntoView, ReadSignal, SignalGet};
 
-use crate::{client::EventsTx, components::panel::Panel};
+use crate::{
+    client::EventsTx,
+    components::{function::Function, panel::Panel},
+};
 
 #[component]
 pub fn CallStack(
     program: ReadSignal<Option<Program>>,
     events: EventsTx,
 ) -> impl IntoView {
-    let _ = events;
-
     let addresses = move || {
         let view = program
             .get()?
@@ -19,9 +20,17 @@ pub fn CallStack(
             .filter_map(|address| {
                 let location =
                     program.get()?.source_map.address_to_location(&address)?;
+                let function = program
+                    .get()?
+                    .functions
+                    .get_from_location(location)
+                    .cloned()?;
 
                 Some(view! {
-                    <li>{format!("{location:?}")}</li>
+                    <Function
+                        program=program
+                        function=function
+                        events=events.clone() />
                 })
             })
             .collect_view();
