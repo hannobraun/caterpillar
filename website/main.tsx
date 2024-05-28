5ea9a4442f74c7c0417d797bd5d8c520bd74743f
Hanno Braun <hello@hannobraun.com>
2024-05-28 23:05:02 +0200

    Style `/daily`

--- website/main.tsx
+++ website/main.tsx
@@ -40,7 +40,7 @@ Deno.serve(async (request) => {
         for (const date of dates) {
             const link = `/daily/${date}`;
             entries.push(
-                <li>
+                <li class="my-4 font-bold text-lg">
                     <a href={link}>
                         {date}
                     </a>
@@ -48,14 +48,48 @@ Deno.serve(async (request) => {
             );
         }
 
+        const css = `
+            html {
+                font-family: sans-serif;
+            }
+            a {
+                color: #0000ff;
+            }
+            ol {
+                list-style-type: none;
+
+                margin: 0;
+                padding: 0;
+            }
+
+            .font-bold {
+                font-weight: 700;
+            }
+            .mx-auto {
+                margin-left: auto;
+                margin-right: auto;
+            }
+            .my-4 {
+                margin-top: 1rem;
+                margin-bottom: 1rem;
+            }
+            .text-lg {
+                font-size: 1.125rem;
+            }
+            .w-fit {
+                width: fit-content;
+            }
+        `;
+
         const page = (
             <>
                 {"<!doctype html>"}
                 <html>
                     <head>
                         <title>Daily Thoughts - Caterpillar</title>
+                        <style>{css}</style>
                     </head>
-                    <body>
+                    <body class="w-fit mx-auto">
                         <ol>{entries}</ol>
                     </body>
                 </html>
