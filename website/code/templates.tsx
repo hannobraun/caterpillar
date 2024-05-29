8132c42665575ea2266e9588d2e57a28d89060d1
Hanno Braun <hello@hannobraun.com>
2024-05-29 18:49:28 +0200

    Move CSS to `templates`

--- website/code/templates.tsx
+++ website/code/templates.tsx
@@ -9,3 +9,39 @@ export const dailyThoughtItem = (date: string) => {
         </li>
     );
 };
+
+export const css = `
+    html {
+        font-family: sans-serif;
+    }
+    a {
+        color: #0000ff;
+    }
+    ol {
+        list-style-type: none;
+
+        margin: 0;
+        padding: 0;
+    }
+
+    .font-bold {
+        font-weight: 700;
+    }
+    .m-8 {
+        margin: 2rem;
+    }
+    .max-w-xl {
+        width: 36rem;
+    }
+    .mx-auto {
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .my-4 {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+    .text-lg {
+        font-size: 1.125rem;
+    }
+`;
