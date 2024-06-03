bd840accc22ddfb7ba5b75ed0c1f50004e3bfafa
Hanno Braun <hello@hannobraun.com>
2024-06-03 23:44:34 +0200

    Move `SyntaxBuilder` to dedicated module

---
+++ capi/capi-runtime/src/syntax/builder.rs
@@ -0,0 +1,43 @@
+use crate::{Expression, ExpressionKind, Location, Value};
+
+#[derive(Debug)]
+pub struct SyntaxBuilder<'r> {
+    next_location: Location,
+    expressions: &'r mut Vec<Expression>,
+}
+
+impl<'r> SyntaxBuilder<'r> {
+    pub fn new(function: String, expressions: &'r mut Vec<Expression>) -> Self {
+        Self {
+            next_location: Location::first_in_function(function),
+            expressions,
+        }
+    }
+
+    pub fn b(
+        &mut self,
+        names: impl IntoIterator<Item = impl Into<String>>,
+    ) -> &mut Self {
+        self.push_expression(ExpressionKind::Binding {
+            names: names.into_iter().map(Into::into).collect(),
+        })
+    }
+
+    pub fn c(&mut self, text: &str) -> &mut Self {
+        self.push_expression(ExpressionKind::Comment { text: text.into() })
+    }
+
+    pub fn v(&mut self, value: impl Into<Value>) -> &mut Self {
+        self.push_expression(ExpressionKind::Value(value.into()))
+    }
+
+    pub fn w(&mut self, name: &str) -> &mut Self {
+        self.push_expression(ExpressionKind::Word { name: name.into() })
+    }
+
+    fn push_expression(&mut self, kind: ExpressionKind) -> &mut Self {
+        let location = self.next_location.increment();
+        self.expressions.push(Expression::new(kind, location));
+        self
+    }
+}
