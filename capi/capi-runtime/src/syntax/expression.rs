7ebd21740cb9a55d87cd0ccb6d2afb4bacf3d31c
Hanno Braun <hello@hannobraun.com>
2024-06-03 23:43:02 +0200

    Move `Expression` to dedicated module

---
+++ capi/capi-runtime/src/syntax/expression.rs
@@ -0,0 +1,40 @@
+use std::fmt;
+
+use crate::{Location, Value};
+
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
+pub struct Expression {
+    pub kind: ExpressionKind,
+    pub location: Location,
+}
+
+impl Expression {
+    pub fn new(kind: ExpressionKind, location: Location) -> Self {
+        Self { kind, location }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
+pub enum ExpressionKind {
+    Binding { names: Vec<String> },
+    Comment { text: String },
+    Value(Value),
+    Word { name: String },
+}
+
+impl fmt::Display for ExpressionKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ExpressionKind::Binding { names } => {
+                write!(f, "=>")?;
+                for name in names {
+                    write!(f, " {name}")?;
+                }
+                writeln!(f, " .")
+            }
+            ExpressionKind::Comment { text } => writeln!(f, "# {text}"),
+            ExpressionKind::Value(value) => write!(f, "{value}"),
+            ExpressionKind::Word { name } => write!(f, "{name}"),
+        }
+    }
+}
--- capi/capi-runtime/src/syntax/mod.rs
+++ capi/capi-runtime/src/syntax/mod.rs
@@ -1,8 +1,10 @@
+mod expression;
 mod location;
 
-pub use self::location::Location;
-
-use std::fmt;
+pub use self::{
+    expression::{Expression, ExpressionKind},
+    location::Location,
+};
 
 use crate::Value;
 
@@ -45,42 +47,5 @@ impl<'r> Syntax<'r> {
         let location = self.next_location.increment();
         self.expressions.push(Expression::new(kind, location));
         self
-    }
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
-pub struct Expression {
-    pub kind: ExpressionKind,
-    pub location: Location,
-}
-
-impl Expression {
-    pub fn new(kind: ExpressionKind, location: Location) -> Self {
-        Self { kind, location }
-    }
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
-pub enum ExpressionKind {
-    Binding { names: Vec<String> },
-    Comment { text: String },
-    Value(Value),
-    Word { name: String },
-}
-
-impl fmt::Display for ExpressionKind {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ExpressionKind::Binding { names } => {
-                write!(f, "=>")?;
-                for name in names {
-                    write!(f, " {name}")?;
-                }
-                writeln!(f, " .")
-            }
-            ExpressionKind::Comment { text } => writeln!(f, "# {text}"),
-            ExpressionKind::Value(value) => write!(f, "{value}"),
-            ExpressionKind::Word { name } => write!(f, "{name}"),
-        }
     }
 }
