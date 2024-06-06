f69ba42bb7132bd9d243ff217a49e8321c80992d
Hanno Braun <hello@hannobraun.com>
2024-06-06 20:59:53 +0200

    Prepare for follow-on change

--- capi-runtime/src/debugger/event.rs
+++ capi-runtime/src/debugger/event.rs
@@ -1,14 +1,14 @@
-use crate::runtime::InstructionAddress;
+use crate::runtime;
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 pub enum DebugEvent {
     Continue {
-        and_stop_at: Option<InstructionAddress>,
+        and_stop_at: Option<runtime::InstructionAddress>,
     },
     Reset,
     Step,
     Stop,
     ToggleBreakpoint {
-        address: InstructionAddress,
+        address: runtime::InstructionAddress,
     },
 }
