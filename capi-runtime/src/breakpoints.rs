09acd4116e567382596cebbb1cc475a33e37dca2
Hanno Braun <hello@hannobraun.com>
2024-06-06 20:52:27 +0200

    Prepare for follow-on change

--- capi-runtime/src/breakpoints.rs
+++ capi-runtime/src/breakpoints.rs
@@ -1,33 +1,36 @@
 use std::collections::BTreeSet;
 
-use crate::runtime::InstructionAddress;
+use crate::runtime;
 
 #[derive(
     Clone, Debug, Default, Eq, PartialEq, serde::Deserialize, serde::Serialize,
 )]
 pub struct Breakpoints {
-    durable: BTreeSet<InstructionAddress>,
-    ephemeral: BTreeSet<InstructionAddress>,
+    durable: BTreeSet<runtime::InstructionAddress>,
+    ephemeral: BTreeSet<runtime::InstructionAddress>,
 }
 
 impl Breakpoints {
-    pub fn toggle_durable_at(&mut self, address: InstructionAddress) {
+    pub fn toggle_durable_at(&mut self, address: runtime::InstructionAddress) {
         if self.durable.take(&address).is_none() {
             self.durable.insert(address);
         }
     }
 
-    pub fn set_ephemeral(&mut self, address: InstructionAddress) {
+    pub fn set_ephemeral(&mut self, address: runtime::InstructionAddress) {
         self.ephemeral.insert(address);
     }
 
-    pub fn durable_breakpoint_at(&self, address: &InstructionAddress) -> bool {
+    pub fn durable_breakpoint_at(
+        &self,
+        address: &runtime::InstructionAddress,
+    ) -> bool {
         self.durable.contains(address)
     }
 
     pub fn should_stop_at_and_clear_ephemeral(
         &mut self,
-        address: &InstructionAddress,
+        address: &runtime::InstructionAddress,
     ) -> bool {
         let ephemeral = self.ephemeral.take(address).is_some();
         ephemeral || self.durable_breakpoint_at(address)
