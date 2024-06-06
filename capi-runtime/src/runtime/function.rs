45470d64363950d9842efdba65e9347081c4e8cd
Hanno Braun <hello@hannobraun.com>
2024-06-06 22:36:28 +0200

    Prepare for follow-on change

--- capi-runtime/src/runtime/function.rs
+++ capi-runtime/src/runtime/function.rs
@@ -1,12 +1,12 @@
 use std::collections::VecDeque;
 
-use super::{Instruction, Location};
+use super::{instructions::InstructionIndex, Instruction, Location};
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct Function {
     pub name: String,
     pub arguments: Vec<String>,
-    pub instructions: VecDeque<(Location, Instruction)>,
+    pub instructions: VecDeque<(InstructionIndex, Instruction)>,
 }
 
 impl Function {
@@ -19,12 +19,27 @@ impl Function {
     }
 
     pub fn next_instruction(&self) -> Option<(Location, Instruction)> {
-        self.instructions.front().cloned()
+        self.instructions
+            .front()
+            .cloned()
+            .map(|(index, instruction)| {
+                let location = Location {
+                    function: self.name.clone(),
+                    index,
+                };
+                (location, instruction)
+            })
     }
 
     pub fn consume_next_instruction(
         &mut self,
     ) -> Option<(Location, Instruction)> {
-        self.instructions.pop_front()
+        self.instructions.pop_front().map(|(index, instruction)| {
+            let location = Location {
+                function: self.name.clone(),
+                index,
+            };
+            (location, instruction)
+        })
     }
 }
