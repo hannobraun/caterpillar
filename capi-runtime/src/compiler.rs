b131902cac8b415aabb9cd71ec49b86647e41105
Hanno Braun <hello@hannobraun.com>
2024-06-06 22:00:24 +0200

    Update variable name

--- capi-runtime/src/compiler.rs
+++ capi-runtime/src/compiler.rs
@@ -107,10 +107,11 @@ impl Compiler<'_> {
         syntax_location: syntax::Location,
         output: &mut runtime::Function,
     ) {
-        let address = self.code.instructions.push(function, instruction);
+        let runtime_location =
+            self.code.instructions.push(function, instruction);
         self.source_map
-            .define_mapping(address.clone(), syntax_location);
-        output.instructions.push_back(address);
+            .define_mapping(runtime_location.clone(), syntax_location);
+        output.instructions.push_back(runtime_location);
     }
 }
 
