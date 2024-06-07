5437245ed45928790cdf158ecad6d368d73c42cd
Hanno Braun <hello@hannobraun.com>
2024-06-07 22:07:18 +0200

    Update import

--- capi-debug/src/main.rs
+++ capi-debug/src/main.rs
@@ -1,15 +1,13 @@
 mod client;
 mod ui;
 
-use crate::client::handle_server;
-
 fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug)
         .expect("Failed to initialize logging to console");
 
     let (set_program, events_rx) = ui::start();
-    leptos::spawn_local(handle_server(set_program, events_rx));
+    leptos::spawn_local(client::handle_server(set_program, events_rx));
 
     log::info!("Caterpillar initialized.");
 }
