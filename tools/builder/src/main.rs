1297034b4aab934d7a0f5d19db429661904eb154
Hanno Braun <hanno@braun-odw.eu>
2024-06-09 20:10:22 +0200

    Simplify watch event

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -5,7 +5,7 @@ async fn main() -> anyhow::Result<()> {
     let mut rx = watch::watch()?;
 
     while let Some(event) = rx.recv().await {
-        let _ = dbg!(event);
+        dbg!(event);
     }
 
     Ok(())
--- tools/builder/src/watch.rs
+++ tools/builder/src/watch.rs
@@ -3,13 +3,12 @@ use std::path::Path;
 use notify::RecursiveMode;
 use tokio::sync::mpsc;
 
-pub fn watch(
-) -> anyhow::Result<mpsc::UnboundedReceiver<notify::Result<notify::Event>>> {
+pub fn watch() -> anyhow::Result<mpsc::UnboundedReceiver<()>> {
     let (tx, rx) = mpsc::unbounded_channel();
 
     use notify::Watcher;
-    let mut watcher = notify::recommended_watcher(move |event| {
-        if tx.send(event).is_err() {
+    let mut watcher = notify::recommended_watcher(move |_| {
+        if tx.send(()).is_err() {
             // The other end has hung up. Not much we can do about that. The
             // thread this is running on will probably also end soon.
         }
