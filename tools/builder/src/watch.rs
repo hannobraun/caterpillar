96c675a6f03ed70999cd3804f0897e5257c1cfd2
Hanno Braun <hello@hannobraun.com>
2024-06-09 23:26:39 +0200

    Debounce changes

--- tools/builder/src/watch.rs
+++ tools/builder/src/watch.rs
@@ -1,11 +1,16 @@
 use std::{
+    future::Future,
     path::Path,
     pin::{pin, Pin},
     task::{Context, Poll},
+    time::Duration,
 };
 
 use notify::{RecursiveMode, Watcher as _};
-use tokio::sync::mpsc;
+use tokio::{
+    sync::mpsc,
+    time::{sleep, Sleep},
+};
 use tokio_stream::{wrappers::UnboundedReceiverStream, Stream};
 
 pub fn watch() -> anyhow::Result<Watcher> {
@@ -23,7 +28,10 @@ pub fn watch() -> anyhow::Result<Watcher> {
 
     Ok(Watcher {
         _watcher: watcher,
-        changes: DebouncedChanges { inner: changes },
+        changes: DebouncedChanges {
+            inner: changes,
+            delay: None,
+        },
     })
 }
 
@@ -34,6 +42,7 @@ pub struct Watcher {
 
 pub struct DebouncedChanges {
     inner: UnboundedReceiverStream<()>,
+    delay: Option<Pin<Box<Sleep>>>,
 }
 
 impl Stream for DebouncedChanges {
@@ -43,7 +52,61 @@ impl Stream for DebouncedChanges {
         mut self: Pin<&mut Self>,
         cx: &mut Context,
     ) -> Poll<Option<Self::Item>> {
-        pin!(&mut self.inner).poll_next(cx)
+        // Start by processing all pending changes. To correctly debounce the
+        // change stream, we need to do this first. Otherwise, if the delay is
+        // over and we trigger a debounced change event during this call, any
+        // unprocessed changes will trigger another debounced event, next time
+        // we get polled.
+        loop {
+            match pin!(&mut self.inner).poll_next(cx) {
+                Poll::Ready(Some(())) => {
+                    if self.delay.is_none() {
+                        // If there's a change, and there's no delay (meaning we
+                        // have no debounced change in the pipeline yet), we
+                        // start a delay, thereby triggering a debounced event.
+                        //
+                        // If there *is* a delay already, we don't need to do
+                        // anything. The change we read just now just gets
+                        // folded into the debounced event.
+                        self.delay =
+                            Some(Box::pin(sleep(Duration::from_millis(20))));
+                    }
+                }
+                Poll::Ready(None) => {
+                    // Looks like there are no more changes. Regardless of
+                    // whatever else is going on, we're done here, forever.
+                    return Poll::Ready(None);
+                }
+                Poll::Pending => {
+                    // We processed all pending changes. Let's move on.
+                    break;
+                }
+            }
+        }
+
+        // Okay, all pending changes got processed. Let's check if there's a
+        // debounced change in the pipeline.
+        if let Some(mut delay) = self.delay.take() {
+            // There indeed is a debounced event in the pipeline. See if it's
+            // ready.
+            match delay.as_mut().poll(cx) {
+                Poll::Ready(_) => {
+                    // Debounced delay is ready! We are already consuming the
+                    // delay here, so all that's left is to return the debounced
+                    // change.
+                    return Poll::Ready(Some(()));
+                }
+                Poll::Pending => {
+                    // The debounced change is not ready yet. Put the delay back
+                    // so we can check again later.
+                    self.delay = Some(delay);
+                }
+            }
+        }
+
+        // If we made it here, that means nothing we looked at here amounted to
+        // anything yet.
+        Poll::Pending
     }
 }
 
