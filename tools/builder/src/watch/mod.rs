7d0f86187a0fd55745e0e3567e89662b73b093e0
Hanno Braun <hello@hannobraun.com>
2024-06-10 00:09:32 +0200

    Move `Watcher` to dedicated module

--- tools/builder/src/watch/mod.rs
+++ tools/builder/src/watch/mod.rs
@@ -1,43 +1,8 @@
 mod debounce;
+mod watcher;
 
-use std::path::Path;
-
-use notify::{RecursiveMode, Watcher as _};
-use tokio::sync::mpsc;
+pub use self::watcher::Watcher;
 
 pub fn watch() -> anyhow::Result<Watcher> {
     Watcher::new()
-}
-
-pub struct Watcher {
-    _watcher: notify::RecommendedWatcher,
-    pub changes: debounce::DebouncedChanges,
-}
-
-impl Watcher {
-    fn new() -> anyhow::Result<Self> {
-        let (tx, rx) = mpsc::unbounded_channel();
-
-        // We interpret the intent behind calling this function as wanting to
-        // "load" the game code, as opposed to just wanting to watch it after
-        // possibly having or not having loaded it via other means.
-        //
-        // Therefore, we need to trigger an initial change.
-        tx.send(())?;
-
-        let mut watcher = notify::recommended_watcher(move |_| {
-            if tx.send(()).is_err() {
-                // The other end has hung up. Not much we can do about that. The
-                // thread this is running on will probably also end soon.
-            }
-        })?;
-        watcher.watch(Path::new("capi"), RecursiveMode::Recursive)?;
-
-        let changes = debounce::DebouncedChanges::new(rx);
-
-        Ok(Self {
-            _watcher: watcher,
-            changes,
-        })
-    }
 }
---
+++ tools/builder/src/watch/watcher.rs
@@ -0,0 +1,39 @@
+use std::path::Path;
+
+use notify::{RecursiveMode, Watcher as _};
+use tokio::sync::mpsc;
+
+use super::debounce::DebouncedChanges;
+
+pub struct Watcher {
+    _watcher: notify::RecommendedWatcher,
+    pub changes: DebouncedChanges,
+}
+
+impl Watcher {
+    pub fn new() -> anyhow::Result<Self> {
+        let (tx, rx) = mpsc::unbounded_channel();
+
+        // We interpret the intent behind calling this function as wanting to
+        // "load" the game code, as opposed to just wanting to watch it after
+        // possibly having or not having loaded it via other means.
+        //
+        // Therefore, we need to trigger an initial change.
+        tx.send(())?;
+
+        let mut watcher = notify::recommended_watcher(move |_| {
+            if tx.send(()).is_err() {
+                // The other end has hung up. Not much we can do about that. The
+                // thread this is running on will probably also end soon.
+            }
+        })?;
+        watcher.watch(Path::new("capi"), RecursiveMode::Recursive)?;
+
+        let changes = DebouncedChanges::new(rx);
+
+        Ok(Self {
+            _watcher: watcher,
+            changes,
+        })
+    }
+}
