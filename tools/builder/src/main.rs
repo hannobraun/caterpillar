13a650fca6609b36df1dce8fdc1d749316665013
Hanno Braun <hanno@braun-odw.eu>
2024-06-09 20:11:27 +0200

    Update module name

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -1,8 +1,8 @@
-mod watch;
+mod watcher;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut rx = watch::watch()?;
+    let mut rx = watcher::watch()?;
 
     while let Some(event) = rx.recv().await {
         dbg!(event);
