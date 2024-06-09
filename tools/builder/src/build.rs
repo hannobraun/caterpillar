150ffbe839b8c7fa2d727dea9980f45bbff64d9f
Hanno Braun <hello@hannobraun.com>
2024-06-10 00:49:36 +0200

    Rebuild on change

--- tools/builder/src/build.rs
+++ tools/builder/src/build.rs
@@ -1,16 +1,22 @@
-use tokio::process::Command;
+use tokio::process::{Child, Command};
 use tokio_stream::{Stream, StreamExt};
 
 pub async fn build(
     mut changes: impl Stream<Item = ()> + Unpin,
 ) -> anyhow::Result<()> {
+    let mut trunk_process: Option<Child> = None;
+
     while let Some(()) = changes.next().await {
-        Command::new("trunk")
+        let new_process = Command::new("trunk")
             .arg("serve")
             .args(["--ignore", "."])
-            .spawn()?
-            .wait()
-            .await?;
+            .spawn()?;
+
+        if let Some(mut old_process) = trunk_process.take() {
+            old_process.kill().await?;
+        }
+
+        trunk_process = Some(new_process);
     }
 
     Ok(())
