5b60473f076e30894451b3f564e538dd12851935
Hanno Braun <hello@hannobraun.com>
2024-06-10 00:48:31 +0200

    Ignore changes to `capi/dist`

--- tools/builder/src/watch/watcher.rs
+++ tools/builder/src/watch/watcher.rs
@@ -1,5 +1,9 @@
-use std::path::Path;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
+use anyhow::Context;
 use notify::{RecursiveMode, Watcher as _};
 use tokio::sync::mpsc;
 use tracing::error;
@@ -23,13 +27,25 @@ impl Watcher {
         tx.send(())?;
 
         let mut watcher = notify::recommended_watcher(move |event| {
-            let _ = match event {
+            let event: notify::Event = match event {
                 Ok(event) => event,
                 Err(err) => {
                     error!("Error watching for changes: {err}");
                     return;
                 }
             };
+
+            match ignore_event(event.paths) {
+                Ok(ignore) => {
+                    if ignore {
+                        return;
+                    }
+                }
+                Err(err) => {
+                    error!("Error while filtering path: {err}");
+                    return;
+                }
+            }
 
             if tx.send(()).is_err() {
                 // The other end has hung up. Not much we can do about that. The
@@ -46,4 +62,29 @@ impl Watcher {
             changes,
         })
     }
+}
+
+fn ignore_event(paths: Vec<PathBuf>) -> anyhow::Result<bool> {
+    let current_dir = env::current_dir()?
+        .canonicalize()
+        .context("Canonicalize current directory")?;
+
+    let mut ignore_all = true;
+    for path in paths {
+        let path = match path.canonicalize() {
+            Ok(path) => path,
+            Err(_) => {
+                // This happens all the time, if the path doesn't point to a
+                // valid file or directory. This is normal, as a removed file is
+                // an event, for example.
+                continue;
+            }
+        };
+
+        if let Ok(path) = path.strip_prefix(&current_dir) {
+            ignore_all &= path.starts_with("capi/dist");
+        }
+    }
+
+    Ok(ignore_all)
 }
