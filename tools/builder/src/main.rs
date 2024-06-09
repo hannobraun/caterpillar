2773cad5770b0ece5fc9d0cc8792ae2f0a7469fd
Hanno Braun <hello@hannobraun.com>
2024-06-10 00:10:11 +0200

    Inline redundant function

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -3,7 +3,7 @@ mod watch;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let watcher = watch::watch()?;
+    let watcher = watch::Watcher::new()?;
     build::build(watcher.changes).await?;
 
     Ok(())
--- tools/builder/src/watch/mod.rs
+++ tools/builder/src/watch/mod.rs
@@ -2,7 +2,3 @@ mod debounce;
 mod watcher;
 
 pub use self::watcher::Watcher;
-
-pub fn watch() -> anyhow::Result<Watcher> {
-    Watcher::new()
-}
