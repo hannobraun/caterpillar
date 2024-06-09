ba8b10a5d73aaba1dc2cb860982254cb2e6eb4dc
Hanno Braun <hanno@braun-odw.eu>
2024-06-09 20:13:20 +0200

    Clean up import

--- tools/builder/src/watcher.rs
+++ tools/builder/src/watcher.rs
@@ -1,12 +1,11 @@
 use std::path::Path;
 
-use notify::RecursiveMode;
+use notify::{RecursiveMode, Watcher};
 use tokio::sync::mpsc;
 
 pub fn watch() -> anyhow::Result<mpsc::UnboundedReceiver<()>> {
     let (tx, rx) = mpsc::unbounded_channel();
 
-    use notify::Watcher;
     let mut watcher = notify::recommended_watcher(move |_| {
         if tx.send(()).is_err() {
             // The other end has hung up. Not much we can do about that. The
