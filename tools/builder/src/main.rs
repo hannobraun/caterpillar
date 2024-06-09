c2f948865393138a75fbb8b5a7fa2e9d9a3401d1
Hanno Braun <hello@hannobraun.com>
2024-06-10 00:23:09 +0200

    Initialize logging

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -3,6 +3,8 @@ mod watch;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
     let watcher = watch::Watcher::new()?;
     build::build(watcher.changes).await?;
 
