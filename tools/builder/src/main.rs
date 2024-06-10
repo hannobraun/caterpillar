98ecf0dd6d02927e14b73dfde0e2ef26b730927a
Hanno Braun <hello@hannobraun.com>
2024-06-10 14:28:30 +0200

    Prepare for follow-on change

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -7,8 +7,8 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().init();
 
     let watcher = watch::Watcher::new()?;
-    serve::start(watcher.changes()).await?;
     let mut updates = build::start(watcher.changes()).await;
+    serve::start(watcher.changes()).await?;
 
     while let Ok(update) = updates.changed().await {
         dbg!(update);
