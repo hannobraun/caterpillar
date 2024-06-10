cd5ae757e1e8d3db7777f754cc58a41328b826d3
Hanno Braun <hello@hannobraun.com>
2024-06-10 12:49:04 +0200

    Simplify debouncer

--- tools/builder/src/watch/debounce.rs
+++ tools/builder/src/watch/debounce.rs
@@ -1,96 +1,43 @@
-use std::{
-    future::Future,
-    pin::{pin, Pin},
-    task::{Context, Poll},
-    time::Duration,
-};
+use std::time::Duration;
 
 use tokio::{
-    sync::mpsc,
-    time::{sleep, Sleep},
+    sync::{mpsc, watch},
+    time::sleep,
 };
-use tokio_stream::{Stream, StreamExt};
 
 pub struct DebouncedChanges {
-    changes: mpsc::UnboundedReceiver<()>,
-    delay: Option<Pin<Box<Sleep>>>,
+    changes: watch::Receiver<()>,
 }
 
 impl DebouncedChanges {
     pub fn new(changes: mpsc::UnboundedReceiver<()>) -> Self {
-        Self {
-            changes,
-            delay: None,
-        }
+        let (tx, rx) = watch::channel(());
+        tokio::spawn(debounce(changes, tx));
+
+        Self { changes: rx }
     }
 
     pub async fn wait_for_change(&mut self) -> bool {
-        self.next().await.is_some()
+        self.changes.changed().await.is_ok()
     }
 }
 
-impl Stream for DebouncedChanges {
-    type Item = ();
-
-    fn poll_next(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context,
-    ) -> Poll<Option<Self::Item>> {
-        // Start by processing all pending changes. To correctly debounce the
-        // change stream, we need to do this first. Otherwise, if the delay is
-        // over and we trigger a debounced change event during this call, any
-        // unprocessed changes will trigger another debounced event, next time
-        // we get polled.
-        let mut change = false;
-        loop {
-            match pin!(self.changes.recv()).poll(cx) {
-                Poll::Ready(Some(())) => {
-                    change = true;
-                }
-                Poll::Ready(None) => {
-                    // Looks like there are no more changes. Regardless of
-                    // whatever else is going on, we're done here, forever.
-                    return Poll::Ready(None);
-                }
-                Poll::Pending => {
-                    // We processed all pending changes. Let's move on.
-                    break;
-                }
-            }
+async fn debounce(mut rx: mpsc::UnboundedReceiver<()>, tx: watch::Sender<()>) {
+    loop {
+        if rx.recv().await.is_none() {
+            // The other end has hung up. This means we're done here too.
+            break;
         }
 
-        if change && self.delay.is_none() {
-            // If there's a change, and there's no delay (meaning we have no
-            // debounced change in the pipeline yet), we start a delay, thereby
-            // triggering a debounced event.
-            //
-            // If there *is* a delay already, we don't need to do anything. The
-            // change we read just now gets folded into the debounced event.
-            self.delay = Some(Box::pin(sleep(Duration::from_millis(20))));
+        // We have a change! Wait for a bit, before passing it on.
+        sleep(Duration::from_millis(20)).await;
+        if tx.send(()).is_err() {
+            // The other end has hung up. This means we're done here too.
+            break;
         }
 
-        // Okay, all pending changes got processed. Let's check if there's a
-        // debounced change in the pipeline.
-        if let Some(mut delay) = self.delay.take() {
-            // There indeed is a debounced event in the pipeline. See if it's
-            // ready.
-            match delay.as_mut().poll(cx) {
-                Poll::Ready(_) => {
-                    // Debounced delay is ready! We are already consuming the
-                    // delay here, so all that's left is to return the debounced
-                    // change.
-                    return Poll::Ready(Some(()));
-                }
-                Poll::Pending => {
-                    // The debounced change is not ready yet. Put the delay back
-                    // so we can check again later.
-                    self.delay = Some(delay);
-                }
-            }
-        }
-
-        // If we made it here, that means nothing we looked at here amounted to
-        // anything yet.
-        Poll::Pending
+        // We also need to throw away any changes that might or might not have
+        // arrived in the meantime, or we haven't actually debounced anything.
+        while rx.try_recv().is_ok() {}
     }
 }
