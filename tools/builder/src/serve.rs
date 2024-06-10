15837249230ec080be5d4320293f6482a567010b
Hanno Braun <hello@hannobraun.com>
2024-06-10 13:17:33 +0200

    Fix builder not building

--- tools/builder/src/serve.rs
+++ tools/builder/src/serve.rs
@@ -1,11 +1,13 @@
+use std::future::IntoFuture;
+
 use axum::{routing::get, Router};
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, task};
 
 pub async fn serve() -> anyhow::Result<()> {
     let router = Router::new().route("/changes", get(serve_changes));
     let listener = TcpListener::bind("localhost:34480").await?;
 
-    axum::serve(listener, router).await?;
+    task::spawn(axum::serve(listener, router).into_future());
 
     Ok(())
 }
