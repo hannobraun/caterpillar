0f7466a060d252bf4655cec2d9753fa790edbee0
Hanno Braun <hello@hannobraun.com>
2024-06-10 14:18:41 +0200

    Improve error handling

--- tools/builder/src/serve.rs
+++ tools/builder/src/serve.rs
@@ -1,5 +1,8 @@
+use std::process;
+
 use axum::{extract::State, http::StatusCode, routing::get, Router};
 use tokio::{net::TcpListener, task};
+use tracing::error;
 
 use crate::watch::DebouncedChanges;
 
@@ -9,7 +12,12 @@ pub async fn start(changes: DebouncedChanges) -> anyhow::Result<()> {
         .with_state(changes);
     let listener = TcpListener::bind("localhost:34480").await?;
 
-    task::spawn(async { axum::serve(listener, router).await });
+    task::spawn(async {
+        if let Err(err) = axum::serve(listener, router).await {
+            error!("Error serving HTTP endpoints: {err}");
+            process::exit(1);
+        }
+    });
 
     Ok(())
 }
