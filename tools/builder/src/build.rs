714c6776c4064b4ad1d6d7e77d7b1d45e0c68595
Hanno Braun <hello@hannobraun.com>
2024-06-10 13:31:17 +0200

    Prepare for follow-on change

--- tools/builder/src/build.rs
+++ tools/builder/src/build.rs
@@ -6,17 +6,23 @@ pub async fn build(mut changes: DebouncedChanges) -> anyhow::Result<()> {
     let mut trunk_process: Option<Child> = None;
 
     while changes.wait_for_change().await {
-        let new_process = Command::new("trunk")
-            .arg("serve")
-            .args(["--ignore", "."])
-            .spawn()?;
-
-        if let Some(mut old_process) = trunk_process.take() {
-            old_process.kill().await?;
-        }
-
-        trunk_process = Some(new_process);
+        single_build(&mut trunk_process).await?;
     }
+
+    Ok(())
+}
+
+async fn single_build(trunk_process: &mut Option<Child>) -> anyhow::Result<()> {
+    let new_process = Command::new("trunk")
+        .arg("serve")
+        .args(["--ignore", "."])
+        .spawn()?;
+
+    if let Some(mut old_process) = trunk_process.take() {
+        old_process.kill().await?;
+    }
+
+    *trunk_process = Some(new_process);
 
     Ok(())
 }
