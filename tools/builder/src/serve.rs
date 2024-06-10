92e7191bad76fb386939723a87296df398974753
Hanno Braun <hello@hannobraun.com>
2024-06-10 22:18:03 +0200

    Take over static file serving

--- tools/builder/src/serve.rs
+++ tools/builder/src/serve.rs
@@ -1,8 +1,13 @@
-use std::process;
+use std::{path::PathBuf, process};
 
-use axum::{extract::State, http::StatusCode, routing::get, Router};
-use tokio::{net::TcpListener, sync::watch, task};
-use tower_http::services::ServeDir;
+use axum::{
+    extract::{Path, State},
+    http::{header, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use tokio::{fs::File, io::AsyncReadExt, net::TcpListener, sync::watch, task};
 use tracing::error;
 
 pub async fn start(updates: watch::Receiver<()>) -> anyhow::Result<()> {
@@ -10,7 +15,8 @@ pub async fn start(updates: watch::Receiver<()>) -> anyhow::Result<()> {
 
     let router = Router::new()
         .route("/updates", get(serve_updates))
-        .nest_service("/", ServeDir::new("capi/dist"))
+        .route("/", get(serve_index))
+        .route("/*path", get(serve_static))
         .with_state(updates);
     let listener = TcpListener::bind(address).await?;
 
@@ -37,4 +43,36 @@ async fn serve_updates(
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
+}
+
+async fn serve_index() -> impl IntoResponse {
+    file_response(PathBuf::from("index.html")).await
+}
+
+async fn serve_static(Path(path): Path<PathBuf>) -> impl IntoResponse {
+    file_response(path).await
+}
+
+async fn file_response(path: PathBuf) -> impl IntoResponse {
+    let path = PathBuf::from("capi/dist").join(path);
+
+    let content_type = match path.extension() {
+        Some(os_str) => match os_str.to_str() {
+            Some("html") => "text/html",
+            Some("js") => "application/javascript",
+            Some("wasm") => "application/wasm",
+            _ => "application/octet-stream",
+        },
+        _ => "application/octet-stream",
+    };
+
+    let mut data = Vec::new();
+    File::open(path)
+        .await
+        .unwrap()
+        .read_to_end(&mut data)
+        .await
+        .unwrap();
+
+    (StatusCode::OK, [(header::CONTENT_TYPE, content_type)], data)
 }
