769ff98aba04ccd6191798cfb0004565bb57c13b
Hanno Braun <hello@hannobraun.com>
2024-06-11 11:13:56 +0200

    Push keys into a buffer

--- capi/src/ffi.rs
+++ capi/src/ffi.rs
@@ -1,4 +1,12 @@
+use std::{collections::VecDeque, sync::Mutex};
+
+static STATE: Mutex<Option<VecDeque<u8>>> = Mutex::new(None);
+
 #[no_mangle]
 pub extern "C" fn on_key(code: u8) {
-    log::debug!("{code}");
+    let mut state = STATE.lock().unwrap();
+    let state = state.get_or_insert_with(Default::default);
+    state.push_back(code);
+
+    log::info!("{state:?}");
 }
