59156d074be883b3d1465f9ddd459e100667bce4
Hanno Braun <hello@hannobraun.com>
2024-06-11 11:14:10 +0200

    Rename argument to be more explicit

--- capi/src/ffi.rs
+++ capi/src/ffi.rs
@@ -3,10 +3,10 @@ use std::{collections::VecDeque, sync::Mutex};
 static STATE: Mutex<Option<VecDeque<u8>>> = Mutex::new(None);
 
 #[no_mangle]
-pub extern "C" fn on_key(code: u8) {
+pub extern "C" fn on_key(key_code: u8) {
     let mut state = STATE.lock().unwrap();
     let state = state.get_or_insert_with(Default::default);
-    state.push_back(code);
+    state.push_back(key_code);
 
     log::info!("{state:?}");
 }
