5d674193bcbf1114b5744046d7a668f5a4c032d7
Hanno Braun <hello@hannobraun.com>
2024-06-11 14:35:15 +0200

    Prepare for follow-on change

--- capi/src/state.rs
+++ capi/src/state.rs
@@ -19,19 +19,18 @@ impl Default for RuntimeState {
 
         let input = Input::default();
         let (updates_tx, updates_rx) = updates(&program);
-        let runner = {
-            let (events_tx, handle) = runner(program, updates_tx);
+        let (events_tx, handle) = runner(program, updates_tx);
 
-            Runner {
-                events_tx,
-                handle: Some(handle),
-            }
-        };
-
-        let set_program = ui::start(runner.events_tx.clone());
+        let set_program = ui::start(events_tx.clone());
         leptos::spawn_local(handle_updates(updates_rx, set_program));
 
-        Self { input, runner }
+        Self {
+            input,
+            runner: Runner {
+                events_tx,
+                handle: Some(handle),
+            },
+        }
     }
 }
 
