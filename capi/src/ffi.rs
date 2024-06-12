2f4f05532129b52cd92eb271b2e4c76771148f59
Hanno Braun <hello@hannobraun.com>
2024-06-12 20:37:53 +0200

    Dismantle dedicated program task

--- capi/src/ffi.rs
+++ capi/src/ffi.rs
@@ -1,6 +1,13 @@
 use std::sync::Mutex;
 
-use crate::state::RuntimeState;
+use rand::random;
+use tokio::sync::mpsc::error::TryRecvError;
+
+use crate::{
+    program::{ProgramEffect, ProgramEffectKind},
+    runtime::{BuiltinEffect, EvaluatorEffectKind, Value},
+    state::RuntimeState,
+};
 
 pub static STATE: StaticRuntimeState = StaticRuntimeState {
     inner: Mutex::new(None),
@@ -24,7 +31,92 @@ pub extern "C" fn on_frame() {
         return;
     };
 
-    state.on_frame.send(()).unwrap();
+    loop {
+        match state.events_rx.try_recv() {
+            Ok(event) => {
+                state.program.process_event(event);
+            }
+            Err(TryRecvError::Empty) => {
+                break;
+            }
+            Err(TryRecvError::Disconnected) => {
+                // The other end has hung up, which happens during
+                // shutdown. Shut down this task, too.
+                return;
+            }
+        }
+    }
+
+    while state.program.can_step() {
+        state.program.step();
+
+        if let Some(ProgramEffect {
+            kind:
+                ProgramEffectKind::Evaluator(EvaluatorEffectKind::Builtin(effect)),
+            ..
+        }) = state.program.effects.front()
+        {
+            match effect {
+                BuiltinEffect::Error(_) => {
+                    // Nothing needs to be done. With an unhandled
+                    // effect, the program won't continue running, and
+                    // the debugger will see the error and display it.
+                }
+                BuiltinEffect::Load { address } => {
+                    let address: usize = (*address).into();
+                    let value = state.program.memory.inner[address];
+                    state.program.push([value]);
+
+                    state.program.effects.pop_front();
+                }
+                BuiltinEffect::Store { address, value } => {
+                    let address: usize = (*address).into();
+                    state.program.memory.inner[address] = *value;
+
+                    state.program.effects.pop_front();
+                }
+                BuiltinEffect::SetTile { x, y, value } => {
+                    let x = *x;
+                    let y = *y;
+                    let value = *value;
+
+                    state.program.effects.pop_front();
+
+                    display.set_tile(
+                        x.into(),
+                        y.into(),
+                        value,
+                        &mut state.tiles,
+                    );
+                }
+                BuiltinEffect::SubmitFrame => {
+                    // This effect means that the game is done rendering. Let's
+                    // break out of this loop now, so we can do our part in that
+                    // and return control to the host.
+                    state.program.effects.pop_front();
+                    break;
+                }
+                BuiltinEffect::ReadInput => {
+                    let input = state
+                        .input
+                        .buffer
+                        .pop_front()
+                        .unwrap_or(0)
+                        .try_into()
+                        .unwrap();
+
+                    state.program.push([Value(input)]);
+                    state.program.effects.pop_front();
+                }
+                BuiltinEffect::ReadRandom => {
+                    state.program.push([Value(random())]);
+                    state.program.effects.pop_front();
+                }
+            }
+        }
+    }
+
+    state.updates_tx.send_if_relevant_change(&state.program);
 
     display.render(&state.tiles);
 }
--- capi/src/state.rs
+++ capi/src/state.rs
@@ -1,150 +1,36 @@
 use std::collections::VecDeque;
 
-use rand::random;
-use tokio::sync::mpsc::{self, error::TryRecvError};
+use tokio::sync::mpsc;
 
 use crate::{
+    debugger::DebugEvent,
     display::Display,
     ffi,
     games::{self, snake::snake},
-    program::{ProgramEffect, ProgramEffectKind},
-    runtime::{BuiltinEffect, EvaluatorEffectKind, Value},
+    program::Program,
     tiles::NUM_TILES,
     ui,
-    updates::updates,
+    updates::{updates, UpdatesTx},
 };
 
 pub struct RuntimeState {
+    pub program: Program,
     pub input: Input,
     pub on_frame: mpsc::UnboundedSender<()>,
     pub tiles: [u8; NUM_TILES],
     pub display: Option<Display>,
+    pub events_rx: mpsc::UnboundedReceiver<DebugEvent>,
+    pub updates_tx: UpdatesTx,
 }
 
 impl Default for RuntimeState {
     fn default() -> Self {
-        let mut program = games::build(snake);
+        let program = games::build(snake);
 
         let input = Input::default();
-        let (on_frame_tx, mut on_frame_rx) = mpsc::unbounded_channel();
-        let (mut updates_tx, updates_rx) = updates(&program);
-        let (events_tx, mut events_rx) = mpsc::unbounded_channel();
-
-        leptos::spawn_local(async move {
-            loop {
-                loop {
-                    match events_rx.try_recv() {
-                        Ok(event) => {
-                            program.process_event(event);
-                        }
-                        Err(TryRecvError::Empty) => {
-                            break;
-                        }
-                        Err(TryRecvError::Disconnected) => {
-                            // The other end has hung up, which happens during
-                            // shutdown. Shut down this task, too.
-                            return;
-                        }
-                    }
-                }
-
-                if !program.can_step() {
-                    // If the program won't step anyway, then there's no point
-                    // in busy-looping while nothing changes.
-                    //
-                    // Just wait until we receive an event from the client.
-                    let event = events_rx.recv().await.unwrap();
-                    program.process_event(event);
-                }
-
-                program.step();
-
-                if let Some(ProgramEffect {
-                    kind:
-                        ProgramEffectKind::Evaluator(EvaluatorEffectKind::Builtin(
-                            effect,
-                        )),
-                    ..
-                }) = program.effects.front()
-                {
-                    match effect {
-                        BuiltinEffect::Error(_) => {
-                            // Nothing needs to be done. With an unhandled
-                            // effect, the program won't continue running, and
-                            // the debugger will see the error and display it.
-                        }
-                        BuiltinEffect::Load { address } => {
-                            let address: usize = (*address).into();
-                            let value = program.memory.inner[address];
-                            program.push([value]);
-
-                            program.effects.pop_front();
-                        }
-                        BuiltinEffect::Store { address, value } => {
-                            let address: usize = (*address).into();
-                            program.memory.inner[address] = *value;
-
-                            program.effects.pop_front();
-                        }
-                        BuiltinEffect::SetTile { x, y, value } => {
-                            let x = *x;
-                            let y = *y;
-                            let value = *value;
-
-                            program.effects.pop_front();
-
-                            let mut state = ffi::STATE.inner.lock().unwrap();
-                            let state =
-                                state.get_or_insert_with(Default::default);
-
-                            let Some(display) = state.display.as_mut() else {
-                                // Display not initialized yet.
-                                continue;
-                            };
-
-                            display.set_tile(
-                                x.into(),
-                                y.into(),
-                                value,
-                                &mut state.tiles,
-                            );
-                        }
-                        BuiltinEffect::SubmitFrame => {
-                            // This effect serves as a synchronization point
-                            // between the program and the display code. Before
-                            // we continue running, we need to wait here, until
-                            // the display code has confirmed that we're ready
-                            // to continue.
-                            let () = on_frame_rx.recv().await.unwrap();
-
-                            program.effects.pop_front();
-                        }
-                        BuiltinEffect::ReadInput => {
-                            let mut state = ffi::STATE.inner.lock().unwrap();
-                            let state =
-                                state.get_or_insert_with(Default::default);
-
-                            let input = state
-                                .input
-                                .buffer
-                                .pop_front()
-                                .unwrap_or(0)
-                                .try_into()
-                                .unwrap();
-
-                            program.push([Value(input)]);
-                            program.effects.pop_front();
-                        }
-                        BuiltinEffect::ReadRandom => {
-                            program.push([Value(random())]);
-                            program.effects.pop_front();
-                        }
-                    }
-                }
-
-                updates_tx.send_if_relevant_change(&program);
-            }
-        });
+        let (on_frame_tx, _) = mpsc::unbounded_channel();
+        let (updates_tx, updates_rx) = updates(&program);
+        let (events_tx, events_rx) = mpsc::unbounded_channel();
 
         ui::start(updates_rx, events_tx);
 
@@ -160,10 +46,13 @@ impl Default for RuntimeState {
         });
 
         Self {
+            program,
             input,
             on_frame: on_frame_tx,
             tiles: [0; NUM_TILES],
             display: None,
+            events_rx,
+            updates_tx,
         }
     }
 }
