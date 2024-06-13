ac1b2a8a8eb787d4206daf0ce5e4a54a525d5000
Hanno Braun <hello@hannobraun.com>
2024-06-13 13:45:16 +0200

    Clean up imports

--- capi/src/games/build.rs
+++ capi/src/games/build.rs
@@ -1,14 +1,19 @@
-pub fn build(game: fn(&mut crate::syntax::Script)) -> crate::process::Process {
+use crate::{
+    compiler::compile, process::Process, runtime::Value, syntax::Script,
+    tiles::TILES_PER_AXIS,
+};
+
+pub fn build(game: fn(&mut Script)) -> Process {
     let mut script = crate::syntax::Script::default();
     game(&mut script);
-    let (code, source_map) = crate::compiler::compile(&script);
+    let (code, source_map) = compile(&script);
 
     let entry = code.functions.get("main").cloned().unwrap();
-    crate::process::Process::new(
+    Process::new(
         script.functions,
         source_map,
         code,
         entry,
-        vec![crate::runtime::Value(crate::tiles::TILES_PER_AXIS as i8); 2],
+        vec![Value(TILES_PER_AXIS as i8); 2],
     )
 }
