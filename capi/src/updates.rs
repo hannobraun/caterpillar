70c8c95ff8662baec30a0aa411eb8046feebe166
Hanno Braun <hello@hannobraun.com>
2024-06-13 13:22:50 +0200

    Update variable name

--- capi/src/updates.rs
+++ capi/src/updates.rs
@@ -23,12 +23,12 @@ pub struct UpdatesTx {
 
 impl UpdatesTx {
     pub fn send_if_relevant_change(&mut self, process: &Process) {
-        if let Some(program_at_client) = &self.process_at_client {
+        if let Some(process_at_client) = &self.process_at_client {
             // The client has previously received a program. We don't want to
             // saturate the connection with useless updates, so use that to
             // determine, if we should send an update.
 
-            if program_at_client.can_step() && process.can_step() {
+            if process_at_client.can_step() && process.can_step() {
                 // While the program is running, sending updates on every change
                 // would result in too many updates.
                 //
@@ -36,7 +36,7 @@ impl UpdatesTx {
                 // sending an update for.
 
                 let breakpoints_unchanged =
-                    program_at_client.breakpoints == process.breakpoints;
+                    process_at_client.breakpoints == process.breakpoints;
 
                 if breakpoints_unchanged {
                     return;
