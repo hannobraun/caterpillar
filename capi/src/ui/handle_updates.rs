5fc248e9b91f1e1bdab25e37618afb31b00d17ab
Hanno Braun <hello@hannobraun.com>
2024-06-14 13:41:49 +0200

    Move `handle_updates` to dedicated module

---
+++ capi/src/ui/handle_updates.rs
@@ -0,0 +1,15 @@
+pub async fn handle_updates(
+    mut updates: crate::updates::UpdatesRx,
+    set_process: leptos::WriteSignal<Option<crate::process::Process>>,
+) {
+    use leptos::SignalSet;
+
+    loop {
+        let process = match updates.changed().await {
+            Ok(()) => updates.borrow_and_update().clone(),
+            Err(err) => panic!("{err}"),
+        };
+
+        set_process.set(Some(process));
+    }
+}
--- capi/src/ui/mod.rs
+++ capi/src/ui/mod.rs
@@ -1,24 +1,10 @@
 mod components;
 mod events;
+mod handle_updates;
 mod start;
 
 pub use self::{
     events::{send_event, EventsTx},
+    handle_updates::handle_updates,
     start::start,
 };
-
-pub async fn handle_updates(
-    mut updates: crate::updates::UpdatesRx,
-    set_process: leptos::WriteSignal<Option<crate::process::Process>>,
-) {
-    use leptos::SignalSet;
-
-    loop {
-        let process = match updates.changed().await {
-            Ok(()) => updates.borrow_and_update().clone(),
-            Err(err) => panic!("{err}"),
-        };
-
-        set_process.set(Some(process));
-    }
-}
