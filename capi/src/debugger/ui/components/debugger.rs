d0e7f715fe5122223360a3d43816e04b2c66f1ea
Hanno Braun <hello@hannobraun.com>
2024-06-14 14:40:56 +0200

    Prepare for follow-on change

--- capi/src/debugger/ui/components/debugger.rs
+++ capi/src/debugger/ui/components/debugger.rs
@@ -20,24 +20,26 @@ pub fn Debugger(
     process: ReadSignal<Option<Process>>,
     events: EventsTx,
 ) -> impl IntoView {
-    let execution_context = create_memo(move |prev| {
-        ExecutionContext::from_process(prev, process.get())
-    });
+    move || {
+        let execution_context = create_memo(move |prev| {
+            ExecutionContext::from_process(prev, process.get())
+        });
 
-    view! {
-        <div>
-            <ControlPanel
-                events=events.clone() />
-            <CallStack
-                process=process
-                events=events.clone() />
-            <ExecutionContext
-                execution_context=execution_context
-                events=events.clone() />
-            <StackExplorer
-                process=process />
-            <MemoryExplorer
-                process=process />
-        </div>
+        view! {
+            <div>
+                <ControlPanel
+                    events=events.clone() />
+                <CallStack
+                    process=process
+                    events=events.clone() />
+                <ExecutionContext
+                    execution_context=execution_context
+                    events=events.clone() />
+                <StackExplorer
+                    process=process />
+                <MemoryExplorer
+                    process=process />
+            </div>
+        }
     }
 }
