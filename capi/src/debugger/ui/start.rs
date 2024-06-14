4fd1d7ce5946accfece0a2ed57bdfb9d9b64b4c3
Hanno Braun <hello@hannobraun.com>
2024-06-14 23:18:26 +0200

    Update variable name

--- capi/src/debugger/ui/start.rs
+++ capi/src/debugger/ui/start.rs
@@ -5,7 +5,7 @@ use crate::{debugger::ui::components::debugger::Debugger, updates::UpdatesRx};
 use super::{handle_updates, EventsTx};
 
 pub fn start(updates_rx: UpdatesRx, events_tx: EventsTx) {
-    let (process, set_process) = create_signal(None);
+    let (process, set_debugger) = create_signal(None);
 
     leptos::mount_to_body(move || {
         leptos::view! {
@@ -13,5 +13,5 @@ pub fn start(updates_rx: UpdatesRx, events_tx: EventsTx) {
         }
     });
 
-    leptos::spawn_local(handle_updates(updates_rx, set_process));
+    leptos::spawn_local(handle_updates(updates_rx, set_debugger));
 }
