a0fe397afe93bff151bdc67e82c2c2cf972712d9
Hanno Braun <hello@hannobraun.com>
2024-06-14 14:33:48 +0200

    Consolidate redundant code in variable

--- capi/src/debugger/ui/components/call_stack.rs
+++ capi/src/debugger/ui/components/call_stack.rs
@@ -17,22 +17,20 @@ pub fn CallStack(
     events: EventsTx,
 ) -> impl IntoView {
     let addresses = move || {
+        let process = process.get()?;
+
         let view = process
-            .get()?
             .evaluator
             .stack()
             .iter()
             .filter_map(|runtime_location| {
-                let syntax_location = process
-                    .get()?
-                    .source_map
-                    .runtime_to_syntax(&runtime_location);
+                let syntax_location =
+                    process.source_map.runtime_to_syntax(&runtime_location);
                 let function = process
-                    .get()?
                     .functions
                     .get_from_location(syntax_location)
                     .cloned()?;
-                let function = Function::new(function, &process.get()?);
+                let function = Function::new(function, &process);
 
                 Some(view! {
                     <Function
