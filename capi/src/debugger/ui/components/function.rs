774ae7fe648a0c0432b14e14ef3f96964905f497
Hanno Braun <hello@hannobraun.com>
2024-06-14 14:10:31 +0200

    Simplify `Expression` component

--- capi/src/debugger/ui/components/function.rs
+++ capi/src/debugger/ui/components/function.rs
@@ -52,72 +52,64 @@ pub fn Function(
 
 #[component]
 pub fn Expression(expression: Expression, events: EventsTx) -> impl IntoView {
-    move || {
-        // Without this line, this closure turns into an `FnOnce` instead of an
-        // `Fn`, and that's no longer an a `leptos::IntoView`.
-        let events = events.clone();
-        let expression = expression.clone();
+    let mut class_outer = String::from("py-1");
+    if expression.has_durable_breakpoint {
+        class_outer.push_str(" bg-blue-300");
+    }
 
-        let mut class_outer = String::from("py-1");
-        if expression.has_durable_breakpoint {
-            class_outer.push_str(" bg-blue-300");
-        }
-
-        let mut class_inner = String::from("px-0.5");
-        if expression.is_comment {
-            class_inner.push_str(" italic text-gray-500");
-        }
-        if let Some(effect) = &expression.effect {
-            match effect.kind {
-                EvaluatorEffectKind::Builtin(BuiltinEffect::Breakpoint) => {
-                    class_inner.push_str(" bg-green-300")
-                }
-                _ => class_inner.push_str(" bg-red-300"),
+    let mut class_inner = String::from("px-0.5");
+    if expression.is_comment {
+        class_inner.push_str(" italic text-gray-500");
+    }
+    if let Some(effect) = &expression.effect {
+        match effect.kind {
+            EvaluatorEffectKind::Builtin(BuiltinEffect::Breakpoint) => {
+                class_inner.push_str(" bg-green-300")
             }
+            _ => class_inner.push_str(" bg-red-300"),
         }
-        if expression.is_on_call_stack {
-            class_inner.push_str(" font-bold");
-        }
+    }
+    if expression.is_on_call_stack {
+        class_inner.push_str(" font-bold");
+    }
 
-        let data_location = expression
-            .location
-            .map(|location| ron::to_string(&location).unwrap());
+    let data_location = expression
+        .location
+        .map(|location| ron::to_string(&location).unwrap());
 
-        let error =
-            expression.effect.map(|effect| format!("{:?}", effect.kind));
+    let error = expression.effect.map(|effect| format!("{:?}", effect.kind));
 
-        let toggle_breakpoint = move |event: MouseEvent| {
-            let event_target = event.target().unwrap();
-            let element = event_target.dyn_ref::<HtmlSpanElement>().unwrap();
+    let toggle_breakpoint = move |event: MouseEvent| {
+        let event_target = event.target().unwrap();
+        let element = event_target.dyn_ref::<HtmlSpanElement>().unwrap();
 
-            let Some(location) = element.get_attribute("data-location") else {
-                // This happens, if the user clicks on a comment.
-                return;
-            };
-            let location = ron::from_str(&location).unwrap();
-
-            leptos::spawn_local(send_event(
-                DebugEvent::ToggleBreakpoint { location },
-                events.clone(),
-            ));
+        let Some(location) = element.get_attribute("data-location") else {
+            // This happens, if the user clicks on a comment.
+            return;
         };
+        let location = ron::from_str(&location).unwrap();
 
-        let expression = format!("{}", expression.kind);
+        leptos::spawn_local(send_event(
+            DebugEvent::ToggleBreakpoint { location },
+            events.clone(),
+        ));
+    };
 
-        Some(view! {
-            <span>
-                <span class=class_outer>
-                    <span
-                        class=class_inner
-                        data-location=data_location
-                        on:click=toggle_breakpoint>
-                        {expression}
-                    </span>
-                </span>
-                <span class="mx-2 font-bold text-red-800">
-                    {error}
+    let expression = format!("{}", expression.kind);
+
+    Some(view! {
+        <span>
+            <span class=class_outer>
+                <span
+                    class=class_inner
+                    data-location=data_location
+                    on:click=toggle_breakpoint>
+                    {expression}
                 </span>
             </span>
-        })
-    }
+            <span class="mx-2 font-bold text-red-800">
+                {error}
+            </span>
+        </span>
+    })
 }
