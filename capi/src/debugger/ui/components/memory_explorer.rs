ced514d96a6d56f6a1651f2879930b2c00bc7ca0
Hanno Braun <hello@hannobraun.com>
2024-06-14 15:39:14 +0200

    Simplify `MemoryExplorer`

--- capi/src/debugger/ui/components/memory_explorer.rs
+++ capi/src/debugger/ui/components/memory_explorer.rs
@@ -5,42 +5,32 @@ use crate::{debugger::ui::components::panel::Panel, process::Process};
 #[allow(unused_braces)] // working around a warning from the `view!` macro
 #[component]
 pub fn MemoryExplorer(process: Process) -> impl IntoView {
-    let memory = move || {
-        let process = process.clone();
+    let mut values = process.memory.inner.into_iter().peekable();
+    let values = values.by_ref();
 
-        let mut values = process.memory.inner.into_iter().peekable();
-        let values = values.by_ref();
+    let mut lines = Vec::new();
 
-        let mut lines = Vec::new();
+    while values.peek().is_some() {
+        let line = values.take(16).collect::<Vec<_>>();
+        lines.push(line);
+    }
 
-        while values.peek().is_some() {
-            let line = values.take(16).collect::<Vec<_>>();
-            lines.push(line);
-        }
-
-        let lines = lines
-            .into_iter()
-            .map(|line| {
-                view! {
-                    <Line line=line />
-                }
-            })
-            .collect_view();
-
-        let view = view! {
-            <Panel class="">
-                <p>"Memory:"</p>
-                <ol>
-                    {lines}
-                </ol>
-            </Panel>
-        };
-
-        Some(view)
-    };
+    let lines = lines
+        .into_iter()
+        .map(|line| {
+            view! {
+                <Line line=line />
+            }
+        })
+        .collect_view();
 
     view! {
-        {memory}
+        <Panel class="">
+            <p>"Memory:"</p>
+            <ol>
+                {lines}
+            </ol>
+        </Panel>
     }
 }
 
