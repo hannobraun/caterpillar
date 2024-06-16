2f87b4e0bcb000c07c8e7286d4c58f5f13ac7037
Hanno Braun <hanno@braun-odw.eu>
2024-06-16 09:51:12 +0200

    Prepare for follow-on change

--- capi/src/debugger/model/active_functions.rs
+++ capi/src/debugger/model/active_functions.rs
@@ -36,7 +36,7 @@ impl ActiveFunctions {
             .iter()
             .filter_map(|runtime_location| {
                 let syntax_location =
-                    process.source_map.runtime_to_syntax(&runtime_location);
+                    source_map.runtime_to_syntax(&runtime_location);
                 let function =
                     functions.get_from_location(syntax_location).cloned()?;
 
