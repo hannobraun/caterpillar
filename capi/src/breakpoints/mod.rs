1285cb8354e57ea3ebedb7dd0f88e237a087c3b6
Hanno Braun <hello@hannobraun.com>
2024-06-18 12:37:40 +0200

    Move `Breakpoints` to dedicated module

--- capi/src/breakpoints/mod.rs
+++ capi/src/breakpoints/mod.rs
@@ -1,43 +1,5 @@
+mod decider;
 mod event;
 mod state;
 
-pub use self::{event::Event, state::State};
-
-use crate::runtime;
-
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
-pub struct Breakpoints {
-    state: State,
-}
-
-impl Breakpoints {
-    pub fn state(&self) -> &State {
-        &self.state
-    }
-
-    pub fn set_durable(&mut self, location: runtime::Location) {
-        self.state.evolve(Event::SetDurable { location })
-    }
-
-    pub fn clear_durable(&mut self, location: runtime::Location) {
-        self.state.evolve(Event::ClearDurable { location })
-    }
-
-    pub fn set_ephemeral(&mut self, location: runtime::Location) {
-        self.state.evolve(Event::SetEphemeral { location })
-    }
-
-    pub fn should_stop_at_and_clear_ephemeral(
-        &mut self,
-        location: runtime::Location,
-    ) -> bool {
-        let durable_at_location = self.state.durable_at(&location);
-        let ephemeral_at_location = self.state.ephemeral_at(&location);
-
-        if ephemeral_at_location {
-            self.state.evolve(Event::ClearEphemeral { location });
-        }
-
-        ephemeral_at_location || durable_at_location
-    }
-}
+pub use self::{decider::Breakpoints, event::Event, state::State};
