ac58e297006b88fc819de57aca1d5706f4accb8c
Hanno Braun <hello@hannobraun.com>
2024-06-19 00:45:04 +0200

    Derive some traits for `process::Event`

--- capi/src/process/event.rs
+++ capi/src/process/event.rs
@@ -1,5 +1,6 @@
 use crate::runtime::{self, EvaluatorEffect};
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Event {
     Step { location: runtime::Location },
     TriggerEffect { effect: EvaluatorEffect },
