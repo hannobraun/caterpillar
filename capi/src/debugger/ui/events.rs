82b40df8c4c1909c495035b298d0da41a6c712d4
Hanno Braun <hello@hannobraun.com>
2024-06-21 22:33:06 +0200

    Update argument name

--- capi/src/debugger/ui/events.rs
+++ capi/src/debugger/ui/events.rs
@@ -5,8 +5,8 @@ use crate::debugger::model::DebugCommand;
 pub type CommandsRx = mpsc::UnboundedReceiver<DebugCommand>;
 pub type CommandsTx = mpsc::UnboundedSender<DebugCommand>;
 
-pub async fn send_event(command: DebugCommand, events: CommandsTx) {
-    if let Err(err) = events.send(command) {
+pub async fn send_event(command: DebugCommand, commands: CommandsTx) {
+    if let Err(err) = commands.send(command) {
         log::error!("Error sending event: {err}");
     }
 }
