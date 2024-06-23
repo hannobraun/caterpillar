2a534305be3b47a576427d9ef530334bef81d311
Hanno Braun <hanno@braun-odw.eu>
2024-06-23 11:53:19 +0200

    Clean up imports

--- crates/capi-process/src/breakpoints.rs
+++ crates/capi-process/src/breakpoints.rs
@@ -1,39 +1,39 @@
 use std::collections::BTreeSet;
 
-use crate::runtime;
+use crate::runtime::Location;
 
 #[derive(
     Clone, Debug, Default, Eq, PartialEq, serde::Deserialize, serde::Serialize,
 )]
 pub struct Breakpoints {
-    durable: BTreeSet<runtime::Location>,
-    ephemeral: BTreeSet<runtime::Location>,
+    durable: BTreeSet<Location>,
+    ephemeral: BTreeSet<Location>,
 }
 
 impl Breakpoints {
-    pub fn durable_at(&self, location: &runtime::Location) -> bool {
+    pub fn durable_at(&self, location: &Location) -> bool {
         self.durable.contains(location)
     }
 
-    pub fn ephemeral_at(&self, location: &runtime::Location) -> bool {
+    pub fn ephemeral_at(&self, location: &Location) -> bool {
         self.ephemeral.contains(location)
     }
 
-    pub fn set_durable(&mut self, location: runtime::Location) {
+    pub fn set_durable(&mut self, location: Location) {
         self.durable.insert(location);
     }
 
-    pub fn clear_durable(&mut self, location: runtime::Location) {
+    pub fn clear_durable(&mut self, location: Location) {
         self.durable.remove(&location);
     }
 
-    pub fn set_ephemeral(&mut self, location: runtime::Location) {
+    pub fn set_ephemeral(&mut self, location: Location) {
         self.ephemeral.insert(location);
     }
 
     pub fn should_stop_at_and_clear_ephemeral(
         &mut self,
-        location: runtime::Location,
+        location: Location,
     ) -> bool {
         let durable_at_location = self.durable_at(&location);
         let ephemeral_at_location = self.ephemeral_at(&location);
