bcd92b049fde3384920e4a9d0e881d527829a91e
Hanno Braun <hello@hannobraun.com>
2024-06-24 23:33:58 +0200

    Decrease dependency on `web-sys`

--- capi/capi/src/debugger/ui/components/function.rs
+++ capi/capi/src/debugger/ui/components/function.rs
@@ -1,7 +1,10 @@
 use capi_process::{BuiltinEffect, EvaluatorEffect};
 use capi_protocol::command::Command;
-use leptos::{component, ev::MouseEvent, view, CollectView, IntoView};
-use web_sys::{wasm_bindgen::JsCast, HtmlSpanElement};
+use leptos::{
+    component, ev::MouseEvent, view, web_sys::HtmlSpanElement, CollectView,
+    IntoView,
+};
+use web_sys::wasm_bindgen::JsCast;
 
 use crate::debugger::{
     model::{Expression, Function},
