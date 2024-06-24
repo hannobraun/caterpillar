61ce476991b49065f1f2693af54dff721e4f42f9
Hanno Braun <hello@hannobraun.com>
2024-06-25 00:07:33 +0200

    Prepare for follow-on change

--- tools/builder/src/watch/watcher.rs
+++ tools/builder/src/watch/watcher.rs
@@ -19,6 +19,7 @@ impl Watcher {
     pub fn new(path: &Path) -> anyhow::Result<Self> {
         let (tx, rx) = watch::channel(());
 
+        let ignore_path = "capi/runtime/dist";
         let mut watcher = notify::recommended_watcher(move |event| {
             let event: notify::Event = match event {
                 Ok(event) => event,
@@ -28,7 +29,7 @@ impl Watcher {
                 }
             };
 
-            match ignore_event(event.paths, "capi/runtime/dist") {
+            match ignore_event(event.paths, ignore_path) {
                 Ok(ignore) => {
                     if ignore {
                         return;
