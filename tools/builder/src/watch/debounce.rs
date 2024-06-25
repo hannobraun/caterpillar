f35acf7fc8e42c39339633e0f62a95dfa6214e8c
Hanno Braun <hello@hannobraun.com>
2024-06-25 12:37:55 +0200

    Prepare for follow-on change

--- tools/builder/src/watch/debounce.rs
+++ tools/builder/src/watch/debounce.rs
@@ -1,8 +1,15 @@
-use std::time::Duration;
+use std::{
+    collections::VecDeque,
+    future::{self, Future},
+    pin::{pin, Pin},
+    task::{Context, Poll},
+    time::Duration,
+};
 
 use tokio::{
+    select,
     sync::{mpsc, watch},
-    time::sleep,
+    time::{sleep, Sleep},
 };
 
 #[derive(Clone)]
@@ -27,21 +34,54 @@ async fn debounce(
     mut rx: mpsc::UnboundedReceiver<notify::Event>,
     tx: watch::Sender<()>,
 ) {
+    let mut timers = VecDeque::new();
+
     loop {
-        if rx.recv().await.is_none() {
-            // The other end has hung up. This means we're done here too.
-            break;
-        }
+        select! {
+            event = rx.recv() => {
+                if event.is_none() {
+                    // The other end has hung up. This means we're done here
+                    // too.
+                    break;
+                }
 
-        // We have a change! Wait for a bit, before passing it on.
-        sleep(Duration::from_millis(20)).await;
-        if tx.send(()).is_err() {
-            // The other end has hung up. This means we're done here too.
-            break;
-        }
+                // We have a change! Wait for a bit, before passing it on.
+                let timer = sleep(Duration::from_millis(20));
+                timers.push_back(Box::pin(timer));
+            }
+            _ = SleepOption(timers.front_mut()) => {
+                timers.pop_front();
 
-        // We also need to throw away any changes that might or might not have
-        // arrived in the meantime, or we haven't actually debounced anything.
-        while rx.try_recv().is_ok() {}
+                if tx.send(()).is_err() {
+                    // The other end has hung up. This means we're done here
+                    // too.
+                    break;
+                }
+
+                // We also need to throw away any changes that might or might
+                // not have arrived in the meantime, or we haven't actually
+                // debounced anything.
+                timers.clear();
+            }
+        }
+    }
+}
+
+pub struct SleepOption<'r>(Option<&'r mut Pin<Box<Sleep>>>);
+
+impl Future for SleepOption<'_> {
+    type Output = <Sleep as Future>::Output;
+
+    fn poll(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Self::Output> {
+        match &mut self.0 {
+            Some(sleep) => sleep.as_mut().poll(cx),
+            None => {
+                let mut pending = pin!(future::pending());
+                pending.as_mut().poll(cx)
+            }
+        }
     }
 }
