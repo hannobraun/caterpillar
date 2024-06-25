6eb50d070228ce2ab626f29b3e1473d18e88f3f3
Hanno Braun <hello@hannobraun.com>
2024-06-25 11:46:05 +0200

    Remove redundant code

--- tools/builder/src/watch/watcher.rs
+++ tools/builder/src/watch/watcher.rs
@@ -1,9 +1,5 @@
-use std::{
-    env,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
-use anyhow::Context;
 use notify::{RecursiveMode, Watcher as _};
 use tokio::sync::watch;
 use tracing::error;
@@ -16,29 +12,17 @@ pub struct Watcher {
 }
 
 impl Watcher {
-    pub fn new(path: &Path, exclude: PathBuf) -> anyhow::Result<Self> {
+    pub fn new(path: &Path, _: PathBuf) -> anyhow::Result<Self> {
         let (tx, rx) = watch::channel(());
 
         let mut watcher = notify::recommended_watcher(move |event| {
-            let event: notify::Event = match event {
+            let _: notify::Event = match event {
                 Ok(event) => event,
                 Err(err) => {
                     error!("Error watching for changes: {err}");
                     return;
                 }
             };
-
-            match ignore_event(event.paths, &exclude) {
-                Ok(ignore) => {
-                    if ignore {
-                        return;
-                    }
-                }
-                Err(err) => {
-                    error!("Error while filtering path: {err}");
-                    return;
-                }
-            }
 
             if tx.send(()).is_err() {
                 // The other end has hung up. Not much we can do about that. The
@@ -58,33 +42,4 @@ impl Watcher {
     pub fn changes(&self) -> DebouncedChanges {
         self.changes.clone()
     }
-}
-
-fn ignore_event(
-    paths: Vec<PathBuf>,
-    ignore_path: impl AsRef<Path>,
-) -> anyhow::Result<bool> {
-    let ignore_path = ignore_path.as_ref();
-    let current_dir = env::current_dir()?
-        .canonicalize()
-        .context("Canonicalize current directory")?;
-
-    let mut ignore = true;
-    for path in paths {
-        let path = match path.canonicalize() {
-            Ok(path) => path,
-            Err(_) => {
-                // This happens all the time, if the path doesn't point to a
-                // valid file or directory. This is normal, as a removed file is
-                // an event, for example.
-                continue;
-            }
-        };
-
-        if let Ok(path) = path.strip_prefix(&current_dir) {
-            ignore &= path.starts_with(ignore_path);
-        }
-    }
-
-    Ok(ignore)
 }
