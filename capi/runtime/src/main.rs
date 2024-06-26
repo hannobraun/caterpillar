cd9d9f9794cdf536251d03aff1e1a75d84319fac
Hanno Braun <hello@hannobraun.com>
2024-06-26 11:37:48 +0200

    Remove redundant code

--- capi/runtime/src/main.rs
+++ capi/runtime/src/main.rs
@@ -6,6 +6,4 @@ mod updates;
 
 fn main() {
     console_error_panic_hook::set_once();
-    console_log::init_with_level(log::Level::Error)
-        .expect("Failed to initialize logging to console");
 }
