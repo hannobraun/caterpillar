265b3ddb580ec06025a98061b9b6ab046536646a
Hanno Braun <hello@hannobraun.com>
2024-06-27 09:58:08 +0200

    Prepare for follow-on change

--- capi/runtime/src/tiles.rs
+++ capi/runtime/src/tiles.rs
@@ -4,4 +4,5 @@ pub const NUM_TILES: usize = TILES_PER_AXIS * TILES_PER_AXIS;
 pub const PIXELS_PER_TILE_AXIS: usize = 8;
 pub const PIXELS_PER_AXIS: usize = TILES_PER_AXIS * PIXELS_PER_TILE_AXIS;
 pub const NUM_PIXELS: usize = PIXELS_PER_AXIS * PIXELS_PER_AXIS;
-pub const NUM_PIXEL_BYTES: usize = NUM_PIXELS * 4;
+pub const NUM_CHANNELS: usize = 4;
+pub const NUM_PIXEL_BYTES: usize = NUM_PIXELS * NUM_CHANNELS;
