bd820eb36e6fa308b61a272ef5bbab9b27a607ab
Hanno Braun <hello@hannobraun.com>
2024-06-27 11:43:10 +0200

    Prepare for follow-on change

--- tools/builder/src/pipeline.rs
+++ tools/builder/src/pipeline.rs
@@ -6,16 +6,8 @@ pub async fn pipeline() -> anyhow::Result<()> {
     let crates_dir = PathBuf::from("capi").canonicalize()?;
 
     let watcher = watch::Watcher::new(crates_dir)?;
-    let mut updates = build::start(watcher.changes);
-    let address = serve::start(updates.clone()).await?;
-
-    while let Ok(()) = updates.changed().await {
-        println!();
-        println!("Caterpillar is ready:");
-        println!();
-        println!("\t🚀 http://{address}/");
-        println!();
-    }
+    let updates = build::start(watcher.changes);
+    serve::start(updates.clone()).await?;
 
     Ok(())
 }
--- tools/builder/src/serve.rs
+++ tools/builder/src/serve.rs
@@ -12,14 +12,14 @@ use tracing::error;
 
 use crate::build::UpdatesRx;
 
-pub async fn start(updates: UpdatesRx) -> anyhow::Result<&'static str> {
+pub async fn start(mut updates: UpdatesRx) -> anyhow::Result<&'static str> {
     let address = "localhost:34480";
 
     let router = Router::new()
         .route("/updates", get(serve_updates))
         .route("/", get(serve_index))
         .route("/*path", get(serve_static))
-        .with_state(updates);
+        .with_state(updates.clone());
     let listener = TcpListener::bind(address).await?;
 
     task::spawn(async {
@@ -28,6 +28,14 @@ pub async fn start(updates: UpdatesRx) -> anyhow::Result<&'static str> {
             process::exit(1);
         }
     });
+
+    while let Ok(()) = updates.changed().await {
+        println!();
+        println!("Caterpillar is ready:");
+        println!();
+        println!("\t🚀 http://{address}/");
+        println!();
+    }
 
     Ok(address)
 }
