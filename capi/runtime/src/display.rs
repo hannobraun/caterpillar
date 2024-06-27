c7faaa3d74489a8dd8d341c31501e9887784168d
Hanno Braun <hello@hannobraun.com>
2024-06-27 09:58:44 +0200

    Consolidate redundant data

--- capi/runtime/src/display.rs
+++ capi/runtime/src/display.rs
@@ -1,4 +1,6 @@
-use crate::tiles::{PIXELS_PER_AXIS, PIXELS_PER_TILE_AXIS, TILES_PER_AXIS};
+use crate::tiles::{
+    NUM_CHANNELS, PIXELS_PER_AXIS, PIXELS_PER_TILE_AXIS, TILES_PER_AXIS,
+};
 
 pub fn set_tile(x: usize, y: usize, value: u8, tiles: &mut [u8]) {
     let index = || x.checked_add(y.checked_mul(TILES_PER_AXIS)?);
@@ -21,15 +23,13 @@ pub fn render(tiles: &[u8], pixels: &mut [u8]) {
 
             for offset_y in 0..PIXELS_PER_TILE_AXIS {
                 for offset_x in 0..PIXELS_PER_TILE_AXIS {
-                    let num_channels = 4;
-
                     let frame_x = (tile_x * PIXELS_PER_TILE_AXIS + offset_x)
-                        * num_channels;
+                        * NUM_CHANNELS;
                     let frame_y = (tile_y * PIXELS_PER_TILE_AXIS + offset_y)
-                        * num_channels;
+                        * NUM_CHANNELS;
 
                     let i = frame_y * PIXELS_PER_AXIS + frame_x;
-                    pixels[i..i + num_channels].copy_from_slice(&color);
+                    pixels[i..i + NUM_CHANNELS].copy_from_slice(&color);
                 }
             }
         }
