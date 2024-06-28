d7b9c07282418293e7c8cc0f531dec135d731adf
Hanno Braun <hello@hannobraun.com>
2024-06-28 21:09:54 +0200

    Derive `Clone` for `SourceCode`

--- capi/protocol/src/update.rs
+++ capi/protocol/src/update.rs
@@ -22,7 +22,7 @@ impl Update {
     }
 }
 
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 pub struct SourceCode {
     pub functions: syntax::Functions,
     pub source_map: SourceMap,
