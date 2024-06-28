2cc80abe6b77866f3aebab61d5f7cb570199142a
Hanno Braun <hello@hannobraun.com>
2024-06-28 12:09:45 +0200

    Extract server into `capi-server`

--- capi/server/src/main.rs
+++ capi/server/src/main.rs
@@ -1,6 +1,20 @@
-use std::path::PathBuf;
+use std::{future, io, path::PathBuf, process};
 
+use axum::{
+    extract::{ws::WebSocket, Path, State, WebSocketUpgrade},
+    http::{header, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use clap::Parser;
+use tokio::{
+    fs::File,
+    io::AsyncReadExt,
+    net::TcpListener,
+    task::{self, JoinHandle},
+};
+use tracing::error;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -9,6 +23,9 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     println!("Address: {}", args.address);
     println!("Directory to serve: {}", args.serve_dir.display());
+
+    let server = start_server(args.address, args.serve_dir).await?;
+    server.await?;
 
     Ok(())
 }
@@ -23,4 +40,85 @@ pub struct Args {
     /// Directory to serve from
     #[arg(short, long)]
     pub serve_dir: PathBuf,
+}
+
+async fn start_server(
+    address: String,
+    serve_dir: PathBuf,
+) -> anyhow::Result<JoinHandle<()>> {
+    let router = Router::new()
+        .route("/is-alive", get(serve_is_alive))
+        .route("/updates", get(serve_updates))
+        .route("/", get(serve_index))
+        .route("/*path", get(serve_static))
+        .with_state(serve_dir);
+    let listener = TcpListener::bind(address).await?;
+
+    let handle = task::spawn(async {
+        if let Err(err) = axum::serve(listener, router).await {
+            error!("Error serving HTTP endpoints: {err}");
+            process::exit(1);
+        }
+    });
+
+    Ok(handle)
+}
+
+async fn serve_is_alive() -> StatusCode {
+    StatusCode::OK
+}
+
+async fn serve_updates(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(serve_updates_handle_socket)
+}
+
+async fn serve_updates_handle_socket(_: WebSocket) {
+    future::pending::<()>().await;
+}
+
+async fn serve_index(State(serve_dir): State<PathBuf>) -> impl IntoResponse {
+    file_response(PathBuf::from("index.html"), serve_dir).await
+}
+
+async fn serve_static(
+    Path(path): Path<PathBuf>,
+    State(serve_dir): State<PathBuf>,
+) -> impl IntoResponse {
+    file_response(path, serve_dir).await
+}
+
+async fn file_response(path: PathBuf, serve_dir: PathBuf) -> Response {
+    let path = serve_dir.join(path);
+
+    let content_type = match path.extension() {
+        Some(os_str) => match os_str.to_str() {
+            Some("html") => "text/html",
+            Some("js") => "application/javascript",
+            Some("wasm") => "application/wasm",
+            _ => "application/octet-stream",
+        },
+        _ => "application/octet-stream",
+    };
+
+    let mut file = match File::open(path).await {
+        Ok(file) => file,
+        Err(err) => return error_to_response(err),
+    };
+
+    let mut data = Vec::new();
+    if let Err(err) = file.read_to_end(&mut data).await {
+        return error_to_response(err);
+    }
+
+    (StatusCode::OK, [(header::CONTENT_TYPE, content_type)], data)
+        .into_response()
+}
+
+fn error_to_response(err: io::Error) -> Response {
+    let status = match err.kind() {
+        io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
+        _ => StatusCode::INTERNAL_SERVER_ERROR,
+    };
+
+    status.into_response()
 }
--- tools/builder/src/serve.rs
+++ tools/builder/src/serve.rs
@@ -1,38 +1,31 @@
-use std::{io, path::PathBuf, process};
-
-use axum::{
-    extract::{
-        ws::{Message, WebSocket},
-        Path, State, WebSocketUpgrade,
-    },
-    http::{header, StatusCode},
-    response::{IntoResponse, Response},
-    routing::get,
-    Router,
-};
-use tokio::{
-    fs::File,
-    io::AsyncReadExt,
-    net::TcpListener,
-    task::{self, JoinHandle},
-};
-use tracing::error;
+use tokio::process::{Child, Command};
 
 use crate::build::UpdatesRx;
 
 pub async fn start(mut updates: UpdatesRx) -> anyhow::Result<()> {
     let address = "localhost:34480";
 
-    let mut server: Option<JoinHandle<_>> = None;
+    let mut server: Option<Child> = None;
 
     updates.mark_unchanged(); // make sure we enter the loop body immediately
     while let Ok(()) = updates.changed().await {
-        if let Some(server) = server.take() {
-            server.abort();
+        let Some(serve_dir) = &*updates.borrow() else {
+            continue;
+        };
+
+        if let Some(mut server) = server.take() {
+            server.kill().await?;
         }
 
-        server =
-            Some(start_server(address.to_string(), updates.clone()).await?);
+        server = Some(
+            Command::new("cargo")
+                .arg("run")
+                .args(["--package", "capi-server"])
+                .arg("--")
+                .args(["--address", address])
+                .args(["--serve-dir", &serve_dir.display().to_string()])
+                .spawn()?,
+        );
 
         println!();
         println!("✅ Build is ready:");
@@ -44,106 +37,4 @@ pub async fn start(mut updates: UpdatesRx) -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-async fn start_server(
-    address: String,
-    updates: UpdatesRx,
-) -> anyhow::Result<JoinHandle<()>> {
-    let router = Router::new()
-        .route("/is-alive", get(serve_is_alive))
-        .route("/updates", get(serve_updates))
-        .route("/", get(serve_index))
-        .route("/*path", get(serve_static))
-        .with_state(updates);
-    let listener = TcpListener::bind(address).await?;
-
-    let handle = task::spawn(async {
-        if let Err(err) = axum::serve(listener, router).await {
-            error!("Error serving HTTP endpoints: {err}");
-            process::exit(1);
-        }
-    });
-
-    Ok(handle)
-}
-
-async fn serve_is_alive() -> StatusCode {
-    StatusCode::OK
-}
-
-async fn serve_updates(
-    ws: WebSocketUpgrade,
-    State(updates): State<UpdatesRx>,
-) -> impl IntoResponse {
-    ws.on_upgrade(|socket| serve_updates_handle_socket(socket, updates))
-}
-
-async fn serve_updates_handle_socket(
-    mut socket: WebSocket,
-    mut updates: UpdatesRx,
-) {
-    updates.mark_unchanged();
-    match updates.changed().await {
-        Ok(()) => {
-            if let Err(err) = socket.send(Message::Text("".to_string())).await {
-                error!("Error sending to socket: {err}");
-                let _ = socket.close().await;
-            }
-        }
-        Err(_) => {
-            error!("Waiting for updates, but updates no longer available.");
-            let _ = socket.close().await;
-        }
-    }
-}
-
-async fn serve_index(State(updates): State<UpdatesRx>) -> impl IntoResponse {
-    file_response(PathBuf::from("index.html"), updates).await
-}
-
-async fn serve_static(
-    Path(path): Path<PathBuf>,
-    State(updates): State<UpdatesRx>,
-) -> impl IntoResponse {
-    file_response(path, updates).await
-}
-
-async fn file_response(path: PathBuf, updates: UpdatesRx) -> Response {
-    let Some(serve_dir) = updates.borrow().clone() else {
-        return StatusCode::NOT_FOUND.into_response();
-    };
-    let path = serve_dir.join(path);
-
-    let content_type = match path.extension() {
-        Some(os_str) => match os_str.to_str() {
-            Some("html") => "text/html",
-            Some("js") => "application/javascript",
-            Some("wasm") => "application/wasm",
-            _ => "application/octet-stream",
-        },
-        _ => "application/octet-stream",
-    };
-
-    let mut file = match File::open(path).await {
-        Ok(file) => file,
-        Err(err) => return error_to_response(err),
-    };
-
-    let mut data = Vec::new();
-    if let Err(err) = file.read_to_end(&mut data).await {
-        return error_to_response(err);
-    }
-
-    (StatusCode::OK, [(header::CONTENT_TYPE, content_type)], data)
-        .into_response()
-}
-
-fn error_to_response(err: io::Error) -> Response {
-    let status = match err.kind() {
-        io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
-        _ => StatusCode::INTERNAL_SERVER_ERROR,
-    };
-
-    status.into_response()
 }
