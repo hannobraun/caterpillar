3398c94c9d37cc815f8883fc932798d55ab0049c
Hanno Braun <hello@hannobraun.com>
2024-06-28 11:57:02 +0200

    Pass command-line arguments

--- capi/server/src/main.rs
+++ capi/server/src/main.rs
@@ -1,6 +1,26 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().init();
-    println!("Hello, world!");
+
+    let args = Args::parse();
+    println!("Address: {}", args.address);
+    println!("Directory to serve: {}", args.serve_dir.display());
+
     Ok(())
+}
+
+/// Caterpillar server
+#[derive(clap::Parser)]
+pub struct Args {
+    /// Address to serve at
+    #[arg(short, long)]
+    pub address: String,
+
+    /// Directory to serve from
+    #[arg(short, long)]
+    pub serve_dir: PathBuf,
 }
