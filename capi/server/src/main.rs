0259e4947e55c9437754a1e6f2530863502378cc
Hanno Braun <hello@hannobraun.com>
2024-06-30 11:06:19 +0200

    Update formatting

--- capi/server/src/main.rs
+++ capi/server/src/main.rs
@@ -7,9 +7,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().init();
 
     let args = args::Args::parse();
-
     let (source_code, bytecode) = build::build_snake().await?;
-
     server::start(args.address, args.serve_dir, source_code, bytecode).await?;
 
     tracing::info!("`capi-server` shutting down.");
