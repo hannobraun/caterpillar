3010d0bda4c876720f04207ae69ff90719dcfb9f
Hanno Braun <hello@hannobraun.com>
2024-07-01 13:10:50 +0200

    Simplify random number conversion

--- capi/runtime/src/ffi.rs
+++ capi/runtime/src/ffi.rs
@@ -155,11 +155,10 @@ pub fn push_random(random: f64) -> bool {
         return false;
     }
 
-    let min: i16 = i8::MIN.into();
-    let max: i16 = i8::MAX.into();
+    let min: f64 = i8::MIN.into();
+    let max: f64 = i8::MAX.into();
 
-    let range: f64 = (max - min).into();
-    let random = random * range;
+    let random = random * (max - min);
 
     let random: i8 = random.floor() as i8;
     state.random.push_back(random);
