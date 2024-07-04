c524f041a97302a5a60762142dd1c97099a917e8
Hanno Braun <hello@hannobraun.com>
2024-07-04 21:33:41 +0200

    Enable debug output

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -5,7 +5,7 @@ mod serve;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
-        .with_env_filter("builder::build=debug")
+        .with_env_filter("builder::build=debug,builder::serve=debug")
         .init();
     pipeline::pipeline().await?;
     Ok(())
