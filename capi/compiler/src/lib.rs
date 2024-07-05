e2079787589ac5ca60bf9a6b4b2b0d0ceac21836
Hanno Braun <hello@hannobraun.com>
2024-07-05 12:47:52 +0200

    Update module name

--- capi/compiler/src/lib.rs
+++ capi/compiler/src/lib.rs
@@ -1,12 +1,12 @@
-pub mod pipeline;
 pub mod source_map;
+pub mod stages;
 pub mod syntax;
 
 pub fn compile(
     script: &crate::syntax::Script,
 ) -> (capi_process::Bytecode, crate::source_map::SourceMap) {
-    let fragments = pipeline::syntax_to_fragments(script.clone());
+    let fragments = stages::syntax_to_fragments(script.clone());
     dbg!(fragments);
 
-    pipeline::compile(script)
+    stages::compile(script)
 }
