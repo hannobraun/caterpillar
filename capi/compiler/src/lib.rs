dc2f4b3a852999b2a482f22c57564bd9c0fa4bcc
Hanno Braun <hello@hannobraun.com>
2024-07-05 12:47:30 +0200

    Update function name

--- capi/compiler/src/lib.rs
+++ capi/compiler/src/lib.rs
@@ -2,7 +2,7 @@ pub mod pipeline;
 pub mod source_map;
 pub mod syntax;
 
-pub fn run(
+pub fn compile(
     script: &crate::syntax::Script,
 ) -> (capi_process::Bytecode, crate::source_map::SourceMap) {
     let fragments = pipeline::syntax_to_fragments(script.clone());
