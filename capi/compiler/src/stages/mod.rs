67578a39a63f885b03a3892e4cb9e534629fc1b7
Hanno Braun <hello@hannobraun.com>
2024-07-06 12:27:00 +0200

    Update module name

--- capi/compiler/src/stages/mod.rs
+++ capi/compiler/src/stages/mod.rs
@@ -1,7 +1,7 @@
-mod syntax_to_bytecode;
+mod fragments_to_bytecode;
 mod syntax_to_fragments;
 
 pub use {
-    syntax_to_bytecode::fragments_to_bytecode,
+    fragments_to_bytecode::fragments_to_bytecode,
     syntax_to_fragments::syntax_to_fragments,
 };
