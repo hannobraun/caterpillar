1103e5d7fa6eda8e67795fb9d582dcf084a4776d
Hanno Braun <hello@hannobraun.com>
2024-07-08 12:49:58 +0200

    Add `SourceMap2`
    This prepares for the transition to fragments.

--- capi/compiler/src/source_map.rs
+++ capi/compiler/src/source_map.rs
@@ -43,3 +43,43 @@ impl SourceMap {
         self.syntax_to_runtime.get(syntax).cloned()
     }
 }
+
+#[derive(
+    Clone, Debug, Default, Eq, PartialEq, serde::Deserialize, serde::Serialize,
+)]
+pub struct SourceMap2 {
+    runtime_to_syntax: BTreeMap<RuntimeLocation, syntax::Location>,
+    syntax_to_runtime: BTreeMap<syntax::Location, RuntimeLocation>,
+}
+
+impl SourceMap2 {
+    pub fn define_mapping(
+        &mut self,
+        runtime: RuntimeLocation,
+        syntax: syntax::Location,
+    ) {
+        self.runtime_to_syntax
+            .insert(runtime.clone(), syntax.clone());
+        self.syntax_to_runtime.insert(syntax, runtime);
+    }
+
+    pub fn runtime_to_syntax(
+        &self,
+        runtime: &RuntimeLocation,
+    ) -> syntax::Location {
+        self.runtime_to_syntax
+            .get(runtime)
+            .cloned()
+            .expect("Expect every runtime location to map to a syntax location")
+    }
+
+    /// Get the runtime location that a given syntax location is mapped to
+    ///
+    /// Can return `None`, as comments have no mapping to runtime locations.
+    pub fn syntax_to_runtime(
+        &self,
+        syntax: &syntax::Location,
+    ) -> Option<RuntimeLocation> {
+        self.syntax_to_runtime.get(syntax).cloned()
+    }
+}
