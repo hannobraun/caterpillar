22d12a5abe1e8e29c7c73c7d24ea49dbe8a08f30
Hanno Braun <hello@hannobraun.com>
2024-07-08 12:24:49 +0200

    Move `Fragments` to dedicated module

---
+++ capi/compiler/src/repr/fragments/fragments.rs
@@ -0,0 +1,55 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+use super::{Fragment, FragmentId};
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+pub struct Fragments {
+    pub functions: BTreeSet<String>,
+    pub by_function: Vec<Function>,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+pub struct Function {
+    pub name: String,
+    pub args: Vec<String>,
+    pub fragments: FunctionFragments,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+pub struct FunctionFragments {
+    first: Option<FragmentId>,
+    inner: FunctionFragmentsInner,
+}
+
+impl FunctionFragments {
+    pub fn new(
+        first: Option<FragmentId>,
+        inner: FunctionFragmentsInner,
+    ) -> Self {
+        Self { first, inner }
+    }
+}
+
+impl FunctionFragments {
+    pub fn remove_first(&mut self) -> Option<Fragment> {
+        let first = self.first.take()?;
+        let first = self
+            .inner
+            .remove(&first)
+            .expect("`self.first` must be present in `self.inner`");
+
+        self.first = first.address.next;
+
+        Some(first)
+    }
+}
+
+impl Iterator for FunctionFragments {
+    type Item = Fragment;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.remove_first()
+    }
+}
+
+type FunctionFragmentsInner = BTreeMap<FragmentId, Fragment>;
