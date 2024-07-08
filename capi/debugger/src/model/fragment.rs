9947410ca65dccb1dbf5777498f16ffeef938d89
Hanno Braun <hello@hannobraun.com>
2024-07-08 13:24:32 +0200

    Update argument name

--- capi/debugger/src/model/fragment.rs
+++ capi/debugger/src/model/fragment.rs
@@ -18,10 +18,10 @@ impl Fragment {
     pub fn new(
         syntax_location: syntax::Location,
         payload: FragmentPayload,
-        source_map: &SourceMap2,
+        source_map_2: &SourceMap2,
         process: &Process,
     ) -> Self {
-        let location = source_map.syntax_to_runtime(&syntax_location);
+        let location = source_map_2.syntax_to_runtime(&syntax_location);
 
         let has_durable_breakpoint = if let Some(location) = &location {
             process.breakpoints().durable_at(location)
@@ -33,7 +33,7 @@ impl Fragment {
 
         let effect =
             process.state().first_unhandled_effect().and_then(|effect| {
-                let effect_location = source_map.runtime_to_syntax(
+                let effect_location = source_map_2.runtime_to_syntax(
                     &process.state().most_recent_step().unwrap(),
                 );
 
