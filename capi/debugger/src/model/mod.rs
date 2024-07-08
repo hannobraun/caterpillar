10252dc32c3ec8a41b986b75a97136ad54d5035d
Hanno Braun <hello@hannobraun.com>
2024-07-08 13:23:04 +0200

    Update module name

--- capi/debugger/src/model/mod.rs
+++ capi/debugger/src/model/mod.rs
@@ -1,9 +1,9 @@
 mod active_functions;
 mod debugger;
-mod expression;
+mod fragment;
 mod function;
 
 pub use self::{
-    active_functions::ActiveFunctions, debugger::Debugger,
-    expression::Fragment, function::Function,
+    active_functions::ActiveFunctions, debugger::Debugger, fragment::Fragment,
+    function::Function,
 };
