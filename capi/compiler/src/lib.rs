2823632a2d65da1a03105e0ed2f1e6847019f19c
Hanno Braun <hello@hannobraun.com>
2024-07-08 13:20:41 +0200

    Remove unnecessary `.clone()`

--- capi/compiler/src/lib.rs
+++ capi/compiler/src/lib.rs
@@ -10,7 +10,7 @@ pub fn compile(
     crate::source_map::SourceMap,
     crate::source_map::SourceMap2,
 ) {
-    let fragments = stages::syntax_to_fragments(script.clone());
+    let fragments = stages::syntax_to_fragments(script);
     let (bytecode, source_map, source_map_2) =
         stages::fragments_to_bytecode(fragments.clone());
 
