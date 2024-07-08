8513ec09bbf8b80b99f8397fbaa7e2326979ffb2
Hanno Braun <hello@hannobraun.com>
2024-07-08 13:46:57 +0200

    Remove unused return value

--- capi/compiler/src/lib.rs
+++ capi/compiler/src/lib.rs
@@ -8,11 +8,10 @@ pub fn compile(
     crate::repr::fragments::Fragments,
     capi_process::Bytecode,
     crate::source_map::SourceMap,
-    crate::source_map::SourceMap2,
 ) {
     let fragments = stages::syntax_to_fragments(script);
-    let (bytecode, source_map, source_map_2) =
+    let (bytecode, source_map, _) =
         stages::fragments_to_bytecode(fragments.clone());
 
-    (fragments, bytecode, source_map, source_map_2)
+    (fragments, bytecode, source_map)
 }
