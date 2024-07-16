ff9ee972f3b7eb6de5252bb96f73a4d838a3cb77
Hanno Braun <hello@hannobraun.com>
2024-07-16 12:56:27 +0200

    Implement `Display` for `FragmentId`

--- capi/compiler/src/repr/fragments/id.rs
+++ capi/compiler/src/repr/fragments/id.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, fmt};
 
 #[derive(
     Clone, Copy, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize,
@@ -26,5 +26,11 @@ impl Ord for FragmentId {
 impl PartialOrd for FragmentId {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
+    }
+}
+
+impl fmt::Display for FragmentId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.hash)
     }
 }
