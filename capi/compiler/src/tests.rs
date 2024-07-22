3fa6e8d4ba1de8b44aae710f43eb5e1e984f5902
Hanno Braun <hello@hannobraun.com>
2024-07-22 10:00:10 +0200

    Improve doc comment

--- capi/compiler/src/tests.rs
+++ capi/compiler/src/tests.rs
@@ -1,4 +1,6 @@
-//! End-to-end testing for `capi-compiler` and `capi-process`
+//! # End-to-end testing for `capi-compiler` and `capi-process`
+//!
+//! ## Implementation Note
 //!
 //! That this module lives in `capi-compiler` is a practical decision. The crate
 //! depends on `capi-process` anyway, so we have everything here that we need.
