6b5922a624a00dc2499a55a39df005e0afcfd86a
Hanno Braun <hello@hannobraun.com>
2024-07-23 12:21:38 +0200

    Update module name

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,7 +1,7 @@
 mod a_script_to_fragments;
-mod fragments_to_bytecode;
+mod b_fragments_to_bytecode;
 
 pub use {
     a_script_to_fragments::script_to_fragments,
-    fragments_to_bytecode::fragments_to_bytecode,
+    b_fragments_to_bytecode::fragments_to_bytecode,
 };
