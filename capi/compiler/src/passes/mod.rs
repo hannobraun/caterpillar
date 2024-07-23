259fbaf1038cea3fb6f4885c58ec1675b26656dc
Hanno Braun <hello@hannobraun.com>
2024-07-23 12:31:04 +0200

    Simplify module name

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,7 +1,7 @@
-mod a_script_to_fragments;
+mod a_fragments;
 mod c_fragments_to_bytecode;
 
 pub use {
-    a_script_to_fragments::generate_fragments,
+    a_fragments::generate_fragments,
     c_fragments_to_bytecode::fragments_to_bytecode,
 };
