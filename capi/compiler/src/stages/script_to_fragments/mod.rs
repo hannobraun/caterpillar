64d49889b7224ab10767ba97cd45067ca566f3c9
Hanno Braun <hello@hannobraun.com>
2024-07-23 12:09:03 +0200

    Inline module

--- capi/compiler/src/stages/script_to_fragments/mod.rs
+++ capi/compiler/src/stages/script_to_fragments/mod.rs
@@ -2,14 +2,15 @@ mod passes;
 
 use std::collections::{BTreeMap, BTreeSet};
 
-use capi_process::Host;
+use capi_process::{builtin, Host};
 
 use crate::repr::{
-    fragments::{FragmentMap, FragmentParent, Fragments, Function},
+    fragments::{
+        Fragment, FragmentExpression, FragmentId, FragmentMap, FragmentParent,
+        FragmentPayload, Fragments, Function,
+    },
     syntax::{Expression, Script},
 };
-
-use self::passes::build_fragments::compile_block;
 
 pub fn script_to_fragments<H: Host>(script: Script) -> Fragments {
     let mut functions = BTreeSet::new();
@@ -340,4 +341,100 @@ pub enum BindingResolved {
 #[derive(Debug)]
 struct Bindings {
     inner: BTreeSet<String>,
+}
+
+pub fn compile_block<H: Host>(
+    expressions: Vec<Expression>,
+    parent: FragmentParent,
+    functions: &BTreeSet<String>,
+    scopes: &mut Scopes,
+    fragments: &mut FragmentMap,
+) -> (FragmentId, BTreeSet<String>) {
+    let mut next = {
+        let terminator = Fragment {
+            parent: parent.clone(),
+            payload: FragmentPayload::Terminator,
+        };
+        let terminator_id = terminator.id();
+
+        fragments.inner.insert(terminator_id, terminator);
+
+        terminator_id
+    };
+    let mut environment = BTreeSet::new();
+
+    for expression in expressions.into_iter().rev() {
+        let fragment = compile_expression::<H>(
+            expression,
+            parent.clone(),
+            next,
+            functions,
+            &mut environment,
+            scopes,
+            fragments,
+        );
+
+        next = fragment.id();
+
+        fragments.inner.insert(fragment.id(), fragment);
+    }
+
+    (next, environment)
+}
+
+pub fn compile_expression<H: Host>(
+    expression: Expression,
+    parent: FragmentParent,
+    next: FragmentId,
+    functions: &BTreeSet<String>,
+    environment: &mut BTreeSet<String>,
+    scopes: &mut Scopes,
+    fragments: &mut FragmentMap,
+) -> Fragment {
+    let expression = match expression {
+        Expression::Binding { names } => {
+            FragmentExpression::BindingDefinitions { names }
+        }
+        Expression::Block { expressions } => {
+            let (start, environment) = compile_block::<H>(
+                expressions,
+                FragmentParent::Fragment { id: next },
+                functions,
+                scopes,
+                fragments,
+            );
+            FragmentExpression::Block { start, environment }
+        }
+        Expression::Comment { text } => FragmentExpression::Comment { text },
+        Expression::Value(value) => FragmentExpression::Value(value),
+        Expression::Word { name } => {
+            // The way this is written, the different types of definitions
+            // shadow each other in a defined order.
+            //
+            // This isn't desirable. There should at least be a warning, if such
+            // shadowing isn't forbidden outright. It'll do for now though.
+            if functions.contains(&name) {
+                FragmentExpression::ResolvedUserFunction { name }
+            } else if let Some(resolved) = scopes.resolve_binding(&name) {
+                if let BindingResolved::InEnvironment = resolved {
+                    environment.insert(name.clone());
+                }
+                FragmentExpression::ResolvedBinding { name }
+            } else if H::function(&name).is_some() {
+                FragmentExpression::ResolvedHostFunction { name }
+            } else if builtin(&name).is_some()
+                || name == "return_if_non_zero"
+                || name == "return_if_zero"
+            {
+                FragmentExpression::ResolvedBuiltinFunction { name }
+            } else {
+                FragmentExpression::UnresolvedWord { name }
+            }
+        }
+    };
+
+    Fragment {
+        parent,
+        payload: FragmentPayload::Expression { expression, next },
+    }
 }
