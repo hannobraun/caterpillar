ec83d15f5bbfc7684a6051c26442e496cf00896b
Hanno Braun <hello@hannobraun.com>
2024-07-24 01:06:57 +0200

    Document `Expression::Reference`

--- capi/compiler/src/repr/syntax/expression.rs
+++ capi/compiler/src/repr/syntax/expression.rs
@@ -11,10 +11,19 @@ pub enum Expression {
     Comment {
         text: String,
     },
+
+    /// A reference to a definition
     Reference {
+        /// The name of the referenced definition, as it appears in the code
         name: String,
+
+        /// The kind of referenced definition
+        ///
+        /// This might be `None`, while the kind of reference has not been
+        /// determined yet, or if it can not be determined.
         kind: Option<ReferenceKind>,
     },
+
     Value(Value),
 }
 
