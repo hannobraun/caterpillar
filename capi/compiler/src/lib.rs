93e301af0de2ef9418e645d479a3bf014fce02ec
Hanno Braun <hello@hannobraun.com>
2024-07-23 12:19:34 +0200

    Update module name

--- capi/compiler/src/lib.rs
+++ capi/compiler/src/lib.rs
@@ -1,6 +1,6 @@
+pub mod passes;
 pub mod repr;
 pub mod source_map;
-pub mod stages;
 
 #[cfg(test)]
 mod tests;
@@ -12,9 +12,9 @@ pub fn compile<H: capi_process::Host>(
     capi_process::Bytecode,
     crate::source_map::SourceMap,
 ) {
-    let fragments = stages::script_to_fragments::<H>(script);
+    let fragments = passes::script_to_fragments::<H>(script);
     let (bytecode, source_map) =
-        stages::fragments_to_bytecode(fragments.clone());
+        passes::fragments_to_bytecode(fragments.clone());
 
     (fragments, bytecode, source_map)
 }
