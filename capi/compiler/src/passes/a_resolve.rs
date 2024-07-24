34bf50ca2a4426fb6bc73699fc2d2af616b1a5ee
Hanno Braun <hello@hannobraun.com>
2024-07-24 03:19:02 +0200

    Track closure environment

--- capi/compiler/src/passes/a_resolve.rs
+++ capi/compiler/src/passes/a_resolve.rs
@@ -14,13 +14,26 @@ pub fn resolve_references<H: Host>(script: &mut Script) {
 
     for function in &mut script.functions {
         scopes.push(function.args.clone().into_iter().collect());
-        resolve_block::<H>(&mut function.body, &mut scopes, &user_functions);
+        let mut environment = Environment::new();
+
+        resolve_block::<H>(
+            &mut function.body,
+            &mut scopes,
+            &mut environment,
+            &user_functions,
+        );
+
+        assert!(
+            environment.is_empty(),
+            "Functions do not have an environment that they could access.",
+        );
     }
 }
 
 fn resolve_block<H: Host>(
     body: &mut [Expression],
     scopes: &mut Scopes,
+    environment: &mut Environment,
     user_functions: &BTreeSet<String>,
 ) {
     for expression in body {
@@ -32,9 +45,9 @@ fn resolve_block<H: Host>(
                     }
                 }
             }
-            Expression::Block { body, .. } => {
+            Expression::Block { body, environment } => {
                 scopes.push(Bindings::new());
-                resolve_block::<H>(body, scopes, user_functions);
+                resolve_block::<H>(body, scopes, environment, user_functions);
             }
             Expression::Reference { name, kind } => {
                 // The way this is written, definitions can silently shadow each
@@ -44,6 +57,12 @@ fn resolve_block<H: Host>(
                 // shouldn't be forbidden outright.
                 if scopes.iter().any(|bindings| bindings.contains(name)) {
                     *kind = Some(ReferenceKind::Binding);
+
+                    if let Some(bindings) = scopes.last() {
+                        if !bindings.contains(name) {
+                            environment.insert(name.clone());
+                        }
+                    }
                 }
                 if builtin(name).is_some()
                     || name == "return_if_non_zero"
@@ -67,6 +86,7 @@ fn resolve_block<H: Host>(
 
 type Scopes = Vec<Bindings>;
 type Bindings = BTreeSet<String>;
+type Environment = BTreeSet<String>;
 
 #[cfg(test)]
 mod tests {
@@ -129,7 +149,8 @@ mod tests {
         resolve_references(&mut script);
 
         let mut function = script.functions.remove(0);
-        let Some(Expression::Block { body, .. }) = function.body.last_mut()
+        let Some(Expression::Block { body, environment }) =
+            function.body.last_mut()
         else {
             panic!("Last expression in the function is a block.");
         };
@@ -141,6 +162,9 @@ mod tests {
                 kind: Some(ReferenceKind::Binding),
             })
         );
+
+        assert!(environment.remove("value"));
+        assert!(environment.is_empty());
     }
 
     #[test]
