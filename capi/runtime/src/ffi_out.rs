2f17591bf35e5f72b62649a6eae13984afa22b0c
Hanno Braun <hello@hannobraun.com>
2024-07-25 11:05:31 +0200

    Prepare for follow-on change

--- capi/runtime/src/ffi_out.rs
+++ capi/runtime/src/ffi_out.rs
@@ -1,3 +1,9 @@
-extern "C" {
-    pub fn on_panic(ptr: *const u8, len: usize);
+pub unsafe fn on_panic(ptr: *const u8, len: usize) {
+    ffi::on_panic(ptr, len);
+}
+
+mod ffi {
+    extern "C" {
+        pub fn on_panic(ptr: *const u8, len: usize);
+    }
 }
