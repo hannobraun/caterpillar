69689e087e9898521f7d0452aba17ab453df08fe
Hanno Braun <hello@hannobraun.com>
2024-07-25 11:56:19 +0200

    Add `ffi_out::print`

--- capi/runtime/src/ffi_out.rs
+++ capi/runtime/src/ffi_out.rs
@@ -6,8 +6,18 @@ pub fn on_panic(message: &str) {
     }
 }
 
+#[allow(unused)] // for debugging; might not always be in use
+pub fn print(message: &str) {
+    // Sound, as the `on_panic` function immediately builds and logs a
+    // JavaScript string, and the pointer it not kept around after that.
+    unsafe {
+        ffi::print(message.as_ptr(), message.len());
+    }
+}
+
 mod ffi {
     extern "C" {
         pub fn on_panic(ptr: *const u8, len: usize);
+        pub fn print(ptr: *const u8, len: usize);
     }
 }
