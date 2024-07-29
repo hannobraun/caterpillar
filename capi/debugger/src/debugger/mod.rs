ce2e4a3dfb8b6bae3737ceca9d35e70941cd0a35
Hanno Braun <hello@hannobraun.com>
2024-07-29 12:44:13 +0200

    Simplify

--- capi/debugger/src/debugger/mod.rs
+++ capi/debugger/src/debugger/mod.rs
@@ -22,7 +22,7 @@ mod tests {
     use capi_protocol::{
         host::GameEngineHost,
         memory::Memory,
-        updates::{SourceCode, Update, Updates},
+        updates::{SourceCode, Updates},
     };
 
     use crate::debugger::{
@@ -46,7 +46,7 @@ mod tests {
         assert!(debugger.operands.is_none());
         assert!(debugger.memory.is_none());
 
-        remote_process.on_update(Update::SourceCode(SourceCode::default()));
+        remote_process.on_source_code(SourceCode::default());
 
         let debugger = remote_process.to_debugger();
         assert_eq!(
@@ -62,7 +62,7 @@ mod tests {
     #[test]
     fn uninitialized_process() {
         let mut remote_process = RemoteProcess::default();
-        remote_process.on_update(Update::SourceCode(SourceCode::default()));
+        remote_process.on_source_code(SourceCode::default());
 
         let process = Process::default();
         let memory = Memory::default();
