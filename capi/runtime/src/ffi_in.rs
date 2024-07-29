a58e8eecdd9e7330b331bdd5d132126f7186eb6a
Hanno Braun <hello@hannobraun.com>
2024-07-29 13:14:42 +0200

    Prepare for removing `/bytecode`

--- capi/runtime/src/ffi_in.rs
+++ capi/runtime/src/ffi_in.rs
@@ -1,9 +1,9 @@
 use std::{str, sync::Mutex};
 
 use capi_ffi::{framed_buffer::FramedBuffer, shared::Shared};
-use capi_process::Bytecode;
 use capi_protocol::{
-    Versioned, CODE_BUFFER_SIZE, COMMANDS_BUFFER_SIZE, UPDATES_BUFFER_SIZE,
+    updates::Code, Versioned, CODE_BUFFER_SIZE, COMMANDS_BUFFER_SIZE,
+    UPDATES_BUFFER_SIZE,
 };
 
 use crate::{state::RuntimeState, tiles::NUM_PIXEL_BYTES};
@@ -148,9 +148,9 @@ pub fn on_new_bytecode() -> u64 {
 
     let bytecode = buffer.read_frame();
     let bytecode = str::from_utf8(bytecode).unwrap();
-    let bytecode: Versioned<Bytecode> = ron::from_str(bytecode).unwrap();
+    let bytecode: Versioned<Code> = ron::from_str(bytecode).unwrap();
 
-    state.on_new_bytecode(bytecode.inner);
+    state.on_new_bytecode(bytecode.inner.bytecode);
 
     bytecode.version
 }
