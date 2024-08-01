76ef30aef83b025c1b3e52f46ce1570b10038cbf
Hanno Braun <hello@hannobraun.com>
2024-08-01 17:50:48 +0200

    Document `parent` field of `Fragment`

--- capi/compiler/src/repr/fragments/fragment.rs
+++ capi/compiler/src/repr/fragments/fragment.rs
@@ -2,7 +2,22 @@ use super::{FragmentExpression, FragmentId};
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 pub struct Fragment {
+    /// The parent of this fragment
+    ///
+    /// Points to the fragment that is the parent of this fragment. If this
+    /// fragment resides in the root context, then this is `None`.
+    ///
+    /// Otherwise, this fragment must reside in a function or block. In this
+    /// case, by convention, this points to the first fragment _after_ the
+    /// function or block (i.e. its next fragment).
+    ///
+    /// This must be so, because the block or function itself, or any fragments
+    /// preceding it, are not complete yet, and thus do not have a hash. Their
+    /// hash depends on the hashes of the fragments they contain, which in turn
+    /// depend on their parents. By making the function's or block's next
+    /// fragment the parent, a circular dependency is avoided.
     pub parent: Option<FragmentId>,
+
     pub payload: FragmentPayload,
 }
 
