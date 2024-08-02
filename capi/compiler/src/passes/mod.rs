43603da1f79a36cb1cd50e4b22eedd78404ca370
Hanno Braun <hello@hannobraun.com>
2024-08-02 09:55:37 +0200

    Rename module to prepare for follow-on change

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,8 +1,8 @@
 mod a_resolve;
-mod b_fragments;
+mod c_fragments;
 mod d_bytecode;
 
 pub use {
-    a_resolve::resolve_references, b_fragments::generate_fragments,
+    a_resolve::resolve_references, c_fragments::generate_fragments,
     d_bytecode::generate_bytecode,
 };
