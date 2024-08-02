f040d3eeefc45667a435192adc22574a96e37017
Hanno Braun <hello@hannobraun.com>
2024-08-02 10:17:50 +0200

    Add test helper to prepare for more tests

--- capi/compiler/src/passes/a_tail_position.rs
+++ capi/compiler/src/passes/a_tail_position.rs
@@ -28,22 +28,30 @@ mod tests {
         determine_tail_position(&mut script.functions);
 
         let function = script.functions.remove(0);
-        let identifiers = function
-            .body
-            .iter()
-            .filter_map(|expression| {
-                if let Expression::Identifier {
-                    name,
-                    is_known_to_be_in_tail_position,
-                    ..
-                } = expression
-                {
-                    Some((name.as_str(), *is_known_to_be_in_tail_position))
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>();
+        let identifiers = function.body.to_identifiers();
         assert_eq!(identifiers, vec![("not_tail", false), ("tail", true)]);
+    }
+
+    trait ToIdentifiers {
+        fn to_identifiers(&self) -> Vec<(&str, bool)>;
+    }
+
+    impl ToIdentifiers for Vec<Expression> {
+        fn to_identifiers(&self) -> Vec<(&str, bool)> {
+            self.iter()
+                .filter_map(|expression| {
+                    if let Expression::Identifier {
+                        name,
+                        is_known_to_be_in_tail_position,
+                        ..
+                    } = expression
+                    {
+                        Some((name.as_str(), *is_known_to_be_in_tail_position))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        }
     }
 }
