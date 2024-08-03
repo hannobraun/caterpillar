df995a78340377bb85059111a749f10b7829a3b1
Hanno Braun <hello@hannobraun.com>
2024-08-03 20:27:48 +0200

    Simplify

--- capi/game-engine/src/game_engine.rs
+++ capi/game-engine/src/game_engine.rs
@@ -129,7 +129,7 @@ impl GameEngine {
                     Effect::Host(GameEngineEffect::ReadRandom) => {
                         // See `GameEngine::push_random` for context.
                         let random = self.random.pop_front().unwrap();
-                        self.process.push([Value(random.to_le_bytes())]);
+                        self.process.push([random]);
                     }
                     Effect::Host(GameEngineEffect::SetTile { x, y, color }) => {
                         let x = *x;
