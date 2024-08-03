aa296723d0fe655eb6da639cb6bc1dafdc98e8cd
Hanno Braun <hello@hannobraun.com>
2024-08-03 19:41:46 +0200

    Add `GameEngine::run_until_end_of_frame`

--- capi/game-engine/src/game_engine.rs
+++ capi/game-engine/src/game_engine.rs
@@ -1,9 +1,10 @@
 use std::collections::VecDeque;
 
-use capi_process::{Bytecode, Process, Value};
+use capi_process::{Bytecode, CoreEffect, Effect, Process, Value};
 
 use crate::{
-    host::{GameEngineHost, TILES_PER_AXIS},
+    display,
+    host::{GameEngineEffect, GameEngineHost, TILES_PER_AXIS},
     input::Input,
     memory::Memory,
 };
@@ -37,6 +38,88 @@ impl GameEngine {
     pub fn reset(&mut self) {
         self.memory = Memory::default();
         self.process.reset(self.arguments);
+    }
+
+    pub fn run_until_end_of_frame(&mut self, pixels: &mut [u8]) {
+        let Some(bytecode) = &self.bytecode else {
+            return;
+        };
+
+        while self.process.state().can_step() {
+            self.process.step(bytecode);
+
+            if let Some(effect) = self.process.state().first_unhandled_effect()
+            {
+                match effect {
+                    Effect::Core(CoreEffect::Breakpoint) => {
+                        // Nothing to do here. With an unhandled effect, the
+                        // program won't continue running. The debugger is in
+                        // control of what happens next.
+                    }
+                    Effect::Host(GameEngineEffect::Load { address }) => {
+                        let address: usize = (*address).into();
+                        let value = self.memory.inner[address];
+                        let value: i32 = value.into();
+                        self.process.push([Value(value.to_le_bytes())]);
+
+                        self.process.handle_first_effect();
+                    }
+                    Effect::Host(GameEngineEffect::Store {
+                        address,
+                        value,
+                    }) => {
+                        let address: usize = (*address).into();
+                        self.memory.inner[address] = *value;
+
+                        self.process.handle_first_effect();
+                    }
+                    Effect::Host(GameEngineEffect::SetTile { x, y, color }) => {
+                        let x = *x;
+                        let y = *y;
+                        let color = *color;
+
+                        self.process.handle_first_effect();
+
+                        display::set_tile(x.into(), y.into(), color, pixels);
+                    }
+                    Effect::Host(GameEngineEffect::SubmitFrame) => {
+                        // This effect means that the game is done rendering.
+                        // Let's break out of this loop now, so we can do our
+                        // part in that and return control to the host.
+                        self.process.handle_first_effect();
+                        break;
+                    }
+                    Effect::Host(GameEngineEffect::ReadInput) => {
+                        let input: i32 =
+                            self.input.buffer.pop_front().unwrap_or(0).into();
+
+                        self.process.push([Value(input.to_le_bytes())]);
+                        self.process.handle_first_effect();
+                    }
+                    Effect::Host(GameEngineEffect::ReadRandom) => {
+                        // We get a lot of random numbers from the host, and
+                        // they are topped off every frame. It should be a
+                        // while, before Caterpillar programs become complex
+                        // enough to run into this limit.
+                        //
+                        // If that happens, and we hit this `unwrap`, we can of
+                        // course just increase the limit. But long-term, it
+                        // probably makes more sense to implement a PRNG, either
+                        // in Rust or Caterpillar, and only seed that with
+                        // randomness from the host.
+                        let random = self.random.pop_front().unwrap();
+
+                        self.process.push([Value(random.to_le_bytes())]);
+                        self.process.handle_first_effect();
+                    }
+                    _ => {
+                        // Nothing needs to be done. With an unhandled
+                        // effect, the program won't continue running, and
+                        // the debugger will see the error and display it.
+                    }
+                }
+            }
+        }
     }
 }
 
--- capi/runtime/src/state.rs
+++ capi/runtime/src/state.rs
@@ -1,11 +1,7 @@
 use std::panic;
 
-use capi_game_engine::{
-    display,
-    game_engine::GameEngine,
-    host::{GameEngineEffect, GameEngineHost},
-};
-use capi_process::{CoreEffect, Effect, Value};
+use capi_game_engine::{game_engine::GameEngine, host::GameEngineHost};
+use capi_process::{CoreEffect, Effect};
 use capi_protocol::{
     command::{Command, SerializedCommand},
     updates::Updates,
@@ -74,98 +70,7 @@ impl RuntimeState {
             }
         }
 
-        let Some(bytecode) = &self.game_engine.bytecode else {
-            return;
-        };
-
-        while self.game_engine.process.state().can_step() {
-            self.game_engine.process.step(bytecode);
-
-            if let Some(effect) =
-                self.game_engine.process.state().first_unhandled_effect()
-            {
-                match effect {
-                    Effect::Core(CoreEffect::Breakpoint) => {
-                        // Nothing to do here. With an unhandled effect, the
-                        // program won't continue running. The debugger is in
-                        // control of what happens next.
-                    }
-                    Effect::Host(GameEngineEffect::Load { address }) => {
-                        let address: usize = (*address).into();
-                        let value = self.game_engine.memory.inner[address];
-                        let value: i32 = value.into();
-                        self.game_engine
-                            .process
-                            .push([Value(value.to_le_bytes())]);
-
-                        self.game_engine.process.handle_first_effect();
-                    }
-                    Effect::Host(GameEngineEffect::Store {
-                        address,
-                        value,
-                    }) => {
-                        let address: usize = (*address).into();
-                        self.game_engine.memory.inner[address] = *value;
-
-                        self.game_engine.process.handle_first_effect();
-                    }
-                    Effect::Host(GameEngineEffect::SetTile { x, y, color }) => {
-                        let x = *x;
-                        let y = *y;
-                        let color = *color;
-
-                        self.game_engine.process.handle_first_effect();
-
-                        display::set_tile(x.into(), y.into(), color, pixels);
-                    }
-                    Effect::Host(GameEngineEffect::SubmitFrame) => {
-                        // This effect means that the game is done rendering.
-                        // Let's break out of this loop now, so we can do our
-                        // part in that and return control to the host.
-                        self.game_engine.process.handle_first_effect();
-                        break;
-                    }
-                    Effect::Host(GameEngineEffect::ReadInput) => {
-                        let input: i32 = self
-                            .game_engine
-                            .input
-                            .buffer
-                            .pop_front()
-                            .unwrap_or(0)
-                            .into();
-
-                        self.game_engine
-                            .process
-                            .push([Value(input.to_le_bytes())]);
-                        self.game_engine.process.handle_first_effect();
-                    }
-                    Effect::Host(GameEngineEffect::ReadRandom) => {
-                        // We get a lot of random numbers from the host, and
-                        // they are topped off every frame. It should be a
-                        // while, before Caterpillar programs become complex
-                        // enough to run into this limit.
-                        //
-                        // If that happens, and we hit this `unwrap`, we can of
-                        // course just increase the limit. But long-term, it
-                        // probably makes more sense to implement a PRNG, either
-                        // in Rust or Caterpillar, and only seed that with
-                        // randomness from the host.
-                        let random =
-                            self.game_engine.random.pop_front().unwrap();
-
-                        self.game_engine
-                            .process
-                            .push([Value(random.to_le_bytes())]);
-                        self.game_engine.process.handle_first_effect();
-                    }
-                    _ => {
-                        // Nothing needs to be done. With an unhandled
-                        // effect, the program won't continue running, and
-                        // the debugger will see the error and display it.
-                    }
-                }
-            }
-        }
+        self.game_engine.run_until_end_of_frame(pixels);
 
         self.updates
             .queue_updates(&self.game_engine.process, &self.game_engine.memory);
