3f59f5363606585226773eb4f9e56af8f95dead3
Hanno Braun <hello@hannobraun.com>
2024-08-04 11:45:02 +0200

    Clean up imports

--- capi/launcher/src/main.rs
+++ capi/launcher/src/main.rs
@@ -1,9 +1,12 @@
+use capi_game_engine::{game_engine::GameEngine, tiles::NUM_PIXEL_BYTES};
+use capi_watch::build_game_once;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let code = capi_watch::build_game_once("snake").await?;
+    let code = build_game_once("snake").await?;
 
-    let mut pixels = [0; capi_game_engine::tiles::NUM_PIXEL_BYTES];
-    let mut game_engine = capi_game_engine::game_engine::GameEngine::new();
+    let mut pixels = [0; NUM_PIXEL_BYTES];
+    let mut game_engine = GameEngine::new();
 
     game_engine.on_new_bytecode(code.bytecode);
 
