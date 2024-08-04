671144b9b195ec282b0ecbba2348c32866861675
Hanno Braun <hello@hannobraun.com>
2024-08-04 11:39:54 +0200

    Run game in launcher
    It doesn't work yet, as no randomness is provided.

--- capi/launcher/src/main.rs
+++ capi/launcher/src/main.rs
@@ -1,6 +1,15 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let code = capi_watch::build_game_once("snake").await?;
-    dbg!(code);
+
+    let mut pixels = [0; capi_game_engine::tiles::NUM_PIXEL_BYTES];
+    let mut game_engine = capi_game_engine::game_engine::GameEngine::new();
+
+    game_engine.on_new_bytecode(code.bytecode);
+
+    while !game_engine.process.state().has_finished() {
+        game_engine.run_until_end_of_frame(&mut pixels)
+    }
+
     Ok(())
 }
