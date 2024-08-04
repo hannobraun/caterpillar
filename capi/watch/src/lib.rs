425cbcc23cbb4df225cc756ca267d1120fe75758
Hanno Braun <hello@hannobraun.com>
2024-08-04 11:05:49 +0200

    Add re-export

--- capi/watch/src/lib.rs
+++ capi/watch/src/lib.rs
@@ -3,4 +3,6 @@ pub mod build;
 mod debounce;
 mod watcher;
 
-pub use self::{debounce::DebouncedChanges, watcher::Watcher};
+pub use self::{
+    build::build_game_once, debounce::DebouncedChanges, watcher::Watcher,
+};
