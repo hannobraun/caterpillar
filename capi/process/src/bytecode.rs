61ef5e566ad28bb4e13ebc8c5075619de30c4347
Hanno Braun <hello@hannobraun.com>
2024-08-06 10:57:26 +0200

    Document `function_arguments` field of `Bytecode`

--- capi/process/src/bytecode.rs
+++ capi/process/src/bytecode.rs
@@ -13,5 +13,27 @@ use crate::{InstructionAddress, Instructions};
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct Bytecode {
     pub instructions: Instructions,
+
+    /// # The arguments of each function
+    ///
+    /// ## Implementation Note
+    ///
+    /// This is used by the evaluator to perform function calls, but it is not
+    /// really desirable to do it this way. There's really no good reason for
+    /// the evaluator to know about the names of functions, except that the
+    /// current implementation requires it.
+    ///
+    /// This is tied to how bindings are handled: Those are also known by name
+    /// to the evaluator, and there's no good reason for that, except that it's
+    /// how it works right now.
+    ///
+    /// Eventually, the compiler should get smarter, and place bindings at fixed
+    /// addresses within a stack frame. Then it can generate the right
+    /// instruction to get the evaluator to do the right things, and no more
+    /// names need to be known at runtime.
+    ///
+    /// Once that has been achieved, this field can be removed. (And maybe this
+    /// whole struct along with it, to be replaced by `Instructions`. But that's
+    /// another topic.)
     pub function_arguments: BTreeMap<InstructionAddress, Vec<String>>,
 }
