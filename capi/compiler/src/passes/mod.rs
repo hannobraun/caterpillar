6f56101be4b9fea040dc1bd477552996ce8bc1a4
Hanno Braun <hello@hannobraun.com>
2024-08-07 01:49:04 +0200

    Update module name

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,11 +1,11 @@
 mod a_tail_position;
-mod b_groups;
 mod b_resolve;
+mod c_clusters;
 mod d_fragments;
 mod e_bytecode;
 
 pub use {
-    a_tail_position::determine_tail_positions, b_groups::group_functions,
-    b_resolve::resolve_identifiers, d_fragments::generate_fragments,
+    a_tail_position::determine_tail_positions, b_resolve::resolve_identifiers,
+    c_clusters::group_functions, d_fragments::generate_fragments,
     e_bytecode::generate_bytecode,
 };
