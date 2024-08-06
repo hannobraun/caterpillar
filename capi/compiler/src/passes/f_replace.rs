d36422c45c29ef75a75dc88c0465bd3b354d0b55
Hanno Braun <hello@hannobraun.com>
2024-08-06 13:49:48 +0200

    Add placeholder for compiler pass

---
+++ capi/compiler/src/passes/f_replace.rs
@@ -0,0 +1,1 @@
+pub fn replace_placeholders() {}
--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -3,9 +3,10 @@ mod b_groups;
 mod c_resolve;
 mod d_fragments;
 mod e_bytecode;
+mod f_replace;
 
 pub use {
     a_tail_position::determine_tail_positions, b_groups::group_functions,
     c_resolve::resolve_identifiers, d_fragments::generate_fragments,
-    e_bytecode::generate_bytecode,
+    e_bytecode::generate_bytecode, f_replace::replace_placeholders,
 };
