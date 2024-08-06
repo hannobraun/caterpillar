2256406d740b991aab5f25cdc3dabe8ae8d3e07a
Hanno Braun <hello@hannobraun.com>
2024-08-06 11:36:10 +0200

    Set `Function`'s `group_index`

--- capi/compiler/src/passes/c_groups.rs
+++ capi/compiler/src/passes/c_groups.rs
@@ -1,3 +1,13 @@
+use std::collections::BTreeMap;
+
 use crate::repr::syntax::Function;
 
-pub fn group_functions(_: &mut Vec<Function>) {}
+pub fn group_functions(functions: &mut Vec<Function>) {
+    let mut groups = BTreeMap::new();
+
+    for function in functions {
+        let next_group_index = groups.entry(function.name.clone()).or_default();
+        function.group_index = Some(*next_group_index);
+        *next_group_index += 1;
+    }
+}
