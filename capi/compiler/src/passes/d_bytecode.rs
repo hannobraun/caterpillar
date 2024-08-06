dd40390f5e452a9f1511c3774e4b4bf29b281f61
Hanno Braun <hello@hannobraun.com>
2024-08-06 10:48:50 +0200

    Prepare for follow-on change

--- capi/compiler/src/passes/d_bytecode.rs
+++ capi/compiler/src/passes/d_bytecode.rs
@@ -152,9 +152,9 @@ impl Compiler<'_> {
                 .collect(),
             start,
         };
+        let arguments = function.arguments;
 
-        self.function_arguments_by_address
-            .insert(start, function.arguments);
+        self.function_arguments_by_address.insert(start, arguments);
         self.function_addresses_by_name.insert(name, function.start);
     }
 
