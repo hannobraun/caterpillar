2237a35984ea5c8c9d3edf141a16d4621f2bc3e1
Hanno Braun <hello@hannobraun.com>
2024-08-06 11:44:49 +0200

    Prepare for follow-on change

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -2,10 +2,10 @@ mod a_tail_position;
 mod b_groups;
 mod c_resolve;
 mod d_fragments;
-mod e_bytecode;
+mod f_bytecode;
 
 pub use {
     a_tail_position::determine_tail_positions, b_groups::group_functions,
     c_resolve::resolve_identifiers, d_fragments::generate_fragments,
-    e_bytecode::generate_bytecode,
+    f_bytecode::generate_bytecode,
 };
