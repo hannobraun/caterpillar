9c62cfda4adfaa37bc2c07ca4e2c573390dab02e
Hanno Braun <hello@hannobraun.com>
2024-08-07 02:42:29 +0200

    Group functions with same name into clusters

--- capi/compiler/src/passes/c_clusters.rs
+++ capi/compiler/src/passes/c_clusters.rs
@@ -4,14 +4,27 @@ pub fn find_clusters(functions: Vec<Function>) -> Vec<Cluster> {
     let mut clusters = Vec::<Cluster>::new();
 
     for function in functions {
-        clusters.push(Cluster {
-            members: vec![function],
-        });
+        let cluster = clusters
+            .iter_mut()
+            .find(|cluster| cluster.name == function.name);
+
+        match cluster {
+            Some(cluster) => {
+                cluster.members.push(function);
+            }
+            None => {
+                clusters.push(Cluster {
+                    name: function.name.clone(),
+                    members: vec![function],
+                });
+            }
+        }
     }
 
     clusters
 }
 
 pub struct Cluster {
+    pub name: String,
     pub members: Vec<Function>,
 }
