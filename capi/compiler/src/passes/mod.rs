299ee7ca16950bdeef2db501e66391c666a51492
Hanno Braun <hello@hannobraun.com>
2024-08-07 13:36:30 +0200

    Simplify module name

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,11 +1,11 @@
-mod b_resolve;
 mod c_clusters;
 mod d_fragments;
 mod e_bytecode;
+mod resolve;
 mod tail_position;
 
 pub use {
-    b_resolve::resolve_identifiers, c_clusters::find_clusters,
-    d_fragments::generate_fragments, e_bytecode::generate_bytecode,
+    c_clusters::find_clusters, d_fragments::generate_fragments,
+    e_bytecode::generate_bytecode, resolve::resolve_identifiers,
     tail_position::determine_tail_positions,
 };
