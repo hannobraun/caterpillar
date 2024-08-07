e0a2614bb99df0ec2fd85eaf47d32d506d7495c7
Hanno Braun <hello@hannobraun.com>
2024-08-07 02:53:15 +0200

    Prepare for follow-on change

--- capi/compiler/src/fragments/fragment.rs
+++ capi/compiler/src/fragments/fragment.rs
@@ -37,7 +37,7 @@ impl Fragment {
 
     pub fn next(&self) -> Option<FragmentId> {
         match &self.payload {
-            FragmentPayload::Cluster { members } => {
+            FragmentPayload::Cluster { members, .. } => {
                 members.last().map(|function| function.next)
             }
             FragmentPayload::Expression { next, .. } => Some(*next),
