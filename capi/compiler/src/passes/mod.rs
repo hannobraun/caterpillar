db14be987db7b6580f7ca67f3a637b53daa8f363
Hanno Braun <hello@hannobraun.com>
2024-08-07 13:49:26 +0200

    Make module name more specific

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,11 +1,11 @@
 mod bytecode;
 mod clusters;
-mod fragments;
+mod generate_fragments;
 mod resolve;
 mod tail_position;
 
 pub use {
     bytecode::generate_bytecode, clusters::find_clusters,
-    fragments::generate_fragments, resolve::resolve_identifiers,
+    generate_fragments::generate_fragments, resolve::resolve_identifiers,
     tail_position::determine_tail_positions,
 };
