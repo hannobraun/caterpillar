cf8b3645840a1dbfbcc3d9161399e428b6218310
Hanno Braun <hello@hannobraun.com>
2024-08-07 21:23:55 +0200

    Document `IdentifierTarget::Cluster`

--- capi/compiler/src/syntax/expression.rs
+++ capi/compiler/src/syntax/expression.rs
@@ -60,6 +60,23 @@ pub enum Expression {
 pub enum IdentifierTarget {
     Binding,
     BuiltinFunction,
-    Cluster { all_arguments: Vec<Vec<Pattern>> },
+
+    /// The identifier resolves to a cluster of functions
+    Cluster {
+        /// # The arguments of all functions in the cluster
+        ///
+        /// ## Implementation Note
+        ///
+        /// Making this available here is a bit of a hack. It is only required
+        /// to also make the arguments available in
+        /// `FragmentExpression::ResolvedCluster`, which itself is a hack to
+        /// avoid some larger complications that would go to far right now. See
+        /// documentation of that, for more information.
+        ///
+        /// Once the arguments are no longer required there, this field can
+        /// probably be removed too.
+        all_arguments: Vec<Vec<Pattern>>,
+    },
+
     HostFunction,
 }
