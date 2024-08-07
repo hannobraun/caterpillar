1c46fb74340af97dc0904f8cc1acada840ba8cb2
Hanno Braun <hello@hannobraun.com>
2024-08-07 12:25:11 +0200

    Improve wording

--- capi/compiler/src/fragments/fragment.rs
+++ capi/compiler/src/fragments/fragment.rs
@@ -51,8 +51,8 @@ pub enum FragmentPayload {
     /// Functions are called by cluster, and the call is dispatched to a
     /// function within the cluster using pattern matching on their arguments.
     ///
-    /// For the sake of uniformity, all functions are organized within clusters,
-    /// so a cluster might have a single member.
+    /// For the sake of uniformity, all functions are organized into clusters,
+    /// so a cluster might have only a single member.
     Cluster {
         cluster: Cluster,
         next: FragmentId,
