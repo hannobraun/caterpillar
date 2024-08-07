326778fb9c89dfa116572fe7274812d0ef03cabd
Hanno Braun <hello@hannobraun.com>
2024-08-07 13:17:56 +0200

    Move `compile` function to dedicated module

---
+++ capi/compiler/src/compile.rs
@@ -0,0 +1,17 @@
+use crate::passes;
+
+pub fn compile<H: capi_process::Host>(
+    mut script: crate::syntax::Script,
+) -> (
+    crate::fragments::Fragments,
+    capi_process::Bytecode,
+    crate::source_map::SourceMap,
+) {
+    passes::determine_tail_positions(&mut script.functions);
+    passes::resolve_identifiers::<H>(&mut script.functions);
+    let clusters = passes::find_clusters(script.functions);
+    let fragments = passes::generate_fragments(clusters);
+    let (bytecode, source_map) = passes::generate_bytecode(fragments.clone());
+
+    (fragments, bytecode, source_map)
+}
--- capi/compiler/src/lib.rs
+++ capi/compiler/src/lib.rs
@@ -2,23 +2,10 @@ pub mod fragments;
 pub mod source_map;
 pub mod syntax;
 
+mod compile;
 mod passes;
 
 #[cfg(test)]
 mod tests;
 
-pub fn compile<H: capi_process::Host>(
-    mut script: crate::syntax::Script,
-) -> (
-    crate::fragments::Fragments,
-    capi_process::Bytecode,
-    crate::source_map::SourceMap,
-) {
-    passes::determine_tail_positions(&mut script.functions);
-    passes::resolve_identifiers::<H>(&mut script.functions);
-    let clusters = passes::find_clusters(script.functions);
-    let fragments = passes::generate_fragments(clusters);
-    let (bytecode, source_map) = passes::generate_bytecode(fragments.clone());
-
-    (fragments, bytecode, source_map)
-}
+pub use self::compile::compile;
