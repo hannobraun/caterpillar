793d9c5538d87bd0cae5dfd3534e4a904c793578
Hanno Braun <hello@hannobraun.com>
2024-08-10 10:56:50 +0200

    Make module name more explicit

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,11 +1,12 @@
 mod clusters;
 mod generate_fragments;
 mod generate_instructions;
-mod resolve;
+mod resolve_identifiers;
 mod tail_position;
 
 pub use {
     clusters::find_clusters, generate_fragments::generate_fragments,
-    generate_instructions::generate_instructions, resolve::resolve_identifiers,
+    generate_instructions::generate_instructions,
+    resolve_identifiers::resolve_identifiers,
     tail_position::determine_tail_positions,
 };
