bb29bd017b4770c0e46ac03965614e1f712b091a
Hanno Braun <hello@hannobraun.com>
2024-08-14 13:44:34 +0200

    Simplify

--- capi/compiler/src/compile.rs
+++ capi/compiler/src/compile.rs
@@ -4,20 +4,19 @@ use crate::{
     fragments::Fragments,
     host::Host,
     passes::{
-        determine_tail_positions, find_functions, generate_fragments,
-        generate_instructions, resolve_identifiers,
+        determine_tail_positions, generate_fragments, generate_instructions,
+        resolve_identifiers,
     },
     source_map::SourceMap,
     syntax::Script,
 };
 
 pub fn compile<H: Host>(
-    script: Script,
+    mut script: Script,
 ) -> (Fragments, Instructions, SourceMap) {
-    let mut functions = find_functions(script.branches);
-    determine_tail_positions(&mut functions);
-    resolve_identifiers::<H>(&mut functions);
-    let fragments = generate_fragments(functions);
+    determine_tail_positions(&mut script.functions);
+    resolve_identifiers::<H>(&mut script.functions);
+    let fragments = generate_fragments(script.functions);
     let (instructions, source_map) =
         generate_instructions::<H>(fragments.clone());
 
--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,11 +1,10 @@
-mod find_functions;
 mod generate_fragments;
 mod generate_instructions;
 mod resolve_identifiers;
 mod tail_position;
 
 pub use {
-    find_functions::find_functions, generate_fragments::generate_fragments,
+    generate_fragments::generate_fragments,
     generate_instructions::generate_instructions,
     resolve_identifiers::resolve_identifiers,
     tail_position::determine_tail_positions,
