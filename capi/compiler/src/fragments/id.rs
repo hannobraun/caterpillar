9ed1ee2b816f80b111ee8baf77fc3a64f0c1ebc6
Hanno Braun <hello@hannobraun.com>
2024-08-14 20:55:17 +0200

    Prepare for follow-on change

--- capi/compiler/src/fragments/id.rs
+++ capi/compiler/src/fragments/id.rs
@@ -4,7 +4,7 @@ use std::{cmp::Ordering, fmt};
     Clone, Copy, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize,
 )]
 pub struct FragmentId {
-    hash: blake3::Hash,
+    pub(super) hash: blake3::Hash,
 }
 
 impl FragmentId {
