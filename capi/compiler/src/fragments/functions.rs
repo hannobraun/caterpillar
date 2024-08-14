aff4a27f57944c387e7557152e496ab5db4d98b2
Hanno Braun <hello@hannobraun.com>
2024-08-14 20:37:26 +0200

    Document `name` field of `Function`

--- capi/compiler/src/fragments/functions.rs
+++ capi/compiler/src/fragments/functions.rs
@@ -4,6 +4,19 @@ use super::FragmentId;
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct Function {
+    /// # The name of this function, if available
+    ///
+    /// A name is not available for anonymous functions.
+    ///
+    /// ## Implementation Note
+    ///
+    /// This happens to work for now, but it's most likely a stopgap. It makes
+    /// more sense to associate a name with a function were it is defined. As of
+    /// this writing, this would be the root scope for all named functions. In
+    /// the future, it might be any module.
+    ///
+    /// This would also allow supporting function aliases, which would break the
+    /// assumption that all functions have at most one name.
     pub name: Option<String>,
 
     /// # The branches of this function
