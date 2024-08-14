ab33d47b4ef0bfc275a78365496e89daef7e664c
Hanno Braun <hello@hannobraun.com>
2024-08-14 12:14:39 +0200

    Update module name

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,11 +1,11 @@
-mod clusters;
+mod find_functions;
 mod generate_fragments;
 mod generate_instructions;
 mod resolve_identifiers;
 mod tail_position;
 
 pub use {
-    clusters::find_functions, generate_fragments::generate_fragments,
+    find_functions::find_functions, generate_fragments::generate_fragments,
     generate_instructions::generate_instructions,
     resolve_identifiers::resolve_identifiers,
     tail_position::determine_tail_positions,
