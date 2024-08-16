d8ab670d045f083503da42d8f1165afc5bbb862e
Hanno Braun <hello@hannobraun.com>
2024-08-16 22:19:12 +0200

    Update parameter name

--- games/snake/src/main.rs
+++ games/snake/src/main.rs
@@ -577,7 +577,7 @@ fn snake(script: &mut Script) {
                             },
                         )
                     })
-                    .fun(|b| b.branch(|b| b, |_| {}))
+                    .fun(|b| b.branch(|p| p, |_| {}))
                     .ident("if");
             },
         )
