805c58b86b471866e19e67728f92951f4ec2335a
Hanno Braun <hello@hannobraun.com>
2024-08-17 11:06:38 +0200

    Tokenize comments

--- capi/compiler/src/passes/tokenize.rs
+++ capi/compiler/src/passes/tokenize.rs
@@ -1,6 +1,41 @@
 pub fn tokenize(_source: String) -> Vec<Token> {
-    Vec::new()
+    let mut state = State::Initial;
+    let mut tokens = Vec::new();
+
+    for c in _source.chars() {
+        match state {
+            State::Initial => match c {
+                '#' => {
+                    state = State::Comment {
+                        buffer: String::new(),
+                    };
+                }
+                c => {
+                    eprintln!("Unexpected char: `{c}`");
+                }
+            },
+            State::Comment { mut buffer } => match c {
+                '\n' => {
+                    tokens.push(Token::Comment { text: buffer });
+                    state = State::Initial;
+                }
+                c => {
+                    buffer.push(c);
+                    state = State::Comment { buffer }
+                }
+            },
+        }
+    }
+
+    tokens
 }
 
 #[derive(Debug)]
-pub enum Token {}
+pub enum Token {
+    Comment { text: String },
+}
+
+enum State {
+    Initial,
+    Comment { buffer: String },
+}
