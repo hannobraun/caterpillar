0fc9d677dd0e83c9a21e0b58db0e6c8a19990846
Hanno Braun <hello@hannobraun.com>
2024-08-17 10:58:53 +0200

    Call `tokenize`

--- games/snake/src/main.rs
+++ games/snake/src/main.rs
@@ -1,11 +1,12 @@
 use std::{fs::File, io::Read};
 
-use capi_compiler::syntax::Script;
+use capi_compiler::{syntax::Script, tokenize};
 
 pub fn main() -> anyhow::Result<()> {
     let mut source = String::new();
     File::open("games/snake/snake.capi")?.read_to_string(&mut source)?;
-    dbg!(source);
+    let tokens = tokenize(source);
+    dbg!(tokens);
 
     let mut script = Script::default();
     snake(&mut script);
