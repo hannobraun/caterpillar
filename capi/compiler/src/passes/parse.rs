3f0ae56445c984d7fde57aff80ee1a4b48567408
Hanno Braun <hello@hannobraun.com>
2024-08-17 13:21:38 +0200

    Parse function names

--- capi/compiler/src/passes/parse.rs
+++ capi/compiler/src/passes/parse.rs
@@ -1,7 +1,75 @@
-use crate::syntax::Script;
+use std::collections::{BTreeSet, VecDeque};
+
+use crate::syntax::{Function, Script};
 
 use super::tokenize::Token;
 
-pub fn parse(_: Vec<Token>) -> Script {
-    Script::default()
+pub fn parse(tokens: Vec<Token>) -> Script {
+    let mut tokens = Tokens {
+        inner: tokens.into(),
+    };
+    let mut script = Script::default();
+
+    while let Some(function) = parse_named_function(&mut tokens) {
+        script.functions.push(function);
+    }
+
+    script
+}
+
+fn parse_named_function(tokens: &mut Tokens) -> Option<Function> {
+    let name = loop {
+        match tokens.inner.pop_front()? {
+            Token::Comment { .. } => {
+                // Comments in the top-level context are currently ignored.
+                continue;
+            }
+            Token::FunctionName { name } => {
+                break name;
+            }
+            token => {
+                eprintln!("Unexpected token: {token:?}");
+                return None;
+            }
+        }
+    };
+
+    parse_function(tokens);
+
+    Some(Function {
+        name: Some(name),
+        branches: Vec::new(),
+        environment: BTreeSet::new(),
+    })
+}
+
+fn parse_function(tokens: &mut Tokens) -> Option<Function> {
+    match tokens.inner.pop_front()? {
+        Token::FunctionStart => {}
+        token => {
+            eprintln!("Unexpected token: {token:?}");
+            return None;
+        }
+    }
+
+    while let Some(token) = tokens.inner.front() {
+        match token {
+            Token::FunctionStart => {
+                parse_function(tokens);
+            }
+            Token::FunctionEnd => {
+                tokens.inner.pop_front();
+                break;
+            }
+            _ => {
+                tokens.inner.pop_front();
+            }
+        }
+    }
+
+    Some(Function::default())
+}
+
+struct Tokens {
+    inner: VecDeque<Token>,
 }
