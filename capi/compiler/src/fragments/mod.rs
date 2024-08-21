017aef5a72d1b668a19115ea769acf478b935317
Hanno Braun <hello@hannobraun.com>
2024-08-21 11:31:36 +0200

    Update name of module

--- capi/compiler/src/fragments/mod.rs
+++ capi/compiler/src/fragments/mod.rs
@@ -1,16 +1,16 @@
 #![allow(clippy::module_inception)]
 
-mod expression;
 mod fragment;
 mod fragments;
 mod function;
 mod hash;
 mod id;
+mod payload;
 
 pub use self::{
-    expression::Payload,
     fragment::{Fragment, FragmentKind},
     fragments::{FragmentMap, Fragments},
     function::{Branch, Function, Parameters},
     id::FragmentId,
+    payload::Payload,
 };
