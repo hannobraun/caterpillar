ebb3a3717a414ac72825d47c871daa3201065f07
Hanno Braun <hello@hannobraun.com>
2024-08-25 09:09:38 +0200

    Prepare for follow-on change

--- capi/debugger/src/ui/start.rs
+++ capi/debugger/src/ui/start.rs
@@ -3,10 +3,13 @@ use capi_protocol::{
     Versioned,
 };
 use gloo_net::http::Request;
-use leptos::{create_signal, SignalSet};
+use leptos::{create_signal, SignalSet, WriteSignal};
 use tokio::sync::mpsc;
 
-use crate::{debugger::RemoteProcess, ui::components::debugger::Debugger};
+use crate::{
+    debugger::{Debugger, RemoteProcess},
+    ui::components::debugger::Debugger,
+};
 
 use super::CommandsTx;
 
@@ -38,10 +41,18 @@ pub fn start(
                 break;
             };
 
-            let update = Update::deserialize(update);
-            remote_process.on_runtime_update(update);
-
-            debugger_write.set(remote_process.to_debugger());
+            on_update(update, &mut remote_process, &debugger_write)
         }
     });
+}
+
+fn on_update(
+    update: Vec<u8>,
+    remote_process: &mut RemoteProcess,
+    debugger_write: &WriteSignal<Debugger>,
+) {
+    let update = Update::deserialize(update);
+    remote_process.on_runtime_update(update);
+
+    debugger_write.set(remote_process.to_debugger());
 }
