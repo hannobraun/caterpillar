28073862ee7df1932a80fcad06e9f0f8819842cc
Hanno Braun <hello@hannobraun.com>
2024-08-30 11:56:40 +0200

    Generate `Intrinsics` using macro

--- capi/compiler/src/intrinsics.rs
+++ capi/compiler/src/intrinsics.rs
@@ -1,43 +1,55 @@
 use std::fmt;
 
-/// # Special functions that are known to the compiler
-///
-/// When encountering a call to an intrinsic, the compiler will directly
-/// translate that into the appropriate instructions.
-#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
-pub enum Intrinsic {
+macro_rules! intrinsics {
+    (
+        $(
+            #[$attrs:meta]
+            $name:expr,
+            $variant:ident;
+        )*
+    ) => {
+        /// # Special functions that are known to the compiler
+        ///
+        /// When encountering a call to an intrinsic, the compiler will directly
+        /// translate that into the appropriate instructions.
+        #[derive(
+            Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize,
+        )]
+        pub enum Intrinsic {
+            $($variant,)*
+        }
+
+        impl Intrinsic {
+            pub fn from_name(name: &str) -> Option<Intrinsic> {
+                let intrinsic = match name {
+                    $($name => Self::$variant,)*
+
+                    _ => {
+                        return None;
+                    }
+                };
+
+                Some(intrinsic)
+            }
+        }
+
+        impl fmt::Display for Intrinsic {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match self {
+                    $(Self::$variant => write!(f, $name),)*
+                }
+            }
+        }
+    };
+}
+
+intrinsics! {
     /// # Add two signed 8-bit integers
-    AddS8,
+    "add_s8", AddS8;
 
     /// # Trigger a breakpoint
-    Brk,
+    "brk", Brk;
 
     /// # Evaluate an anonymous function
-    Eval,
-}
-
-impl Intrinsic {
-    pub fn from_name(name: &str) -> Option<Intrinsic> {
-        let intrinsic = match name {
-            "add_s8" => Self::AddS8,
-            "brk" => Self::Brk,
-            "eval" => Self::Eval,
-
-            _ => {
-                return None;
-            }
-        };
-
-        Some(intrinsic)
-    }
-}
-
-impl fmt::Display for Intrinsic {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::AddS8 => write!(f, "add_s8"),
-            Self::Brk => write!(f, "brk"),
-            Self::Eval => write!(f, "eval"),
-        }
-    }
+    "eval", Eval;
 }
