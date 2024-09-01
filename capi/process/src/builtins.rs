4c761ce1cebd1a7eca3dae567d74c2e49340008b
Hanno Braun <hello@hannobraun.com>
2024-09-01 22:42:56 +0200

    Remove shadowed built-in function

--- capi/process/src/builtins.rs
+++ capi/process/src/builtins.rs
@@ -1,30 +1,9 @@
 use crate::{Effect, Stack};
 
-pub fn builtin_by_name(name: &str) -> Option<Builtin> {
-    let builtin = match name {
-        "sub_u8_wrap" => sub_u8_wrap,
-
-        _ => {
-            return None;
-        }
-    };
-
-    Some(builtin)
+pub fn builtin_by_name(_name: &str) -> Option<Builtin> {
+    None
 }
 
 pub type Builtin = fn(&mut Stack) -> Result;
-
-fn sub_u8_wrap(stack: &mut Stack) -> Result {
-    let b = stack.pop_operand()?;
-    let a = stack.pop_operand()?;
-
-    let a = a.to_u8()?;
-    let b = b.to_u8()?;
-
-    let c = a.wrapping_sub(b);
-    stack.push_operand(c);
-
-    Ok(())
-}
 
 pub type Result = std::result::Result<(), Effect>;
