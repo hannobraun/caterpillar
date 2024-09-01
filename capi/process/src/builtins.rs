0e915cd1b09ad89da56cc41d1a6120d1b31b4760
Hanno Braun <hello@hannobraun.com>
2024-09-01 22:35:57 +0200

    Remove shadowed built-in function

--- capi/process/src/builtins.rs
+++ capi/process/src/builtins.rs
@@ -1,8 +1,7 @@
-use crate::{value::IntegerOverflow, Effect, Stack};
+use crate::{Effect, Stack};
 
 pub fn builtin_by_name(name: &str) -> Option<Builtin> {
     let builtin = match name {
-        "sub_u8" => sub_u8,
         "sub_u8_wrap" => sub_u8_wrap,
 
         _ => {
@@ -14,22 +13,6 @@ pub fn builtin_by_name(name: &str) -> Option<Builtin> {
 }
 
 pub type Builtin = fn(&mut Stack) -> Result;
-
-fn sub_u8(stack: &mut Stack) -> Result {
-    let b = stack.pop_operand()?;
-    let a = stack.pop_operand()?;
-
-    let a = a.to_u8()?;
-    let b = b.to_u8()?;
-
-    let Some(c) = a.checked_sub(b) else {
-        return Err(IntegerOverflow.into());
-    };
-
-    stack.push_operand(c);
-
-    Ok(())
-}
 
 fn sub_u8_wrap(stack: &mut Stack) -> Result {
     let b = stack.pop_operand()?;
