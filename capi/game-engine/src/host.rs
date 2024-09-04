555938ae887f369953e84ca287354f434ff721f5
Hanno Braun <hello@hannobraun.com>
2024-09-04 12:52:00 +0200

    Document `GameEngineEffect`

--- capi/game-engine/src/host.rs
+++ capi/game-engine/src/host.rs
@@ -22,6 +22,18 @@ impl Host for GameEngineHost {
     }
 }
 
+/// # An effect handled by the game engine host
+///
+/// ## Implementation Note
+///
+/// The host functions that are backed by these effects are not purely
+/// functional. Long-term, they should be, but for now there's not much point to
+/// it.
+///
+/// Without a type system, piping any values that represent I/O resources
+/// through host functions is only complexity for now real gain. And without a
+/// _linear_ type system, there's no way to guarantee any sane semantics around
+/// such functions anyway.
 #[derive(
     Clone,
     Copy,
@@ -35,12 +47,84 @@ impl Host for GameEngineHost {
 )]
 #[repr(u8)]
 pub enum GameEngineEffect {
+    /// # Load a value from a given memory address
+    ///
+    /// ## Input
+    ///
+    /// - `u8`: The address of the value to read.
+    ///
+    /// ## Output
+    ///
+    /// - `u8`: The value at the provided address address.
     Load,
+
+    /// # Store a value at the given memory address
+    ///
+    /// ## Input
+    ///
+    /// - `u8`: The value to store.
+    /// - `u8`: The address to store the value at.
+    ///
+    /// ## Output
+    ///
+    /// none
     Store,
 
+    /// # Read the next input event from the buffer
+    ///
+    /// ## Input
+    ///
+    /// none
+    ///
+    /// ## Output
+    ///
+    /// - `u8`: A value representing the type of input event.
     ReadInput,
+
+    /// # Read a random value from the buffer
+    ///
+    /// ## Input
+    ///
+    /// none
+    ///
+    /// ## Output
+    ///
+    /// - `s32`: The random value.
     ReadRandom,
 
+    /// # Set a pixel in the frame buffer
+    ///
+    /// ## Input
+    ///
+    /// - `u8`: The x-coordinate of the pixel.
+    /// - `u8`: The y-coordinate of the pixel.
+    /// - `u8`: The red channel value of the pixel.
+    /// - `u8`: The green channel value of the pixel.
+    /// - `u8`: The blue channel value of the pixel.
+    /// - `u8`: The alpha channel value of the pixel.
+    ///
+    /// ## Output
+    ///
+    /// none
     SetPixel,
+
+    /// # Submit the current frame, causing the game engine to display it
+    ///
+    /// This must be called regularly, or the game engine will freeze.
+    ///
+    /// ## Input
+    ///
+    /// none
+    ///
+    /// ## Output
+    ///
+    /// none
+    ///
+    /// ## Implementation Note
+    ///
+    /// The possibility of the game engine freezing is undesirable. At some
+    /// future point, there will likely be an enforced timeout. This is tracked
+    /// in the following issue:
+    /// <https://github.com/hannobraun/caterpillar/issues/42>
     SubmitFrame,
 }
