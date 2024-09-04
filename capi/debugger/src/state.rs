29e31c04e9ecd1a3de1fcc1e39874c7e22a7b65e
Hanno Braun <hello@hannobraun.com>
2024-09-04 23:14:34 +0200

    Remove non-UI code out of `ui` module

--- capi/debugger/src/state.rs
+++ capi/debugger/src/state.rs
@@ -1,7 +1,15 @@
-use capi_protocol::updates::SerializedUpdate;
-use tokio::sync::mpsc;
+use capi_protocol::{
+    updates::{Code, SerializedUpdate, Update},
+    Versioned,
+};
+use gloo_net::http::{Request, Response};
+use leptos::SignalSet;
+use tokio::{select, sync::mpsc};
 
-use crate::ui::{self, CommandsRx};
+use crate::{
+    debugger::RemoteProcess,
+    ui::{self, CommandsRx},
+};
 
 pub struct DebuggerState {
     pub updates_tx: mpsc::UnboundedSender<SerializedUpdate>,
@@ -11,9 +19,40 @@ pub struct DebuggerState {
 impl DebuggerState {
     pub fn new() -> Self {
         let (commands_tx, commands_rx) = mpsc::unbounded_channel();
-        let (updates_tx, updates_rx) = mpsc::unbounded_channel();
+        let (updates_tx, mut updates_rx) = mpsc::unbounded_channel();
 
-        ui::start(updates_rx, commands_tx);
+        let mut remote_process = RemoteProcess::default();
+        let (debugger_read, debugger_write) =
+            leptos::create_signal(remote_process.to_debugger());
+
+        leptos::spawn_local(async move {
+            let code = Request::get("/code").send().await;
+            let mut timestamp = on_new_code(code, &mut remote_process).await;
+
+            loop {
+                select! {
+                    code = Request::get(&format!("/code/{timestamp}")).send() => {
+                        timestamp = on_new_code(code, &mut remote_process).await;
+                    }
+                    update = updates_rx.recv() => {
+                        let Some(update) = update else {
+                            // This means the other end has hung up. Nothing we
+                            // can do, except end this task too.
+                            break;
+                        };
+
+                        on_process_update(
+                            update,
+                            &mut remote_process,
+                        );
+                    }
+                }
+
+                debugger_write.set(remote_process.to_debugger());
+            }
+        });
+
+        ui::start(debugger_read, commands_tx);
 
         Self {
             updates_tx,
@@ -26,4 +65,21 @@ impl Default for DebuggerState {
     fn default() -> Self {
         Self::new()
     }
+}
+
+async fn on_new_code(
+    code: Result<Response, gloo_net::Error>,
+    remote_process: &mut RemoteProcess,
+) -> u64 {
+    let code = code.unwrap().text().await.unwrap();
+    let code: Versioned<Code> = ron::from_str(&code).unwrap();
+
+    remote_process.on_code_update(code.inner);
+
+    code.timestamp
+}
+
+fn on_process_update(update: Vec<u8>, remote_process: &mut RemoteProcess) {
+    let update = Update::deserialize(update);
+    remote_process.on_runtime_update(update);
 }
--- capi/debugger/src/ui/start.rs
+++ capi/debugger/src/ui/start.rs
@@ -1,23 +1,13 @@
-use capi_protocol::{
-    updates::{Code, SerializedUpdate, Update},
-    Versioned,
-};
-use gloo_net::http::{Request, Response};
-use leptos::{create_signal, SignalSet};
-use tokio::{select, sync::mpsc};
+use leptos::ReadSignal;
 
-use crate::{debugger::RemoteProcess, ui::components::debugger::Debugger};
+use crate::{debugger::Debugger, ui::components::debugger::Debugger};
 
 use super::CommandsTx;
 
 pub fn start(
-    mut updates_rx: mpsc::UnboundedReceiver<SerializedUpdate>,
+    debugger_read: ReadSignal<Debugger>,
     commands_tx: CommandsTx,
 ) {
-    let mut remote_process = RemoteProcess::default();
-    let (debugger_read, debugger_write) =
-        create_signal(remote_process.to_debugger());
-
     leptos::mount_to_body(move || {
         leptos::view! {
             <Debugger
@@ -25,48 +15,4 @@ pub fn start(
                 commands=commands_tx />
         }
     });
-
-    leptos::spawn_local(async move {
-        let code = Request::get("/code").send().await;
-        let mut timestamp = on_new_code(code, &mut remote_process).await;
-
-        loop {
-            select! {
-                code = Request::get(&format!("/code/{timestamp}")).send() => {
-                    timestamp = on_new_code(code, &mut remote_process).await;
-                }
-                update = updates_rx.recv() => {
-                    let Some(update) = update else {
-                        // This means the other end has hung up. Nothing we can
-                        // do, except end this task too.
-                        break;
-                    };
-
-                    on_process_update(
-                        update,
-                        &mut remote_process,
-                    );
-                }
-            }
-
-            debugger_write.set(remote_process.to_debugger());
-        }
-    });
-}
-
-async fn on_new_code(
-    code: Result<Response, gloo_net::Error>,
-    remote_process: &mut RemoteProcess,
-) -> u64 {
-    let code = code.unwrap().text().await.unwrap();
-    let code: Versioned<Code> = ron::from_str(&code).unwrap();
-
-    remote_process.on_code_update(code.inner);
-
-    code.timestamp
-}
-
-fn on_process_update(update: Vec<u8>, remote_process: &mut RemoteProcess) {
-    let update = Update::deserialize(update);
-    remote_process.on_runtime_update(update);
 }
