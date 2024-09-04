379813d003f2a408702dd79ab218815645d4eb18
Hanno Braun <hello@hannobraun.com>
2024-09-04 23:15:15 +0200

    Simplify parameter name

--- capi/debugger/src/ui/start.rs
+++ capi/debugger/src/ui/start.rs
@@ -4,12 +4,12 @@ use crate::{debugger::Debugger, ui::components::debugger::Debugger};
 
 use super::CommandsTx;
 
-pub fn start(debugger: ReadSignal<Debugger>, commands_tx: CommandsTx) {
+pub fn start(debugger: ReadSignal<Debugger>, commands: CommandsTx) {
     leptos::mount_to_body(move || {
         leptos::view! {
             <Debugger
                 debugger=debugger
-                commands=commands_tx />
+                commands=commands />
         }
     });
 }
