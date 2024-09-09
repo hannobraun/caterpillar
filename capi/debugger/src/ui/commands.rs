0dee00df58a0fb0463207aa0b588e64479104147
Hanno Braun <hello@hannobraun.com>
2024-09-09 13:36:30 +0200

    Update error message

--- capi/debugger/src/ui/commands.rs
+++ capi/debugger/src/ui/commands.rs
@@ -2,6 +2,11 @@ use super::{Action, ActionsTx};
 
 pub async fn send_command(action: Action, actions: ActionsTx) {
     if let Err(err) = actions.send(action) {
-        log::error!("Error sending command: {err}");
+        log::error!(
+            "Sending a UI action failed, as the receive is no longer \
+            available: {err:#?}\n\
+            \n\
+            This is most likely a bug in the Caterpillar debugger."
+        );
     }
 }
