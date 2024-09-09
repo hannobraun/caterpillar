6a6853ed1055e8f8b183f9526300200301a56314
Hanno Braun <hello@hannobraun.com>
2024-09-09 12:56:14 +0200

    Update parameter name

--- capi/debugger/src/ui/commands.rs
+++ capi/debugger/src/ui/commands.rs
@@ -2,8 +2,8 @@ use capi_protocol::command::CommandToRuntime;
 
 use super::ActionsTx;
 
-pub async fn send_command(command: CommandToRuntime, commands: ActionsTx) {
-    if let Err(err) = commands.send(command.serialize()) {
+pub async fn send_command(command: CommandToRuntime, actions: ActionsTx) {
+    if let Err(err) = actions.send(command.serialize()) {
         log::error!("Error sending command: {err}");
     }
 }
