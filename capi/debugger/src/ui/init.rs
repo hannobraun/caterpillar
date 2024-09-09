d6260bacf7a9f7e78fa81b24c8af0e32be36968c
Hanno Braun <hello@hannobraun.com>
2024-09-09 13:02:42 +0200

    Update parameter name

--- capi/debugger/src/ui/init.rs
+++ capi/debugger/src/ui/init.rs
@@ -4,12 +4,12 @@ use crate::{debugger::Debugger, ui::components::debugger::Debugger};
 
 use super::ActionsTx;
 
-pub fn init(debugger: ReadSignal<Debugger>, commands: ActionsTx) {
+pub fn init(debugger: ReadSignal<Debugger>, actions: ActionsTx) {
     leptos::mount_to_body(move || {
         leptos::view! {
             <Debugger
                 debugger=debugger
-                actions=commands />
+                actions=actions />
         }
     });
 }
