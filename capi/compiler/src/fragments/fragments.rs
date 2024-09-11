4433fc88f5b79b33cfb40831bb3b360b0ee39abb
Hanno Braun <hello@hannobraun.com>
2024-09-11 21:56:49 +0200

    Make method more widely available

--- capi/compiler/src/fragments/fragments.rs
+++ capi/compiler/src/fragments/fragments.rs
@@ -14,74 +14,6 @@ pub struct Fragments {
     pub inner: FragmentMap,
 }
 
-impl Fragments {
-    /// Find the function that contains the provided fragment
-    ///
-    /// Any fragment that is syntactically a part of the function body will do.
-    /// This specifically includes fragments within blocks that are defined in
-    /// the function.
-    pub fn find_function_by_fragment_in_body(
-        &self,
-        fragment_id: &FragmentId,
-    ) -> Option<(&Function, &Branch)> {
-        let mut fragment_id = *fragment_id;
-
-        loop {
-            let previous = self
-                .inner
-                .inner
-                .values()
-                .find(|fragment| fragment.next() == Some(fragment_id));
-
-            if let Some(previous) = previous {
-                // There's a previous fragment. Continue the search there.
-                fragment_id = previous.id();
-                continue;
-            }
-
-            // If there's no previous fragment, this might be the first fragment
-            // in a branch of a function.
-            let function = self
-                .inner
-                .inner
-                .values()
-                .filter_map(|fragment| match &fragment.kind {
-                    FragmentKind::Payload {
-                        payload: Payload::Function { function },
-                        ..
-                    } => Some((fragment.id(), function)),
-                    _ => None,
-                })
-                .find_map(|(id, function)| {
-                    let branch = function
-                        .branches
-                        .iter()
-                        .find(|branch| branch.start == fragment_id)?;
-                    Some((id, function, branch))
-                });
-
-            if let Some((id, function, branch)) = function {
-                // We have found a function!
-
-                if function.name.is_some() {
-                    // It's a named function! Exactly what we've been looking
-                    // for.
-                    return Some((function, branch));
-                } else {
-                    // An anonymous function. Let's continue our search in the
-                    // context where it was defined.
-                    fragment_id = id;
-                    continue;
-                }
-            }
-
-            // We haven't found anything. Not even a new fragment to look at.
-            // We're done here.
-            break None;
-        }
-    }
-}
-
 impl Deref for Fragments {
     type Target = FragmentMap;
 
@@ -119,6 +51,70 @@ impl FragmentMap {
                     None
                 }
             })
+    }
+
+    /// Find the function that contains the provided fragment
+    ///
+    /// Any fragment that is syntactically a part of the function body will do.
+    /// This specifically includes fragments within blocks that are defined in
+    /// the function.
+    pub fn find_function_by_fragment_in_body(
+        &self,
+        fragment_id: &FragmentId,
+    ) -> Option<(&Function, &Branch)> {
+        let mut fragment_id = *fragment_id;
+
+        loop {
+            let previous = self
+                .inner
+                .values()
+                .find(|fragment| fragment.next() == Some(fragment_id));
+
+            if let Some(previous) = previous {
+                // There's a previous fragment. Continue the search there.
+                fragment_id = previous.id();
+                continue;
+            }
+
+            // If there's no previous fragment, this might be the first fragment
+            // in a branch of a function.
+            let function = self
+                .inner
+                .values()
+                .filter_map(|fragment| match &fragment.kind {
+                    FragmentKind::Payload {
+                        payload: Payload::Function { function },
+                        ..
+                    } => Some((fragment.id(), function)),
+                    _ => None,
+                })
+                .find_map(|(id, function)| {
+                    let branch = function
+                        .branches
+                        .iter()
+                        .find(|branch| branch.start == fragment_id)?;
+                    Some((id, function, branch))
+                });
+
+            if let Some((id, function, branch)) = function {
+                // We have found a function!
+
+                if function.name.is_some() {
+                    // It's a named function! Exactly what we've been looking
+                    // for.
+                    return Some((function, branch));
+                } else {
+                    // An anonymous function. Let's continue our search in the
+                    // context where it was defined.
+                    fragment_id = id;
+                    continue;
+                }
+            }
+
+            // We haven't found anything. Not even a new fragment to look at.
+            // We're done here.
+            break None;
+        }
     }
 
     pub fn iter_from(&self, id: FragmentId) -> impl Iterator<Item = &Fragment> {
