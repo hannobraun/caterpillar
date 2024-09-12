748eab4d3f66aecd2e9dee69c78237e30ec3bb27
Hanno Braun <hello@hannobraun.com>
2024-09-12 14:27:09 +0200

    Add test to cover setting breakpoint and stopping
    This was previously broken, due to how effects were triggered. It has
    been  fixed with the changes to that over the last few commits.

--- capi/debugger/src/model/tests/suite/breakpoints.rs
+++ capi/debugger/src/model/tests/suite/breakpoints.rs
@@ -1,4 +1,10 @@
-use crate::model::{tests::infra::debugger, UserAction};
+use crate::model::{
+    tests::infra::{
+        debugger, ActiveFunctionsEntriesExt, ActiveFunctionsExt,
+        DebugFunctionExt, FunctionsExt,
+    },
+    UserAction,
+};
 
 #[test]
 fn display_breakpoint_that_was_set() -> anyhow::Result<()> {
@@ -30,6 +36,50 @@ fn display_breakpoint_that_was_set() -> anyhow::Result<()> {
 
     debugger.on_user_action(UserAction::BreakpointSet { fragment: nop })?;
     assert!(debugger.expect_fragment(&nop).data.has_durable_breakpoint);
+
+    Ok(())
+}
+
+#[test]
+fn set_breakpoint_and_stop_there() -> anyhow::Result<()> {
+    // If a breakpoint has been set, the program should run up until there, then
+    // stop.
+
+    let mut debugger = debugger();
+    debugger.provide_source_code(
+        r"
+            main: { |size_x size_y|
+                nop
+            }
+        ",
+    );
+
+    let fragments = debugger.expect_code();
+    let nop = fragments
+        .find_function_by_name("main")
+        .unwrap()
+        .expect_one_branch()
+        .iter(fragments)
+        .next()
+        .unwrap()
+        .id();
+    debugger.on_user_action(UserAction::BreakpointSet { fragment: nop })?;
+
+    debugger.run_process();
+
+    assert_eq!(
+        debugger
+            .state
+            .generate_transient_state()
+            .active_functions
+            .expect_entries()
+            .expect_functions()
+            .with_name("main")
+            .active_fragment()
+            .data
+            .id,
+        nop,
+    );
 
     Ok(())
 }
