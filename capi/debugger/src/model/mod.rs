fec4dc63eb9e28bfc27b371c28bf108f595fb1ee
Hanno Braun <hello@hannobraun.com>
2024-09-12 22:40:30 +0200

    Update module name

--- capi/debugger/src/model/mod.rs
+++ capi/debugger/src/model/mod.rs
@@ -1,18 +1,18 @@
-mod action;
 mod active_functions;
 mod branch;
 mod fragment;
 mod function;
 mod state;
+mod user_action;
 
 #[cfg(test)]
 mod tests;
 
 pub use self::{
-    action::UserAction,
     active_functions::{ActiveFunctions, ActiveFunctionsEntry},
     branch::Branch,
     fragment::{DebugFragment, DebugFragmentData, DebugFragmentKind},
     function::DebugFunction,
     state::{PersistentState, TransientState},
+    user_action::UserAction,
 };
