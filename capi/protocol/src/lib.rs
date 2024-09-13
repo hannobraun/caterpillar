398d8856e78663502eee2b7714e8f9520ab32cd1
Hanno Braun <hello@hannobraun.com>
2024-09-13 23:40:48 +0200

    Remove unused code

--- capi/protocol/src/lib.rs
+++ capi/protocol/src/lib.rs
@@ -2,8 +2,6 @@ pub mod command;
 pub mod runtime_state;
 pub mod updates;
 
-pub const CODE_BUFFER_SIZE: usize = 1024 * 1024;
-
 /// The size of the updates buffer
 ///
 /// This is a ridiculous 1 MiB large. It should be possible to make this much
