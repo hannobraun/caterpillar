1d3083cfcf50eb75a5bdb257e66cccee8b4a38a6
Hanno Braun <hello@hannobraun.com>
2024-09-13 14:38:53 +0200

    Remove unused code

--- capi/process/src/breakpoints.rs
+++ capi/process/src/breakpoints.rs
@@ -15,10 +15,6 @@ impl Breakpoints {
         self.durable.contains(instruction)
     }
 
-    pub fn ephemeral_at(&self, instruction: &InstructionAddress) -> bool {
-        self.ephemeral.contains(instruction)
-    }
-
     pub fn set_durable(&mut self, instruction: InstructionAddress) {
         self.durable.insert(instruction);
     }
@@ -33,31 +29,6 @@ impl Breakpoints {
 
     pub fn clear_all_ephemeral(&mut self) {
         self.ephemeral.clear();
-    }
-
-    pub fn should_stop_at_and_clear_ephemeral(
-        &mut self,
-        instruction: &InstructionAddress,
-    ) -> bool {
-        let should_stop =
-            self.ephemeral_at(instruction) || self.durable_at(instruction);
-
-        if self.ephemeral_at(instruction) {
-            // The debugger sets ephemeral breakpoints to implement its "Step"
-            // functionality. It might only set one such breakpoint, that needs
-            // to be cleared once it's hit.
-            //
-            // But if the next step is into a branch, the debugger might not
-            // know where exactly it's going to go, and might set breakpoints
-            // into all of the branches.
-            //
-            // We don't want those breakpoints to hang around, to stop execution
-            // at some later point. Therefore, we clear all of them when a
-            // single one is hit.
-            self.clear_all_ephemeral();
-        }
-
-        should_stop
     }
 
     pub fn iter(&self) -> impl Iterator<Item = InstructionAddress> + '_ {
