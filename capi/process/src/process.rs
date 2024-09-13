d5cdd89a694cf2fef6b24ca4a4dcab2b02a6ad42
Hanno Braun <hello@hannobraun.com>
2024-09-13 12:11:25 +0200

    Document `Process::ignore_next_instruction`

--- capi/process/src/process.rs
+++ capi/process/src/process.rs
@@ -146,6 +146,34 @@ impl Process {
         }
     }
 
+    /// # Ignore the next instruction
+    ///
+    /// This advances the instruction that the evaluator is going to evaluate
+    /// next by one This must be done after a host effect has been handled.
+    ///
+    /// When an instruction triggers an effect, the evaluator does not advance
+    /// to the next instruction. This makes breakpoints work as expected: Once
+    /// the breakpoint effect triggers, the developer still has the opportunity
+    /// to step into the current instruction; as opposed to that instruction
+    /// already being in the past.
+    ///
+    /// This behavior also provides the opportunity for recovering from an
+    /// error, by updating the instruction that triggered it and then re-trying.
+    ///
+    /// But after a host effect has been handled, the instruction that triggered
+    /// it must not be executed again. This can be ensured by calling this
+    /// method after the host effect has been handled.
+    ///
+    /// ## Implementation Note
+    ///
+    /// Leaving this important responsibility to the host, which means _every_
+    /// host implementation must remember to call this function at the
+    /// appropriate time, is a questionable design decision.
+    ///
+    /// For now, I (Hanno Braun) figured that it's best to keep effects simple
+    /// and uniform to see how the overall design shakes out, instead of
+    /// burdening it with complications (like handling host effects specially)
+    /// from early on.
     pub fn ignore_next_instruction(&mut self) {
         self.evaluator.next_instruction =
             self.evaluator.next_instruction.next();
