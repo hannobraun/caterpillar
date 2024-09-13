b78e72d0fd39d283f0eafa52591ac610ac920feb
Hanno Braun <hello@hannobraun.com>
2024-09-14 00:18:58 +0200

    Add comment

--- capi/protocol/src/lib.rs
+++ capi/protocol/src/lib.rs
@@ -11,6 +11,11 @@ pub const CODE_BUFFER_SIZE: usize = 1024 * 1024;
 /// format.
 pub const UPDATES_BUFFER_SIZE: usize = 1024 * 1024;
 
+/// The size of the commands buffer
+///
+/// This is a ridiculous 1 MiB large. It should be possible to make this much
+/// smaller, but for now, we're using a very space-inefficient serialization
+/// format.
 pub const COMMANDS_BUFFER_SIZE: usize = 1024 * 1024;
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
