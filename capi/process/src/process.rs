e47efbce1b8e8b04a8b6c952dd4d252227a172ee
Hanno Braun <hello@hannobraun.com>
2024-09-13 12:58:15 +0200

    Prepare for follow-on change

--- capi/process/src/process.rs
+++ capi/process/src/process.rs
@@ -120,7 +120,7 @@ impl Process {
         }
     }
 
-    fn continue_(&mut self, and_stop_at: Option<InstructionAddress>) {
+    pub fn continue_(&mut self, and_stop_at: Option<InstructionAddress>) {
         if let Some(Effect::Breakpoint) = self.effects.inspect_first() {
             if let Some(address) = and_stop_at {
                 self.breakpoints.set_ephemeral(address);
