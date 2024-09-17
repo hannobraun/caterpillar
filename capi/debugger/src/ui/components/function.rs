b872897b4ff69ac66473cc17c976d86f115380f9
Hanno Braun <hello@hannobraun.com>
2024-09-17 10:33:11 +0200

    Prepare for follow-on change

--- capi/debugger/src/ui/components/function.rs
+++ capi/debugger/src/ui/components/function.rs
@@ -189,7 +189,7 @@ fn make_single_expression(
         class_inner.push_str(" font-bold");
     }
 
-    let data_fragment = ron::to_string(&data.id)
+    let data_fragment = ron::to_string(&data.fragment.id())
         .expect("Expecting serialization of `FragmentId` to always work.");
     let data_breakpoint = data.has_durable_breakpoint;
 
