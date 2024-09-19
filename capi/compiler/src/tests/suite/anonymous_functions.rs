8c15f4a439a1600fc945583d70a073692225d17e
Hanno Braun <hello@hannobraun.com>
2024-09-19 22:57:09 +0200

    Inline redundant variables

--- capi/compiler/src/tests/suite/anonymous_functions.rs
+++ capi/compiler/src/tests/suite/anonymous_functions.rs
@@ -2,66 +2,74 @@ use crate::tests::infra::runtime;
 
 #[test]
 fn anonymous_function_eval() {
-    let source = r"
-        main: { ||
-            { || 0 send }
-                eval
-        }
-    ";
-
-    runtime().update_code(source).run_until_receiving(0);
+    runtime()
+        .update_code(
+            r"
+                main: { ||
+                    { || 0 send }
+                        eval
+                }
+            ",
+        )
+        .run_until_receiving(0);
 }
 
 #[test]
 fn anonymous_function_parameter() {
-    let source = r"
-        main: { ||
-            0
-            { |channel| channel }
-                eval
-                send
-        }
-    ";
-
-    runtime().update_code(source).run_until_receiving(0);
+    runtime()
+        .update_code(
+            r"
+                main: { ||
+                    0
+                    { |channel| channel }
+                        eval
+                        send
+                }
+            ",
+        )
+        .run_until_receiving(0);
 }
 
 #[test]
 fn anonymous_function_parameter_shadowing() {
-    let source = r"
-        main: { ||
-            0
-            { |channel|
-                channel
-                { |channel| channel }
-                    eval
-            }
-                eval
-                send
-        }
-    ";
-
-    runtime().update_code(source).run_until_receiving(0);
+    runtime()
+        .update_code(
+            r"
+                main: { ||
+                    0
+                    { |channel|
+                        channel
+                        { |channel| channel }
+                            eval
+                    }
+                        eval
+                        send
+                }
+            ",
+        )
+        .run_until_receiving(0);
 }
 
 #[test]
 fn anonymous_function_captured_binding() {
-    let source = r"
-        main: { ||
-            0
-            { |channel|
-                { ||
-                    # We are not using `channel` here, to make sure that
-                    # capturing works even from a grandparent scope.
+    runtime()
+        .update_code(
+            r"
+                main: { ||
+                    0
+                    { |channel|
+                        { ||
+                            # We are not using `channel` here, to make sure that
+                            # capturing works even from a grandparent scope.
 
-                    { || channel send }
+                            { || channel send }
+                                eval
+                        }
+                            eval
+                    }
                         eval
                 }
-                    eval
-            }
-                eval
-        }
-    ";
-
-    runtime().update_code(source).run_until_receiving(0);
+            ",
+        )
+        .run_until_receiving(0);
 }
