f29c63158cb25fc35550e68e72ea474d5605652f
Hanno Braun <hello@hannobraun.com>
2024-09-19 11:22:13 +0200

    Update comment

--- capi/debugger/src/model/state.rs
+++ capi/debugger/src/model/state.rs
@@ -304,8 +304,8 @@ impl PersistentState {
             effect: Effect::Breakpoint,
         } = self.code.instruction(&origin)?
         {
-            // We don't need to explicitly revert this replacement later, as
-            // we'll re-apply breakpoints based on the original code.
+            // We don't need to explicitly revert this with another replacement
+            // later, as we'll re-apply breakpoints based on the original code.
             instructions.replace(&origin, Instruction::Nop);
         }
 
