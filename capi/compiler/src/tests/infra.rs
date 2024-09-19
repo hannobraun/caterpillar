30924b932c48871d5fafb404dd239e1885e62950
Hanno Braun <hello@hannobraun.com>
2024-09-19 22:34:46 +0200

    Prepare for follow-on change

--- capi/compiler/src/tests/infra.rs
+++ capi/compiler/src/tests/infra.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-use capi_runtime::{Effect, Runtime};
+use capi_runtime::{Effect, Instructions, Runtime};
 
 use crate::{compile, host::Host};
 
@@ -9,34 +9,7 @@ pub fn compile_and_run(source: &str) -> BTreeMap<u32, u32> {
 
     let mut signals = BTreeMap::new();
 
-    let mut runtime = runtime();
-
-    while runtime.inner.state().is_running() {
-        runtime.inner.evaluate_next_instruction(&instructions);
-
-        match runtime.inner.effects_mut().handle_first() {
-            Some(Effect::Host) => {
-                let effect = runtime.inner.stack_mut().pop_operand().unwrap();
-                assert_eq!(effect.to_u32(), 0);
-
-                let channel = runtime.inner.stack_mut().pop_operand().unwrap();
-                let channel: u32 = u32::from_le_bytes(channel.0);
-
-                *signals.entry(channel).or_default() += 1;
-
-                runtime.inner.ignore_next_instruction();
-            }
-            Some(effect) => {
-                panic!(
-                    "Unexpected effect: {effect}\n\
-                    Runtime: {:#?}\n\
-                    Instructions: {instructions:#?}",
-                    runtime.inner,
-                );
-            }
-            None => {}
-        }
-    }
+    runtime().run_until_finished(&instructions, &mut signals);
 
     signals
 }
@@ -48,6 +21,43 @@ pub fn runtime() -> TestRuntime {
 #[derive(Default)]
 pub struct TestRuntime {
     inner: Runtime,
+}
+
+impl TestRuntime {
+    pub fn run_until_finished(
+        &mut self,
+        instructions: &Instructions,
+        signals: &mut BTreeMap<u32, u32>,
+    ) -> &mut Self {
+        while self.inner.state().is_running() {
+            self.inner.evaluate_next_instruction(instructions);
+
+            match self.inner.effects_mut().handle_first() {
+                Some(Effect::Host) => {
+                    let effect = self.inner.stack_mut().pop_operand().unwrap();
+                    assert_eq!(effect.to_u32(), 0);
+
+                    let channel = self.inner.stack_mut().pop_operand().unwrap();
+                    let channel: u32 = u32::from_le_bytes(channel.0);
+
+                    *signals.entry(channel).or_default() += 1;
+
+                    self.inner.ignore_next_instruction();
+                }
+                Some(effect) => {
+                    panic!(
+                        "Unexpected effect: {effect}\n\
+                        Runtime: {:#?}\n\
+                        Instructions: {instructions:#?}",
+                        self.inner,
+                    );
+                }
+                None => {}
+            }
+        }
+
+        self
+    }
 }
 
 #[derive(Debug)]
