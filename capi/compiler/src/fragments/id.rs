ceab3f3ae0dc2d76bc5338905a0fbac860b6b34c
Hanno Braun <hello@hannobraun.com>
2024-09-22 15:29:52 +0200

    Simplify

--- capi/compiler/src/fragments/id.rs
+++ capi/compiler/src/fragments/id.rs
@@ -1,27 +1,15 @@
-use std::cmp::Ordering;
-
 #[derive(
     Clone,
     Copy,
     Debug,
     Eq,
     PartialEq,
+    Ord,
+    PartialOrd,
     serde::Deserialize,
     serde::Serialize,
     udigest::Digestable,
 )]
 pub struct Hash {
     pub(super) hash: [u8; 32],
-}
-
-impl Ord for Hash {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.hash.cmp(&other.hash)
-    }
-}
-
-impl PartialOrd for Hash {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
 }
