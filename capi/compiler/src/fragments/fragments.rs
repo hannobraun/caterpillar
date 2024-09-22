e6bbc1d4a4db59a49701c0b82da86ef80834c3f0
Hanno Braun <hello@hannobraun.com>
2024-09-22 21:39:34 +0200

    Update name of struct field

--- capi/compiler/src/fragments/fragments.rs
+++ capi/compiler/src/fragments/fragments.rs
@@ -31,15 +31,14 @@ impl DerefMut for Fragments {
 #[derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize)]
 pub struct FragmentMap {
     fragments_by_hash: BTreeMap<Hash<Fragment>, Fragment>,
-    fragments_by_location: BTreeMap<FragmentId, Fragment>,
+    fragments_by_id: BTreeMap<FragmentId, Fragment>,
     locations_by_hash: BTreeMap<Hash<FragmentId>, FragmentId>,
 }
 
 impl FragmentMap {
     pub fn insert(&mut self, location: FragmentId, fragment: Fragment) {
         self.locations_by_hash.insert(location.hash(), location);
-        self.fragments_by_location
-            .insert(location, fragment.clone());
+        self.fragments_by_id.insert(location, fragment.clone());
         self.fragments_by_hash.insert(fragment.hash(), fragment);
     }
 
