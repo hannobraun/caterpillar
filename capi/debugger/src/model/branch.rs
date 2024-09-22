59207fec99e4d898d73dba7b3d4ca4f346668f63
Hanno Braun <hello@hannobraun.com>
2024-09-23 01:03:12 +0200

    Prepare for follow-on change

--- capi/debugger/src/model/branch.rs
+++ capi/debugger/src/model/branch.rs
@@ -94,7 +94,7 @@ impl DebugBranch {
         let mut fragments = self
             .body
             .iter()
-            .skip_while(|fragment| fragment.hash() != *hash);
+            .skip_while(|fragment| fragment.data.id.this != *hash);
 
         // This is the fragment we've been passed as an argument. Need to ignore
         // it, to advance the iterator to the one we're actually looking for.
