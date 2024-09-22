4597d9588f4e34d3f3bac8a455eaf60434eb7882
Hanno Braun <hello@hannobraun.com>
2024-09-22 15:46:39 +0200

    Update parameter name

--- capi/debugger/src/model/tests/infra.rs
+++ capi/debugger/src/model/tests/infra.rs
@@ -104,7 +104,7 @@ impl TestDebugger {
         &self.persistent.code.inner.as_ref().unwrap().fragments
     }
 
-    pub fn expect_fragment(&mut self, id: &Hash<Fragment>) -> DebugFragment {
+    pub fn expect_fragment(&mut self, hash: &Hash<Fragment>) -> DebugFragment {
         let Some(fragment) = self
             .transient_state()
             .active_functions
@@ -114,7 +114,7 @@ impl TestDebugger {
             .find_map(|function| {
                 function.branches.iter().find_map(|branch| {
                     branch.body.iter().find_map(|fragment| {
-                        if fragment.hash() == *id {
+                        if fragment.hash() == *hash {
                             Some(fragment.clone())
                         } else {
                             None
@@ -123,7 +123,7 @@ impl TestDebugger {
                 })
             })
         else {
-            panic!("Expected to find fragment with ID `{id:?}`");
+            panic!("Expected to find fragment with ID `{hash:?}`");
         };
 
         fragment
