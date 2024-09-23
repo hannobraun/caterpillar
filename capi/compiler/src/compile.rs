933b11242254be42158ea904dee266d689faa745
Hanno Braun <hello@hannobraun.com>
2024-09-23 13:40:46 +0200

    Prepare for follow-on change

--- capi/compiler/src/compile.rs
+++ capi/compiler/src/compile.rs
@@ -17,10 +17,11 @@ pub fn tokenize_and_parse(source: &str) -> Script {
 }
 
 pub fn compile<H: Host>(source: &str) -> (Fragments, Instructions, SourceMap) {
-    let mut script = tokenize_and_parse(source);
-    determine_tail_positions(&mut script.functions);
-    resolve_identifiers::<H>(&mut script.functions);
-    let fragments = generate_fragments(script.functions);
+    let script = tokenize_and_parse(source);
+    let mut functions = script.functions;
+    determine_tail_positions(&mut functions);
+    resolve_identifiers::<H>(&mut functions);
+    let fragments = generate_fragments(functions);
     let (instructions, source_map) = generate_instructions(fragments.clone());
 
     (fragments, instructions, source_map)
