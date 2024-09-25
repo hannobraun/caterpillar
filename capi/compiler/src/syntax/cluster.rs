b265c63e6bdeb93855d9fdd3237db09dc45d5311
Hanno Braun <hello@hannobraun.com>
2024-09-25 13:05:37 +0200

    Add `Cluster`

---
+++ capi/compiler/src/syntax/cluster.rs
@@ -0,0 +1,24 @@
+use super::Function;
+
+/// # A cluster of functions
+///
+/// During compilation, all functions are grouped into clusters. A cluster can
+/// consist of a single function, or a group of mutually recursive functions.
+///
+/// All mutually recursive functions are grouped into a single clusters with the
+/// other functions in their recursive group.
+pub struct Cluster {
+    functions: Vec<Function>,
+}
+
+impl Cluster {
+    /// # Create a new cluster
+    pub fn new(functions: Vec<Function>) -> Self {
+        Self { functions }
+    }
+
+    /// # Access the functions in the cluster
+    pub fn into_functions(self) -> impl Iterator<Item = Function> {
+        self.functions.into_iter()
+    }
+}
--- capi/compiler/src/syntax/mod.rs
+++ capi/compiler/src/syntax/mod.rs
@@ -1,7 +1,9 @@
+mod cluster;
 mod expression;
 mod function;
 
 pub use self::{
+    cluster::Cluster,
     expression::{Expression, IdentifierTarget},
     function::{Branch, Function, Pattern},
 };
