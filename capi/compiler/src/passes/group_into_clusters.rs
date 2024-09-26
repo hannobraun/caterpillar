df81d9718be34322b696905cb68f2ef9d441dc56
Hanno Braun <hello@hannobraun.com>
2024-09-26 21:50:43 +0200

    Prepare for follow-on change

--- capi/compiler/src/passes/group_into_clusters.rs
+++ capi/compiler/src/passes/group_into_clusters.rs
@@ -3,13 +3,15 @@ use crate::syntax::{Cluster, Clusters, Function};
 pub fn group_into_clusters(functions: Vec<Function>) -> Clusters {
     // This is just a placeholder implementation, while support for clusters is
     // still being implemented.
+    let clusters = functions
+        .iter()
+        .cloned()
+        .map(|function| [function])
+        .map(Vec::from)
+        .map(|functions| Cluster { functions })
+        .collect();
     Clusters {
         functions: functions.clone(),
-        clusters: functions
-            .into_iter()
-            .map(|function| [function])
-            .map(Vec::from)
-            .map(|functions| Cluster { functions })
-            .collect(),
+        clusters,
     }
 }
