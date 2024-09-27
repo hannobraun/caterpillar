e879c0098951689eac9e7839530f2ef734274838
Hanno Braun <hello@hannobraun.com>
2024-09-27 13:09:30 +0200

    Simplify

--- capi/compiler/src/passes/mark_recursive_calls.rs
+++ capi/compiler/src/passes/mark_recursive_calls.rs
@@ -1,8 +1,6 @@
 use std::collections::BTreeMap;
 
-use crate::syntax::{
-    Clusters, Expression, FunctionIndexInCluster, IdentifierTarget,
-};
+use crate::syntax::{Clusters, Expression, IdentifierTarget};
 
 pub fn mark_recursive_calls(clusters: &mut Clusters) {
     for cluster in &mut clusters.clusters {
@@ -34,11 +32,9 @@ pub fn mark_recursive_calls(clusters: &mut Clusters) {
                         ..
                     } = expression
                     {
-                        if let Some(index) =
+                        if let Some(&index) =
                             indices_in_cluster_by_function_name.get(name)
                         {
-                            let index = FunctionIndexInCluster(index.0);
-
                             *is_known_to_be_recursive_call_to_index =
                                 Some(index);
                         }
