f8a7612d3b327a508b8dc586e4f22fdbd7674df3
Hanno Braun <hello@hannobraun.com>
2024-09-27 11:57:00 +0200

    Add failing test for known bug

--- capi/compiler/src/passes/mark_recursive_calls.rs
+++ capi/compiler/src/passes/mark_recursive_calls.rs
@@ -46,3 +46,70 @@ pub fn mark_recursive_calls(clusters: &mut Clusters) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        host::NoHost,
+        passes::{group_into_clusters, parse, resolve_identifiers, tokenize},
+        syntax::{Clusters, Expression, IdentifierTarget},
+    };
+
+    #[test]
+    #[should_panic] // known bug; not tracked as an issue yet
+    fn self_recursive_functions() {
+        let clusters = mark_recursive_calls(
+            r"
+                f: {
+                    \ ->
+                        f
+                }
+
+                # Need a second function in this test, because for the first,
+                # the index of the function within the list of all named
+                # functions and the index of the function within its cluster,
+                # are both `0`.
+                #
+                # This is prone to hide a bug, so we have this second function,
+                # that has a non-zero index in the list of all named functions.
+                g: {
+                    \ ->
+                        g
+                }
+            ",
+        );
+
+        for mut function in clusters.functions.into_values() {
+            let Expression::Identifier { target, .. } =
+                function.branches.remove(0).body.remove(0)
+            else {
+                panic!("Expected expression to be an identifier.");
+            };
+            let Some(IdentifierTarget::Function {
+                is_known_to_be_recursive_call_to_index,
+            }) = target
+            else {
+                panic!("Expected expression to be a function call");
+            };
+            let Some(index) = is_known_to_be_recursive_call_to_index else {
+                panic!("Expected function call to be recursive.");
+            };
+
+            assert_eq!(
+                index, 0,
+                "Function is only self-recursive, not mutually recursive. \
+                Expecting it to be alone in a cluster, hence index referring \
+                to it must be `0`."
+            );
+        }
+    }
+
+    fn mark_recursive_calls(source: &str) -> Clusters {
+        let tokens = tokenize(source);
+        let mut functions = parse(tokens);
+        resolve_identifiers::<NoHost>(&mut functions);
+        let mut clusters = group_into_clusters(functions);
+        super::mark_recursive_calls(&mut clusters);
+        clusters
+    }
+}
