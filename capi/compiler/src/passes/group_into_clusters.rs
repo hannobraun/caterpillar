d096aced450e947a01bed02485dc31ae9fb864c7
Hanno Braun <hello@hannobraun.com>
2024-10-02 14:07:35 +0200

    Set `index_in_cluster` field of `syntax::Function`

--- capi/compiler/src/passes/group_into_clusters.rs
+++ capi/compiler/src/passes/group_into_clusters.rs
@@ -9,14 +9,16 @@ pub fn group_into_clusters(functions: Vec<Function>) -> Clusters {
     // This is just a placeholder implementation, while support for clusters is
     // still being implemented.
 
-    let functions = iter::successors(Some(0), |i| Some(i + 1))
+    let mut functions = iter::successors(Some(0), |i| Some(i + 1))
         .map(NamedFunctionIndex)
         .zip(functions)
         .collect::<BTreeMap<_, _>>();
     let clusters = functions
-        .keys()
-        .map(|named_function_index| {
+        .iter_mut()
+        .map(|(named_function_index, function)| {
             let function_index_in_cluster = FunctionIndexInCluster(0);
+
+            function.index_in_cluster = Some(function_index_in_cluster);
 
             Cluster {
                 functions: BTreeMap::from([(
