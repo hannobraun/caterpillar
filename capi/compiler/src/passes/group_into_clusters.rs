aa7c5916f03917354bf93e5b2d8875fc9d1ef6bb
Hanno Braun <hello@hannobraun.com>
2024-10-02 14:04:16 +0200

    Consolidate redundant code

--- capi/compiler/src/passes/group_into_clusters.rs
+++ capi/compiler/src/passes/group_into_clusters.rs
@@ -9,22 +9,19 @@ pub fn group_into_clusters(functions: Vec<Function>) -> Clusters {
     // This is just a placeholder implementation, while support for clusters is
     // still being implemented.
 
-    let clusters = iter::successors(Some(0), |i| Some(i + 1))
-        .take(functions.len())
-        .map(|i| Cluster {
+    let functions = iter::successors(Some(0), |i| Some(i + 1))
+        .map(NamedFunctionIndex)
+        .zip(functions)
+        .collect::<BTreeMap<_, _>>();
+    let clusters = functions
+        .keys()
+        .map(|named_function_index| Cluster {
             functions: BTreeMap::from([(
                 FunctionIndexInCluster(0),
-                NamedFunctionIndex(i),
+                *named_function_index,
             )]),
         })
         .collect();
-    let functions = {
-        let indices = iter::successors(Some(0), |i| Some(i + 1));
-        indices
-            .zip(functions)
-            .map(|(index, function)| (NamedFunctionIndex(index), function))
-            .collect()
-    };
 
     Clusters {
         functions,
