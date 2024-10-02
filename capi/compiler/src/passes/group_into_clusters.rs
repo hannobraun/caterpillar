88edc7f239f3b81014557c45345854db7d97e155
Hanno Braun <hello@hannobraun.com>
2024-10-02 13:56:06 +0200

    Improve formatting

--- capi/compiler/src/passes/group_into_clusters.rs
+++ capi/compiler/src/passes/group_into_clusters.rs
@@ -8,6 +8,7 @@ use crate::{
 pub fn group_into_clusters(functions: Vec<Function>) -> Clusters {
     // This is just a placeholder implementation, while support for clusters is
     // still being implemented.
+
     let clusters = iter::successors(Some(0), |i| Some(i + 1))
         .take(functions.len())
         .map(|i| Cluster {
@@ -24,6 +25,7 @@ pub fn group_into_clusters(functions: Vec<Function>) -> Clusters {
             .map(|(index, function)| (NamedFunctionIndex(index), function))
             .collect()
     };
+
     Clusters {
         functions,
         clusters,
