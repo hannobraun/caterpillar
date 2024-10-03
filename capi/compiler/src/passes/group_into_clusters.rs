b425c85efeef15413d2222e58953a9e90e59e262
Hanno Braun <hello@hannobraun.com>
2024-10-03 06:23:24 +0200

    Make comment more specific

--- capi/compiler/src/passes/group_into_clusters.rs
+++ capi/compiler/src/passes/group_into_clusters.rs
@@ -6,13 +6,13 @@ use crate::{
 };
 
 pub fn group_into_clusters(functions: Vec<Function>) -> Clusters {
-    // This is just a placeholder implementation, while support for clusters is
-    // still being implemented.
-
     let mut functions = iter::successors(Some(0), |i| Some(i + 1))
         .map(NamedFunctionIndex)
         .zip(functions)
         .collect::<BTreeMap<_, _>>();
+
+    // This is just a placeholder implementation, while support for clusters is
+    // still being implemented.
     let clusters = functions
         .iter_mut()
         .map(|(named_function_index, function)| {
