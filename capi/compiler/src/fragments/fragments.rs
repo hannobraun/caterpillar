baea793c1aaa06ccabe760368c6204445d0079cd
Hanno Braun <hello@hannobraun.com>
2024-10-07 12:56:53 +0200

    Remove unused struct field

--- capi/compiler/src/fragments/fragments.rs
+++ capi/compiler/src/fragments/fragments.rs
@@ -3,9 +3,7 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
-use super::{
-    FragmentId, FragmentMap, FragmentsByLocation, Function, FunctionIndexInRootContext
-};
+use super::{FragmentId, FragmentMap, Function, FunctionIndexInRootContext};
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 pub struct Fragments {
@@ -21,14 +19,6 @@ pub struct Fragments {
     pub clusters: Vec<Cluster>,
 
     pub map: FragmentMap,
-
-    /// # All code fragments, tracked by their location
-    ///
-    /// ## Implementation Note
-    ///
-    /// This is a placeholder. The refactoring to fill it out is currently in
-    /// progress.
-    pub by_location: FragmentsByLocation,
 }
 
 impl Fragments {
--- capi/compiler/src/passes/generate_fragments.rs
+++ capi/compiler/src/passes/generate_fragments.rs
@@ -73,7 +73,6 @@ pub fn generate_fragments(clusters: syntax::Clusters) -> Fragments {
         functions,
         clusters: compiled_clusters,
         map: fragments,
-        by_location: fragments_by_location,
     }
 }
 
