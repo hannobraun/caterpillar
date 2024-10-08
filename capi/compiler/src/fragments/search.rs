64400ca891bba62d92244e9eb427168406552a1e
Hanno Braun <hello@hannobraun.com>
2024-10-08 18:34:37 +0200

    Add `FoundFunction`

--- capi/compiler/src/fragments/search.rs
+++ capi/compiler/src/fragments/search.rs
@@ -2,3 +2,14 @@
 //!
 //! Contains types that are returned by searches in code. Those types themselves
 //! provide more convenient functionality for searching within them.
+
+use super::{Function, FunctionLocation};
+
+/// # A function that was found by a search
+pub struct FoundFunction<'r> {
+    /// # The function that was found
+    pub function: &'r Function,
+
+    /// # The location of the function that was found
+    pub location: &'r FunctionLocation,
+}
