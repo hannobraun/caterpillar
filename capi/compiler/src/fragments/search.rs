25e5bbfbd741b07e51c0c106050bfc79d0f5524e
Hanno Braun <hello@hannobraun.com>
2024-10-08 18:46:37 +0200

    Implement `Deref` for `FoundFunction`

--- capi/compiler/src/fragments/search.rs
+++ capi/compiler/src/fragments/search.rs
@@ -3,6 +3,8 @@
 //! Contains types that are returned by searches in code. Those types themselves
 //! provide more convenient functionality for searching within them.
 
+use std::ops::Deref;
+
 use super::{Function, FunctionLocation};
 
 /// # A function that was found by a search
@@ -12,4 +14,12 @@ pub struct FoundFunction<'r> {
 
     /// # The location of the function that was found
     pub location: FunctionLocation,
+}
+
+impl Deref for FoundFunction<'_> {
+    type Target = Function;
+
+    fn deref(&self) -> &Self::Target {
+        self.function
+    }
 }
