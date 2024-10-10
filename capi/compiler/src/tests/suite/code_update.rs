20175bd01555fbd73df8e4dd019216867a270519
Hanno Braun <hello@hannobraun.com>
2024-10-10 11:52:49 +0200

    Update code update test suite
    My plans have changed a bit since I originally wrote that, and I'm now
    targeting a simpler initial implementation, that is more coarse-grained
    and based on functions.

--- capi/compiler/src/tests/suite/code_update.rs
+++ capi/compiler/src/tests/suite/code_update.rs
@@ -1,73 +1,9 @@
 use crate::tests::infra::runtime;
 
 #[test]
-fn replace_single_instruction() {
-    // If the new code only replaces a single instruction in the old one, we
-    // expect the new instruction to be used from then on.
-
-    let mut runtime = runtime();
-
-    runtime
-        .update_code(
-            r"
-                main: { \ ->
-                    0 send
-                    1 send
-                    main
-                }
-            ",
-        )
-        .run_until_receiving(0);
-
-    runtime
-        .update_code(
-            r"
-                main: { \ ->
-                    0 send
-                    2 send
-                    main
-                }
-            ",
-        )
-        .run_until_receiving(2);
-}
-
-#[test]
-fn replace_block_of_instructions() {
-    // If the new code replaces a block of multiple neighboring instructions, we
-    // expect the new instructions to be used from then on.
-
-    let mut runtime = runtime();
-
-    runtime
-        .update_code(
-            r"
-                main: { \ ->
-                    0 send
-                    1 send
-                    main
-                }
-            ",
-        )
-        .run_until_receiving(0);
-
-    runtime
-        .update_code(
-            r"
-                main: { \ ->
-                    1 send
-                    2 send
-                    main
-                }
-            ",
-        )
-        .run_until_receiving(2);
-}
-
-#[test]
-fn add_additional_instructions_after_current_instruction() {
-    // If the new code adds new instructions after the current instruction, we
-    // expect those to be executed next.
+fn use_updated_code_on_next_function_call() {
+    // If a function is updated, we expect the next call to it to execute the
+    // new version.
 
     let mut runtime = runtime();
 
@@ -86,7 +22,6 @@ fn add_additional_instructions_after_current_instruction() {
         .update_code(
             r"
                 main: { \ ->
-                    0 send
                     1 send
                     main
                 }
@@ -97,42 +32,13 @@ fn add_additional_instructions_after_current_instruction() {
 
 #[test]
 #[should_panic] // https://github.com/hannobraun/caterpillar/issues/50
-fn add_additional_instructions_before_current_instruction() {
-    // If the new code adds new instructions before the current instruction, we
-    // expect those to not disturb the flow of execution.
-
-    let mut runtime = runtime();
-
-    runtime
-        .update_code(
-            r"
-                main: { \ ->
-                    1 send
-                    2 send
-                    main
-                }
-            ",
-        )
-        .run_until_receiving(1);
-
-    runtime
-        .update_code(
-            r"
-                main: { \ ->
-                    0 send
-                    1 send
-                    2 send
-                    main
-                }
-            ",
-        )
-        .run_until_receiving(2);
-}
-
-#[test]
-fn remove_instructions_after_current_instruction() {
-    // If the new code removes n instruction after the current instruction, we
-    // expect those to not be executed.
+fn use_old_code_before_next_function_call() {
+    // If a function is updated while it's running, we expect it to still
+    // execute the old code, until the next call to it.
+    //
+    // It would be better to have more fine-grained updates, but this behavior
+    // is more practical, as it's relatively easy to achieve without a code
+    // database and a custom editor.
 
     let mut runtime = runtime();
 
@@ -142,7 +48,6 @@ fn remove_instructions_after_current_instruction() {
                 main: { \ ->
                     0 send
                     1 send
-                    2 send
                     main
                 }
             ",
@@ -159,14 +64,14 @@ fn remove_instructions_after_current_instruction() {
                 }
             ",
         )
-        .run_until_receiving(2);
+        .run_until_receiving(1);
 }
 
 #[test]
 #[should_panic] // https://github.com/hannobraun/caterpillar/issues/50
-fn remove_instructions_before_current_instruction() {
-    // If the new code removes an instruction before the current instruction, we
-    // expect that to not disturb the flow of execution.
+fn handle_update_that_makes_function_larger() {
+    // The update procedure laid out by previous tests should still work, if the
+    // update makes the function larger.
 
     let mut runtime = runtime();
 
@@ -175,24 +80,55 @@ fn remove_instructions_before_current_instruction() {
             r"
                 main: { \ ->
                     0 send
+                    main
+                }
+            ",
+        )
+        .run_until_receiving(0);
+
+    runtime
+        .update_code(
+            r"
+                main: { \ ->
+                    0 send
                     1 send
-                    2 send
                     main
                 }
             ",
         )
         .run_until_receiving(0)
         .run_until_receiving(1);
+}
+
+#[test]
+#[should_panic] // https://github.com/hannobraun/caterpillar/issues/50
+fn handle_update_that_makes_function_smaller() {
+    // The update procedure laid out by previous tests should still work, if the
+    // update makes the function smaller.
+
+    let mut runtime = runtime();
 
     runtime
         .update_code(
             r"
                 main: { \ ->
+                    0 send
                     1 send
-                    2 send
                     main
                 }
             ",
         )
-        .run_until_receiving(2);
+        .run_until_receiving(0);
+
+    runtime
+        .update_code(
+            r"
+                main: { \ ->
+                    0 send
+                    main
+                }
+            ",
+        )
+        .run_until_receiving(1)
+        .run_until_receiving(0);
 }
