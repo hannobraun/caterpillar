31af07c6b5926ce04312a74ec69815513f5043ef
Hanno Braun <hello@hannobraun.com>
2024-10-10 11:24:53 +0200

    Document `Changes`

--- capi/compiler/src/fragments/changes.rs
+++ capi/compiler/src/fragments/changes.rs
@@ -1,7 +1,11 @@
 use super::Function;
 
+/// # The changes between two versions of code
 #[derive(Debug)]
 pub struct Changes {
+    /// # The functions that were added in the new version
     pub added: Vec<Function>,
+
+    /// # The functions that were updated in the new version
     pub updated: Vec<(Function, Function)>,
 }
