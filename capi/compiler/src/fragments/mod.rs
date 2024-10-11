818119ee329084842e57f26d298ee043d867cfa3
Hanno Braun <hello@hannobraun.com>
2024-10-11 13:59:30 +0200

    Add missing re-export

--- capi/compiler/src/fragments/mod.rs
+++ capi/compiler/src/fragments/mod.rs
@@ -9,7 +9,7 @@ mod functions;
 mod location;
 
 pub use self::{
-    changes::{Changes, FunctionUpdate},
+    changes::{Changes, FunctionInUpdate, FunctionUpdate},
     fragment::Fragment,
     fragments::Fragments,
     functions::{Branch, Function, Parameters},
