084b5f7111ecdc7a8bb5fab982396c07d86efdb2
Hanno Braun <hello@hannobraun.com>
2024-10-13 20:45:39 +0200

    Make variable name more explicit

--- capi/compiler/src/passes/detect_changes.rs
+++ capi/compiler/src/passes/detect_changes.rs
@@ -8,7 +8,7 @@ use crate::{
 };
 
 pub fn detect_changes(old: Option<NamedFunctions>, new: &Fragments) -> Changes {
-    let mut old = old
+    let mut old_functions = old
         .map(|old_functions| old_functions.inner)
         .unwrap_or_default();
     let mut new = new.named_functions.inner.clone();
@@ -17,39 +17,41 @@ pub fn detect_changes(old: Option<NamedFunctions>, new: &Fragments) -> Changes {
     let mut updated = Vec::new();
 
     while let Some((new_index, new_function)) = new.pop_first() {
-        let same_hash = old.iter().find_map(|(&index, old_function)| {
-            if Hash::new(old_function) == Hash::new(&new_function) {
-                Some(index)
-            } else {
-                None
-            }
-        });
+        let same_hash =
+            old_functions.iter().find_map(|(&index, old_function)| {
+                if Hash::new(old_function) == Hash::new(&new_function) {
+                    Some(index)
+                } else {
+                    None
+                }
+            });
         if same_hash.is_some() {
             // Function has not changed. We can forget about it.
             continue;
         }
 
-        let same_name = old.iter().find_map(|(&index, old_function)| {
-            assert!(
-                old_function.name.is_some(),
-                "Named function should have a name."
-            );
-            assert!(
-                new_function.name.is_some(),
-                "Named function should have a name."
-            );
+        let same_name =
+            old_functions.iter().find_map(|(&index, old_function)| {
+                assert!(
+                    old_function.name.is_some(),
+                    "Named function should have a name."
+                );
+                assert!(
+                    new_function.name.is_some(),
+                    "Named function should have a name."
+                );
 
-            if old_function.name == new_function.name {
-                Some(index)
-            } else {
-                None
-            }
-        });
+                if old_function.name == new_function.name {
+                    Some(index)
+                } else {
+                    None
+                }
+            });
         if let Some(old_index) = same_name {
             // Found a function with the same name. But it can't have the same
             // hash, or we wouldn't have made it here. Assuming the new function
             // is an updated version of the old.
-            let old_function = old.remove(&old_index).expect(
+            let old_function = old_functions.remove(&old_index).expect(
                 "Just found index in map; expecting it to still be there.",
             );
             updated.push(FunctionUpdate {
