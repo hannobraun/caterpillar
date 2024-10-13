857251db8c71b5387d48fa918504d28c66595b76
Hanno Braun <hello@hannobraun.com>
2024-10-14 00:35:21 +0200

    Update module name

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,7 +1,7 @@
+mod create_call_graph;
 mod detect_changes;
 mod generate_fragments;
 mod generate_instructions;
-mod group_into_clusters;
 mod mark_recursive_calls;
 mod parse;
 mod resolve_identifiers;
@@ -9,9 +9,9 @@ mod tail_position;
 mod tokenize;
 
 pub use {
-    detect_changes::detect_changes, generate_fragments::generate_fragments,
+    create_call_graph::create_call_graph, detect_changes::detect_changes,
+    generate_fragments::generate_fragments,
     generate_instructions::generate_instructions,
-    group_into_clusters::create_call_graph,
     mark_recursive_calls::mark_recursive_calls, parse::parse,
     resolve_identifiers::resolve_identifiers,
     tail_position::determine_tail_positions, tokenize::tokenize,
