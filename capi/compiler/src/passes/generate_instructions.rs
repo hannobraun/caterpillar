b8613397c64d281503b5d35d1dce7dc22071b2be
Hanno Braun <hello@hannobraun.com>
2024-10-16 12:58:29 +0200

    Start splitting up function that's too big

--- capi/compiler/src/passes/generate_instructions.rs
+++ capi/compiler/src/passes/generate_instructions.rs
@@ -29,24 +29,7 @@ pub fn generate_instructions(
     };
     let mut functions = BTreeMap::default();
 
-    // Create placeholder for call to `main` function.
-    //
-    // If there's no `main` function, this won't get replaced. Since this is a
-    // result of invalid code, an instruction generating the `BuildError` effect
-    // is an appropriate placeholder.
-    let call_to_main = output.instructions.push(Instruction::TriggerEffect {
-        effect: Effect::BuildError,
-    });
-    if let Some(function) = named_functions.find_by_name("main") {
-        output
-            .placeholders
-            .entry(Hash::new(&function))
-            .or_default()
-            .push(CallToFunction {
-                address: call_to_main,
-                is_tail_call: true,
-            });
-    }
+    create_placeholder_for_call_to_main(named_functions, &mut output);
 
     let mut added_and_updated_functions = changes
         .added
@@ -192,6 +175,28 @@ pub fn generate_instructions(
                 },
             );
         }
+    }
+}
+
+fn create_placeholder_for_call_to_main(
+    named_functions: &NamedFunctions,
+    output: &mut Output,
+) {
+    // If there's no `main` function, this won't get replaced. Since this is a
+    // result of invalid code, an instruction generating the `BuildError` effect
+    // is an appropriate placeholder.
+    let call_to_main = output.instructions.push(Instruction::TriggerEffect {
+        effect: Effect::BuildError,
+    });
+    if let Some(function) = named_functions.find_by_name("main") {
+        output
+            .placeholders
+            .entry(Hash::new(&function))
+            .or_default()
+            .push(CallToFunction {
+                address: call_to_main,
+                is_tail_call: true,
+            });
     }
 }
 
