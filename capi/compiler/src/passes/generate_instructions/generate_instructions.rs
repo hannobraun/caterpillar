80287ea40b0151a72e74a51ba64eb24136f157e9
Hanno Braun <hello@hannobraun.com>
2024-10-18 13:37:23 +0200

    Add comment

--- capi/compiler/src/passes/generate_instructions/generate_instructions.rs
+++ capi/compiler/src/passes/generate_instructions/generate_instructions.rs
@@ -70,6 +70,9 @@ fn compile_call_to_main(
     functions: &mut BTreeMap<Hash<Function>, capi_runtime::Function>,
 ) {
     let Some(main) = named_functions.find_by_name("main") else {
+        // If we can't find the call to `main`, that is a result of invalid
+        // code. Leaving the placeholder instruction is appropriate in that
+        // case.
         return;
     };
 
