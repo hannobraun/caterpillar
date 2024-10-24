e9c90110648c5aeb93efcf087593bd903d79f212
Hanno Braun <hello@hannobraun.com>
2024-10-24 12:56:38 +0200

    Add `IntrinsicFunction::signature`

--- capi/compiler/src/intrinsics.rs
+++ capi/compiler/src/intrinsics.rs
@@ -5,7 +5,8 @@ macro_rules! intrinsics {
         $(
             #[$attrs:meta]
             $name:expr,
-            $variant:ident;
+            $variant:ident,
+            $signature:expr;
         )*
     ) => {
         /// # Special functions that are known to the compiler
@@ -40,6 +41,12 @@ macro_rules! intrinsics {
 
                 Some(intrinsic)
             }
+
+            pub fn signature(&self) -> Option<[u32; 2]> {
+                match self {
+                    $(Self::$variant => $signature,)*
+                }
+            }
         }
 
         impl fmt::Display for IntrinsicFunction {
@@ -54,77 +61,77 @@ macro_rules! intrinsics {
 
 intrinsics! {
     /// # Add two signed 8-bit integers, triggering an error on overflow
-    "add_s8", AddS8;
+    "add_s8", AddS8, Some([2, 1]);
 
     /// # Add two signed 32-bit integers, triggering an error on overflow
-    "add_s32", AddS32;
+    "add_s32", AddS32, Some([2, 1]);
 
     /// # Add two unsigned 8-bit integers, triggering an error on overflow
-    "add_u8", AddU8;
+    "add_u8", AddU8, Some([2, 1]);
 
     /// # Add two unsigned 8-bit integers, wrapping on overflow
-    "add_u8_wrap", AddU8Wrap;
+    "add_u8_wrap", AddU8Wrap, Some([2, 1]);
 
     /// # Logical and
-    "and", And;
+    "and", And, Some([2, 1]);
 
     /// # Trigger a breakpoint
-    "brk", Brk;
+    "brk", Brk, Some([0, 0]);
 
     /// # Copy a value
-    "copy", Copy;
+    "copy", Copy, Some([1, 2]);
 
     /// # Divide two signed 32-bit integers
-    "div_s32", DivS32;
+    "div_s32", DivS32, Some([2, 1]);
 
     /// # Divide two unsigned 8-bit integers
-    "div_u8", DivU8;
+    "div_u8", DivU8, Some([2, 1]);
 
     /// # Drop a value
-    "drop", Drop;
+    "drop", Drop, Some([1, 0]);
 
     /// # Compare two values for equality
-    "eq", Eq;
+    "eq", Eq, Some([2, 1]);
 
     /// # Evaluate an anonymous function
-    "eval", Eval;
+    "eval", Eval, None;
 
     /// # Determine if the first of two signed 8-bit numbers is greater
-    "greater_s8", GreaterS8;
+    "greater_s8", GreaterS8, Some([2, 1]);
 
     /// # Determine if the first of two signed 32-bit numbers is greater
-    "greater_s32", GreaterS32;
+    "greater_s32", GreaterS32, Some([2, 1]);
 
     /// # Determine if the first of two unsigned 8-bit numbers is greater
-    "greater_u8", GreaterU8;
+    "greater_u8", GreaterU8, Some([2, 1]);
 
     /// # Multiply two signed 32-bit numbers, triggering an error on overflow
-    "mul_s32", MulS32;
+    "mul_s32", MulS32, Some([2, 1]);
 
     /// # Multiply two unsigned 8-bit numbers, wrapping on overflow
-    "mul_u8_wrap", MulU8Wrap;
+    "mul_u8_wrap", MulU8Wrap, Some([2, 1]);
 
     /// # Negate a signed 32-bit number
-    "neg_s32", NegS32;
+    "neg_s32", NegS32, Some([1, 1]);
 
     /// No operation
-    "nop", Nop;
+    "nop", Nop, Some([0, 0]);
 
     /// # Logical not
-    "not", Not;
+    "not", Not, Some([1, 1]);
 
     /// # Compute the remainder of the division of two signed 32-bit numbers
-    "remainder_s32", RemainderS32;
+    "remainder_s32", RemainderS32, Some([2, 1]);
 
     /// # Convert a signed 32-bit number to a signed 8-bit number
-    "s32_to_s8", S32ToS8;
+    "s32_to_s8", S32ToS8, Some([1, 1]);
 
     /// # Subtract two signed 32-bit numbers, triggering an error on overflow
-    "sub_s32", SubS32;
+    "sub_s32", SubS32, Some([2, 1]);
 
     /// # Subtract two unsigned 8-bit numbers, triggering an error on overflow
-    "sub_u8", SubU8;
+    "sub_u8", SubU8, Some([2, 1]);
 
     /// # Subtract two unsigned 8-bit numbers, wrapping on overflow
-    "sub_u8_wrap", SubU8Wrap;
+    "sub_u8_wrap", SubU8Wrap, Some([2, 1]);
 }
