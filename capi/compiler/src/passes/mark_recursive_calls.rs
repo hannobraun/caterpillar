c68e40a60eb0e940e7b12f8b8991b79df68a96fa
Hanno Braun <hello@hannobraun.com>
2024-10-24 13:42:02 +0200

    Update name of variable

--- capi/compiler/src/passes/mark_recursive_calls.rs
+++ capi/compiler/src/passes/mark_recursive_calls.rs
@@ -47,8 +47,8 @@ fn mark_recursive_calls_in_function(
     >,
 ) {
     for branch in function.branches.values_mut() {
-        for expression in branch.body.values_mut() {
-            match expression {
+        for fragment in branch.body.values_mut() {
+            match fragment {
                 Fragment::Function { function } => {
                     mark_recursive_calls_in_function(
                         function,
