90242f5b7a8fbe6ca3f9ba4c167f6998eaa2152a
Hanno Braun <hello@hannobraun.com>
2024-10-25 00:02:52 +0200

    Implement `serde` traits unconditionally

--- capi/compiler/src/code/index.rs
+++ capi/compiler/src/code/index.rs
@@ -1,9 +1,7 @@
 use std::marker::PhantomData;
 
 /// # The index of a named function in the root context
-#[derive(
-    Debug, Eq, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize,
-)]
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Index<T> {
     pub value: u32,
     pub t: PhantomData<T>,
@@ -16,6 +14,27 @@ impl<T> Clone for Index<T> {
 }
 
 impl<T> Copy for Index<T> {}
+
+impl<'de, T> serde::Deserialize<'de> for Index<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        Ok(Self {
+            value: u32::deserialize(deserializer)?,
+            t: PhantomData,
+        })
+    }
+}
+
+impl<T> serde::Serialize for Index<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.value.serialize(serializer)
+    }
+}
 
 impl<T> udigest::Digestable for Index<T> {
     fn unambiguously_encode<B: udigest::Buffer>(
