ce6a70f87cac6feb5a5260bca41a473c5fc420c4
Hanno Braun <hello@hannobraun.com>
2024-10-26 13:14:54 +0200

    Move `start` to dedicated module

--- capi/server/src/lib.rs
+++ capi/server/src/lib.rs
@@ -1,17 +1,4 @@
 mod server;
+mod start;
 
-pub async fn start(
-    address: String,
-    serve_dir: std::path::PathBuf,
-) -> anyhow::Result<()> {
-    use std::path::PathBuf;
-
-    use capi_build_game::build_and_watch_game;
-    use capi_watch::Watcher;
-
-    let watcher = Watcher::new(PathBuf::from("games"))?;
-    let game = build_and_watch_game("snake", watcher.changes).await?;
-    server::start(address, serve_dir, game).await?;
-
-    Ok(())
-}
+pub use self::start::start;
