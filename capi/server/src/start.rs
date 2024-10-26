e14819cfd7934c4d4c294b82587ae2aa34189144
Hanno Braun <hello@hannobraun.com>
2024-10-26 13:15:18 +0200

    Clean up imports

--- capi/server/src/start.rs
+++ capi/server/src/start.rs
@@ -1,14 +1,14 @@
+use std::path::PathBuf;
+
+use capi_build_game::build_and_watch_game;
+use capi_watch::Watcher;
+
 use crate::server;
 
 pub async fn start(
     address: String,
     serve_dir: std::path::PathBuf,
 ) -> anyhow::Result<()> {
-    use std::path::PathBuf;
-
-    use capi_build_game::build_and_watch_game;
-    use capi_watch::Watcher;
-
     let watcher = Watcher::new(PathBuf::from("games"))?;
     let game = build_and_watch_game("snake", watcher.changes).await?;
     server::start(address, serve_dir, game).await?;
