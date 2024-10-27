a474666984e20b437719eda592bfb51e5b78f137
Hanno Braun <hello@hannobraun.com>
2024-10-27 09:43:03 +0100

    Consolidate redundant code

--- capi/server/src/start.rs
+++ capi/server/src/start.rs
@@ -1,27 +1,16 @@
 use std::path::PathBuf;
 
-use capi_build_game::{build_and_watch_game, Event};
+use capi_build_game::build_and_watch_game;
 use capi_watch::Watcher;
 
-use crate::server;
+use crate::server::{self, CodeTx};
 
 pub async fn start(address: String, serve_dir: PathBuf) -> anyhow::Result<()> {
     let watcher = Watcher::new(PathBuf::from("games"))?;
     let mut build_events =
         build_and_watch_game("snake", watcher.changes).await?;
 
-    let Some(Event::ChangeDetected) = build_events.recv().await else {
-        // The channel has been closed already. This means we're shutting down.
-        return Ok(());
-    };
-    println!("build:change");
-    let Some(Event::BuildFinished(code)) = build_events.recv().await else {
-        // The channel has been closed already. This means we're shutting down.
-        return Ok(());
-    };
-    println!("build:finish");
-
-    let code_tx = server::start(address, serve_dir, code);
+    let mut server_task = ServerTask::Uninitialized { address, serve_dir };
 
     while let Some(event) = build_events.recv().await {
         match event {
@@ -31,12 +20,26 @@ pub async fn start(address: String, serve_dir: PathBuf) -> anyhow::Result<()> {
             capi_build_game::Event::BuildFinished(code) => {
                 println!("build:finish");
 
-                if code_tx.send(code).is_err() {
-                    return Ok(());
+                match server_task {
+                    ServerTask::Uninitialized { address, serve_dir } => {
+                        let code_tx = server::start(address, serve_dir, code);
+                        server_task = ServerTask::Initialized { code_tx };
+                    }
+                    ServerTask::Initialized { code_tx } => {
+                        if code_tx.send(code).is_err() {
+                            return Ok(());
+                        }
+                        server_task = ServerTask::Initialized { code_tx };
+                    }
                 }
             }
         }
     }
 
     Ok(())
+}
+
+enum ServerTask {
+    Uninitialized { address: String, serve_dir: PathBuf },
+    Initialized { code_tx: CodeTx },
 }
