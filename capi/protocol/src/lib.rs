81397fd65c8e2c19661387bdf3c7090991276afa
Hanno Braun <hello@hannobraun.com>
2024-10-28 13:14:33 +0100

    Prepare for follow-on change

--- capi/protocol/src/lib.rs
+++ capi/protocol/src/lib.rs
@@ -21,3 +21,20 @@ pub struct Versioned<T> {
     pub timestamp: u64,
     pub inner: T,
 }
+
+pub fn ron_options() -> ron::Options {
+    ron::Options::default()
+        // The default recursion limit (as of this writing) is `128`. After some
+        // recent changes, this started resulting in a
+        // `ron::Error::ExceededRecursionLimit`.
+        //
+        // I don't think there's a deep reason for that. What we're sending is
+        // just _very_ unoptimized, and another layer of abstraction that I
+        // added recently pushed us over the edge.
+        //
+        // Let's not worry about it too much for now. Once the traffic between
+        // the various components in a development setup becomes relevant, we'll
+        // need to replace RON with something more space-efficient anyway. And
+        // worry more about what we're sending in the first place.
+        .with_recursion_limit(256)
+}
--- capi/server/src/server.rs
+++ capi/server/src/server.rs
@@ -8,7 +8,7 @@ use axum::{
     Router,
 };
 use capi_build_game::CompilerOutput;
-use capi_protocol::Versioned;
+use capi_protocol::{ron_options, Versioned};
 use tokio::{
     fs::File,
     io::AsyncReadExt,
@@ -114,21 +114,7 @@ async fn serve_code(
             inner: &code.inner,
         };
 
-        let ron_options = ron::Options::default()
-            // The default recursion limit (as of this writing) is `128`. After
-            // some recent changes, this started resulting in a
-            // `ron::Error::ExceededRecursionLimit`.
-            //
-            // I don't think there's a deep reason for that. What we're sending
-            // is just _very_ unoptimized, and another layer of abstraction that
-            // I added recently pushed us over the edge.
-            //
-            // Let's not worry about it too much for now. Once the traffic
-            // between the various components in a development setup becomes
-            // relevant, we'll need to replace RON with something more space-
-            // efficient anyway. And worry more about what we're sending in the
-            // first place.
-            .with_recursion_limit(256);
+        let ron_options = ron_options();
 
         return ron_options
             .to_string(&code)
