084f64e5326c2cb24d13ba3cd448138de6cc8ac0
Hanno Braun <hello@hannobraun.com>
2024-10-29 14:17:39 +0100

    Display signature for each fragment

--- capi/debugger/src/ui/components/function.rs
+++ capi/debugger/src/ui/components/function.rs
@@ -182,6 +182,11 @@ fn make_single_expression(
     class_outer: &mut String,
     actions_tx: ActionsTx,
 ) -> (View, Option<HtmlElement<Span>>, Option<HtmlElement<Span>>) {
+    let expression = format!(
+        "{expression}: {:?} -> {:?}",
+        data.signature.inputs, data.signature.outputs
+    );
+
     if data.has_durable_breakpoint {
         class_outer.push_str(" bg-blue-300");
     }
