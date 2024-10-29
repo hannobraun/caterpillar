5cb03f17bafa759c59e536eeb64041d648732e7e
Hanno Braun <hello@hannobraun.com>
2024-10-29 13:33:41 +0100

    Update variable name

--- capi/compiler/src/passes/resolve_functions.rs
+++ capi/compiler/src/passes/resolve_functions.rs
@@ -27,8 +27,8 @@ fn resolve_calls_in_function(
     resolved_hashes_by_name: &mut BTreeMap<String, Hash<Function>>,
 ) {
     for branch in function.branches.values_mut() {
-        for typed_fragment in branch.body.values_mut() {
-            resolve_calls_in_fragment(typed_fragment, resolved_hashes_by_name);
+        for fragment in branch.body.values_mut() {
+            resolve_calls_in_fragment(fragment, resolved_hashes_by_name);
         }
     }
 }
