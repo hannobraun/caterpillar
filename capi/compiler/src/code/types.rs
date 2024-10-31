3af1808cbbe924734d95dbbfdc4cc920b616c2da
Hanno Braun <hello@hannobraun.com>
2024-10-31 13:15:18 +0100

    Derive `Eq` for `ConcreteSignature`

--- capi/compiler/src/code/types.rs
+++ capi/compiler/src/code/types.rs
@@ -107,7 +107,7 @@ impl Signature {
 /// This type is only intended for type signatures that have a lifetime
 /// extending beyond that of a running compiler. Like those of intrinsic or host
 /// functions.
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct ConcreteSignature {
     /// # The inputs that the function consumes
     pub inputs: Vec<Type>,
