7cade57087519136b0dac88c57aa7fc7c27b2e99
Hanno Braun <hello@hannobraun.com>
2024-11-01 12:05:35 +0100

    Order types in module according to relevance
    `Type` is the first thing that's used in `Types`, so introducing it
    right after makes sense. `Signature` is mentioned later and also uses
    it, so I think that's easier to understand with `Type` having been
    introduced.

--- capi/compiler/src/code/types.rs
+++ capi/compiler/src/code/types.rs
@@ -50,6 +50,47 @@ pub struct Types {
     pub for_functions: BTreeMap<FunctionLocation, Signature>,
 }
 
+/// # The type of a value
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    serde::Deserialize,
+    serde::Serialize,
+    udigest::Digestable,
+)]
+pub enum Type {
+    /// # A function
+    Function {
+        /// # The function's signature
+        signature: Signature,
+    },
+
+    /// # A number
+    ///
+    /// ## Implementation Note
+    ///
+    /// Since the language is still mostly untyped, this actually covers many
+    /// different types that are all represented as a 32-bit number.
+    ///
+    /// I expect that this will get split into multiple, more specific numeric
+    /// types at some point.
+    Number,
+
+    /// # The type is unknown
+    ///
+    /// This is used as a placeholder, while the type is still being inferred.
+    /// It can also be the end result, if the type _can't_ be inferred.
+    ///
+    /// For now, the type system isn't advanced enough to guarantee full
+    /// inference. Code that deals with types should be able to deal with this
+    /// variant.
+    Unknown,
+}
+
 /// # The signature of an expression
 #[derive(
     Clone,
@@ -128,45 +169,4 @@ where
             outputs: outputs.into_iter().collect(),
         }
     }
-}
-
-/// # The type of a value
-#[derive(
-    Clone,
-    Debug,
-    Eq,
-    PartialEq,
-    Ord,
-    PartialOrd,
-    serde::Deserialize,
-    serde::Serialize,
-    udigest::Digestable,
-)]
-pub enum Type {
-    /// # A function
-    Function {
-        /// # The function's signature
-        signature: Signature,
-    },
-
-    /// # A number
-    ///
-    /// ## Implementation Note
-    ///
-    /// Since the language is still mostly untyped, this actually covers many
-    /// different types that are all represented as a 32-bit number.
-    ///
-    /// I expect that this will get split into multiple, more specific numeric
-    /// types at some point.
-    Number,
-
-    /// # The type is unknown
-    ///
-    /// This is used as a placeholder, while the type is still being inferred.
-    /// It can also be the end result, if the type _can't_ be inferred.
-    ///
-    /// For now, the type system isn't advanced enough to guarantee full
-    /// inference. Code that deals with types should be able to deal with this
-    /// variant.
-    Unknown,
 }
