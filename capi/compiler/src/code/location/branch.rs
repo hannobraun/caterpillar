c51013bc9911485d668fc44c540bb6201f5ce048
Hanno Braun <hello@hannobraun.com>
2024-11-08 13:54:27 +0100

    Split module

---
+++ capi/compiler/src/code/location/branch.rs
@@ -0,0 +1,53 @@
+use std::fmt;
+
+use crate::code::{Branch, Functions, Index};
+
+use super::FunctionLocation;
+
+/// # The location of a branch in the source code
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    Ord,
+    PartialEq,
+    PartialOrd,
+    serde::Deserialize,
+    serde::Serialize,
+)]
+pub struct BranchLocation {
+    pub parent: Box<FunctionLocation>,
+    pub index: Index<Branch>,
+}
+
+impl BranchLocation {
+    /// # Create a helper that implements [`fmt::Display`]
+    pub fn display<'r>(
+        &'r self,
+        functions: &'r Functions,
+    ) -> BranchLocationDisplay<'r> {
+        BranchLocationDisplay {
+            location: self,
+            functions,
+        }
+    }
+}
+
+/// # Helper struct to display [`BranchLocation`]
+///
+/// Implements [`fmt::Display`], which [`BranchLocation`] itself doesn't.
+pub struct BranchLocationDisplay<'r> {
+    location: &'r BranchLocation,
+    functions: &'r Functions,
+}
+
+impl fmt::Display for BranchLocationDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "branch {} of {}",
+            self.location.index,
+            self.location.parent.display(self.functions),
+        )
+    }
+}
