f976558aef048f424f6cb829e69669dc479a8c6f
Hanno Braun <hello@hannobraun.com>
2024-11-08 11:23:25 +0100

    Update name of module

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -1,5 +1,6 @@
 mod build_call_graph;
 mod detect_changes;
+mod determine_tail_positions;
 mod find_divergent_functions;
 mod generate_instructions;
 mod infer_types;
@@ -7,15 +8,14 @@ mod mark_recursive_calls;
 mod parse;
 mod resolve_functions;
 mod resolve_most_identifiers;
-mod tail_position;
 mod tokenize;
 
 pub use {
     build_call_graph::build_call_graph, detect_changes::detect_changes,
+    determine_tail_positions::determine_tail_positions,
     find_divergent_functions::find_divergent_functions,
     generate_instructions::generate_instructions, infer_types::infer_types,
     mark_recursive_calls::mark_recursive_calls, parse::parse,
     resolve_functions::resolve_calls_to_user_defined_functions,
-    resolve_most_identifiers::resolve_most_identifiers,
-    tail_position::determine_tail_positions, tokenize::tokenize,
+    resolve_most_identifiers::resolve_most_identifiers, tokenize::tokenize,
 };
