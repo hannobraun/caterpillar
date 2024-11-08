0d8006ad4694a1679a0a86da0f9d4b6850ba5a01
Hanno Braun <hello@hannobraun.com>
2024-11-08 11:21:59 +0100

    Update name of module

--- capi/compiler/src/passes/mod.rs
+++ capi/compiler/src/passes/mod.rs
@@ -6,7 +6,7 @@ mod infer_types;
 mod mark_recursive_calls;
 mod parse;
 mod resolve_functions;
-mod resolve_identifiers;
+mod resolve_most_identifiers;
 mod tail_position;
 mod tokenize;
 
@@ -16,6 +16,6 @@ pub use {
     generate_instructions::generate_instructions, infer_types::infer_types,
     mark_recursive_calls::mark_recursive_calls, parse::parse,
     resolve_functions::resolve_calls_to_user_defined_functions,
-    resolve_identifiers::resolve_most_identifiers,
+    resolve_most_identifiers::resolve_most_identifiers,
     tail_position::determine_tail_positions, tokenize::tokenize,
 };
