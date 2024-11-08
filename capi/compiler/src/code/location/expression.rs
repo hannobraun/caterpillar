fb68e9ebf0395a223cd05fe131cd191ae30c6c3a
Hanno Braun <hello@hannobraun.com>
2024-11-08 13:51:41 +0100

    Split module

---
+++ capi/compiler/src/code/location/expression.rs
@@ -0,0 +1,53 @@
+use std::fmt;
+
+use crate::code::{Expression, Functions, Index};
+
+use super::BranchLocation;
+
+/// # The location of an expression in the source code
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    Ord,
+    PartialEq,
+    PartialOrd,
+    serde::Deserialize,
+    serde::Serialize,
+)]
+pub struct ExpressionLocation {
+    pub parent: Box<BranchLocation>,
+    pub index: Index<Expression>,
+}
+
+impl ExpressionLocation {
+    /// # Create a helper that implements [`fmt::Display`]
+    pub fn display<'r>(
+        &'r self,
+        functions: &'r Functions,
+    ) -> ExpressionLocationDisplay<'r> {
+        ExpressionLocationDisplay {
+            location: self,
+            functions,
+        }
+    }
+}
+
+/// # Helper struct to display [`ExpressionLocation`]
+///
+/// Implements [`fmt::Display`], which [`ExpressionLocation`] itself doesn't.
+pub struct ExpressionLocationDisplay<'r> {
+    location: &'r ExpressionLocation,
+    functions: &'r Functions,
+}
+
+impl fmt::Display for ExpressionLocationDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "expression {}\n    in {}",
+            self.location.index,
+            self.location.parent.display(self.functions)
+        )
+    }
+}
--- capi/compiler/src/code/location/mod.rs
+++ capi/compiler/src/code/location/mod.rs
@@ -1,3 +1,7 @@
+mod expression;
+
+pub use self::expression::ExpressionLocation;
+
 use std::{fmt, ops::Deref};
 
 use super::{Branch, Expression, Functions, Index, NamedFunction};
@@ -107,54 +111,6 @@ impl Located<Branch, BranchLocation> {
                     index,
                 },
             })
-    }
-}
-
-/// # The location of an expression in the source code
-#[derive(
-    Clone,
-    Debug,
-    Eq,
-    Ord,
-    PartialEq,
-    PartialOrd,
-    serde::Deserialize,
-    serde::Serialize,
-)]
-pub struct ExpressionLocation {
-    pub parent: Box<BranchLocation>,
-    pub index: Index<Expression>,
-}
-
-impl ExpressionLocation {
-    /// # Create a helper that implements [`fmt::Display`]
-    pub fn display<'r>(
-        &'r self,
-        functions: &'r Functions,
-    ) -> ExpressionLocationDisplay<'r> {
-        ExpressionLocationDisplay {
-            location: self,
-            functions,
-        }
-    }
-}
-
-/// # Helper struct to display [`ExpressionLocation`]
-///
-/// Implements [`fmt::Display`], which [`ExpressionLocation`] itself doesn't.
-pub struct ExpressionLocationDisplay<'r> {
-    location: &'r ExpressionLocation,
-    functions: &'r Functions,
-}
-
-impl fmt::Display for ExpressionLocationDisplay<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "expression {}\n    in {}",
-            self.location.index,
-            self.location.parent.display(self.functions)
-        )
     }
 }
 
