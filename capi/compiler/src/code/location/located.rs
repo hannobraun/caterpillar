6679a6b560c0a1437318275c9a2a4a28e697f838
Hanno Braun <hello@hannobraun.com>
2024-11-08 14:07:16 +0100

    Remove unused code

--- capi/compiler/src/code/location/located.rs
+++ capi/compiler/src/code/location/located.rs
@@ -95,25 +95,6 @@ where
     }
 }
 
-impl Located<Branch, BranchLocation> {
-    /// # Iterate over the expressions in the branch's body
-    pub fn body(
-        &self,
-    ) -> impl Iterator<Item = Located<Expression, ExpressionLocation>> {
-        let location = self.location.clone();
-        self.body
-            .clone()
-            .into_iter()
-            .map(move |(index, expression)| Located {
-                fragment: expression,
-                location: ExpressionLocation {
-                    parent: Box::new(location.clone()),
-                    index,
-                },
-            })
-    }
-}
-
 impl Located<&Branch, BranchLocation> {
     /// # Iterate over the expressions in the branch's body
     pub fn body(
