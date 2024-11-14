ad4cb01ed3cf2ae0c08c4f9662a77e5a73196513
Hanno Braun <hello@hannobraun.com>
2024-11-14 12:11:12 +0100

    Finish cleaning up overloaded module

--- capi/compiler/src/code/location/located.rs
+++ capi/compiler/src/code/location/located.rs
@@ -1,9 +1,5 @@
 use std::ops::{Deref, DerefMut};
 
-use crate::code::{Function, Index, NamedFunction};
-
-use super::FunctionLocation;
-
 /// # A fragment of code, with its location attached
 #[derive(Clone, Debug)]
 pub struct Located<T: HasLocation> {
@@ -25,44 +21,6 @@ impl<T: HasLocation> Deref for Located<T> {
 impl<T: HasLocation> DerefMut for Located<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.fragment
-    }
-}
-
-impl Located<&NamedFunction> {
-    /// # Access the index of the found function
-    ///
-    /// This is a convenience accessor, to make code that would otherwise access
-    /// `metadata` directly more readable.
-    pub fn index(&self) -> Index<NamedFunction> {
-        self.location
-    }
-
-    /// # Access the location of the found function
-    pub fn location(&self) -> FunctionLocation {
-        let index = self.location;
-        index.into()
-    }
-
-    /// # Convert this located named function to a located function
-    pub fn as_located_function(&self) -> Located<&Function> {
-        Located {
-            fragment: &self.fragment.inner,
-            location: FunctionLocation::NamedFunction {
-                index: self.location,
-            },
-        }
-    }
-}
-
-impl Located<&mut NamedFunction> {
-    /// # Convert this located named function to a located function
-    pub fn as_located_function_mut(&mut self) -> Located<&mut Function> {
-        Located {
-            fragment: &mut self.fragment.inner,
-            location: FunctionLocation::NamedFunction {
-                index: self.location,
-            },
-        }
     }
 }
 
--- capi/compiler/src/code/location/named_function.rs
+++ capi/compiler/src/code/location/named_function.rs
@@ -1,7 +1,45 @@
-use crate::code::{Index, NamedFunction};
+use crate::code::{Function, Index, NamedFunction};
 
-use super::located::HasLocation;
+use super::{located::HasLocation, FunctionLocation, Located};
 
 impl HasLocation for NamedFunction {
     type Location = Index<NamedFunction>;
+}
+
+impl Located<&NamedFunction> {
+    /// # Access the index of the found function
+    ///
+    /// This is a convenience accessor, to make code that would otherwise access
+    /// `metadata` directly more readable.
+    pub fn index(&self) -> Index<NamedFunction> {
+        self.location
+    }
+
+    /// # Access the location of the found function
+    pub fn location(&self) -> FunctionLocation {
+        let index = self.location;
+        index.into()
+    }
+
+    /// # Convert this located named function to a located function
+    pub fn as_located_function(&self) -> Located<&Function> {
+        Located {
+            fragment: &self.fragment.inner,
+            location: FunctionLocation::NamedFunction {
+                index: self.location,
+            },
+        }
+    }
+}
+
+impl Located<&mut NamedFunction> {
+    /// # Convert this located named function to a located function
+    pub fn as_located_function_mut(&mut self) -> Located<&mut Function> {
+        Located {
+            fragment: &mut self.fragment.inner,
+            location: FunctionLocation::NamedFunction {
+                index: self.location,
+            },
+        }
+    }
 }
