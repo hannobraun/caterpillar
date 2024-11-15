0635f93af6a8440f2932d133bf2fc6e8a4b8a541
Hanno Braun <hello@hannobraun.com>
2024-11-15 13:28:35 +0100

    Update name of variable

--- capi/compiler/src/passes/build_call_graph.rs
+++ capi/compiler/src/passes/build_call_graph.rs
@@ -19,8 +19,8 @@ fn build_pet_call_graph(functions: &Functions) -> PetCallGraph {
     let mut call_graph = Graph::new();
     let mut graph_index_by_function_location = BTreeMap::new();
 
-    for named_function in functions.all_functions() {
-        let location = named_function.location;
+    for function in functions.all_functions() {
+        let location = function.location;
 
         graph_index_by_function_location
             .entry(location.clone())
