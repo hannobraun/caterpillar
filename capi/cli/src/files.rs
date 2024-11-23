def64081a0dc18a8ad522c2784d743e45ad9195e
Hanno Braun <hello@hannobraun.com>
2024-11-23 22:07:47 +0100

    Consolidate redundant code

--- capi/cli/src/files.rs
+++ capi/cli/src/files.rs
@@ -1,51 +1,52 @@
 pub const FILES: Files = Files::new();
 
-#[derive(Debug)]
-pub struct Files {
-    pub capi_debugger_bg_wasm: &'static [u8],
-    pub capi_debugger_js: &'static [u8],
-    pub capi_host_wasm: &'static [u8],
-    pub index_html: &'static [u8],
-    pub tailwind_js: &'static [u8],
-}
-
-impl Files {
-    pub const fn new() -> Self {
-        Self {
-            capi_debugger_bg_wasm: include_bytes!(concat!(
-                env!("FILES"),
-                "/capi-debugger_bg.wasm"
-            )),
-            capi_debugger_js: include_bytes!(concat!(
-                env!("FILES"),
-                "/capi-debugger.js"
-            )),
-            capi_host_wasm: include_bytes!(concat!(
-                env!("FILES"),
-                "/capi_host.wasm"
-            )),
-            index_html: include_bytes!(concat!(env!("FILES"), "/index.html")),
-            tailwind_js: include_bytes!(concat!(env!("FILES"), "/tailwind.js")),
+macro_rules! files {
+    ($($field:ident, $name:expr;)*) => {
+        #[derive(Debug)]
+        pub struct Files {
+            $(
+                pub $field: &'static [u8],
+            )*
         }
-    }
 
-    pub fn list_invalid(&self) -> Vec<&'static str> {
-        let mut invalid_files = Vec::new();
+        impl Files {
+            pub const fn new() -> Self {
+                Self {
+                    $(
+                        $field: include_bytes!(concat!(
+                            env!("FILES"),
+                            "/",
+                            $name,
+                        )),
+                    )*
+                }
+            }
 
-        let files = [
-            (self.capi_debugger_bg_wasm, "capi-debugger_bg.wasm"),
-            (self.capi_debugger_js, "capi-debugger.js"),
-            (self.capi_host_wasm, "capi_host.wasm"),
-            (self.index_html, "index.html"),
-            (self.tailwind_js, "tailwind.js"),
-        ];
+            pub fn list_invalid(&self) -> Vec<&'static str> {
+                let mut invalid_files = Vec::new();
 
-        for (file, name) in files {
-            if file.is_empty() {
-                invalid_files.push(name);
+                let files = [
+                    $(
+                        (self.$field, $name),
+                    )*
+                ];
+
+                for (file, name) in files {
+                    if file.is_empty() {
+                        invalid_files.push(name);
+                    }
+                }
+
+                invalid_files
             }
         }
-
-        invalid_files
-    }
+    };
 }
+
+files!(
+    capi_debugger_bg_wasm, "capi-debugger_bg.wasm";
+    capi_debugger_js, "capi-debugger.js";
+    capi_host_wasm, "capi_host.wasm";
+    index_html, "index.html";
+    tailwind_js, "tailwind.js";
+);
