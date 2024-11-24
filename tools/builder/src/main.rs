172e8b187e30cbc355a350cbe8b479ffe32ca1a0
Hanno Braun <hello@hannobraun.com>
2024-11-24 10:49:09 +0100

    Prepare to deploy games

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -4,7 +4,27 @@ mod serve;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    use clap::Parser;
+
     tracing_subscriber::fmt().init();
-    pipeline::pipeline().await?;
+
+    let args = Args::parse();
+    match args.command {
+        None | Some(Command::Serve) => {
+            pipeline::pipeline().await?;
+        }
+    }
+
     Ok(())
+}
+
+#[derive(clap::Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    Serve,
 }
