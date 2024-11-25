84d8348fd4f972c8bc05403e69e478941e3198dd
Hanno Braun <hello@hannobraun.com>
2024-11-25 13:16:45 +0100

    Simplify

--- capi/cli/src/main.rs
+++ capi/cli/src/main.rs
@@ -59,49 +59,49 @@ fn check_files() -> anyhow::Result<()> {
 
     let invalid_files = files::FILES.list_invalid();
 
-    if !invalid_files.is_empty() {
-        let mut err = String::new();
-
-        write!(
-            err,
-            "Can't start the server because the following files are not \
-            available:\n\
-            \n",
-        )?;
-
-        for file in invalid_files {
-            writeln!(err, "- `{file}`")?;
-        }
-
-        write!(
-            err,
-            "\n\
-            All of those files should be included\n\
-            with this tool! That they aren't, means\n\
-            that the tool has not been built\n\
-            correctly.\n\
-            \n\
-            Are you trying to run the server from\n\
-            within the Caterpillar repository?\n\
-            \n\
-            \tThen do so through the build tool!\n\
-            \tJust execute `cargo run` from the\n\
-            \trepository root.\n\
-            \n\
-            Are you trying to build a version of\n\
-            this tool for use outside of the\n\
-            repository?\n\
-            \n\
-            \tSorry, but as of 2024-11-23, this is\n\
-            \tnot supported yet! If you're reading\n\
-            \tthis some time after that date, then\n\
-            \tmaybe it has become possible in the\n\
-            \tmeantime, and this error message has not\n\
-            \tbeen updated.\n",
-        )?;
-
-        return Err(anyhow!("{}", err));
+    if invalid_files.is_empty() {
+        return Ok(());
     }
 
-    Ok(())
+    let mut err = String::new();
+
+    write!(
+        err,
+        "Can't start the server because the following files are not \
+        available:\n\
+        \n",
+    )?;
+
+    for file in invalid_files {
+        writeln!(err, "- `{file}`")?;
+    }
+
+    write!(
+        err,
+        "\n\
+        All of those files should be included\n\
+        with this tool! That they aren't, means\n\
+        that the tool has not been built\n\
+        correctly.\n\
+        \n\
+        Are you trying to run the server from\n\
+        within the Caterpillar repository?\n\
+        \n\
+        \tThen do so through the build tool!\n\
+        \tJust execute `cargo run` from the\n\
+        \trepository root.\n\
+        \n\
+        Are you trying to build a version of\n\
+        this tool for use outside of the\n\
+        repository?\n\
+        \n\
+        \tSorry, but as of 2024-11-23, this is\n\
+        \tnot supported yet! If you're reading\n\
+        \tthis some time after that date, then\n\
+        \tmaybe it has become possible in the\n\
+        \tmeantime, and this error message has not\n\
+        \tbeen updated.\n",
+    )?;
+
+    Err(anyhow!("{}", err))
 }
