5ab14ce30d8ada557d886a69fd9813d43783b226
Hanno Braun <hello@hannobraun.com>
2024-11-28 12:21:20 +0100

    Remove support from website builder to serve site
    It never worked very well, as it doesn't reload on changes. Using Deno
    directly is much better.

--- tools/website/src/main.rs
+++ tools/website/src/main.rs
@@ -1,25 +1,13 @@
-use std::{fs, path::Path, process::Command};
+use std::{fs, path::Path};
 
-use anyhow::anyhow;
-use clap::Parser;
-use tempfile::TempDir;
 use walkdir::WalkDir;
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
-
     let source_dir = Path::new("website");
 
-    if args.build {
-        let target_dir = Path::new("website-output");
+    let target_dir = Path::new("website-output");
 
-        build(source_dir, target_dir)?;
-    } else {
-        let target_dir = TempDir::new()?;
-
-        build(source_dir, target_dir.path())?;
-        serve(target_dir.path())?;
-    }
+    build(source_dir, target_dir)?;
 
     Ok(())
 }
@@ -45,26 +33,4 @@ fn build(source_dir: &Path, target_dir: &Path) -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-fn serve(path: &Path) -> anyhow::Result<()> {
-    let exit_status = Command::new("deno")
-        .args(["task", "start"])
-        .current_dir(path)
-        .status()?;
-
-    if !exit_status.success() {
-        return Err(anyhow!(
-            "`deno task start` returned exit code `{:?}`",
-            exit_status.code()
-        ));
-    }
-
-    Ok(())
-}
-
-#[derive(clap::Parser)]
-pub struct Args {
-    #[arg(short, long)]
-    pub build: bool,
 }
