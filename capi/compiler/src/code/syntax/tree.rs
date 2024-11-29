ca93201500b35794eb11711a8691cc3e24817095
Hanno Braun <hello@hannobraun.com>
2024-11-29 23:34:45 +0100

    Add `SyntaxTree::function_by_name`

--- capi/compiler/src/code/syntax/tree.rs
+++ capi/compiler/src/code/syntax/tree.rs
@@ -1,6 +1,27 @@
-use crate::code::{IndexMap, NamedFunction};
+use crate::code::{IndexMap, Located, NamedFunction};
 
 #[derive(Default)]
 pub struct SyntaxTree {
     pub named_functions: IndexMap<NamedFunction>,
+}
+
+impl SyntaxTree {
+    /// # Find the function with the provided name
+    ///
+    /// Return `None`, if no function with this name can be found.
+    pub fn function_by_name(
+        &self,
+        name: &str,
+    ) -> Option<Located<&NamedFunction>> {
+        self.named_functions.iter().find_map(|(&index, function)| {
+            if function.name == name {
+                Some(Located {
+                    fragment: function,
+                    location: index,
+                })
+            } else {
+                None
+            }
+        })
+    }
 }
