d349901f4bea8519f88aaa85d750903f2b5e7b8e
Hanno Braun <hello@hannobraun.com>
2024-12-07 11:26:29 +0100

    Infer signatures of host function calls

--- capi/compiler/src/code/types/infer.rs
+++ capi/compiler/src/code/types/infer.rs
@@ -12,12 +12,19 @@ pub fn infer_expression(
 ) -> Option<Signature> {
     match expression {
         Expression::Identifier { .. } => {
+            let host = function_calls.is_call_to_host_function(location);
             let intrinsic =
                 function_calls.is_call_to_intrinsic_function(location);
 
-            match intrinsic {
-                Some(intrinsic) => intrinsic.signature(),
-                None => None,
+            match (host, intrinsic) {
+                (Some(host), None) => Some(host.signature.clone()),
+                (None, Some(intrinsic)) => intrinsic.signature(),
+                (None, None) => None,
+                _ => {
+                    unreachable!(
+                        "Single identifier resolved to multiple function calls."
+                    );
+                }
             }
         }
         Expression::LiteralNumber { .. } => Some(Signature {
