4f3e2ac2aee10c6c68dc49b21a711af5e68373eb
Hanno Braun <hello@hannobraun.com>
2024-12-07 11:13:26 +0100

    Infer signatures of intrinsic function calls

--- capi/compiler/src/code/types/infer.rs
+++ capi/compiler/src/code/types/infer.rs
@@ -7,10 +7,19 @@ use super::{Signature, Type};
 
 pub fn infer_expression(
     expression: &Expression,
-    _: &MemberLocation,
-    _: &FunctionCalls,
+    location: &MemberLocation,
+    function_calls: &FunctionCalls,
 ) -> Option<Signature> {
     match expression {
+        Expression::Identifier { .. } => {
+            let intrinsic =
+                function_calls.is_call_to_intrinsic_function(location);
+
+            match intrinsic {
+                Some(intrinsic) => intrinsic.signature(),
+                None => None,
+            }
+        }
         Expression::LiteralNumber { .. } => Some(Signature {
             inputs: vec![],
             outputs: vec![Type::Number],
