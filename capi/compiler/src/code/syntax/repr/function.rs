f43f64d62fe9e5e93ab5be23a21fefcbf14c54b7
Hanno Braun <hello@hannobraun.com>
2024-12-08 08:06:26 +0100

    Add `Branch::identifier_index`

--- capi/compiler/src/code/syntax/repr/function.rs
+++ capi/compiler/src/code/syntax/repr/function.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 use capi_runtime::Value;
 
 use crate::code::IndexMap;
@@ -68,6 +70,34 @@ pub struct Branch {
 
     /// # The body of the branch
     pub body: IndexMap<Member>,
+}
+
+impl Branch {
+    /// # Compute the index of given identifier
+    ///
+    /// Compute the 0-based index of the parameter with the given name, within
+    /// the list of this branch's parameters, only counting parameters that bind
+    /// to an identifier within the branch.
+    ///
+    /// Parameters are patterns that could bind a value to an identifier that is
+    /// then available within the branch, or they could just match an argument,
+    /// but not make any value available in the branch.
+    ///
+    /// This index is required to keep track of the branch parameters on the
+    /// stack. Parameters that do not bind to an identifier are not relevant for
+    /// that, since they are not available in the branch.
+    pub fn identifier_index(&self, name: &str) -> Option<u32> {
+        let indices = iter::successors(Some(0), |i| Some(i + 1));
+        let identifiers =
+            self.parameters.iter().filter_map(|pattern| match pattern {
+                Pattern::Identifier { name } => Some(name),
+                Pattern::Literal { .. } => None,
+            });
+
+        indices
+            .zip(identifiers)
+            .find_map(|(i, identifier)| (identifier == name).then_some(i))
+    }
 }
 
 /// # A pattern
