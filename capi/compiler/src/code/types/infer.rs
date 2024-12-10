ba14e6a235fb4e061412b8e6c1381dcd5ce64458
Hanno Braun <hello@hannobraun.com>
2024-12-10 13:17:51 +0100

    Prepare to track stacks per expression

--- capi/compiler/src/code/types/infer.rs
+++ capi/compiler/src/code/types/infer.rs
@@ -59,7 +59,7 @@ fn infer_branch(
     explicit_types: &ExplicitTypes,
     function_calls: &FunctionCalls,
     signatures: &mut Signatures,
-    _: &mut Stacks,
+    stacks: &mut Stacks,
 ) -> Result<(), TypeError> {
     let mut stack = Some(Vec::new());
 
@@ -70,6 +70,7 @@ fn infer_branch(
             explicit_types,
             function_calls,
             signatures,
+            stacks,
             &mut stack,
         )?;
     }
@@ -83,6 +84,7 @@ fn infer_expression(
     explicit_types: &ExplicitTypes,
     function_calls: &FunctionCalls,
     signatures: &mut Signatures,
+    _: &mut Stacks,
     stack: &mut Option<Stack>,
 ) -> Result<(), TypeError> {
     let explicit = explicit_types.signature_of(&expression.location);
