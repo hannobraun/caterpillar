db86db25d29dc348daa4884e8fd27dd6e7663e18
Hanno Braun <hello@hannobraun.com>
2024-12-17 13:26:55 +0100

    Move tests closer to the interface they test

--- capi/compiler/src/code/dependencies/repr.rs
+++ capi/compiler/src/code/dependencies/repr.rs
@@ -150,3 +150,231 @@ impl DependencyCluster {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        code::{
+            syntax::SyntaxTree, Dependencies, DependencyCluster, FunctionCalls,
+            Index, Tokens,
+        },
+        host::NoHost,
+    };
+
+    #[test]
+    fn no_recursion() {
+        let (syntax_tree, dependencies) = resolve_dependencies(
+            r"
+                f: fn
+                    \ ->
+                        g
+                end
+
+                g: fn
+                    \ ->
+                        nop
+                end
+            ",
+        );
+
+        let f = syntax_tree.function_by_name("f").unwrap().location();
+        let g = syntax_tree.function_by_name("g").unwrap().location();
+
+        assert_eq!(
+            dependencies.clusters().cloned().collect::<Vec<_>>(),
+            [(Index::from(0), g), (Index::from(0), f),]
+                .into_iter()
+                .map(|locations_by_index| DependencyCluster {
+                    functions: [locations_by_index].into_iter().collect(),
+                })
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    #[test]
+    fn self_recursion() {
+        let (syntax_tree, dependencies) = resolve_dependencies(
+            r"
+                f: fn
+                    \ ->
+                        g
+                end
+
+                g: fn
+                    \ ->
+                        g
+                end
+            ",
+        );
+
+        let f = syntax_tree.function_by_name("f").unwrap().location();
+        let g = syntax_tree.function_by_name("g").unwrap().location();
+
+        assert_eq!(
+            dependencies.clusters().cloned().collect::<Vec<_>>(),
+            [(Index::from(0), g), (Index::from(0), f),]
+                .into_iter()
+                .map(|locations_by_index| DependencyCluster {
+                    functions: [locations_by_index].into_iter().collect(),
+                })
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    #[test]
+    fn mutual_recursion() {
+        let (syntax_tree, dependencies) = resolve_dependencies(
+            r"
+                f: fn
+                    \ ->
+                        g
+                end
+
+                g: fn
+                    \ ->
+                        h
+                end
+
+                h: fn
+                    \ ->
+                        g
+                end
+            ",
+        );
+
+        let f = syntax_tree.function_by_name("f").unwrap().location();
+        let g = syntax_tree.function_by_name("g").unwrap().location();
+        let h = syntax_tree.function_by_name("h").unwrap().location();
+
+        assert_eq!(
+            dependencies.clusters().cloned().collect::<Vec<_>>(),
+            [
+                [(Index::from(0), g), (Index::from(1), h)].as_slice(),
+                [(Index::from(0), f)].as_slice(),
+            ]
+            .into_iter()
+            .map(|locations_by_index| DependencyCluster {
+                functions: locations_by_index.iter().cloned().collect(),
+            })
+            .collect::<Vec<_>>(),
+        );
+    }
+
+    #[test]
+    fn sort_clusters_by_call_graph() {
+        let (syntax_tree, dependencies) = resolve_dependencies(
+            r"
+                f: fn
+                    \ ->
+                        # Call a function that comes is placed after this one in
+                        # the source code.
+                        h
+                end
+
+                g: fn
+                    \ ->
+                        nop
+                end
+
+                h: fn
+                    \ ->
+                        # And for some variety, call a function that is placed
+                        # before.
+                        g
+                end
+            ",
+        );
+
+        let f = syntax_tree.function_by_name("f").unwrap().location();
+        let g = syntax_tree.function_by_name("g").unwrap().location();
+        let h = syntax_tree.function_by_name("h").unwrap().location();
+
+        assert_eq!(
+            dependencies.clusters().cloned().collect::<Vec<_>>(),
+            [
+                [(Index::from(0), g)].as_slice(),
+                [(Index::from(0), h)].as_slice(),
+                [(Index::from(0), f)].as_slice(),
+            ]
+            .into_iter()
+            .map(|locations_by_index| DependencyCluster {
+                functions: locations_by_index.iter().cloned().collect(),
+            })
+            .collect::<Vec<_>>(),
+        );
+    }
+
+    #[test]
+    fn consider_anonymous_functions_in_call_graph() {
+        let (syntax_tree, dependencies) = resolve_dependencies(
+            r"
+                f: fn
+                    \ ->
+                        fn
+                            \ ->
+                                # Call a function that is placed after this one
+                                # in the source code.
+                                h
+                        end
+                end
+
+                g: fn
+                    \ ->
+                        nop
+                end
+
+                h: fn
+                    \ ->
+                        fn
+                            \ ->
+                                # And for some variety, call a function that is
+                                # placed before.
+                                g
+                        end
+                end
+            ",
+        );
+
+        let [(f, f_a), (h, h_a)] = ["f", "h"].map(|name| {
+            let named = syntax_tree.function_by_name(name).unwrap();
+            let anonymous = named
+                .into_located_function()
+                .find_single_branch()
+                .unwrap()
+                .expressions()
+                .next()
+                .unwrap()
+                .into_local_function()
+                .map(|function| function.location)
+                .unwrap();
+
+            (named.location(), anonymous)
+        });
+        let g = syntax_tree.function_by_name("g").unwrap().location();
+
+        assert_eq!(
+            dependencies.clusters().cloned().collect::<Vec<_>>(),
+            [
+                [(Index::from(0), g)].as_slice(),
+                [(Index::from(0), h_a)].as_slice(),
+                [(Index::from(0), h)].as_slice(),
+                [(Index::from(0), f_a)].as_slice(),
+                [(Index::from(0), f)].as_slice(),
+            ]
+            .into_iter()
+            .map(|locations_by_index| DependencyCluster {
+                functions: locations_by_index.iter().cloned().collect(),
+            })
+            .collect::<Vec<_>>(),
+        );
+    }
+
+    fn resolve_dependencies(input: &str) -> (SyntaxTree, Dependencies) {
+        let tokens = Tokens::tokenize(input);
+        let syntax_tree = SyntaxTree::parse(tokens);
+        let function_calls = FunctionCalls::resolve(&syntax_tree, &NoHost);
+        let dependencies = Dependencies::resolve(&syntax_tree, &function_calls);
+
+        (syntax_tree, dependencies)
+    }
+}
