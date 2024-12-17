0f07e1e3b11f5641828c6bc2384b3e97cc3bafe7
Hanno Braun <hello@hannobraun.com>
2024-12-17 12:45:46 +0100

    Prepare to simplify function compilation

--- capi/compiler/src/passes/generate_instructions/compile_function.rs
+++ capi/compiler/src/passes/generate_instructions/compile_function.rs
@@ -5,8 +5,7 @@ use capi_runtime::{Effect, Instruction, InstructionAddress, Value};
 use crate::{
     code::{
         syntax::{
-            Branch, Expression, Function, FunctionLocation, Located,
-            MemberLocation, Pattern,
+            Branch, Expression, Function, FunctionLocation, Located, Pattern,
         },
         Binding,
     },
@@ -392,13 +391,50 @@ fn compile_expression(
 
                 address
             } else {
-                compile_local_function(
-                    function,
-                    expression.location,
-                    cluster_context,
+                // We have encountered an anonymous function. We need to emit an
+                // instruction that allocates it, and takes care of its\
+                // environment.
+                //
+                // But we haven't compiled the anonymous function yet, and we
+                // can't do that right now. If we did, we would be emitting its
+                // instructions in the middle of whatever function (anonymous or
+                // named) that we're currently compiling.
+                //
+                // The result of that would be, that every anonymous function
+                // would be executed right where it's defined, which would
+                // defeat the purpose of having them in the first place.
+                //
+                // But we still somehow need to emit that instruction to
+                // allocate the anonymous function and take care of its
+                // environment. We'll do that later, after we've actually
+                // compiled the anonymous function.
+                //
+                // For now, we'll just emit a placeholder that can be replaced
+                // with the real instruction then.
+                let address = emit_instruction(
+                    Instruction::TriggerEffect {
+                        effect: Effect::CompilerBug,
+                    },
                     functions_context.instructions,
-                    &mut mapping,
-                )
+                    Some(&mut mapping),
+                );
+
+                // We've done what we could. Let's arrange for the anonymous
+                // function to be compiled, and the placeholder instruction to
+                // be replaced, at a later time.
+                cluster_context.queue_of_functions_to_compile.push_front(
+                    FunctionToCompile {
+                        function: function.clone(),
+                        location: FunctionLocation::Local {
+                            location: expression.location,
+                        },
+                        address_of_instruction_to_make_anon_function: Some(
+                            address,
+                        ),
+                    },
+                );
+
+                address
             }
         }
     }
@@ -474,52 +510,6 @@ fn compile_intrinsic(
     };
 
     emit_instruction(instruction, instructions, Some(mapping))
-}
-
-fn compile_local_function(
-    function: &Function,
-    location: MemberLocation,
-    cluster_context: &mut ClusterContext,
-    instructions: &mut Instructions,
-    mapping: &mut Mapping,
-) -> InstructionAddress {
-    // We have encountered an anonymous function. We need to emit an instruction
-    // that allocates it, and takes care of its environment.
-    //
-    // But we haven't compiled the anonymous function yet, and we can't do that
-    // right now. If we did, we would be emitting its instructions in the middle
-    // of whatever function (anonymous or named) that we're currently compiling.
-    //
-    // The result of that would be, that every anonymous function would be
-    // executed right where it's defined, which would defeat the purpose of
-    // having them in the first place.
-    //
-    // But we still somehow need to emit that instruction to allocate the
-    // anonymous function and take care of its environment. We'll do that later,
-    // after we've actually compiled the anonymous function.
-    //
-    // For now, we'll just emit a placeholder that can be replaced with the real
-    // instruction then.
-    let address = emit_instruction(
-        Instruction::TriggerEffect {
-            effect: Effect::CompilerBug,
-        },
-        instructions,
-        Some(mapping),
-    );
-
-    // We've done what we could. Let's arrange for the anonymous function to be
-    // compiled, and the placeholder instruction to be replaced, at a later
-    // time.
-    cluster_context.queue_of_functions_to_compile.push_front(
-        FunctionToCompile {
-            function: function.clone(),
-            location: FunctionLocation::Local { location },
-            address_of_instruction_to_make_anon_function: Some(address),
-        },
-    );
-
-    address
 }
 
 fn emit_instruction(
