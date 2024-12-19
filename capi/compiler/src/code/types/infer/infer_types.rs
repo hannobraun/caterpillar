87dec7c07fe6d9f66469dd3318293979d1ae543e
Hanno Braun <hello@hannobraun.com>
2024-12-19 12:08:59 +0100

    Move more code out of module that's too big

--- capi/compiler/src/code/types/infer/infer_types.rs
+++ capi/compiler/src/code/types/infer/infer_types.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeMap, BTreeSet},
+    collections::BTreeMap,
     fmt::{self, Write},
     result,
 };
@@ -12,12 +12,12 @@ use crate::{
         },
         types::repr::Stacks,
         Binding, Bindings, Dependencies, Environment, IdentifierTarget,
-        Identifiers, Index, IndexMap, Signature, Type, TypeAnnotations,
+        Identifiers, Index, Signature, Type, TypeAnnotations,
     },
     intrinsics::IntrinsicFunction,
 };
 
-use super::types::InferredType;
+use super::types::{InferredType, LocalTypes};
 
 pub fn infer_types(context: Context) -> InferenceOutput {
     let mut output = InferenceOutput::default();
@@ -716,47 +716,6 @@ struct TypeError {
 
 type ClusterFunctions =
     BTreeMap<FunctionLocation, Signature<Index<InferredType>>>;
-
-#[derive(Debug, Default)]
-struct LocalTypes {
-    inner: IndexMap<InferredType>,
-}
-
-impl LocalTypes {
-    fn push(&mut self, type_: InferredType) -> Index<InferredType> {
-        self.inner.push(type_)
-    }
-
-    fn get(&self, index: &Index<InferredType>) -> &InferredType {
-        let Some(type_) = self.inner.get(index) else {
-            unreachable!(
-                "We're never removing any local types. Any index must be valid."
-            );
-        };
-
-        type_
-    }
-
-    fn unify(&mut self, types: BTreeSet<Index<InferredType>>) {
-        let mut known_types = BTreeSet::new();
-
-        for index in &types {
-            if let Some(type_) = self.get(index).clone().into_type() {
-                known_types.insert(type_);
-            }
-        }
-
-        if known_types.len() > 1 {
-            panic!("Conflicting types: {known_types:?}");
-        }
-
-        if let Some(type_) = known_types.into_iter().next() {
-            for index in types {
-                self.inner.insert(index, InferredType::Known(type_.clone()));
-            }
-        }
-    }
-}
 
 #[derive(Debug)]
 struct LocalStack {
--- capi/compiler/src/code/types/infer/types.rs
+++ capi/compiler/src/code/types/infer/types.rs
@@ -1,4 +1,47 @@
-use crate::code::Type;
+use std::collections::BTreeSet;
+
+use crate::code::{Index, IndexMap, Type};
+
+#[derive(Debug, Default)]
+pub struct LocalTypes {
+    pub inner: IndexMap<InferredType>,
+}
+
+impl LocalTypes {
+    pub fn push(&mut self, type_: InferredType) -> Index<InferredType> {
+        self.inner.push(type_)
+    }
+
+    pub fn get(&self, index: &Index<InferredType>) -> &InferredType {
+        let Some(type_) = self.inner.get(index) else {
+            unreachable!(
+                "We're never removing any local types. Any index must be valid."
+            );
+        };
+
+        type_
+    }
+
+    pub fn unify(&mut self, types: BTreeSet<Index<InferredType>>) {
+        let mut known_types = BTreeSet::new();
+
+        for index in &types {
+            if let Some(type_) = self.get(index).clone().into_type() {
+                known_types.insert(type_);
+            }
+        }
+
+        if known_types.len() > 1 {
+            panic!("Conflicting types: {known_types:?}");
+        }
+
+        if let Some(type_) = known_types.into_iter().next() {
+            for index in types {
+                self.inner.insert(index, InferredType::Known(type_.clone()));
+            }
+        }
+    }
+}
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum InferredType {
