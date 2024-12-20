dce3952f7ceb7f9987edd5e766e210c23de463e6
Hanno Braun <hello@hannobraun.com>
2024-12-20 14:58:11 +0100

    Update name of function parameter

--- capi/compiler/src/code/syntax/location/branch.rs
+++ capi/compiler/src/code/syntax/location/branch.rs
@@ -17,8 +17,8 @@ impl<'r> Located<&'r Branch> {
     /// # Iterate over the parameters of the branch that bind a value to a name
     pub fn bindings(&self) -> impl Iterator<Item = Binding> + 'r {
         let identifiers =
-            self.fragment.parameters.values().filter_map(|pattern| {
-                if let Parameter::Identifier { name } = pattern {
+            self.fragment.parameters.values().filter_map(|parameter| {
+                if let Parameter::Identifier { name } = parameter {
                     Some(name)
                 } else {
                     None
