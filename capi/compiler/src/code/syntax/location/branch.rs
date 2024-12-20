9445a1605e83b01240157f9db054d76b43470655
Hanno Braun <hello@hannobraun.com>
2024-12-20 14:45:02 +0100

    Remove redundant code

--- capi/compiler/src/code/syntax/location/branch.rs
+++ capi/compiler/src/code/syntax/location/branch.rs
@@ -16,7 +16,6 @@ impl HasLocation for Branch {
 impl<'r> Located<&'r Branch> {
     /// # Iterate over the parameters of the branch that bind a value to a name
     pub fn bindings(&self) -> impl Iterator<Item = Binding> + 'r {
-        let indices = iter::successors(Some(0), |i| Some(i + 1));
         let identifiers =
             self.fragment
                 .parameters
@@ -30,12 +29,10 @@ impl<'r> Located<&'r Branch> {
                 });
         let location = self.location.clone();
 
-        indices
-            .zip(identifiers)
-            .map(move |(_, (_, identifier))| Binding {
-                name: identifier.clone(),
-                branch: location.clone(),
-            })
+        identifiers.map(move |(_, identifier)| Binding {
+            name: identifier.clone(),
+            branch: location.clone(),
+        })
     }
 
     /// # Iterate over the members of the branch's body
