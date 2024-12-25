8b66793b6dae66512e45881b76f39157b856d0b7
Hanno Braun <hello@hannobraun.com>
2024-12-25 18:56:49 +0100

    Make name of test more explicit

--- capi/compiler/src/code/dependencies/repr.rs
+++ capi/compiler/src/code/dependencies/repr.rs
@@ -230,7 +230,7 @@ mod tests {
     }
 
     #[test]
-    fn self_recursion() {
+    fn function_dependencies_with_self_recursion() {
         let (syntax_tree, dependencies) = resolve_dependencies(
             r"
                 f: fn
