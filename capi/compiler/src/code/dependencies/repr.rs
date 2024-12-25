e93a1c4587ace5f8d9f7fb54dc5902007d14ea41
Hanno Braun <hello@hannobraun.com>
2024-12-25 20:38:06 +0100

    Update variable name

--- capi/compiler/src/code/dependencies/repr.rs
+++ capi/compiler/src/code/dependencies/repr.rs
@@ -355,7 +355,7 @@ mod tests {
             ",
         );
 
-        let (f_a, f_b, h_a, h_b) = ["f", "g"]
+        let (f_a, f_b, g_a, h_b) = ["f", "g"]
             .map(|name| syntax_tree.function_by_name(name).unwrap())
             .into_iter()
             .flat_map(|function| {
@@ -382,7 +382,7 @@ mod tests {
         // The other two are mutually dependent, so there is no clear order.
         let rest = branches.collect::<BTreeSet<_>>();
         assert!(rest.contains(&f_a));
-        assert!(rest.contains(&h_a));
+        assert!(rest.contains(&g_a));
     }
 
     #[test]
