205e15c1c79808b02c9b032cf8aa7b66ec0d5a60
Hanno Braun <hello@hannobraun.com>
2024-12-31 21:18:59 +0100

    Prepare to re-use `email_link`

--- website/server/templates.tsx
+++ website/server/templates.tsx
@@ -87,6 +87,8 @@ const subscribe = () => {
                 <span class="font-bold">Hey, you!</span>{" "}
                 Want to subscribe to my daily thoughts? Just{" "}
                 {email_link("let me know", {
+                    subject:
+                        "I%27d%20like%20to%20subscribe%20to%20your%20daily%20thoughts%21",
                     body:
                         "Hey%20Hanno%2C%20please%20send%20me%20email%20every%20single%20day.",
                 })}{" "}
@@ -97,11 +99,12 @@ const subscribe = () => {
     );
 };
 
-const email_link = (text: string, extra: { body: string }) => {
+const email_link = (text: string, extra: { subject: string; body: string }) => {
+    const subject = extra.subject || "";
     const body = extra.body || "";
 
     const url =
-        `mailto:Hanno%20Braun%20%3Chello%40hannobraun.com%3E?subject=I%27d%20like%20to%20subscribe%20to%20your%20daily%20thoughts%21&body=${body}`;
+        `mailto:Hanno%20Braun%20%3Chello%40hannobraun.com%3E?subject=${subject}&body=${body}`;
 
     return link(
         url,
