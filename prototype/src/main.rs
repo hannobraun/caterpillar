337726a7101df4851438b9f9c978276776fb7c5f
Hanno Braun <hello@hannobraun.com>
2025-01-04 01:46:07 +0100

    Improve error handling

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,28 +1,54 @@
+use std::sync::mpsc::{self, RecvError, SendError};
+
 use winit::{
     application::ApplicationHandler,
+    error::OsError,
     event::WindowEvent,
     event_loop::{ActiveEventLoop, EventLoop},
     window::{Window, WindowId},
 };
 
 fn main() -> anyhow::Result<()> {
-    let mut application = Application { window: None };
+    let (error_tx, error_rx) = mpsc::channel();
+
+    let mut application = Application {
+        window: None,
+        error: error_tx,
+    };
 
     let event_loop = EventLoop::new()?;
     event_loop.run_app(&mut application)?;
+
+    match error_rx.recv() {
+        Ok(err) => return Err(err.into()),
+        Err(RecvError) => {
+            // The other end has hung up. If it didn't send us an error before,
+            // then all should be well.
+        }
+    }
 
     Ok(())
 }
 
 struct Application {
     window: Option<Window>,
+    error: mpsc::Sender<OsError>,
 }
 
 impl ApplicationHandler for Application {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window = event_loop
-            .create_window(Window::default_attributes())
-            .unwrap();
+        let window =
+            match event_loop.create_window(Window::default_attributes()) {
+                Ok(window) => window,
+                Err(err) => {
+                    if let Err(SendError(_)) = self.error.send(err) {
+                        // The other end has hung up. Nothing else we can do
+                        // with this error now.
+                    };
+                    event_loop.exit();
+                    return;
+                }
+            };
 
         self.window = Some(window);
     }
