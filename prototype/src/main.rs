8cc8470dcf7a4e83c1efea06f4bd461b8099ce1b
Hanno Braun <hello@hannobraun.com>
2025-01-04 01:29:59 +0100

    Prepare to open window

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,4 +1,38 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowId},
+};
+
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new()?;
+
+    let mut application = Application::default();
+    event_loop.run_app(&mut application)?;
+
     Ok(())
+}
+
+#[derive(Default)]
+struct Application {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for Application {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop
+            .create_window(Window::default_attributes())
+            .unwrap();
+
+        self.window = Some(window);
+    }
+
+    fn window_event(
+        &mut self,
+        _: &ActiveEventLoop,
+        _: WindowId,
+        _: WindowEvent,
+    ) {
+    }
 }
